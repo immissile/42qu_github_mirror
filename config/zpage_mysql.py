a0012e8436d30a264994029112f1f1dbd04cf68c
zuroc <zsp007@gmail.com>
2011-05-08 20:17:11 +0800

    fix

--- config/zpage_mysql.py
+++ config/zpage_mysql.py
@@ -5,7 +5,7 @@ MYSQLPORT = 3306
 MYSQLPASSWD = "zpagepasswd"
 MYSQLUSER = "zpage"
 
-DB_HOST_MAIN = "127.0.0.1:%s:zpage:%s:%s"%(MYSQLPORT, MYSQLUSER, MYSQLPASSWD)
+DB_HOST_MAIN = "127.0.0.1:%s:zpage_main:%s:%s"%(MYSQLPORT, MYSQLUSER, MYSQLPASSWD)
 
 DB_CONFIG = {
     "main":{
