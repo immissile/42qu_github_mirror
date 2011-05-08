e960d4a6cefd216e35583a5d611c1a21700421ae
zuroc <zsp007@gmail.com>
2011-05-08 22:33:30 +0800

    fix

--- ctrl/auth.py
+++ ctrl/auth.py
@@ -9,11 +9,15 @@ from _urlmap import urlmap
 @urlmap("/login")
 class Login(_handler.Base):
     def get(self):
-        self.render(test="test")
+        self.render()
 
     def post(self):
-        mail = self.get_argument('mail')
-        password = self.get_argument('password')
-        print mail,password
-        self.render()
+        mail = self.get_argument('mail',None)
+        password = self.get_argument('password',None)
+        if mail and password:
+
+        self.render(
+            mail = mail,
+            password = password
+        )
 
