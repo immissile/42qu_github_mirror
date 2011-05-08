b4d4b1e1a84b644fbdf3709317770660ff01458a
zuroc <zsp007@gmail.com>
2011-05-08 23:48:02 +0800

    fix

--- ctrl/auth.py
+++ ctrl/auth.py
@@ -4,7 +4,7 @@
 
 import _handler
 from _urlmap import urlmap
-
+from zkit.txt import EMAIL_VALID
 
 @urlmap("/login")
 class Login(_handler.Base):
@@ -14,10 +14,25 @@ class Login(_handler.Base):
     def post(self):
         mail = self.get_argument('mail',None)
         password = self.get_argument('password',None)
-        if mail and password:
-            pass
+
+        error_mail = None
+        error_password = None
+
+        if mail:
+            mail = mail.strip().lower()
+        if not mail:
+            error_mail = "请输入邮箱"
+        elif not EMAIL_VALID.match(mail):
+            error_mail = "邮箱格式有误"
+
+        if not password:
+            error_password = "请输入密码"
+
+
         self.render(
             mail = mail,
-            password = password
+            password = password,
+            error_mail = error_mail,
+            error_password = error_password
         )
 
--- zkit/txt.py
+++ zkit/txt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 EMAIL_VALID = re.compile(r"^\w+[-+.\w]*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")
