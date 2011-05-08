2738ff3670105ea1816e352ff4319ad795c95664
zuroc <zsp007@gmail.com>
2011-05-08 21:57:03 +0800

    fix

--- model/user_task.py
+++ model/user_task.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from _db import Model, McModel, McCache
 
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+USER_TASK_CID_MAIL = 1
 
-
-def main():
+class UserTask(McModel):
     pass
 
-
-if "__main__" == __name__:
-    main()
-
+def user_task_new_mail(user_id, mail_id):
+    task = UserTask.get_or_create(user_id=user_id, rid=mail_id, cid=USER_TASK_CID_MAIL)
+    task.save()
