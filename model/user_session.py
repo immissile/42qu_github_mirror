8c1ecdcea1da231a9ac7b2a51deca68c10a4e1d9
zuroc <zsp007@gmail.com>
2011-05-08 21:37:15 +0800

    fix

--- model/user_session.py
+++ model/user_session.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from _db import Model,McModel
+from _db import Model, McModel, McCache
+from os import urandom
+from struct import pack, unpack
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+
+mc_user_session = McCache("UserSession:%s")
 
 class UserSession(McModel):
     pass
 
+@mc_user_session("{user_id}")
+def man_session_by_db(user_id):
+    u = UserSession.get(user_id)
+    if u is not None:
+        s = u.value
+        return s
 
+def man_session(user_id):
+    s = man_session_by_db(user_id)
+    if s is None:
+        u = UserSession.get_or_create(id=user_id)
+        if u.value is None:
+            s = urandom(12)
+            u.value = s
+            u.save()
+        mc.set(mc_key, s)
 
+    user_id_key = pack("I", int(user_id))
+    user_id_key = urlsafe_b64encode(user_id_key)[:6]
+
+    ck_key = urlsafe_b64encode(s)
+    return "%s%s"%(user_id_key, ck_key)
+
+if __name__ == "__main__":
+    print man_session(1)
