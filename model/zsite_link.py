34a6dfd53e038530e46346127b1e7f3ca6cf9196
zuroc <zsp007@gmail.com>
2011-05-15 17:31:36 +0800

    fix

--- model/zsite_link.py
+++ model/zsite_link.py
@@ -4,7 +4,7 @@ from _db import Model, McModel
 from hashlib import sha256
 from zsite import zsite_new_user, Zsite
 
-def zsite_id_by_domain():
+def zsite_id_by_domain(domain):
     pass
 
 @property
