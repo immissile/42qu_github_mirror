fa1748a8ee84b91f62b0a4ba3b593df4fd26236f
zuroc <zsp007@gmail.com>
2011-05-15 23:00:24 +0800

    fix

---
+++ model/mysql_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class KvTable(object):
+    def __init__(self, tabel):
+        self.__tabel__ = tabel
+
+    def get(key):
+        pass
+
+    def set(key, value):
+        pass
+
+    def delete(key):
+        pass
+
