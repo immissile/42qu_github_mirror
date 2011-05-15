c194de3dc96ff681dffdae10c8f273e274403609
zuroc <zsp007@gmail.com>
2011-05-16 01:37:33 +0800

    fix

--- ctrl/setting.py
+++ ctrl/setting.py
@@ -4,11 +4,28 @@
 
 import _handler
 from _urlmap import urlmap
+from zkit.pic import pic_fit, pic_square, picopen
 
 @urlmap("/setting")
 class Setting(_handler.Base):
     def get(self):
-        files = self.request.files
-        print files
         self.render()
-    post = get
+
+    def post(self):
+        files = self.request.files
+        self.render()
+        if 'pic' in files:
+            pic = files['pic']['body']
+            img = picopen(img.file)
+            if img:
+                pos = ""
+                if pic_show_id:
+                    pic_show_replace(img, pic_show_id)
+                else:
+                    pic_show_id = pic_show_add(man_id, img)
+                pic_show_admin_new(man_id)
+                return pic_show_id
+            else:
+                error.img = "图片格式有误"
+                return False
+        self.render()
