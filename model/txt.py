67dec9457dc98e2c0a22b5747095658d0d9af7d7
zuroc <zsp007@gmail.com>
2011-05-18 18:01:57 +0800

    fix

--- model/txt.py
+++ model/txt.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from _db import cursor_by_table
-from import time
-cursor = cursor_by_table('txt')
+from _db import  Model, cursor_by_table
+from time import time
+
+class Txt(Model):
+    pass
 
 def txt_set(id, txt):
-    pass
+    txt = txt.rstrip()
+    t = Txt.get(id)
+    if t is None:
+        Txt(id=id, txt=txt).save()
+    elif txt != t.txt:
+        c = cursor_by_table('txt_history')
+        c.execute(
+            "insert into txt_history (rid,txt,create_time) values (%s,%s,%s)",
+            (id, t.txt, int(time()))
+        )
+        c.connection.commit()
+        t.txt = txt
+        t.save()
 
-def txt_get(id, txt):
-    pass
+def txt_get(id):
+    c = Txt.raw_sql('select txt from txt where id=%s', id)
+    r = c.fetchone()
+    if r:
+        return r[0]
+    return ''
 
 if __name__ == "__main__":
-    print gid()
+    from gid import gid
+    id = 1#gid()
+    txt_set(id, "test3")
+    print txt_get(id)
+
+
