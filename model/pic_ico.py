e72d2fee0da97e614058cf47d039200b5555ad7d
zuroc <zsp007@gmail.com>
2011-05-18 17:31:09 +0800

    fix

--- model/pic_ico.py
+++ model/pic_ico.py
@@ -8,7 +8,7 @@ from fs import fs_set_jpg,fs_url_jpg
 """
 CREATE TABLE `pic_ico_history` (
 `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-`zpage_id` INTEGER UNSIGNED NOT NULL,
+`zsite_id` INTEGER UNSIGNED NOT NULL,
 `create_time` INTEGER UNSIGNED NULL,
 `admin_id` INTEGER UNSIGNED NOT NULL DEFAULT 0,
 `state` TINYINT UNSIGNED NOT NULL DEFAULT 1,
@@ -30,9 +30,9 @@ pic_ico = KvTable('pic_ico')
 class PicIcoHistory(Model):
     pass
 
-def pic_ico_new(zpage_id, pic):
+def pic_ico_new(zsite_id, pic):
     p = PicIcoHistory(
-        zpage_id=zpage_id,
+        zsite_id=zsite_id,
         create_time=int(time()),
     ).save()
     pic_id = p.id
@@ -44,7 +44,7 @@ def pic_ico_new(zpage_id, pic):
     p2 = pic_zoom_inner(pic, 320, 320)
     fs_set_jpg("2", pic_id, p1)
 
-    pic_ico.set(zpage_id, pic_id)
+    pic_ico.set(zsite_id, pic_id)
     return pic_id
 
 def pic_ico_url(id, size="1"):
