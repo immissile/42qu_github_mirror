4e364074ffb2707d40df73934d10164f101d0e76
zuroc <zsp007@gmail.com>
2011-05-18 17:40:26 +0800

    fix

---
+++ model/gid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from _db import cursor_by_table
+
+cursor = cursor_by_table('gid')
+
+def gid():
+    cursor.execute("insert into gid () values()")
+    cursor.connection.commit()
+    return cursor.lastrowid
+
+if __name__ == "__main__":
+    print gid()
--- model/txt.py
+++ model/txt.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
-
-def main():
-    pass
-
-
-if "__main__" == __name__:
-    main()
-
