a3e164a956f05bf5a7783ca204b3cdc0b41ac78d
zuroc <zsp007@gmail.com>
2011-05-18 17:47:09 +0800

    fix

--- model/txt.py
+++ model/txt.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from _db import cursor_by_table
+from import time
+cursor = cursor_by_table('txt')
+
+def txt_set(id, txt):
+    pass
+
+def txt_get(id, txt):
+    pass
+
+if __name__ == "__main__":
+    print gid()
