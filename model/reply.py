ffd390e8ac8c6d69c282571c76cf59c8e025f432
zuroc <zsp007@gmail.com>
2011-05-18 20:43:34 +0800

    fix

--- model/reply.py
+++ model/reply.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python
 #coding:utf-8
-#from gid import gid
-#
-#class ReplyMixin(object):
-#    def reply_new(self, user_id, txt):
-#        rid = self.id
-#        id = gid()
-#        s = Rcls(id=id, rid=rid, user_id=user_id, state=state)
-#        s.txt = txt
-#        s.save()
-#        return s
-#
-#
-#def mixin_reply(reply_cls):
-#    """
-#@mixin_reply(XxxReply)
-#class Xxx(McModel):
-#    pass
-#    """
-#    def _(cls):
-#        cls.__bases__ = tuple(list(cls.__bases__)+[ReplyMixin, ])
-#        table_title = cls.Meta.table.title()
-#        cls._REPLY_CLS = reply_cls
-#        return cls
-#    return _
+from gid import gid
 
-#@can_reply
+class ReplyMixin(object):
+    def reply_new(self, user_id, txt):
+      #  rid = self.id
+      #  id = gid()
+      #  s = Rcls(id=id, rid=rid, user_id=user_id, state=state)
+      #  s.txt = txt
+      #  s.save()
+        print self.Meta.table
+        return s
+
+
