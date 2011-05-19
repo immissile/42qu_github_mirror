fdef3480db9b2bea810f32a6594e8c929e125725
zuroc <zsp007@gmail.com>
2011-05-19 19:07:06 +0800

    fix

--- model/zsite_link.py
+++ model/zsite_link.py
@@ -9,8 +9,8 @@ def zsite_by_domain(domain):
     zsite = None
     if domain.endswith(SITE_DOMAIN_SUFFIX):
         domain = domain[:-len(SITE_DOMAIN_SUFFIX)]
-        if domain.startswith("-"):
-            zsite = Zsite.mc_get(domain[1:])
+        if domain.isdigit():
+            zsite = Zsite.mc_get(domain)
     return zsite
 
 @property
