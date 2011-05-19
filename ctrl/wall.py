c997de619dbf3208d409c047a3157464349cd808
zuroc <zsp007@gmail.com>
2011-05-19 14:55:36 +0800

    fix

--- ctrl/wall.py
+++ ctrl/wall.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-
 import _handler
 from _urlmap import urlmap
 from model.reply import REPLY_STATE_SECRET, REPLY_STATE_ACTIVE
+from zkit.page import page_limit_offset
 
 @urlmap("/wall")
 class Wall(_handler.LoginBase):
@@ -14,7 +14,7 @@ class Wall(_handler.LoginBase):
 
     def post(self):
         zsite = self.zsite
-        txt = self.get_argument('txt',None)
+        txt = self.get_argument('txt', None)
         if txt:
             secret = self.get_argument('secret', None)
             current_user = self.current_user
@@ -27,9 +27,23 @@ class Wall(_handler.LoginBase):
 
         return self.redirect(zsite.link)
 
+PAGE_LIMIT = 12
 
 @urlmap("/wall/(\-?\d+)")
 class Page(_handler.LoginBase):
     def get(self, page):
-        return self.render()
+        zsite = self.zsite
+        zsite_link = zsite.link
+        page, limit, offset = page_limit_offset(
+            "%s/wall/%%s"%zsite_link,
+            zsite.reply_total,
+            page,
+            PAGE_LIMIT
+        )
+        reply_list = zsite.reply_list(limit, offset)
+
+        return self.render(
+            reply_list = reply_list,
+            page = page
+        )
 
