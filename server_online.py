ee9567350fbe2428f5ba322bf98b4a78f20acd79
zuroc <zsp007@gmail.com>
2011-05-19 17:23:51 +0800

    fix

--- server_online.py
+++ server_online.py
@@ -3,11 +3,10 @@
 import config
 from zweb.server_tornado import WSGIServer
 from ctrl._application import application
-import config.zpage_ctrl
 import sys
 if len(sys.argv) > 1 and sys.argv[1].isdigit():
     port = int(sys.argv[1])
 else:
-    port = config.zpage_ctrl.PORT
+    port = config.PORT
 print "server on port %s"%port
 WSGIServer(port, application)
