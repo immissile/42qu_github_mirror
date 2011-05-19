0008c0fd996d8f874cf9b5707416c22129626697
zuroc <zsp007@gmail.com>
2011-05-19 19:25:48 +0800

    fix

--- script/hosts/hosts.py
+++ script/hosts/hosts.py
@@ -3,14 +3,16 @@
 import init_env
 from zweb.orm import ormiter
 from model.zsite import Zsite
+from model import zsite_link
 
 def host_line(host):
     l = ["""192.168.1.106 %s p.%s s.%s god.%s"""%(
         host, host, host, host
     )]
     for i in ormiter(Zsite):
-        print i.link
+        l.append(i.link.split("://")[-1])
     return "".join(l)
+
 
 if __name__ == "__main__":
     for name in ("zuroc.me","zjd.me","yup.me"):
