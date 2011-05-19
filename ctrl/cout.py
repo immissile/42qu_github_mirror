0762aba7dfa2957110da13fbc82cb8fefef36558
zuroc <zsp007@gmail.com>
2011-05-19 23:38:19 +0800

    fix

--- ctrl/cout.py
+++ ctrl/cout.py
@@ -5,7 +5,13 @@
 import _handler
 from zweb._urlmap import urlmap
 
-@urlmap("/cout/write")
-class write(_handler.Base):
+@urlmap("/cout/note")
+class Note(_handler.Base):
     def get(self):
         return self.render()
+
+@urlmap("/cout/word")
+class Word(_handler.Base):
+    def get(self):
+        return self.render()
+
