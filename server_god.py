5f235014122facb0ab6491f866dabbd5ba97ac09
zuroc <zsp007@gmail.com>
2011-05-20 15:28:06 +0800

    fix

--- server_god.py
+++ server_god.py
@@ -2,12 +2,16 @@
 #coding:utf-8
 import config
 
-from zweb.server_tornado import WSGIServer
-from god._application import application
-import sys
-if len(sys.argv) > 1 and sys.argv[1].isdigit():
-    port = int(sys.argv[1])
-else:
-    port = config.GOD_PORT
-print 'server on port %s'%port
-WSGIServer(port, application)
+def run():
+    from zweb.server_tornado import WSGIServer
+    from god._application import application
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        port = int(sys.argv[1])
+    else:
+        port = config.GOD_PORT
+    print 'server on port %s'%port
+    WSGIServer(port, application)
+
+if __name__ == "__main__":
+    run()
---
+++ server_god_dev.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#coding:utf-8
+import config
+config.DEBUG = True
+
+
+if __name__ == '__main__':
+    from zkit.reloader.reload_server import auto_reload
+    from server_god import run
+    auto_reload(run)
