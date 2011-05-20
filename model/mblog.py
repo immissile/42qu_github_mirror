4924e264e9d20dcce00a97571950ed464f145975
zuroc <zsp007@gmail.com>
2011-05-20 22:35:38 +0800

    fix

--- model/mblog.py
+++ model/mblog.py
@@ -5,6 +5,7 @@ from time import time
 from cid import CID_WORD, CID_NOTE
 from feed import feed_entry_new
 from gid import gid
+from txt import txt_new
 
 class Mblog(McModel):
     pass
@@ -31,24 +32,38 @@ def mblog_word_lastest(user_id):
         r = ''
     return r
 
-def mblog_word_new( user_id, name, create_time=None):
-    if create_time is None:
-        create_time = int(time())
-    if name.rstrip() and name != mblog_word_lastest(user_id):
-        id = gid()
-        m = Mblog(
-            id=id,
-            name=name,
-            user_id=user_id,
-            create_time=create_time,
-            cid=CID_WORD,
-            state=MBLOG_STATE_ACTIVE
-        )
+
+def mblog_new(cid, user_id, name, state):
+    m = Mblog(
+        id=gid(),
+        name=name.strip(),
+        user_id=user_id,
+        create_time=int(time()),
+        cid=CID_WORD,
+        state=state
+    )
+    m.save()
+    return m
+
+def edit_name(id, name):
+    m = Mblog.mc_get(id)
+    if m is None:return
+    name = name.strip()
+    if m.name != name and name:
+        m.name = name
         m.save()
+
+def mblog_word_new(user_id, name):
+    if name.rstrip() and name != mblog_word_lastest(user_id):
+        m = mblog_new(CID_WORD, user_id, name, MBLOG_STATE_ACTIVE)
         mc_mblog_word_lastest.set(user_id, name)
         feed_entry_new(id, user_id, CID_WORD)
         return m
 
+def mblog_question_new(user_id, name , txt):
+    m = mblog_new(CID_WORD, user_id, name, MBLOG_STATE_SECRET)
+    txt_new(m.id, txt)
+    return m
 
 
 
