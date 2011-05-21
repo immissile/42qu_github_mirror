0f53869a2518ae4daa5255ad99517abd03279bab
underdarkeye <underdarkeye@gmail.com>
2011-05-21 15:04:00 +0800

    f

--- script/data_migration.sql
+++ script/data_migration.sql
@@ -19,3 +19,10 @@ insert into zpage.zsite (id, name, cid, state)
 select id, name, cid, state from qu.man order by id;
 
 truncate zpage.pic;
+insert into zpage.pic (id, zsite_id, tid)
+select id, man_id, 2 from qu.pic order by id;
+
+truncate zpage.ico;
+insert into zpage.ico (id, value)
+select man_id, pic_id from qu.pic_show order by id desc
+on duplicate key update zpage.ico.value=zpage.ico.value;
