c7b619023ecd17b9e3d6304f56c933e748f7aa29
zuroc <zsp007@gmail.com>
2011-05-22 03:57:20 +0800

    fix

--- config/host_nuva.py
+++ config/host_nuva.py
@@ -1,11 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import conf
-conf.MYSQL_MAIN = 'zpage'
-conf.MQ_PORT = 14711
-
-conf.SMTP = "127.0.0.1"
-conf.SMTP_USERNAME = "work"
-conf.SMTP_PASSWORD = "kanrss"
-conf.SENDER_MAIL = "zuroc@42qu.com"
-
+zpage_private_config/host_nuva.py
