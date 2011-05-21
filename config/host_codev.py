961a30cb8ada33d77408d8c6b6377113afc91838
zuroc <zsp007@gmail.com>
2011-05-21 14:33:09 +0800

    fix

--- config/host_codev.py
+++ config/host_codev.py
@@ -5,6 +5,6 @@ conf.MYSQL_MAIN = 'zpage'
 conf.MQ_PORT = 14711
 
 conf.SMTP = "smtp.sina.com.cn"
-conf.SMTP_USERNAME = "zuroc"
+conf.SMTP_USERNAME = "zuroc586"
 conf.SMTP_PASSWORD = "kanrss"
 conf.SENDER_MAIL = "zuroc586@sina.com"
