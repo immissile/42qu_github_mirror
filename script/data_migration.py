9fc4c879ec96e2a3e49d06af06175fb1dfe584e0
underdarkeye <underdarkeye@gmail.com>
2011-05-21 18:56:18 +0800

    f

--- script/data_migration.py
+++ script/data_migration.py
@@ -3,31 +3,22 @@
 
 from qu.myconf.config import KV_PATH
 
-from zpage.model.fs import FS_PATH, fs_file
-from zpage.model.pic import ico_save
-from zpage.zkit.pic import picopen
+from zpage.model.fs import fs_file
 
 from os import walk, makedirs
 from os.path import join, getsize, splitext, exists, dirname
 from shutil import copy
 
-for root, dirs, files in walk(join(KV_PATH, 'pic_show', '0')):
-    for name in files:
-        opath = join(root, name)
-        id = splitext(name)[0]
-        if id.isdigit():
-            id = int(id)
-            path = fs_file('0', id, 'jpg')
-            if not exists(path):
-                dirpath = dirname(path)
-                if not exists(dirpath):
-                    makedirs(dirpath)
-                copy(opath, path)
-
-#for root, dirs, files in walk(join(FS_PATH, '0')):
-#    for name in files:
-#        path = join(root, name)
-#        id = splitext(name)[0]
-#        id = int(id)
-#        with open(path) as f:
-#            ico_save(id, picopen(f))
+for prefix in ('0', '721', '470', '219'):
+    for root, dirs, files in walk(join(KV_PATH, 'pic_show', prefix)):
+        for name in files:
+            opath = join(root, name)
+            id = splitext(name)[0]
+            if id.isdigit():
+                id = int(id)
+                path = fs_file(prefix, id, 'jpg')
+                if not exists(path):
+                    dirpath = dirname(path)
+                    if not exists(dirpath):
+                        makedirs(dirpath)
+                    copy(opath, path)
