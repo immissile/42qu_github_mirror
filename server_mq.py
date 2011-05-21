ae13c87b99be9573e5acc26e58918ebef3f82938
zuroc <zsp007@gmail.com>
2011-05-21 15:04:44 +0800

    fix

---
+++ server_mq.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+
+def run():
+    from model.mq import mq_server
+    from model.feed import mq_mc_flush_zsite_follow
+    from model.mail import mq_rendermail
+    mq_server()
+
+if __name__ == "__main__":
+    run()
