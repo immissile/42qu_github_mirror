f0aa757e017c17ed6c0c0a583b6583f95ef688b5
zuroc <zsp007@gmail.com>
2011-05-21 17:43:49 +0800

    fix

--- model/feed.py
+++ model/feed.py
@@ -4,10 +4,13 @@
 from _db import cursor_by_table, Model, McCache, McLimitA, McCacheA
 from zkit.mc_func import mc_func_get_multi
 from follow import follow_id_list_by_zsite_id
-mc_id_by_feed_id = McLimitA("IdByFeedId:%s", 256)
+from zkit.algorithm.merge import imerge
+
+mc_feed_entry_iter = McCacheA("FeedEntryIter:%s")
 mc_feed_id_by_zsite_id_cid = McCache("FeedIdByZsiteIdCid:%s")
 mc_feed_id_list_by_zsite_id = McCacheA("FeedIdByZsiteId:%s")
 mc_feed_id_by_for_zsite_follow = McCache("FeedIdForZsiteFollow.%s")
+
 
 class Feed(Model):
     pass
@@ -21,7 +24,7 @@ def feed_entry_new(id, zsite_id, cid):
         (id, feed_id)
     )
     cursor.connection.commit()
-    mc_id_by_feed_id.delete(feed_id)
+    mc_feed_entry_iter.delete(feed_id)
     return id
 
 def feed_entry_rm(id):
@@ -30,7 +33,7 @@ def feed_entry_rm(id):
         return
     feed_id = o.feed_id
     o.delete()
-    mc_id_by_feed_id.delete(feed_id)
+    mc_feed_entry_iter.delete(feed_id)
 
 @mc_feed_id_by_zsite_id_cid("{zsite_id}_{cid}")
 def feed_id_by_zsite_id_cid(zsite_id, cid):
@@ -41,15 +44,41 @@ def feed_id_by_zsite_id_cid(zsite_id, cid):
         mq_mc_flush_zsite_follow(zsite_id)
     return feed.id
 
-@mc_id_by_feed_id("{feed_id}")
-def id_by_feed_id(feed_id, limit, offset):
+import sys
+MAXINT = sys.maxint
+
+@mc_feed_entry_iter("{feed_id}")
+def feed_entry_id_lastest(feed_id):
     cursor.execute(
-        "select id from feed_entry where feed_id=%s order by id desc limit %s offset %s",
-        (feed_id, limit, offset)
+        "select id from feed_entry where feed_id=%s order by id desc limit 128",
+        feed_id
     )
     return [
         i for i, in cursor
     ]
+
+def feed_entry_id_iter(id, start_id=MAXINT, ):
+    if start_id == MAXINT:
+        id_list = feed_entry_id_lastest(id)
+        if id_list:
+            for i in id_list:
+                yield i
+            start_id = i
+        else:
+            return
+    while True:
+        cursor.execute(
+            "select id from feed_entry where "
+            "feed_id=%s and id<%s "
+            "order by id desc limit 128",
+            (id, start_id)
+        )
+        c = cursor.fetchall()
+        if not c:
+            break
+        for i, in c:
+            yield i
+        start_id = i
 
 @mc_feed_id_list_by_zsite_id("{zsite_id}")
 def feed_id_list_by_zsite_id(zsite_id):
@@ -75,5 +104,6 @@ mq_mc_flush_zsite_follow = mq_client(mc_flush_zsite_follow)
 
 if __name__ == "__main__":
     print feed_id_list_for_zsite_follow(10000000)
-
+    for i in feed_entry_id_iter(2):
+        print i
 
