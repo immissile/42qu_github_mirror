71587eae64fd1fed725fb1ef83b81b8dbd35f465
zuroc <zsp007@gmail.com>
2011-05-22 13:59:53 +0800

    ifx

--- config/dev.py
+++ config/dev.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import conf
+from conf import load
+
+import socket
+load('host_dev_%s' % socket.gethostname())
+
+import getpass
+load('conf_dev_%s' % getpass.getuser())
 
 conf.DEBUG = True
 import __init__
 reload(__init__)
+
