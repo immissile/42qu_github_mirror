09e2448d39469a31ae4f138c0149324dbeae840b
zuroc <zsp007@gmail.com>
2011-05-23 23:40:58 +0800

    ifx

--- print_urlmap.py
+++ print_urlmap.py
@@ -8,8 +8,14 @@ def print_urlmap(module):
     from pprint import pprint
     import zweb
     prefix = len(module)+1
+    premn = None
     for url, cls in sorted(zweb._urlmap.URLMAP):
-        print "\t%s\t%s%s"%(url.ljust(24), "%s"%(cls.__module__[prefix:]+".py").ljust(16) , cls.__name__)
+        mn = cls.__module__[prefix:]+".py"
+        if mn == premn:
+            mn = ""
+        else:
+            premn = mn
+        print "\t%s\t%s%s"%(url.ljust(24), "%s"%(mn).ljust(16) , cls.__name__)
 
     zweb._urlmap.URLMAP = []
 
