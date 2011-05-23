b635e569523c8d08c687aebbc00fa70034b1b4b6
underdarkeye <underdarkeye@gmail.com>
2011-05-23 21:32:05 +0800

    f

--- model/tag.py
+++ model/tag.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #coding:utf-8
-from kv_table import KvTable
+from kv import Kv
 
-Tag = KvTable('tag')
+Tag = Kv('tag')
 
 if __name__ == "__main__":
     print Tag.id_by_value_new("test2")
