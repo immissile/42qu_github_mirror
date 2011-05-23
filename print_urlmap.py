9d4c36891b4a74ce9be91a63c577ac043ea52006
zuroc <zsp007@gmail.com>
2011-05-23 21:12:09 +0800

    fix

--- print_urlmap.py
+++ print_urlmap.py
@@ -4,14 +4,13 @@ import ctrl
 
 def print_urlmap(module):
     __import__(module, globals(), locals(), [], -1)
-    print ""
-    print (" %s "%module).center(72, "=")
+    print "\n%s :"%module
 
     from pprint import pprint
     import zweb
 
     for url, cls in sorted(zweb._urlmap.URLMAP):
-        print "%s\t%s%s"%(url.ljust(24), cls.__module__.ljust(12),cls.__name__)
+        print "\t%s\t%s%s"%(url.ljust(24), cls.__module__.ljust(12),cls.__name__)
 
     zweb._urlmap.URLMAP = []
 
