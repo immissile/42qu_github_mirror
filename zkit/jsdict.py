a91fea70e24f2b41ad726776978fa0a0ac92623b
zuroc <zsp007@gmail.com>
2011-05-23 20:08:32 +0800

    fix

--- zkit/jsdict.py
+++ zkit/jsdict.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-class StrCls(object):
+class JsDict(object):
     def __init__(self, o):
-        self.__o__ = o
+        if o is None:
+            o = {}
+        self.__dict__["_JsDict__o"] = o
 
     def __iter__(self):
-        for i in self.__d.iteritems():
+        for i in self.__o.iteritems():
             yield i
 
     def __nonzero__(self):
-        return bool(self.__d)
+        return bool(self.__o)
 
     def __setattr__(self, name, val):
         if val is not None:
-            self.__d[name] = val
+            self.__o[name] = val
 
     def __getattr__(self, name):
-        return self.__d.get(name, "")
+        return self.__o.get(name, "")
 
     def __getitem__(self, name):
-        return self.__d.get(name, "")
+        return self.__o.get(name, "")
 
     def __contains__(self, b):
-        return b in self.__d
+        return b in self.__o
 
     def __setitem__(self, name, val):
         if val is not None:
-            self.__d[name] = val
+            self.__o[name] = val
 
     def __delitem__(self, name):
-        del self.__d[name]
+        del self.__o[name]
 
 
