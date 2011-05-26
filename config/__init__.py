1276f5995a27207fc02e144d1b7b0f5441074f4a
zuroc <zsp007@gmail.com>
2011-05-26 15:50:09 +0800

    fix

--- config/__init__.py
+++ config/__init__.py
@@ -4,8 +4,30 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
+from os.path import abspath, dirname, join, normpath
+import sys
 from os.path import abspath, dirname, join, normpath
 from hmako.lookup import TemplateLookup
 
-print "load init"
+#初始化python的查找路径
+PREFIX = normpath(dirname(dirname(abspath(__file__))))
+if PREFIX not in sys.path:
+    sys.path = [PREFIX] + sys.path
+
+HTM_PATH = join(dirname(dirname(abspath(__file__))), 'htm')
+
+MAKOLOOKUP = TemplateLookup(
+    directories=HTM_PATH,
+    module_directory='/tmp/%s'%HTM_PATH.strip('/').replace('/', '.'),
+    disable_unicode=True,
+    encoding_errors='ignore',
+    default_filters=['str', 'h'],
+    filesystem_checks=DEBUG,
+    input_encoding='utf-8',
+    output_encoding=''
+)
+
+def render(htm, **kwds):
+    mytemplate = MAKOLOOKUP.get_template(htm)
+    return mytemplate.render(**kwds)
+
