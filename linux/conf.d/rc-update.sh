e958ee70c01d9b2342987158238213ed72b2475e
zuroc <devnull@localhost>
2011-05-27 23:45:17 +0800

    fix

---
+++ linux/conf.d/rc-update.sh
@@ -0,0 +1,10 @@
+rc-update add mysql default
+rc-update add nginx default
+rc-update add beanstalkd default
+rc-update add apache2 default
+rc-update add dnsmasq default
+rc-update add sshd default
+rc-update add memcached default
+rc-update add vixie-cron default
+rc-update add svscan default
+
