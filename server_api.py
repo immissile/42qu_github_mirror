28f92312a6555323fb8f5f8f5e2517b50b500a9e
zuroc <zsp007@gmail.com>
2011-05-28 09:39:13 +0800

    fix

---
+++ server_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding:utf-8
+import config
+import tornado.ioloop
+
+def run():
+    from api._application import application
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        port = int(sys.argv[1])
+    else:
+        port = config.API_PORT
+    print 'server on port %s'%port
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    run()
