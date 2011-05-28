f43a2b00a0d80abd1845ae0ddbf9210562c9479e
zuroc <zsp007@gmail.com>
2011-05-28 09:34:39 +0800

    fix

--- api/_application.py
+++ api/_application.py
@@ -2,20 +2,13 @@
 #coding:utf-8
 
 
-def main():
-    import tornado.ioloop
-    import tornado.web
-    from zweb import _urlmap
+import tornado.ioloop
+import tornado.web
+from zweb import _urlmap
 
-    urlmap = tuple(_urlmap.URLMAP)
 
-    application = tornado.web.Application(
-        urlmap
-    )
-
-    application = default_middleware(application)
-    return application
-
-application = main()
+application = tornado.web.Application(
+    tuple(_urlmap.URLMAP)
+)
 
 
