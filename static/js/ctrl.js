aa010c3e0c3fe455793d81961d13cb6193d9f693
zuroc <zsp007@gmail.com>
2011-05-31 16:54:12 +0800

    fix

--- static/js/ctrl.js
+++ static/js/ctrl.js
@@ -88,5 +88,3 @@ _gaq=[['_setAccount', 'UA-23361634-1'],['_trackPageview'],['_trackPageLoadTime']
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     /*google 百度 统计*/
 })()
-
-
