e3b8f803a2b6193f1587b2f13623529887b42bce
zuroc <zsp007@gmail.com>
2011-05-31 21:39:54 +0800

    fix

---
+++ model/can.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from cid import CID_USER
+from _db import Model, McModel
+
+ZSITE_STATE_BAN = 1
+ZSITE_STATE_NO_MAIL = 5
+ZSITE_STATE_APPLY = 9
+ZSITE_STATE_SILENT = 13
+ZSITE_STATE_ACTIVE = 17
+ZSITE_STATE_VERIFY = 21
+
+def can_reply()
