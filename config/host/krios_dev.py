5e6d2b84e55b49077825282ff6ffde8f57580286
zuroc <zsp007@gmail.com>
2011-06-03 20:57:39 +0800

    fix

--- config/host/krios_dev.py
+++ config/host/krios_dev.py
@@ -1,5 +1,7 @@
+#coding : utf-8
+
 def prepare(o):
-    o.SITE_DOMAIN = 'work.xxx'
-    o.MYSQL_MAIN = 'zpage'
-    o.PIC_DOMAIN = "p.%s"%o.SITE_DOMAIN
-    o.FS_DOMAIN = "s.%s"%o.SITE_DOMAIN
+    o.ALIPAY_SALT = "ri3woiadltar53f4snayzf78mhpmfgtf"
+    o.ALIPAY_ID = "2088002484028020"
+    o.ALIPAY_EMAIL = "zsp007@gmail.com"
+
