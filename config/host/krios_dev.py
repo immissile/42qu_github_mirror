604c8fcd948379a70dc7ab53a2fc63aa86eef3d6
zuroc <zsp007@gmail.com>
2011-06-03 21:04:48 +0800

    fix

--- config/host/krios_dev.py
+++ config/host/krios_dev.py
@@ -1,7 +1,1 @@
-#coding : utf-8
-
-def prepare(o):
-    o.ALIPAY_SALT = "ri3woiadltar53f4snayzf78mhpmfgtf"
-    o.ALIPAY_ID = "2088002484028020"
-    o.ALIPAY_EMAIL = "zsp007@gmail.com"
-
+zpage_private_config/krios_dev.py
