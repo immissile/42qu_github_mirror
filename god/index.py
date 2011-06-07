d15236622bd194542c8f86c9c33cba2981e48df4
zuroc <zsp007@gmail.com>
2011-06-08 03:39:20 +0800

    fix

--- god/index.py
+++ god/index.py
@@ -4,7 +4,6 @@
 
 import _handler
 from zweb._urlmap import urlmap
-from model.zsite import Zsite
 
 @urlmap("/")
 class Index(_handler.Base):
@@ -16,9 +15,4 @@ class Chart(_handler.Base):
     def get(self):
         self.render()
 
-@urlmap("/zsite/(\d+)")
-class Zsite(_handler.Base):
-    def get(self, id):
-        zsite = Zsite.mc_get(id)
-        self.render(zsite=zsite)
 
