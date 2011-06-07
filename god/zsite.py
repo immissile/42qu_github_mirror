ce8bb586412a5124eb7a70eb2a98d5bfd1908055
zuroc <zsp007@gmail.com>
2011-06-08 04:27:32 +0800

    fix

--- god/zsite.py
+++ god/zsite.py
@@ -3,7 +3,7 @@
 import _handler
 from model.zsite import Zsite
 from zweb._urlmap import urlmap
-
+from model.zsite_list_0 import zsite_list_new, zsite_list_rm
 
 @urlmap("/zsite/(\d+)")
 class Index(_handler.Base):
@@ -11,4 +11,16 @@ class Index(_handler.Base):
         zsite = Zsite.mc_get(id)
         self.render(zsite=zsite)
 
+@urlmap("/zsite/show/(\d+)")
+class Show(_handler.Base):
+    def get(self, id):
+        zsite = Zsite.mc_get(id)
+        if zsite:
+            zsite_list_new(id)
+        self.redirect("/zsite/%s"%id)
 
+@urlmap("/zsite/show/rm/(\d+)")
+class ShowRm(_handler.Base):
+    def get(self, id):
+        zsite_list_rm(id)
+        self.redirect("/zsite/%s"%id)
--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -3,21 +3,6 @@ from _db import McLimitA
 from zsite_list import ZsiteList
 from zsite import Zsite
 
-ZSITE_LIST_0_CID = (
-    (1, '创业'),
-    (2, '程序'),
-    (3, '设计'),
-    (4, '产品'),
-    (5, '运营'),
-    (6, '文化'),
-    (7, '媒体'),
-    (8, '金融'),
-    (9, '商务'),
-    (10, '管理'),
-    (11, '公关'),
-    (12, '科研'),
-    (13, '健康'),
-)
 
 mc_reply_id_list = McLimitA("ZsiteListId0:%s", 1024)
 
@@ -28,12 +13,18 @@ def zsite_list_id_0(cid=0, limit=None, offset=None):
         zsite_list = zsite_list.where(cid=cid)
     return zsite_list.id_list(limit, offset)
 
-def zsite_list_new(zsite_id, cid, rank=1000):
+def zsite_list_new(zsite_id, cid=0, rank=1000):
     zsite = ZsiteList.get_or_create(
-        zsite_id=zsite_id, owner_id=owner_id, cid=0, rank=rank
+        zsite_id=zsite_id, owner_id=0, cid=cid, rank=rank
     )
     zsite.rank = rank
     zsite.save()
+    mc_reply_id_list.delete(cid)
+
+def zsite_list_rm(zsite_id):
+    for i in ZsiteList.where(zsite_id=zsite_id, owner_id=0):
+        mc_reply_id_list.delete(i.cid)
+        i.delete()
 
 def zsite_list_get(zsite_id):
     return ZsiteList.get(zsite_id=zsite_id, owner_id=0)
