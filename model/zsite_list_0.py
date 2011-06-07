431ee211b1af3c952f380c67ea3052b760a9c489
zuroc <zsp007@gmail.com>
2011-06-08 04:10:21 +0800

    fix

--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -23,15 +23,20 @@ mc_reply_id_list = McLimitA("ZsiteListId0:%s", 1024)
 
 @mc_reply_id_list("{cid}")
 def zsite_list_id_0(cid=0, limit=None, offset=None):
-    zsite_list = ZsiteList.where()
+    zsite_list = ZsiteList.where().order_by("rank desc")
     if cid:
         zsite_list = zsite_list.where(cid=cid)
     return zsite_list.id_list(limit, offset)
 
 def zsite_list_new(zsite_id, cid, rank=1000):
-    zsite = ZsiteList.get_or_create(zsite_id=zsite_id, owner_id=owner_id, cid=cid)
+    zsite = ZsiteList.get_or_create(
+        zsite_id=zsite_id, owner_id=owner_id, cid=0, rank=rank
+    )
     zsite.rank = rank
     zsite.save()
+
+def zsite_list_get(zsite_id):
+    return ZsiteList.get(zsite_id=zsite_id, owner_id=0)
 
 if __name__ == "__main__":
     print zsite_list_id_0()
