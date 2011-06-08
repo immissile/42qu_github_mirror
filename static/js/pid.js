458e8ba360b02c26f53a6bea6c74e45516760c59
zuroc <zsp007@gmail.com>
2011-06-09 00:35:44 +0800

    fix

--- static/js/pid.js
+++ static/js/pid.js
@@ -9,26 +9,23 @@ COUNTRY_DICT= {"ae": "秘鲁", "c3": "萨尔瓦多", "c4": "萨巴", "d8": "马�
 COUNTRY= ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','10','11','12','13','14','15','16','17','18','19','1a','1b','1c','1d','1e','1f','20','21','22','23','24','25','26','27','28','29','2a','2b','2c','2d','2e','2f','30','31','32','33','34','35','36','37','38','39','3a','3b','3c','3d','3e','3f','40','41','42','43','44','45','46','47','48','49','4a','4b','4c','4d','4e','4f','50','51','52','53','54','55','56','57','58','59','5a','5b','5c','5d','5e','5f','60','61','62','63','64','65','66','67','68','69','6a','6b','6c','6d','6e','6f','70','71','72','73','74','75','76','77','78','79','7a','7b','7c','7d','7e','7f','80','81','82','83','84','85','86','87','88','89','8a','8b','8c','8d','8e','8f','90','91','92','93','94','95','96','97','98','99','9a','9b','9c','9d','9e','9f','a0','a1','a2','a3','a4','a5','a6','a7','a8','a9','aa','ab','ac','ad','ae','af','b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','ba','bb','bc','bd','be','bf','c0','c1','c2','c3','c4','c5','c6','c7','c8','c9','ca','cb','cc','cd','ce','cf','d0','d1','d2','d3','d4','d5','d6','d7','d8','d9','da','db','dc','dd','de','df','e0','e1','e2']
 
 function _option(cc,dd){
-    var i=0,l=cc.length,c;
+    var i=0,l=cc.length,c,r=[];
     for(;i<l;++i){
         c=cc[i]
-        document.write('<option title="name" value="'+c+'">'+dd[c]+'</option>')
-    }
-}
-function _option2(cc,pid){
-    var p=cc[pid],c,j=0,r=[];
-    if(!p)return ''
-    for(;j<p.length;++j){
-        c=p[j]
-        r.push('<option title="name" value="'+c+'">'+PID2NAME[c]+'</option>')
+        r.push('<option value="'+parseInt(c,16)+'">'+dd[c]+'</option>')
     }
     return r.join('')
 }
+function _option2(cc,pid){
+    var p=cc[(pid-0).toString(16)],c,j=0,r=[];
+    if(!p)return ''
+    return _option(p,PID2NAME)
+}
 function option_conutry(){
-    _option(COUNTRY,COUNTRY_DICT)
+    return _option(COUNTRY,COUNTRY_DICT, 16)
 }
 function option_city(){
-    _option(PLACE_L1,PID2NAME)
+    return _option(PLACE_L1,PID2NAME)
 }
 function option_town(pid){
     return _option2(PLACE_L1L2,pid)
@@ -37,3 +34,46 @@ function option_loc(pid){
     return _option2(PLACE_L2L3,pid)
 }
 
+function select_pid(id){
+    function select(name){
+        return pid.find("select[name="+name+"]")
+    }
+    var pid=$('#'+id).html(
+        [
+            '<input name="'+id+'" type="hidden" value="1">',
+            '<select name="country">',
+            option_conutry(),
+            '</select><select name="city"><option value="0">- 请选择 -</option>',
+            option_city(),
+            '</select><select name="town"></select><select name="loc"></select>'
+        ].join('')
+        ),
+        country=select('country'), 
+        city=select('city').val('0'), 
+        town=select('town').hide(),
+        loc=select('loc').hide(),
+        town_loc=$(town,loc);
+        
+        pid.find('select').change(function(){
+            if(this.value!='0'){
+                pid.find('input[name='+id+']').val(this.value)
+            }
+        })
+        
+        country.change(function(){
+            '1'==this.value?city.show():city.hide();
+            city.val('0');
+            town_loc.hide();
+        }).val('1');
+        function option(e,h,gen,s){
+            e.change(function(){
+                h.hide()
+                var p=gen(this.value);
+                if(p!=''){
+                    s.html('<option value="0">- 请选择 -</option>'+p).show()
+                }
+            }).val('0')
+        }
+        option(city, town_loc, option_town, town)
+        option(town, loc, option_loc, loc)
+}
