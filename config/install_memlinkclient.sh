2cd1ed5661139da2b532508ca39860afadf37a24
zuroc <zsp007@gmail.com>
2011-06-11 04:47:14 +0800

    fix

---
+++ config/install_memlinkclient.sh
@@ -0,0 +1,7 @@
+PYTHONLIBPATH=/usr/lib/python2.6/site-packages/ 
+mkdir -p memlink
+touch memlink/__init__.py
+cp *.py memlink/ 
+cp *.so memlink/
+rm $PYTHONLIBPATH/memlink -rf
+mv memlink $PYTHONLIBPATH
