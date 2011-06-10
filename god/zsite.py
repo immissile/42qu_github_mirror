f18c9592572d408f0fe928ef05b24377864ef290
underdarkeye <underdarkeye@gmail.com>
2011-06-10 23:43:51 +0800

    f

--- god/zsite.py
+++ god/zsite.py
@@ -3,7 +3,7 @@
 import _handler
 from model.zsite import Zsite
 from zweb._urlmap import urlmap
-from model.zsite_list_0 import zsite_list_new, zsite_list_rm
+from model.zsite_list_0 import zsite_show_new, zsite_show_rm
 
 @urlmap('/zsite/(\d+)')
 class Index(_handler.Base):
@@ -16,11 +16,11 @@ class Show(_handler.Base):
     def get(self, id):
         zsite = Zsite.mc_get(id)
         if zsite:
-            zsite_list_new(id)
+            zsite_show_new(id)
         self.redirect('/zsite/%s'%id)
 
 @urlmap('/zsite/show/rm/(\d+)')
 class ShowRm(_handler.Base):
     def get(self, id):
-        zsite_list_rm(id)
+        zsite_show_rm(id)
         self.redirect('/zsite/%s'%id)
--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -1,21 +1,20 @@
 #coding:utf-8
-from zsite import Zsite
-import zsite_list as zl
+from zsite_list import ZsiteList, zsite_list, zsite_list_new, zsite_list_rm, zsite_list_get, zsite_list_rank
 
 OWNER_ID = 0
 
-def zsite_list(cid, limit=None, offset=None):
-    return zl.zsite_list(0, cid, limit, offset)
+def zsite_show(cid, limit=None, offset=None):
+    return zsite_list(0, cid, limit, offset)
 
-def zsite_list_new(zsite_id, rank=1000):
+def zsite_show_new(zsite_id, rank=1000):
     cid_list = [] # TODO
-    zl.zsite_list_new(zsite_id, 0, cid_list, rank)
+    zsite_list_new(zsite_id, 0, cid_list, rank)
 
-def zsite_list_rm(zsite_id):
-    zl.zsite_list_rm(zsite_id, 0)
+def zsite_show_rm(zsite_id):
+    zsite_list_rm(zsite_id, 0)
 
-def zsite_list_get(zsite_id):
-    zl.zsite_list_get(zsite_id, 0)
+def zsite_show_get(zsite_id):
+    zsite_list_get(zsite_id, 0)
 
-def zsite_list_rank(zsite_id, rank):
-    zl.zsite_list_rank(zsite_id, 0, rank)
+def zsite_show_rank(zsite_id, rank):
+    zsite_list_rank(zsite_id, 0, rank)
