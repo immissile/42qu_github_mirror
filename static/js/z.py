7265334ea24d069096d39a75c7320bd17ea9a71c
zuroc <zsp007@gmail.com>
2011-06-14 05:17:46 +0800

    fix

--- static/js/z.py
+++ static/js/z.py
@@ -2,7 +2,6 @@ JS = [
 'jquery-1.6.1.min.js.h',
 'jquery.poshytip.js',
 'jquery.defaultvalue.source.js.h',
-'jquery.timeago.js.h',
 'jquery.tmpl.js.h',
 'jquery.fancybox-1.3.4.js.h',
 'jquery.elastic.source.js.h',
