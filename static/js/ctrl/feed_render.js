7a04ec5d3b5e3e340c9c616b2c036a2ec897b1e7
zuroc <zsp007@gmail.com>
2011-06-14 02:25:23 +0800

    fix

--- static/js/ctrl/feed_render.js
+++ static/js/ctrl/feed_render.js
@@ -7,44 +7,53 @@ var FEED_ATTR_BASE = "id zsite rt_list zsite_id cid reply_total name is_rt vote_
     FEED_ATTR = {
         61:FEED_ATTR_BASE,
         62:FEED_ATTR_BASE+" txt"
-    }
+    };
+
     for(var i in FEED_ATTR){
         FEED_ATTR[i]=FEED_ATTR[i].split(' ')
     }
 
-function init(result){
-    var rt_list=[],
-    data = {},
-    i=0,
-    attr=FEED_ATTR[result[4]];
-
-    for(;i<attr.length;++i){
-        data[attr[i]] = result[i]
+    function array2zsite(a){
+        return {
+            name: a[0],
+            link: a[1]
+        } 
     }
-    var zsite = data.zsite;
-    data.zsite = {
-        name: zsite[0],
-        link: zsite[1]
+
+    function init(result){
+        var rt_list=[],
+        data = {},
+        i=0,
+        attr=FEED_ATTR[result[4]];
+
+        for(;i<attr.length;++i){
+            data[attr[i]] = result[i]
+        }
+        data.zsite = array2zsite(data.zsite);
+        data.link = "/po/"+data.id;
+        
+        $.each(data.rt_list, function(){
+            rt_list.push(array2zsite(this))
+        })
+        data.rt_list = rt_list
+        return data
     }
-    data.link = "/po/"+data.id
-    return data
-}
 
-function init_result(result){
-    var length = result.length, item=[], i=0;
+    function init_result(result){
+        var length = result.length, item=[], i=0;
 
-    for(;i<length;++i){
-        item.push(init(result[i]))
+        for(;i<length;++i){
+            item.push(init(result[i]))
+        }
+        return item 
     }
-    return item 
-}
 
-function render_feed(){
-    $.postJSON(
-    "/j/feed",
-    function(result){
-        $('#feed').tmpl(init_result(result)).appendTo("#feeds")
-    })
-}
-render_feed()
+    function render_feed(){
+        $.postJSON(
+        "/j/feed",
+        function(result){
+            $('#feed').tmpl(init_result(result)).appendTo("#feeds")
+        })
+    }
+    render_feed()
 })()
