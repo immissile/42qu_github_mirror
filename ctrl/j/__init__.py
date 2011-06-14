580d27ed6ea47aea22197d6f126fc76f167f0c60
zuroc <zsp007@gmail.com>
2011-06-15 03:19:15 +0800

    fix

--- ctrl/j/__init__.py
+++ ctrl/j/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from main import _handler
+from ctrl.main import _handler
 from model.vote import vote_state
 from zweb._urlmap import urlmap
 from model.follow import follow_rm, follow_new
@@ -44,47 +44,4 @@ class Login(_handler.Base):
     def get(self):
         self.render()
 
-
-@urlmap('/j/feed/up1/(\d+)')
-class FeedUp(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        vote_up(current_user_id, id)
-        self.finish('{}')
-
-@urlmap('/j/feed/up0/(\d+)')
-class FeedUpX(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        vote_up_x(current_user_id, id)
-        self.finish('{}')
-
-@urlmap('/j/feed/down1/(\d+)')
-class FeedDown(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        vote_down(current_user_id, id)
-        self.finish('{}')
-
-@urlmap('/j/feed/down0/(\d+)')
-class FeedDownX(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        vote_down_x(current_user_id, id)
-        self.finish('{}')
-
-@urlmap('/j/feed')
-class Feed(_handler.JLoginBase):
-    def get(self, id=MAXINT):
-        current_user_id = self.current_user_id
-
-        result = render_feed_by_zsite_id(current_user_id, PAGE_LIMIT, id)
-        for i in result:
-            id = i[0]
-            i.insert(FEED_TUPLE_DEFAULT_LEN, vote_state(current_user_id, id))
-            i.insert(FEED_TUPLE_DEFAULT_LEN, feed_rt_id(current_user_id, id))
-
-        self.finish(dumps(result))
-
-    post = get
 
---
+++ ctrl/j/feed.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ctrl.main import _handler
+from model.vote import vote_state
+from zweb._urlmap import urlmap
+from model.follow import follow_rm, follow_new
+from model.po import Po, CID_NOTE
+from json import dumps
+from zkit.pic import picopen
+from model.po_pic import pic_can_add, po_pic_new, po_pic_rm
+from model.fs import fs_url_jpg
+from model.vote import vote_down_x, vote_down, vote_up_x, vote_up
+from model.feed_render import MAXINT, PAGE_LIMIT, render_feed_by_zsite_id, FEED_TUPLE_DEFAULT_LEN
+from model.feed import feed_rt, feed_rt_rm, feed_rt_id
+
+
+@urlmap('/j/feed/up1/(\d+)')
+class FeedUp(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        vote_up(current_user_id, id)
+        self.finish('{}')
+
+@urlmap('/j/feed/up0/(\d+)')
+class FeedUpX(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        vote_up_x(current_user_id, id)
+        self.finish('{}')
+
+@urlmap('/j/feed/down1/(\d+)')
+class FeedDown(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        vote_down(current_user_id, id)
+        self.finish('{}')
+
+@urlmap('/j/feed/down0/(\d+)')
+class FeedDownX(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        vote_down_x(current_user_id, id)
+        self.finish('{}')
+
+@urlmap('/j/feed')
+class Feed(_handler.JLoginBase):
+    def get(self, id=MAXINT):
+        current_user_id = self.current_user_id
+
+        result = render_feed_by_zsite_id(current_user_id, PAGE_LIMIT, id)
+        for i in result:
+            id = i[0]
+            i.insert(FEED_TUPLE_DEFAULT_LEN, vote_state(current_user_id, id))
+            i.insert(FEED_TUPLE_DEFAULT_LEN, feed_rt_id(current_user_id, id))
+
+        self.finish(dumps(result))
+
+    post = get
+
