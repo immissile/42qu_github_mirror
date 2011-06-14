22bd635a0a8b87b1a908edbd2822320c9a0ce008
zuroc <zsp007@gmail.com>
2011-06-15 03:24:07 +0800

    fix

--- ctrl/j/__init__.py
+++ ctrl/j/__init__.py
@@ -2,41 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import _handler
-from model.vote import vote_state
 from zweb._urlmap import urlmap
-from model.follow import follow_rm, follow_new
-from model.po import Po, CID_NOTE
-from json import dumps
-from zkit.pic import picopen
-from model.po_pic import pic_can_add, po_pic_new, po_pic_rm
-from model.fs import fs_url_jpg
-from model.vote import vote_down_x, vote_down, vote_up_x, vote_up
-from model.feed_render import MAXINT, PAGE_LIMIT, render_feed_by_zsite_id, FEED_TUPLE_DEFAULT_LEN
-from model.feed import feed_rt, feed_rt_rm, feed_rt_id
-
-
-@urlmap('/j/rt/(\d+)')
-class Rt(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        if current_user_id:
-            feed_rt(current_user_id, id)
-        self.finish("{}")
-
-
-@urlmap('/j/rt/rm/(\d+)')
-class RtRm(_handler.JLoginBase):
-    def post(self, id):
-        current_user_id = self.current_user_id
-        if current_user_id:
-            feed_rt_rm(current_user_id, id)
-        self.finish("{}")
-
-
-@urlmap('/j/txt')
-class Txt(_handler.Base):
-    def get(self):
-        self.render()
 
 
 @urlmap('/j/login')
---
+++ ctrl/j/rt.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import _handler
+from model.feed import feed_rt, feed_rt_rm, feed_rt_id
+from zweb._urlmap import urlmap
+
+@urlmap('/j/rt/(\d+)')
+class Rt(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        if current_user_id:
+            feed_rt(current_user_id, id)
+        self.finish("{}")
+
+
+@urlmap('/j/rt/rm/(\d+)')
+class RtRm(_handler.JLoginBase):
+    def post(self, id):
+        current_user_id = self.current_user_id
+        if current_user_id:
+            feed_rt_rm(current_user_id, id)
+        self.finish("{}")
+
