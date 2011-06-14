10b64a0d3ec6ac65159a751d0c6b49f58c5bb1ce
underdarkeye <underdarkeye@gmail.com>
2011-06-14 17:39:58 +0800

    f

--- static/js/ctrl/feed.js
+++ static/js/ctrl/feed.js
@@ -1,6 +1,4 @@
-
-
-(function(){
+(function() {
 /*
  0 -1 -1
  1 -1 -2
@@ -10,54 +8,53 @@
 -1  1  2
 */
 
-    $(".reply,.decr,.incr,.num,.rt,.rted").poshytip({
-        className: 'tip-twitter',
-        showTimeout: 100,
-        alignTo: 'target',
-        alignX: 'center',
-        offsetY: 5,
-        allowTipHover: false,
-        fade: false,
-        slide: false,
-        liveEvents: true
-    });
+	$(".reply,.down,.up,.num,.rt,.rted").poshytip({
+		className: 'tip-twitter',
+		showTimeout: 100,
+		alignTo: 'target',
+		alignX: 'center',
+		offsetY: 5,
+		allowTipHover: false,
+		fade: false,
+		slide: false,
+		liveEvents: true
+	});
 
-    var decr="decr",incr="incr",vote="vote";
-    function _(a,b,id,v){
-        var wj=$("#"+vote+id), w=wj[0],
-            state=w.className.slice(4)-0,
-            num=wj.find('.num'),
-            numv=num.text()-0,
-            c=v, 
-            notsame=(v!=state)-0;
-        if(notsame){
-            v-=state
-        }else{
-            c=0;
-            v=-v
-        }
-        $.postJSON("/j/feed/"+a+notsame+"/"+id)
-        w.className = vote+c;
-        num.text(numv+v)
-    }
-    vote_incr = function(id){
-        _(incr,decr,id,1)
-    }
-    vote_decr = function (id){
-        _(decr,incr,id,-1)
-    }
+	var down = "down", up = "up", vote = "vote";
+	function _(a, b, id, v) {
+		var wj = $("#" + vote + id),
+		w = wj[0],
+		state = w.className.slice(4) - 0,
+		num = wj.find('.num'),
+		numv = num.text() - 0,
+		c = v,
+		notsame = (v != state) - 0;
+		if (notsame) {
+			v -= state
+		} else {
+			c = 0;
+			v = - v
+		}
+		$.postJSON("/j/feed/" + a + notsame + "/" + id)
+		w.className = vote + c;
+		num.text(numv + v)
+	}
+	vote_up = function(id) {
+		_(up, down, id, 1)
+	}
+	vote_down = function(id) {
+		_(down, up, id, - 1)
+	}
 })()
 
-$(".rt").live("click", function(){
-    this.className="rted"
-    var self=$(this);
-    $()
-    self.poshytip('show').poshytip('update','转发成功',true)
+$(".rt").live("click", function() {
+	this.className = "rted"
+	var self = $(this);
+	$()
+	self.poshytip('show').poshytip('update', '转发成功', true)
 })
-$(".rted").live("click",function(){
-    this.className="rt"
-    var self=$(this);    
-    self.poshytip('update','已转发').poshytip('show').poshytip('update','转发被取消',true)
+$(".rted").live("click", function() {
+	this.className = "rt"
+	var self = $(this);
+	self.poshytip('update', '已转发').poshytip('show').poshytip('update', '转发被取消', true)
 })
-
-
