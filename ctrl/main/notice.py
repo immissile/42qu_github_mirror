a0cc4efb3e2b7c4caf5f5c91801ed429f7a98aac
underdarkeye <underdarkeye@gmail.com>
2011-06-15 23:55:57 +0800

    f

--- ctrl/main/notice.py
+++ ctrl/main/notice.py
@@ -2,26 +2,26 @@
 # -*- coding: utf-8 -*-
 from _handler import Base, LoginBase, XsrfGetBase
 from zweb._urlmap import urlmap
+from model.notice import notice_list, notice_count
 from zkit.page import page_limit_offset
-from model.buzz import buzz_list, buzz_count
 
 PAGE_LIMIT = 100
 
-@urlmap('/buzz')
-@urlmap('/buzz/(\d+)')
+@urlmap('/notice')
+@urlmap('/notice/(\d+)')
 class Page(LoginBase):
     def get(self, n=1):
         user_id = self.current_user_id
-        total = buzz_count(user_id)
+        total = notice_count(user_id)
         page, limit, offset = page_limit_offset(
-            '/buzz/%s',
+            '/notice/%s',
             total,
             n,
             PAGE_LIMIT
         )
         if type(n) == str and offset >= total:
-            return self.redirect('/buzz')
+            return self.redirect('/notice')
         self.render(
-            buzz_list=buzz_list(user_id, limit, offset),
+            notice_list=notice_list(user_id, limit, offset),
             page=page,
         )
--- model/notice.py
+++ model/notice.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 from _db import Model, McModel, McCache, McCacheA, McLimitA, McNum
 from time import time
-from cid import CID_INVITE_REGISTER, CID_notice_REGISTER, CID_INVITE_QUESTION, CID_notice_QUESTION
+from cid import CID_INVITE_REGISTER, CID_NOTICE_REGISTER, CID_INVITE_QUESTION, CID_NOTICE_QUESTION
 from state import STATE_DEL, STATE_APPLY, STATE_ACTIVE
+from po import Po
 
 STATE_GTE_APPLY = 'state>=%s' % STATE_APPLY
+
+NOTICE_DIC = {
+    CID_INVITE_QUESTION: Po,
+    CID_notice_QUESTION: Po,
+}
 
 class Notice(McModel):
     pass
@@ -35,6 +41,31 @@ mc_notice_id_list = McLimitA('NoticeIdList.%s')
 @mc_notice_id_list('{to_id}')
 def notice_id_list(to_id, limit, offset):
     return Notice.where(to_id=to_id).where('state>=%s' % STATE_APPLY).order_by('id desc').field_list(limit, offset)
+
+def notice_list(to_id, limit, offset):
+    li = Notice.mc_get_list(notice_id_list(to_id, limit, offset))
+    dic = OrderedDict()
+    cls_dic = defaultdict(set)
+    for i in li:
+        cls_dic[Zsite].add(i.from_id)
+        cls_dic[NOTICE_DIC.get(i.cid)].add(i.rid)
+    for cls, id_list in cls_dic.items():
+        if cls:
+            cls_dic[cls] = cls.mc_get_dict(id_list)
+        else:
+            cls_dic[cls] = {}
+    for i in li:
+        i.from_user = cls_dic[Zsite][i.from_id]
+        i.entry = cls_dic[NOTICE_DIC.get(i.cid)].get(i.rid)
+    return li
+
+def notice_rm(to_id, notice_id):
+    n = Notice.mc_get(notice_id)
+    if n and n.to_id = to_id and n.state > STATE_DEL:
+        n.state = STATE_DEL
+        n.save()
+        mc_flush(to_id)
+        return True
 
 def mc_flush(to_id):
     to_id = str(to_id)
