acc803af29e95328ce57686ed165f65d98dfbb17
zuroc <zsp007@gmail.com>
2011-06-16 05:38:01 +0800

    fix

--- zweb/urlmap.py
+++ zweb/urlmap.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-URLMAP = []
-
-def urlmap(url, **kwds):
-    def _(cls):
-        URLMAP.append(
-            (url, cls, kwds)
-        )
-        return cls
-    return _
-
 
 class Urlmap(object):
     def __init__(self):
