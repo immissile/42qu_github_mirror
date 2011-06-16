8ada9192e08e362e2318343b4ac056c0378fd454
zuroc <zsp007@gmail.com>
2011-06-16 18:46:44 +0800

    fix

--- ctrl/me/feed.py
+++ ctrl/me/feed.py
@@ -1,18 +1,1 @@
 # -*- coding: utf-8 -*-
-from _handler import Base, LoginBase, XsrfGetBase
-from _urlmap import urlmap
-from model.feed_render import render_feed_by_zsite_id
-from model.feed import PAGE_LIMIT, MAXINT
-
-@urlmap('/feed')
-class Index(LoginBase):
-    def get(self):
-        begin_id = MAXINT
-        current_user = self.current_user
-        current_user_id = current_user.id
-        entry_list = render_feed_by_zsite_id(
-            current_user_id, PAGE_LIMIT, begin_id
-        )
-        return self.render(
-            entry_list=entry_list
-        )
--- ctrl/zsite/live.py
+++ ctrl/zsite/live.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import  _handler
+from _handler import Base, LoginBase, XsrfGetBase
 from _urlmap import urlmap
+from model.feed_render import render_feed_by_zsite_id
+from model.feed import PAGE_LIMIT, MAXINT
 
 @urlmap('/live')
-class Index(_handler.Base):
+class Index(Base):
     def get(self):
-        self.render()
-
-
+        begin_id = MAXINT
+        zsite = self.zsite
+        zsite_id = zsite.id
+        entry_list = render_feed_by_zsite_id(
+            zsite_id, PAGE_LIMIT, begin_id
+        )
+        return self.render(
+            entry_list=entry_list
+        )
