36cf4d63ce5ad24825b3406676655a565d92d65f
zuroc <zsp007@gmail.com>
2011-06-16 18:27:33 +0800

    fix

---
+++ ctrl/main/_urlmap.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from zweb.urlmap import Urlmap
+
+urlmap = Urlmap()
---
+++ ctrl/main/index.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from _handler import Base, LoginBase, XsrfGetBase
+from _urlmap import urlmap
+
+@urlmap('/')
+class Index(Base):
+    def get(self):
+        self.render()
