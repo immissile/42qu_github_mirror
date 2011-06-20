f82bdd3589669caeccf055a42379cc6af5a2fc27
zuroc <zsp007@gmail.com>
2011-06-21 02:23:07 +0800

    fix

--- api/_handler.py
+++ api/_handler.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from config import render
 import model._db
-from zweb._handler import Base as _Base, _login_redirect, login
+from model.api_client import api_sign_verify, api_login_verify
+from model.api_error import API_ERROR_SIGN, API_ERROR_LOGIN
+from zweb._handler import Base as _Base, _login_redirect, login, LoginBase
 
 
 class Base(_Base):
@@ -17,15 +19,31 @@ class Base(_Base):
         super(Base, self).render(template_name, **kwds)
 
 
-class LoginBase(Base):
-    def prepare(self):
-        super(LoginBase, self).prepare()
-        _login_redirect(self)
+class ApiBase(Base):
+    pass
 
 
-class XsrfGetBase(LoginBase):
+class ApiSignBase(ApiBase):
     def prepare(self):
-        super(XsrfGetBase, self).prepare()
-        self.check_xsrf_cookie()
+        arguments = self.request.arguments
+        arguments = dict([
+            (k, v[0]) for k, v in arguments.iteritems()
+        ])
+        if api_sign_verify(arguments):
+            super(ApiBase, self).prepare()
+        else:
+            self.finish(API_ERROR_SIGN)
+
+
+
+class ApiLoginBase(ApiSignBase):
+    def prepare(self):
+        super(ApiBase, self).prepare()
+        if self._finished:
+            return
+        S = self.get_argument("S")
+        client_id = self.get_argument('client_id')
+        if not api_login_verify(client_id, S):
+            self.finish(API_ERROR_LOGIN)
 
 
