c470effe9d7ca48deb82bba4e8fed1e961d6e382
zuroc <zsp007@gmail.com>
2011-06-20 14:37:06 +0800

    fix

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@
 
 from zweb._handler.me import Base, LoginBase
 from _urlmap import urlmap
-
+from model.api_client import api_client_new
 
 @urlmap('/')
 class Index(Base):
@@ -12,8 +12,16 @@ class Index(Base):
         self.render()
 
 
-@urlmap("/apply")
+@urlmap('/apply')
 class Apply(LoginBase):
     def get(self):
         self.render()
+
+    def post(self):
+        current_user = self.current_user
+        user_id = self.current_user_id
+        name = self.get_argument('name', current_user.name)
+        txt = self.get_argument('txt', '')
+        api_client_new(user_id, name, txt)
+        return self.redirect("/")
 
