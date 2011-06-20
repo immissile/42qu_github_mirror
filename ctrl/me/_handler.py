f6e84d9d8e182a4709b53748dd9e658fb94637aa
zuroc <zsp007@gmail.com>
2011-06-20 13:52:39 +0800

    fix

--- ctrl/me/_handler.py
+++ ctrl/me/_handler.py
@@ -1,31 +1,4 @@
 # -*- coding: utf-8 -*-
 from config import render
 import model._db
-from zweb._handler import Base as _Base, _login_redirect, login
-
-
-class Base(_Base):
-    def get(self, *args):
-        self.redirect('/')
-
-    @property
-    def _xsrf(self):
-        return '_xsrf=%s'%self.xsrf_token
-
-    def render(self, template_name=None, **kwds):
-        kwds['_xsrf'] = self._xsrf
-        super(Base, self).render(template_name, **kwds)
-
-
-class LoginBase(Base):
-    def prepare(self):
-        super(LoginBase, self).prepare()
-        _login_redirect(self)
-
-
-class XsrfGetBase(LoginBase):
-    def prepare(self):
-        super(XsrfGetBase, self).prepare()
-        self.check_xsrf_cookie()
-
-
+from zweb._handler.me import * 
