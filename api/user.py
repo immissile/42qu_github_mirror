9276b2ec80414ead843e81b1e844f5bb7821fc0f
zuroc <zsp007@gmail.com>
2011-06-20 12:03:44 +0800

    fix

--- api/user.py
+++ api/user.py
@@ -8,7 +8,7 @@ from model.user_mail import user_by_mail
 from model.ico import ico96
 
 @urlmap('/user/info/mail')
-class Index(_handler.Base):
+class Index(_handler.ApiBase):
     def get(self):
         mail = self.get_argument('mail',0)
         user = user_by_mail(mail)
@@ -19,4 +19,15 @@ class Index(_handler.Base):
             data['name'] = user.name
             data['ico96'] = ico96.get(user_id) 
         self.finish(data)
+
+
+@urlmap('/user/auth/login')
+def Login(_handler.ApiSignBase):
+    def get(self):
+        user_id = self.get_argument(user_id,0)
+        auth = self.get_argument(auth)
+        
+        
+
+
 
--- model/user_auth.py
+++ model/user_auth.py
@@ -4,6 +4,7 @@ from _db import Model, McModel
 from hashlib import sha256
 from user_mail import user_mail_new, user_id_by_mail
 from zsite import zsite_new_user, Zsite
+from binascii import hexlify
 
 def hash_password(id, password):
     return sha256('%s%s'%(password, id)).digest()
@@ -11,11 +12,9 @@ def hash_password(id, password):
 class UserPassword(Model):
     pass
 
-def user_new_session(user_id):
-    return
-
 def user_password_sha256(user_id):
-    return 
+    password = UserPassword.get(id=user_id)
+    return hexlify(password.password)
 
 def user_password_new(user_id, password):
     if password is not None:
@@ -40,4 +39,6 @@ def user_new_by_mail(mail, password=None):
     return user_id
 
 if __name__ == '__main__':
-    pass
+    print user_password_sha256(1)
+    
+
