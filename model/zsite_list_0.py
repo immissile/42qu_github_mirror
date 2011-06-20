5db059ac932f4a3bef110c092b114a23759f7b30
underdarkeye <underdarkeye@gmail.com>
2011-06-20 21:22:50 +0800

    f

--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -1,4 +1,5 @@
-#coding:utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from zsite_list import ZsiteList, zsite_list, zsite_list_new, zsite_list_rm, zsite_list_get, zsite_list_rank
 
 OWNER_ID = 0
