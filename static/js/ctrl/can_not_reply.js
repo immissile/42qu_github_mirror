b9a446b18a99f5bda909e9a5d0d47fb25840de2d
zuroc <zsp007@gmail.com>
2011-06-21 16:12:18 +0800

    fix

---
+++ static/js/ctrl/can_not_reply.js
@@ -0,0 +1,10 @@
+(function(){var reply_txt=$("#reply_txt"),form=reply_txt.parents('form');
+function _(){
+    $.fancybox({
+        content:'<div class="tc f16 pd16" style="width:225px"><p>未认证用户不能回复哦 , 亲 ! <p><p><a href="/i/verify">点此申请认证吧</a></p></div>'
+    })
+    return false;
+}
+reply_txt.focus(_)
+form.submit(_)
+})()
