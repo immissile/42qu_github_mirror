1d402e71fe5e7f52013e7fcad8bcef80d5ce116b
zuroc <zsp007@gmail.com>
2011-06-21 15:50:57 +0800

    fix

--- model/reply.py
+++ model/reply.py
@@ -27,13 +27,16 @@ class ReplyMixin(object):
     def reply_new(self, user, txt, state=STATE_ACTIVE):
         from zsite import user_can_reply
         user_id = user.id
+
         if not user_can_reply(user):
+            return
+        if is_spammer(user_id):
             return
 
         txt = txt.rstrip()
         cid = self.cid
         rid = self.id
-        if not txt or is_spammer(user_id) or is_same_post(user_id, cid, rid, txt, state):
+        if not txt or is_same_post(user_id, cid, rid, txt, state):
             return
 
         id = gid()
