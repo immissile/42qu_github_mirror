9d40deff6c1d36b97e2cd3ad9ff500b12b7f0be8
zuroc <zsp007@gmail.com>
2011-06-22 07:45:37 +0800

    fix

--- static/js/ctrl/po/note.js
+++ static/js/ctrl/po/note.js
@@ -1,6 +1,12 @@
 $(function(){
     $("#name").placeholder()
     $("#txt").elastic()
-    $("")
+    $("#po_form").submit(function(){
+        $("[placeholder]").each(function(){
+            if(this.value==this.placeholder){
+                this.value='';
+            }
+        })
+    })
 })
 
