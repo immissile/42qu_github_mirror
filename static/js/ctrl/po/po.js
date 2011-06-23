5fc995cae4c9e5465c2b98900ac5788774239642
zuroc <zsp007@gmail.com>
2011-06-23 16:04:53 +0800

    fix

--- static/js/ctrl/po/po.js
+++ static/js/ctrl/po/po.js
@@ -1,3 +1,24 @@
 rm = _rm("#reply","/po/reply/rm/")
 $("#txt_form").elastic_login()
 
+$(".reply_at").live("click", function(){
+    var txt=$("#reply_txt").focus(),
+        val=txt.val(),
+        id=this.rel,
+        aname=$('#reply_name'+id),
+        href=aname[0].parentNode.href,
+        name=aname.text(),
+        add;
+    href = href.split("/");
+    href = href[href.length-1];
+    add =  "@"+name+'('+href+') '
+    if(val.length){
+        if($.trim(val)==val){
+            val+=" "
+        }
+        val+=add;
+    }else val=add;
+
+    txt.val(val)
+
+})
