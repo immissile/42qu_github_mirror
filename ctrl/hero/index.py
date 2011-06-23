6d72b6dd582038df8cc62e99047ca7c70eee63e5
zuroc <zsp007@gmail.com>
2011-06-23 23:58:51 +0800

    fix

--- ctrl/hero/index.py
+++ ctrl/hero/index.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from _handler import Base, LoginBase, XsrfGetBase
-from ctrl._urlmap.main import urlmap
+from ctrl._urlmap.hero import urlmap
 
 @urlmap('/')
 class Index(Base):
