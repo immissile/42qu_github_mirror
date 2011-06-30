158fff65a364c1f6ef5ca1376b1d5c24cc5498dc
zuroc <zsp007@gmail.com>
2011-07-01 07:58:23 +0800

    fix

--- static/js/date.js
+++ static/js/date.js
@@ -13,17 +13,17 @@ function select_date(id, value, year_begin, year_end){
     ], i, span=$("#"+id),day_option='<option value="0">- 日 -</option>';
     if(year_begin>year_end){
         i=year_begin; 
-        r.push('<select class="'+id+'_year" name="year"><option value="0000">- 年 -</option>')
+        r.push('<select class="'+id+'_year date_year" name="year"><option value="0000">- 年 -</option>')
         for(;i>year_end;--i){
             r.push('<option value="'+i+'">'+i+'</option>')
         }
         r.push('</select>')
     }
-    r.push('<select class="'+id+'_month" name="month"><option value="0">- 月 -</option>')
+    r.push('<select class="'+id+'_month date_month" name="month"><option value="0">- 月 -</option>')
     for(i=1;i<13;++i){
         r.push('<option value="'+i+'">'+i+'</option>')
     }
-    r.push('</select><select class="'+id+'_day" name="day">'+day_option+'</select>')
+    r.push('</select><select class="'+id+'_day date_day" name="day">'+day_option+'</select>')
     function select(name){
         return span.find("select[name="+name+"]")
     }
