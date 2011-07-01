05d75493f082091568fa93caf5ca0134c0aaeb4f
zuroc <zsp007@gmail.com>
2011-07-01 13:24:28 +0800

    fix

--- linux/conf.d/sudo_copy.sh
+++ linux/conf.d/sudo_copy.sh
@@ -4,4 +4,6 @@ cp sudoers /etc/sudoers
 cp bin/* /usr/bin
 cp j2 /etc/bash/ -R
 cp bashrc /etc/bash/bashrc
-cp dnsmasq.conf /etc/dnsmasq.conf 
+cp dnsmasq.conf /etc/dnsmasq.conf
+mkdir -p /var/log/nginx_backup/
+cp logrotate.d/* /etc/logrotate.d/ 
