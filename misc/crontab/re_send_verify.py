e74b1b22eb578134801e9731d0d19c1d9f752c4d
silegon <silegon@gmail.com>
2011-07-04 07:43:57 +0800

    fix

--- misc/crontab/re_send_verify.py
+++ misc/crontab/re_send_verify.py
@@ -2,20 +2,12 @@
 # -*- coding: utf-8 -*-
 import init_env
 from time import time
-from model._db import Model
 from model.mail import rendermail
 from model.cid import CID_VERIFY_MAIL
 from model.user_mail import mail_by_user_id
-
+from model.verify import VERIFY_TEMPLATE, Verify
 
 TIME_LIMIT = 3600 * 24 * 7
-
-VERIFY_TEMPLATE = {
-    CID_VERIFY_MAIL: '/mail/auth/verify/mail.txt',
-}
-
-class Verify(Model):
-    pass
 
 def re_send_verify():
     import time
@@ -29,7 +21,6 @@ def re_send_verify():
         template = VERIFY_TEMPLATE[CID_VERIFY_MAIL]
         rendermail(template, mail, name, id=id, ck=ck)
         item.update(create_time = now)
-        
 
 
 if __name__=='__main__':
