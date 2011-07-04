fc2ea7a587e70eb948e8bd34d02c797dfaa136b9
silegon <silegon@gmail.com>
2011-07-04 16:59:01 +0800

    fix

--- misc/crontab/re_send_verify.py
+++ misc/crontab/re_send_verify.py
@@ -3,23 +3,24 @@
 import init_env
 from time import time
 from model.mail import rendermail
-from model.cid import CID_VERIFY_MAIL
+from model.cid import CID_VERIFY_MISS
 from model.user_mail import mail_by_user_id
 from model.verify import VERIFY_TEMPLATE, Verify
 
-TIME_LIMIT = 3600 * 24 * 7
+SIX_DAYS = 3600 * 24 * 6 
+A_DAY = 3600 * 24
 
 def re_send_verify():
     now = int(time())
-    week_ago = now - TIME_LIMIT
-    for item in Verify.where('create_time<%s'%week_ago):
+    ago = now - SIX_DAYS
+    week_ago = ago - A_DAY
+    for item in Verify.where('create_time<%s and create_time>%s', ago, week_ago):
         mail = mail_by_user_id(item.user_id)
         name = mail
         ck = item.value
         id = item.user_id
-        template = VERIFY_TEMPLATE[CID_VERIFY_MAIL]
+        template = VERIFY_TEMPLATE[CID_VERIFY_MISS]
         rendermail(template, mail, name, id=id, ck=ck)
-        item.update(create_time = now)
 
 
 if __name__=='__main__':
