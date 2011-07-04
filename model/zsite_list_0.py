18b9bf8f3db1e54fda051718c92f4d85ff8ed10b
underdarkeye <underdarkeye@gmail.com>
2011-07-04 15:45:29 +0800

    f

--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -3,6 +3,7 @@
 from zsite_list import ZsiteList, zsite_id_list, zsite_list_new, zsite_list_rm, zsite_list_get, zsite_list_rank, mc_flush_owner_id_cid
 from functools import partial
 from zsite_rank import zsite_rank_get
+from zweb.orm import ormiter
 
 OWNER_ID = 0
 
@@ -11,7 +12,7 @@ zsite_show = partial(zsite_id_list, 0, 0)
 #    return zsite_list(0, 0, limit, offset)
 
 def zsite_show_update():
-    for i in zsite_show():
+    for i in ormiter(ZsiteList, 'owner_id=0 and cid=0'):
         i.rank = zsite_rank_get(i.id)
         i.save()
     mc_flush_owner_id_cid(0, 0)
--- model/zsite_uv_daily.py
+++ model/zsite_uv_daily.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import init_env
 from model._db import Model
 
 
