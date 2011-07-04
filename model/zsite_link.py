01593e447afa84b29e8003fda5ac2e38894df988
zuroc <zsp007@gmail.com>
2011-07-04 17:06:41 +0800

    fix

--- model/zsite_link.py
+++ model/zsite_link.py
@@ -6,6 +6,28 @@ from hashlib import sha256
 from zsite import zsite_new_user, Zsite
 from config import SITE_DOMAIN, SITE_DOMAIN_SUFFIX
 
+OAUTH_GOOGLE = 1
+OAUTH_DOUBAN = 2
+OAUTH_SINA = 3
+OAUTH_TWITTER = 4
+OAUTH_WWW163 = 5
+OAUTH_BUZZ = 6
+OAUTH_SOHU = 7
+OAUTH_QQ = 8
+OAUTH_RENREN = 9
+OAUTH_LINKEDIN = 10
+
+OAUTH2NAME = (
+    (OAUTH_DOUBAN, '豆瓣'),
+    (OAUTH_SINA, '新浪微博'),
+    (OAUTH_QQ, '腾讯微博'),
+    (OAUTH_BUZZ, 'Google+'),
+    (OAUTH_TWITTER, 'Twitter'),
+    (OAUTH_LINKEDIN, 'LinkedIn'),
+
+)
+OAUTH2NAME_DICT = dict(OAUTH2NAME)
+
 
 mc_link_id_name = McCache("LinkIdName:%s")
 mc_link_id_cid = McCache("LinkIdCid:%s")
@@ -24,10 +46,25 @@ def link_id_name_by_zsite_id(zsite_id):
 
 @mc_link_by_id("{id}")
 def link_by_id(id):
-    return  
+    link = ZsiteLink.get(id)
+    return link.link 
 
-def link_by_cid_id(id):
-    return
+def link_cid_id(id):
+    c = ZsiteLink.raw_sql('select id, cid from zsite_link where zsite_id=%s', zsite_id)
+    return c.fetchall()
+
+def link_list_save(zsite_id, link_list):
+    for link, cid in link_list:
+        link = ZsiteLink.get_or_create(zsite_id=zsite_id, cid=cid)
+        link.link = link
+        link.name = OAUTH2NAME_DICT[cid]  
+        link_id = link.id
+        link.save()
+        if link_id:
+            mc_link_by_id.delete(link.id)
+    mc_flush(zsite_id)
+
+
 
 if __name__ == "__main__":
     print link_id_name_by_zsite_id(1)
