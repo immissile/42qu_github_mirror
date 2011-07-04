be2f59748cc6a3c0985494aec5c7143b28a4703d
silegon <silegon@gmail.com>
2011-07-04 17:20:39 +0800

    fix

--- misc/crontab/re_send_verify.py
+++ misc/crontab/re_send_verify.py
@@ -5,13 +5,14 @@ from time import time
 from model.mail import rendermail
 from model.user_mail import mail_by_user_id
 from model.verify import VERIFY_TEMPLATE, Verify
+from model.days import today_days
 
 SIX_DAYS = 3600 * 24 * 6 
 A_DAY = 3600 * 24
 
 def re_send_verify():
-    now = int(time())
-    ago = now - SIX_DAYS
+    today = today_days() * A_DAY
+    ago = today - SIX_DAYS
     week_ago = ago - A_DAY
     for item in Verify.where('create_time<%s and create_time>%s', ago, week_ago):
         mail = mail_by_user_id(item.user_id)
