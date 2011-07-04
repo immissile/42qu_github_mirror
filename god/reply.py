edcd5b78e491f994790418f8ecdd9d390b7fa3ea
zuroc <zsp007@gmail.com>
2011-07-04 10:31:05 +0800

    fix

--- god/reply.py
+++ god/reply.py
@@ -8,31 +8,32 @@ from model.txt import txt_bind
 from model.po import Po
 PAGE_LIMIT = 50
 
-@urlmap('/reply_lsit/(\d+)(?:-(\d+))?')
+@urlmap('/reply_list/(\d+)(?:-(\d+))?')
 class ReplyList(Base):
-    def get(self,cid,n=1):
-        if cid:
-            qs = Reply.where('cid = %s',cid)
-            total = qs.count()
-            page, limit, offset = page_limit_offset(
-                    '/reply_lsit/%s'%cid+'-%s',
-                    total,
-                    n,
-                    PAGE_LIMIT,
-                    )
-            li = qs.order_by('id desc')[offset: offset + limit]
-            txt_bind(li)
-            Po.mc_bind(li,'po','rid')
-            self.render(
-                    reply_list=li,
-                    page=page,
-                    )
+    def get(self, cid, n=1):
+        qs = Reply.where('cid = %s', cid)
+        total = qs.count()
+        page, limit, offset = page_limit_offset(
+            '/reply_list/%s-%%s'%cid,
+            total,
+            n,
+            PAGE_LIMIT,
+        )
+        li = qs.order_by('id desc')[offset: offset + limit]
+        txt_bind(li)
+        Po.mc_bind(li, 'po', 'rid')
+        self.render(
+            reply_list=li,
+            page=page,
+        )
+
+
 
 @urlmap('/reply/rm/(\d+)')
 class ReplyRm(Base):
     def get(self, id):
         r = Reply.mc_get(id)
-        if r: 
+        if r:
             r.rm()
             self.finish('Y')
         else:
