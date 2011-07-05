f48d1d56592bb78e738471b7a4ee1c150971db23
zuroc <zsp007@gmail.com>
2011-07-06 04:08:47 +0800

    fix

--- misc/backup/backup_table.sh
+++ misc/backup/backup_table.sh
@@ -4,4 +4,5 @@ cd $PREFIX
 python backup_table.py
 sleep 2
 cat table_main| sed 's/ AUTO_INCREMENT=[0-9]*\b//' >  table_main.sql
+cat table_google| sed 's/ AUTO_INCREMENT=[0-9]*\b//' >  table_google.sql
 rm table_main
