c9de5ffd492d39f43381e7129fbb2a17975ab97a
silegon <silegon@gmail.com>
2011-07-08 19:23:47 +0800

    fix

---
+++ ctrl/main/money.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _handler import Base
+from ctrl._urlmap.main import urlmap
+from config import SITE_URL
+from model.money_alipay import alipay_url_recall
+
+@urlmap('/money/alipay_sync')
+class AlipaySync(Base):
+    def get(self):
+        query = self.request.query
+        t = alipay_url_recall(query)
+        url = '%s/money' % SITE_URL
+        if t:
+            if t.id:
+                url = "/donate/result/%s"
+            else:
+                url = '%s/charged/%s/%s'%(url, t.id, t.to_id)
+        return self.redirect(url)
