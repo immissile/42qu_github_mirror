13850726624b0926406ea3aa66d6398466579f97
zuroc <zsp007@gmail.com>
2011-07-08 15:28:07 +0800

    fix

--- config/host/krios.py
+++ config/host/krios.py
@@ -3,3 +3,9 @@ def prepare(o):
     o.MYSQL_MAIN = 'zpage'
     o.PIC_DOMAIN = 'p.%s'%o.SITE_DOMAIN
     o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
+    try:
+        import _private
+        _private.prepare(o)
+    except ImportError:
+        pass
+        
