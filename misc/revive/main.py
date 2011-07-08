01926f9efd5077de1be3f47e8233a796afb5f022
underdarkeye <underdarkeye@gmail.com>
2011-07-09 02:29:13 +0800

    f

---
+++ misc/revive/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import init_env
+
+from qu.myconf.config import KV_PATH
+
+from zpage.model.fs import fs_file
+
+from os import walk, makedirs
+from os.path import join, getsize, splitext, exists, dirname
+from shutil import copy
+
+for prefix in ('0', '721', '470', '219'):
+    for root, dirs, files in walk(join(KV_PATH, 'pic_show', prefix)):
+        for name in files:
+            opath = join(root, name)
+            id = splitext(name)[0]
+            if id.isdigit():
+                id = int(id)
+                path = fs_file(prefix, id, 'jpg')
+                if not exists(path):
+                    dirpath = dirname(path)
+                    if not exists(dirpath):
+                        makedirs(dirpath)
+                    copy(opath, path)
