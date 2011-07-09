1d0213c10a82145c21c221ea1bb20f76cb562f38
zuroc <zsp007@gmail.com>
2011-07-09 22:29:19 +0800

    f

--- config/user/work.py
+++ config/user/work.py
@@ -1,3 +1,7 @@
 def prepare(o):
+    o.SITE_DOMAIN = 'work.xxx'
     o.PORT = 30000
+    o.MYSQL_MAIN = 'zpage'
+    o.PIC_DOMAIN = 'p.%s'%o.SITE_DOMAIN
+    o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
     return o
