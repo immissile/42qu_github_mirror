78ff2676b9858b9e8077e0cc12647f989253ec6b
zuroc <zsp007@gmail.com>
2011-07-12 01:40:41 +0800

    fix

---
+++ misc/revive/rank.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import _env
+#from qu.mysite.model.follow import Follow, CID_MAN
+from model.follow import _follow_new, Follow
+from model.zsite import Zsite
+from model.zsite_rank import zsite_rank_by_zsite_id, zsite_rank_update_by_zsite_id
+
+def main():
+    for i in Zsite.where().col_list():
+        rank = zsite_rank_by_zsite_id(i)
+        zsite_rank_update_by_zsite_id(i, rank)
+
+if __name__ == '__main__':
+    main()
+
