3e46ff9cffeeb2195c7156cb5e890e990e987c3d
zuroc <zsp007@gmail.com>
2011-07-12 01:35:52 +0800

    fix

--- misc/revive/wall.py
+++ misc/revive/wall.py
@@ -2,17 +2,26 @@
 # -*- coding: utf-8 -*-
 import _env
 from qu.mysite.model.man_wall import ManWall, ManWallReply
-from model.wall import po_new, CID_QUESTION,txt_new
+from qu.mysite.util.orm import ormiter
+from model.zsite import Zsite
+from model.wall import Wall, wall_id_by_from_id_to_id, mc_wall_id_by_from_id_to_id
 from model.state import STATE_DEL, STATE_SECRET, STATE_ACTIVE
 from model.zsite_tag import zsite_tag_new_by_tag_name
 from model.po_question import po_answer_new
-from qu.mysite.model import review 
 from mysite.model.vote_review import VoteReview
 from model.vote import vote_up
 
 USER_ID = 10000000
 
 def init_wall():
+    for o in ormiter(ManWall, 'state>3'):
+        from_id = o.from_id
+        to_id = o.to_id
+        from_ = Zsite.mc_get(from_id)
+        to = Zsite.mc_get(to_id)
+        if from_ and to:
+            w = Wall.get(from_id=from_id, to_id=to_id)
+            to.reply_new(from_, 
     question_id_dict = {}
     for question in reversed(Question.mc_get_list(question_id_list())):
         m = po_new(CID_QUESTION, USER_ID, question.title, STATE_ACTIVE)
