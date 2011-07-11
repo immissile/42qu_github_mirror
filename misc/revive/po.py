6390f5a79afef19370cbbffce63f6cfa625b7104
zuroc <zsp007@gmail.com>
2011-07-12 03:43:17 +0800

    fix

--- misc/revive/po.py
+++ misc/revive/po.py
@@ -5,3 +5,9 @@ from qu.mysite.model.note import Note
 from model.po import po_new, po_note_new, po_word_new
 from model.state import STATE_DEL, STATE_SECRET, STATE_ACTIVE
 from model.zsite_tag import zsite_tag_new_by_tag_name
+from model.zsite import Zsite
+
+def init_po():
+    for i in Note.where():
+        user_id = i.man_id
+        subject = 
