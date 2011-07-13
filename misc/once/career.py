c524778166fd8a856cf6147ff9d971662145fcf6
zuroc <zsp007@gmail.com>
2011-07-13 17:10:03 +0800

    fix

--- misc/once/career.py
+++ misc/once/career.py
@@ -9,8 +9,9 @@ id_list = ZsiteList.where( owner_id=0, state=1).col_list(col='zsite_id')
 for n,i in enumerate(id_list):
     z = Zsite.mc_get(i)
     if z:
-        if not z.career:
+        title, unit = z.career
+        if not title and not unit:
             print n,' ',z.name
             print "http://god.42qu.com/zsite/%s"%i
             print "http://god.42qu.com/zsite/avatar/%s?next=%s/i/career"%(i,z.link)
-
+            print ""
