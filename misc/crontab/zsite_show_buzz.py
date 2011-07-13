a5b45e65ee49ad5bcb3f687352ca0b5af4591f0f
zuroc <zsp007@gmail.com>
2011-07-13 12:52:03 +0800

    f

--- misc/crontab/zsite_show_buzz.py
+++ misc/crontab/zsite_show_buzz.py
@@ -13,7 +13,7 @@ def buzz_show():
     pos = c.fetchone()[0]
     #print pos
     if pos > prev_pos:
-        c = ZsiteList.raw_sql('select zsite_id from zsite_list where owner_id=0 and cid=0 and state=1 and %s<id and id<=%s order by id', prev_pos, pos)
+        c = ZsiteList.raw_sql('select zsite_id from zsite_list where owner_id=0 and cid=0 and state=1 and id>%s and id<=%s order by id', prev_pos, pos)
         for zsite_id, in c.fetchall():
             buzz_show_new_all(zsite_id)
         kv_int.set(KV_SHOW_BUZZ_POS, pos)
