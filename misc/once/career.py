c29de77ddbf0446ccebe007868bd22e04d111b2a
zuroc <zsp007@gmail.com>
2011-07-13 17:53:57 +0800

    fix

--- misc/once/career.py
+++ misc/once/career.py
@@ -4,14 +4,17 @@ import _env
 from model.zsite_list import ZsiteList
 from model.zsite import Zsite
 id_list = ZsiteList.where( owner_id=0, state=1).col_list(col='zsite_id')
+n = 0
 
-
-for n,i in enumerate(id_list):
+for i in id_list:
     z = Zsite.mc_get(i)
     if z:
         title, unit = z.career
         if not title and not unit:
+            n += 1
             print n,' ',z.name
             print "http://god.42qu.com/zsite/%s"%i
             print "http://god.42qu.com/zsite/avatar/%s?next=%s/i/career"%(i,z.link)
             print ""
+        else:
+            print "----",title, unit
