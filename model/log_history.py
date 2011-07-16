8ea5bc1a4788095a07dda94606848f02ffa02dce
yupbank <yupbank@gmail.com>
2011-07-16 10:48:43 +0800

    yup

--- model/log_history.py
+++ model/log_history.py
@@ -7,6 +7,7 @@ import time
 
 LOG_HISTORY_NEW_USER_CID = 1
 LOG_HISTORY_NEW_PO_CID = 2
+LOG_HISTORY_NEW_PO_USER_CID = 3
 
 class LogHistory(Model):
     pass
@@ -21,20 +22,27 @@ def today_int():
 def log_new_user():
     num_today = Zsite.raw_sql('select count(1) from zsite').fetchone()[0]
     today = today_int()
-    num_pre = LogHistory.where(day = today-1 ).col_list(col =' num')
+    num_pre = LogHistory.where(day = today-1,cid = LOG_HISTORY_NEW_USER_CID ).col_list(col =' num')
     if num_pre:
         LogHistory.raw_sql('insert into log_history (day, num, incr,cid) values(%s,%s,%s,%s)',today,num_today,num_today-num_pre,LOG_HISTORY_NEW_USER_CID)
     else:
         LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,0,LOG_HISTORY_NEW_USER_CID)
 
-def log_po():
-    po_num = Po.raw_sql('select count(1) from po').fetchone()[0]
-    po_peo = Po.raw_sql('select count(distinct(user_id)) from po').fetchone()[0]
-    return po_num,po_peo
+def log_new_po():
+    num_today = Po.raw_sql('select count(1) from po').fetchone()[0]
+    print num_today
+    today = today_int()
+    num_pre = LogHistory.where(day = today-1,cid = LOG_HISTORY_NEW_PO_CID).col_list(col='num')
+    print num_pre
+    if num_pre:
+        LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,num_today-num_pre,LOG_HISTORY_NEW_PO_CID)
+    else:
+        LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,0,LOG_HISTORY_NEW_PO_CID)
 
-def reply():
+def log_new_po_user():
+    po_peo = Po.raw_sql('select count(distinct(user_id)) from po').fetchone()[0]
     pass
 if __name__ == '__main__':
-    print log_po()
+    print log_new_po()
 
 
