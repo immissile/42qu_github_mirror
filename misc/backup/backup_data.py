3def53c90a396316efd8cc740b59d2214f230681
underdarkeye <underdarkeye@gmail.com>
2011-07-16 18:31:28 +0800

    f

--- misc/backup/backup_data.py
+++ misc/backup/backup_data.py
@@ -10,29 +10,31 @@ sys.path = [dirname(dirname(PREFIX))] + sys.path
 from config import DB_CONFIG
 
 COMM_OPTION = ' -h%s -P%s -u%s -p%s %s '
-def backup_table(key, host, port, name, user, password):
+def backup_data(host, port, name, user, password):
     comm_option = COMM_OPTION % (host, port, user, password, name)
 
     """
     备份一个表数据的命令实例
     mysqldump --skip-opt --no-create-info 数据库名字 表名 --where="id<2000"
     """
-    create_table_option = '--skip-comments --no-data --default-character-set=utf8 --skip-opt --add-drop-table --create-options --quick --hex-blob ' + comm_option
+    create_table_option = ' --no-create-info --quick --default-character-set=utf8 --skip-opt --hex-blob '+comm_option
 
     cmd = 'mysqldump ' + create_table_option
-    #print cmd
-
-    with open(join(PREFIX, 'table_%s'%key), 'w') as backfile:
-        subprocess.Popen(
-            cmd.split(),
-            stdout=backfile
-        )
+#    print cmd
+    subprocess.Popen(
+        cmd.split()
+        #stdout=sys.stdout
+    )
 
 
-for key, value in DB_CONFIG.iteritems():
-    host, port, name, user, password = value.get('master').split(':')
-    backup_table(key, host, port, name, user, password)
+def backup_main():
+    table = DB_CONFIG['main']['master']
+    host, port, name, user, password = table.split(':')
+    backup_data(host, port, name, user, password)
 
+
+if __name__ == '__main__':
+    backup_main()
 
 """
 create_table_option = comm_option +  "--skip-opt --no-create-info hao123"
