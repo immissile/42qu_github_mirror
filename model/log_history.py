2d7a567b2cce19e868e894b0ce2531b121144919
zwtaoo <zwtaoo@gmail.com>
2011-07-16 11:16:37 +0800

    tao

--- model/log_history.py
+++ model/log_history.py
@@ -30,19 +30,26 @@ def log_new_user():
 
 def log_new_po():
     num_today = Po.raw_sql('select count(1) from po').fetchone()[0]
-    print num_today
     today = today_int()
     num_pre = LogHistory.where(day = today-1,cid = LOG_HISTORY_NEW_PO_CID).col_list(col='num')
-    print num_pre
     if num_pre:
         LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,num_today-num_pre,LOG_HISTORY_NEW_PO_CID)
     else:
         LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,0,LOG_HISTORY_NEW_PO_CID)
 
 def log_new_po_user():
-    po_peo = Po.raw_sql('select count(distinct(user_id)) from po').fetchone()[0]
+    num_today = Po.raw_sql('select count(distinct(user_id)) from po').fetchone()[0]
+    today = today_int()
+    num_pre = LogHistory.where(day = today-1, cid = LOG_HISTORY_NEW_USER_CID).col_list(col='num')[:1] 
+    if num_pre:
+        LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,num_today-num_pre[0],LOG_HISTORY_NEW_PO_USER_CID)
+    else:
+        LogHistory.raw_sql('insert into log_history (day,num,incr,cid) values(%s,%s,%s,%s)',today,num_today,0,LOG_HISTORY_NEW_PO_USER_CID)
+
+def new_reply():
     pass
+    
 if __name__ == '__main__':
-    print log_new_po()
+    print log_new_po_user()
 
 
