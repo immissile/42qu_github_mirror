a6845615960ef99cf8adcf95f874766253837549
zuroc <zsp007@gmail.com>
2011-07-18 10:19:05 +0800

    f

--- misc/backup/backup_data.sh
+++ misc/backup/backup_data.sh
@@ -4,6 +4,8 @@ FILEDIR=/mnt/zpage_db/mysql
 FILE=$FILEDIR/mysql.7z
 python $PREFIX/backup_data.py | 7z a -si -bd $FILE > /dev/null
 cp $FILE $FILEDIR/`date +%Y_%m_%d_%H_%M_%S`.7z
+
+
 #scp $FILE work@pc4.stdyun.com:/home/work/backup/mysql_daily/
 #rsync -aSvH /fdist1/qu/qufs/* work@pc4.stdyun.com:/mnt/file/42qu/nginx/ > /dev/null
 #python $PREFIX/backup_data.py | 7z a -si -bd /mnt/sb/backup/mysql.`date +%Y_%m_%d_%H_%M_%S`.7z > /dev/null
