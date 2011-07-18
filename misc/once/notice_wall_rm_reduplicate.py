6a4199c0ccffe31daf84df7b1f5d5ad508534426
underdarkeye <underdarkeye@gmail.com>
2011-07-18 18:33:04 +0800

    f

--- misc/once/notice_wall_rm_reduplicate.py
+++ misc/once/notice_wall_rm_reduplicate.py
@@ -7,17 +7,13 @@ from model.cid import CID_NOTICE_WALL, CID_NOTICE_WALL_REPLY
 def rm_reduplicate():
     a = set()
     for i in Notice.where('cid in (%s, %s)' % (CID_NOTICE_WALL, CID_NOTICE_WALL_REPLY)).where(STATE_GTE_APPLY).order_by('id desc'):
-        from_id = i.from_id
-        to_id = i.to_id
-        cid = i.cid
-        rid = i.rid
-        t = (from_id, to_id, cid, rid)
+        t = (i.from_id, i.to_id, i.cid, i.rid)
         if t not in a:
             if notice_id_count(*t):
                 a.add(t)
 
     for from_id, to_id, cid, rid in a:
-        for seq, i in enumerate(Notice.where(from_id=from_id, to_id=to_id, cid=cid, rid=rid).order_by('id desc')):
+        for seq, i in enumerate(Notice.where(from_id=from_id, to_id=to_id, cid=cid, rid=rid).where(STATE_GTE_APPLY).order_by('id desc')):
             if seq:
                 i.rm(to_id)
 
