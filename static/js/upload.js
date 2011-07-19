dde74723eccbe7813e08b8a253cda29a817613f4
zuroc <zsp007@gmail.com>
2011-07-19 10:30:18 +0800

    fix

--- static/js/upload.js
+++ static/js/upload.js
@@ -48,7 +48,7 @@ jQuery.extend({
 
     ajaxFileUpload: function(s) {
         s = jQuery.extend({}, jQuery.ajaxSettings, s);
-        if(!s.allowType.test($('#'+s.fileElementId)[0].value)){
+        if(!s.allowType.test(($('#'+s.fileElementId)[0].value||'').toLowerCase())){
             s.begin(12);
             return false;
         }
