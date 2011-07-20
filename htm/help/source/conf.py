55957231357602d9a2685a1512e0eb672e6a3e2c
zuroc <zsp007@gmail.com>
2011-07-20 12:29:05 +0800

    k

--- htm/help/source/conf.py
+++ htm/help/source/conf.py
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', '42qucom', u'42qu.com Documentation',
+    ('index', '42qucom', u'42qu.com 帮助',
      [u'42qu.com'], 1)
 ]
