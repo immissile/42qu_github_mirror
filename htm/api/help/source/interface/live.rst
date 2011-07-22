1c1272094e8bfb0018c439a74195bb55bbc55aa2
underdarkeye <underdarkeye@gmail.com>
2011-07-22 19:20:56 +0800

    f

--- htm/api/help/source/interface/live.rst
+++ htm/api/help/source/interface/live.rst
@@ -1,10 +1,10 @@
-/user/live 获取用户产生的消息流
+/live 获取当前登录用户的消息流
 =======================================
 
 输入 ::
 
     {
-        user_id:
+        access_token:
         begin_id: //可选 (第一页不传)
     }
 
@@ -18,6 +18,14 @@
                     cid:
                     name:
                     txt: // 日记、问题、回答有
+                    user:
+                        {
+                            name:
+                            unit:
+                            title:
+                            id:
+                            ico_url: // 可无
+                        }
                     question_id: // 回答 与 微波回答有
                     question_name: // 同上
                     question_user: // 同上
@@ -33,10 +41,9 @@
         begin_id: // 0 表示没有更多了
     }
 
-
 演示链接 ::
 
-    http://api.yup.xxx/user/live?user_id=10000000
+    http://api.yup.xxx/live?access_token=xxxxxxxxxxxxxxx
 
 演示返回 ::
 
@@ -47,11 +54,27 @@
                     "cid":65,
                     "id":10046873,
                     "name":"Megan Fox",
+                    "user":
+                        {
+                            "name":"真谛~",
+                            "ico_url":"http://p.zjd.xxx/96/664/26264.jpg",
+                            "id":10001299,
+                            "unit":"42区",
+                            "title":"运营&死coder"
+                        }
                 },
                 {
                     "cid":65,
                     "id":10046864,
                     "name":"HELLO WORLD",
+                    "user":
+                        {
+                            "name":"真谛~",
+                            "ico_url":"http://p.zjd.xxx/96/664/26264.jpg",
+                            "id":10001299,
+                            "unit":"42区",
+                            "title":"运营&死coder"
+                        }
                 }
             ],
         "begin_id":10046864
--- model/feed_api_render.py
+++ model/feed_api_render.py
@@ -4,11 +4,13 @@ from cid import CID_WORD, CID_NOTE, CID_QUESTION, CID_ANSWER
 from po import Po
 from feed_render import zsite_id_list_by_follow
 from feed import MAXINT, PAGE_LIMIT
-from feed_po import feed_po_iter, feed_po_merge_iter, mc_feed_po_dict
+from feed_po import feed_po_iter, FeedPoMerge, mc_feed_po_dict, mc_feed_user_dict
 from zsite import Zsite
 from zkit.txt2htm import txt_withlink
-from zkit.mc_func import mc_func_get_list
+from zkit.mc_func import mc_func_get_list, mc_func_get_dict
+from txt import txt_get
 
+@mc_feed_user_dict('{user_id}')
 def user_dict(user_id):
     user = Zsite.mc_get(user_id)
     unit, title = user.career
@@ -27,40 +29,56 @@ def feed_po_dict_by_db(id):
     cid = o.cid
     rid = o.rid
 
-    d = dict(id=id, cid=cid, user=user_dict(o.user_id))
+    d = dict(id=id, cid=cid)
 
     if cid == CID_WORD and rid or cid == CID_ANSWER:
         d.update(question_dict(rid))
     else:
         d['name'] = o.name
 
-    txt = o.txt
-    if txt:
-        d['txt'] = txt
-
     return d
 
 def feed_po_dict_list(id_list):
     return mc_func_get_list(mc_feed_po_dict, feed_po_dict_by_db, id_list)
 
+def feed_user_dict_dict(id_list):
+    id_list = set(id_list)
+    return mc_func_get_dict(mc_feed_user_dict, user_dict, id_list)
+
 def render_feed_api_by_zsite_id(zsite_id, limit=PAGE_LIMIT, begin_id=None):
-    zsite_id_list = zsite_id_list_by_follow(zsite_id)
+    feed_merge = FeedPoMerge(zsite_id_list_by_follow(zsite_id))
     id_list = []
-    i = 0
-    for i in feed_po_merge_iter(zsite_id_list, limit, begin_id):
-        id_list.append(i)
-    return feed_po_dict_list(id_list), i
+    zsite_id_list = []
+    id = 0
+    for i in feed_merge.merge_iter(limit, begin_id):
+        id = i.id
+        zsite_id = i.zsite_id
+        id_list.append(id)
+        zsite_id_list.append(zsite_id)
+    user_dict_dict = feed_user_dict_dict(zsite_id_list)
+    li = feed_po_dict_list(id_list)
+    for i, zsite_id in zip(li, zsite_id_list):
+        i['user'] = user_dict_dict[zsite_id]
+        txt = txt_get(i['id'])
+        if txt:
+            i['txt'] = txt
+    return li, id
 
 def render_user_feed_api_by_zsite_id(zsite_id, limit=PAGE_LIMIT, begin_id=None):
     id_list = []
-    i = 0
+    id = 0
     count = 0
-    for i in feed_po_iter(zsite_id, begin_id):
-        id_list.append(i)
+    for id in feed_po_iter(zsite_id, begin_id):
+        id_list.append(id)
         count += 1
         if count >= limit:
             break
-    return feed_po_dict_list(id_list), i
+    li = feed_po_dict_list(id_list)
+    for i in li:
+        txt = txt_get(i['id'])
+        if txt:
+            i['txt'] = txt
+    return li, id
 
 if __name__ == '__main__':
     pass
