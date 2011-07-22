d69a968414ad329b16dfcc4e27d60687b89ac433
underdarkeye <underdarkeye@gmail.com>
2011-07-22 15:18:45 +0800

    f

--- api/feed.py
+++ api/feed.py
@@ -10,7 +10,7 @@ from yajl import dumps
 class UserFeed(_handler.OauthBase):
     def get(self, id=MAXINT):
         user_id = int(self.get_argument('user_id'))
-        li, begin_id = render_feed_by_zsite_id(user_id, PAGE_LIMIT, id)
+        li, begin_id = render_feed_api_by_zsite_id(user_id, PAGE_LIMIT, id)
         self.finish({
             'items': li,
             'begin_id': begin_id,
