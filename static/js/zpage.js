50bbbbe68c4e6e26972864e841bf139e2b596671
silegon <silegon@gmail.com>
2011-07-23 23:09:46 +0800

    oh

--- static/js/zpage.js
+++ static/js/zpage.js
@@ -119,3 +119,39 @@ cnenlen = function(str) {
 function uuid(){
     return (""+ Math.random()).slice(2)
 }
+
+jQuery.fn.topLink = function(settings) {
+  settings = jQuery.extend({
+    min: 1,
+    fadeSpeed: 200
+  }, settings);
+  return this.each(function() {
+    //listen for scroll
+    var el = $(this);
+    el.hide(); //in case the user forgot
+    $(window).scroll(function() {
+      if($(window).scrollTop() >= settings.min)
+      {
+        el.fadeIn(settings.fadeSpeed);
+      }
+      else
+      {
+        el.fadeOut(settings.fadeSpeed);
+      }
+    });
+  });
+};
+
+//usage w/ smoothscroll
+$(document).ready(function() {
+  //set the link
+  $('#top-link').topLink({
+    min: 400,
+    fadeSpeed: 500
+  });
+  //smoothscroll
+  $('#top-link').click(function(e) {
+    e.preventDefault();
+    $.scrollTo(0,300);
+  });
+});
