af663055105d3270310b6a5d26fb548e5ba15001
silegon <silegon@gmail.com>
2011-07-25 15:16:20 +0800

    m

--- ctrl/main/compatible.py
+++ ctrl/main/compatible.py
@@ -3,11 +3,14 @@ from _handler import Base, LoginBase, XsrfGetBase
 from ctrl._urlmap.main import urlmap
 from config import SITE_DOMAIN
 
-@urlmap('/-(\d+)')
+@urlmap('/-(\d+)/pay')
 class Index(Base):
-    def get(self, id):
-        return self.redirect('//%s.%s'%(id, SITE_DOMAIN), True)
+    def get(self, name):
+        url = '//%s.%s/pay'%(name, SITE_DOMAIN)
+        query = self.request.query
+        if query:
+            url = '%s?%s' % (url, query)
+        #import pdb;pdb.set_trace()
+        return self.redirect(url, True)
 
     post = get
-
-
