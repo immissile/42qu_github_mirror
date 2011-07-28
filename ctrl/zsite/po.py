8dc4d5a3f217790ff40ed4c33b13561bac041cb6
zuroc <zsp007@gmail.com>
2011-07-28 23:51:16 +0800

    fix

--- ctrl/zsite/po.py
+++ ctrl/zsite/po.py
@@ -70,6 +70,11 @@ class PoPage(ZsiteBase):
 
         po_list = po_view_list(zsite_id, cid, is_self, limit, offset)
 
+        if cid == CID_WORD:
+            rid_po_list = [i for i in po_list if i.rid]
+            Po.mc_bind(rid_po_list,"question","rid")
+            Zsite.mc_bind([i.question for i in rid_po_list],"user","user_id")
+
         if zsite_id == user_id:
             back_a = "/live"
         else:
