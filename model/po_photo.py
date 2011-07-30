7731f6d2db21e76d18a2d6723592c5040835180d
zuroc <zsp007@gmail.com>
2011-07-30 11:14:31 +0800

    fix

--- model/po_photo.py
+++ model/po_photo.py
@@ -9,9 +9,8 @@ from cid import CID_PHOTO
 from fs import fs_set_jpg, fs_url_jpg
 from model.po import po_new , txt_new , is_same_post , STATE_SECRET, STATE_ACTIVE, time_title
 from po_pic import PoPic
-from zsite_tag import ZsiteTagPo, zsite_tag_new_by_tag_id
+from zsite_tag import  zsite_tag_new_by_tag_id
 
-mc_po_photo_prev_next = McCacheA("PoPhotoPrev,%s")
 
 
 PIC_SIZE = 721
@@ -28,94 +27,8 @@ def po_photo_save(photo_id, photo):
     p1 = pic_fit_width_cut_height_if_large(photo, 677)
     fs_set_jpg('677', photo_id, p1)
 
-@mc_po_photo_prev_next("{zsite_id}_{po_id}_{tag_id}")
-def po_photo_prev_next(zsite_id, po_id, tag_id):
-    t = ZsiteTagPo.get(zsite_id=zsite_id, po_id=po_id, zsite_tag_id=tag_id)
-    if not t:
-        result = (0, 0)
-    else:
-        id = t.id 
-        result = [
-            _po_photo_goto(
-                'select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and cid=%s and id>%s order by id limit 1',
-                zsite_id,
-                tag_id,
-                id,
-            ) or 0
-            ,
-            _po_photo_goto(
-                'select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and cid=%s and id<%s order by id desc limit 1',
-                zsite_id,
-                tag_id,
-                id,
-            ) or 0
-        ]
-   
-    if result[0] != result[1]:
-        if not result[0]:
-            c = ZsiteTagPo.raw_sql(
-                'select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and cid=%s order by id limit 1',
-                zsite_id,
-                tag_id,
-                CID_PHOTO, 
-            )
-            result[0] = c.fetchone()[0] 
-        elif not result[1]:
-            c = ZsiteTagPo.raw_sql(
-                'select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and cid=%s order by id desc limit 1',
-                zsite_id,
-                tag_id,
-                CID_PHOTO, 
-            )
-            result[1] = c.fetchone()[0]
-    return result
-
-
-def _po_photo_goto(sql, zsite_id, zsite_tag_id, id):
-    c = ZsiteTagPo.raw_sql(
-        sql,
-        zsite_id,
-        zsite_tag_id, 
-        CID_PHOTO, 
-        id
-    )
-    r = c.fetchone()
-    if r:
-        r = r[0]
-    return r
-
-def mc_flush(zsite_id, zsite_tag_id, id, po_id):
-    _mc_flush(
-        "select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and id>%s and cid=%s order by id limit 1",
-        zsite_id,
-        zsite_tag_id,
-        id
-    )
-    _mc_flush( 
-        "select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and id<%s and cid=%s order by id desc limit 1",
-        zsite_id,
-        zsite_tag_id,
-        id
-    )
-    mc_po_photo_prev_next.delete("%s_%s_%s"%(zsite_id, zsite_tag_id, po_id))
-
-
-def _mc_flush(sql, zsite_id, zsite_tag_id, id):
-    c = ZsiteTagPo.raw_sql(
-        sql,
-        zsite_id,
-        zsite_tag_id,
-        id,
-        CID_PHOTO,
-    )
-    r = c.fetchone()
-    if r:
-        mc_po_photo_prev_next.delete("%s_%s_%s"%(zsite_id, r[0], zsite_tag_id))
-
-
 
 def po_photo_new(user_id, name, txt, img, state=STATE_ACTIVE):
-
     if not name and not txt:
         return
 
---
+++ model/po_prev_next.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _db import cursor_by_table, McModel, McLimitA, McCache, McNum
+from zsite_tag import ZsiteTagPo
+
+mc_po_prev_next = McCacheA("PoPrevNext:%s")
+
+
+@mc_po_prev_next("{state}_{cid}_{zsite_id}_{tag_id}_{po_id}")
+def po_prev_next(state, cid, zsite_id, tag_id,  po_id):
+    t = ZsiteTagPo.get(zsite_id=zsite_id, po_id=po_id, zsite_tag_id=tag_id)
+    if not t:
+        result = (0, 0)
+    else:
+        id = t.id 
+        result = [
+            _po_goto(
+                'select po_id from zsite_tag_po where state>=%s and cid=%s and zsite_id=%s and zsite_tag_id=%s and id>%s order by id limit 1',
+                state,
+                cid,
+                zsite_id,
+                tag_id,
+                id,
+            ) or 0
+            ,
+            _po_goto(
+                'select po_id from zsite_tag_po where state>=%s and cid=%s and zsite_id=%s and zsite_tag_id=%s and id<%s order by id desc limit 1',
+                state,
+                cid,
+                zsite_id,
+                tag_id,
+                id,
+            ) or 0
+        ]
+   
+    if result[0] != result[1]:
+        if not result[0]:
+            c = ZsiteTagPo.raw_sql(
+                'select po_id from zsite_tag_po where state>=%s and cid=%s and zsite_id=%s and zsite_tag_id=%s order by id limit 1',
+                state,
+                cid,
+                zsite_id,
+                tag_id,
+            )
+            result[0] = c.fetchone()[0] 
+        elif not result[1]:
+            c = ZsiteTagPo.raw_sql(
+                'select po_id from zsite_tag_po where state>=%s and cid=%s and zsite_id=%s and zsite_tag_id=%s order by id desc limit 1',
+                state,
+                cid,
+                zsite_id,
+                tag_id,
+            )
+            result[1] = c.fetchone()[0]
+    return result
+
+def mc_flush(zsite_id, zsite_tag_id, id, po_id):
+    _mc_flush(
+        "select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and id>%s and cid=%s order by id limit 1",
+        zsite_id,
+        zsite_tag_id,
+        id
+    )
+    _mc_flush( 
+        "select po_id from zsite_tag_po where zsite_id=%s and zsite_tag_id=%s and id<%s and cid=%s order by id desc limit 1",
+        zsite_id,
+        zsite_tag_id,
+        id
+    )
+    mc_po_prev_next.delete("%s_%s_%s_%s_%s"%(state, cid, zsite_id, zsite_tag_id, po_id))
+
+
+def _mc_flush(sql, zsite_id, zsite_tag_id, id):
+    c = ZsiteTagPo.raw_sql(
+        sql,
+        zsite_id,
+        zsite_tag_id,
+        id,
+        CID_PHOTO,
+    )
+    r = c.fetchone()
+    if r:
+        mc_po_prev_next.delete("%s_%s_%s"%(zsite_id, r[0], zsite_tag_id))
+
+
+                #'select po_id from zsite_tag_po where state>=%s and cid=%s and zsite_id=%s and zsite_tag_id=%s and id>%s order by id limit 1',
+                state,
+                cid,
+                zsite_id,
+                tag_id,
+                id,
+
+def _po_goto(
+    sql,
+    state,
+    cid,
+    zsite_id,
+    tag_id,
+    id,
+):
+    c = ZsiteTagPo.raw_sql(
+        sql,
+        state,
+        cid,
+        zsite_id,
+        tag_id,
+        id,
+    )
+    r = c.fetchone()
+    if r:
+        r = r[0]
+    return r
+
+if __name__ == '__main__':
+    pass
+
+
