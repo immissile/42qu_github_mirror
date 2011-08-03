e5a5de0b88f0d5d3bf89b0a3c87e2fb0191d10e4
silegon <silegon@gmail.com>
2011-08-03 18:25:37 +0800

    fix

--- model/zsite_tag.py
+++ model/zsite_tag.py
@@ -177,10 +177,10 @@ def zsite_tag_rm_by_po(po):
     id = po.id
     cid = po.cid
     for i in ZsiteTagPo.where(po_id=id):
-        mc_flush_zsite_tag_id(i.zsite_tag_id)
-        i.delete()
         from model.po_prev_next import mc_flush
         mc_flush(cid, i.zsite_id, i.zsite_tag_id, id)
+        mc_flush_zsite_tag_id(i.zsite_tag_id)
+        i.delete()
 
 
 @mc_tag_by_po_id('{zsite_id}_{po_id}')
