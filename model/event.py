b235a23b85e167c6bd5af40687bdc300a3cb4fa4
underdarkeye <underdarkeye@gmail.com>
2011-08-04 19:48:35 +0800

    f

---
+++ model/event.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _db import Model, McModel, McCache, McCacheA, McLimitA, McNum
+
+
+class Event(McModel):
+    pass
+
+
+if __name__ == '__main__':
+    pass
