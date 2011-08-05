ce9c8c8d148491e04fdd5a069365bad8f3c9b9e0
zuroc <zsp007@gmail.com>
2011-08-05 16:12:38 +0800

    fix

---
+++ ctrl/me/po_event.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from _handler import LoginBase
+from ctrl._urlmap.me import urlmap
+from model.po import Po
+from model.po_event import po_event_new
+
+@urlmap('/po/event')
+@urlmap('/po/event/(\d+)')
+class PoVideo(LoginBase):
+    def post(self, po_id=0):
+        pass
+
+    def get(self):
+        pass
+
+
+
+
---
+++ model/po_event.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _db import McCache, McNum
+from cid import CID_EVENT
+from po import Po, po_new, po_word_new, po_note_new, po_rm, po_cid_set
+from state import STATE_DEL, STATE_SECRET, STATE_ACTIVE
+from txt import txt_new, txt_get
+from zkit.time_format import time_title
+from zsite import Zsite
+
+def po_event_new(user_id, name, txt, state):
+    pass
+
+
+
+
+
+
+
+
+
