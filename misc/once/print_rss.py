a6002f1914d84ea0e5f737ca8f1686cb7ca795cc
zuroc <zsp007@gmail.com>
2011-08-08 19:38:45 +0800

    fix

--- misc/once/print_rss.py
+++ misc/once/print_rss.py
@@ -16,33 +16,38 @@ def get_uri():
         links.append(ZsiteLink.raw_sql('select link from zpage.zsite_link where zsite_id = %s and cid = 0',*id).fetchone())
     for link in links:
         if link:
-            rss = feeds(*link)
+            try:
+                rss = feeds(*link)
+            except:
+                continue
             if rss:
                 yield rss
 
 
 
 if __name__ == '__main__':
-    print """<?xml version="1.0" encoding="UTF-8"?>
-    <opml version="1.0">
-    <head>
-    <title>Google 阅读器中 peng 的订阅</title>
-    </head>
-    <body>
-    """
-    for i in get_uri():
-        if i[0]:
-            txt = get_rss_link_title_by_rss(i[0])[-1]
-            print """<outline text="%s"
-                              title="%s" 
-            type="rss" xmlUrl="%s"
-            htmlUrl="%s"/>
-            """%(txt,txt,i[0],i[0])
+    with open("x.xml","w") as output:
+        output.write( """<?xml version="1.0" encoding="UTF-8"?>
+<opml version="1.0">
+<head>
+<title>Google 阅读器中 peng 的订阅</title>
+</head>
+<body>
+        """)
+        for i in get_uri():
+            if i[0]:
+                txt = get_rss_link_title_by_rss(i[0])[-1]
+                output.write( """<outline text="%s"
+                                  title="%s" 
+                type="rss" xmlUrl="%s"
+                htmlUrl="%s"/>
+                """%(txt,txt,i[0],i[0]))
 
-    print """
-           </body>
-           </opml>
-    """
+
+        output.write( """
+    </body>
+    </opml>
+        """)
 
 
 
