df5ad39414a27551146650accc0549df49f5512e
yupbank <yupbank@gmail.com>
2011-08-12 17:35:43 +0800

    ok

--- api/po.py
+++ api/po.py
@@ -5,7 +5,13 @@ from _urlmap import urlmap
 from model.po import po_word_new, Po, po_rm
 from model.zsite import user_can_reply, Zsite
 from model import reply
-
+from model.po_video import po_video_new, VIDEO_CID_YOUKU, VIDEO_CID_TUDOU, VIDEO_CID_SINA
+from model.cid import CID_VIDEO, CID_PHOTO
+from model.zsite_tag import zsite_tag_new_by_tag_id
+from model.po_photo import po_photo_new
+from model.state import STATE_ACTIVE
+from zkit.pic import picopen
+from model.po_audio import po_audio_new
 
 @urlmap('/po/word')
 class PoWord(_handler.OauthAccessBase):
@@ -88,3 +94,138 @@ class PoReplyRm(_handler.OauthAccessBase):
                 if can_rm:
                     r.rm()
         self.finish({'status': can_rm})
+
+@urlmap('/po/video')
+class PoVideo(_handler.OauthAccessBase):
+    def get(self):
+        po_id = int(self.get_argument('id',0))
+        cid = CID_VIDEO
+        url = self.get_argument('video', None)
+        name = self.get_argument('name', None)
+        txt = self.get_argument('txt', None)
+
+        link = '/live'
+
+        if po_id:
+            po_id = int(po_id)
+            po = Po.mc_get(po_id)
+            if po and po.user_id == self.current_user_id:
+                po.name_ = name
+                po.txt_set(txt)
+                po.save()
+                link = '/po/tag/%s' % po_id
+        else:
+            if url:
+                video, video_site = self._video(url)
+                if video:
+                    user_id = self.current_user_id
+                    po = po_video_new(user_id, name, txt, video, video_site)
+                    if po:
+                        po_id = po.id
+                        link = '/po/tag/%s' % po_id
+
+
+        return self.finish({'link':link})
+
+    def _video(self, url):
+        if url.startswith('http://v.youku.com/v_show/id_'):
+            video = url[29:url.rfind('.')]
+            video_site = VIDEO_CID_YOUKU
+        elif url.startswith('http://player.youku.com/player.php/sid/'):
+            video = url[39:url.find("/",39)]
+            video_site = VIDEO_CID_YOUKU
+        elif url.startswith('http://www.tudou.com/programs/view/'):
+            video = url[35:].rstrip("/")
+            video_site = VIDEO_CID_TUDOU
+        elif url.startswith('http://video.sina.com.cn/v/b/'):
+            video = url[29:url.rfind('.')]
+            video_site = VIDEO_CID_SINA
+        else:
+            video = None
+            video_site = None
+        return video, video_site
+
+
+@urlmap('/po/photo')
+class PoPhoto(_handler.OauthAccessBase):
+    def post(self):
+        cid = CID_PHOTO
+        po_id = int(self.get_argument('id',0))
+
+        name = self.get_argument('name', None)
+        txt = self.get_argument('txt', None)
+
+        
+        link = '/live'
+
+        if po_id:
+            po_id = int(po_id)
+            po = Po.mc_get(po_id) 
+            if po and po.user_id == self.current_user_id:
+                po.name_ = name
+                po.txt_set(txt)
+                po.save()
+                link = '/po/tag/%s' % po_id
+        else:
+            img = self._img()
+            if img:
+                user_id = self.current_user_id
+                po = po_photo_new(user_id, name, txt, img, STATE_ACTIVE)
+                if po:
+                    po_id = po.id
+
+                    link = '/po/tag/%s' % po_id
+
+
+        return self.finish({'link':link})
+        
+    def _img(self):
+        files = self.request.files
+        img = files.get('photo')
+        if img:
+            img = img[0]['body']
+            if not len(img) > 1024*1024*12:
+                img = picopen(img)
+                if not img:
+                    return
+                return img
+
+
+@urlmap('/po/audio')
+class PoAudio(_handler.OauthAccessBase):
+    def post(self):
+        cid = CID_AUDIO
+        po_id = int(self.get_argument('id',0))
+        name = self.get_argument('name', None)
+        txt = self.get_argument('txt', None)
+        
+        link = '/live'
+
+        if po_id:
+            po_id = int(po_id)
+            po = Po.mc_get(po_id) 
+            if po and po.user_id == self.current_user_id:
+                po.name_ = name
+                po.txt_set(txt)
+                po.save()
+                link = '/po/tag/%s' % po_id
+        else:
+            audio = self._audio()
+            if audio:
+                user_id = self.current_user_id
+                po = po_audio_new(user_id, name, txt, audio)
+                if po:
+                    po_id = po.id
+                    link = '/po/tag/%s' % po_id
+
+        return self.finish({'link':link})
+        
+    def _audio(self):
+        files = self.request.files
+        audio = files.get('audio')
+        if audio:
+            audio = audio[0]['body']
+            if not audio:
+                return
+            if not (len(audio) > 1024*1024*512):
+                return audio
