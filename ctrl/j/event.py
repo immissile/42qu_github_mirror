987a61bce101c26f2c1ea35cfd4c63bef600e6cf
zuroc <zsp007@gmail.com>
2011-08-13 02:00:26 +0800

    fix

--- ctrl/j/event.py
+++ ctrl/j/event.py
@@ -4,7 +4,7 @@ from ctrl._urlmap.j import urlmap
 from _handler import JLoginBase
 from model.event import EventJoiner, event_joiner_yes, event_joiner_no, event_joiner_state
 from model.state import STATE_DEL, STATE_APPLY, STATE_SECRET, STATE_ACTIVE
-from model.buzz import buzz_event_join_new
+from model.buzz import mq_buzz_event_join_new
 
 
 @urlmap('/j/event/check/(\d+)/(0|1)')
@@ -19,7 +19,7 @@ class EventCheck(JLoginBase):
                 if event.zsite_id == current_user_id and o.state == STATE_APPLY:
                     if state:
                         event_joiner_yes(o)
-                        buzz_event_join_new(o.user_id, o.event_id)
+                        mq_buzz_event_join_new(o.user_id, o.event_id)
                     else:
                         event_joiner_no(o)
         self.finish('{}')
