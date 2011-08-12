411a7602d95c596f2a6e362a7ca123f058c46d9d
underdarkeye <underdarkeye@gmail.com>
2011-08-12 15:06:18 +0800

    f

--- static/js/ctrl/zsite.js
+++ static/js/ctrl/zsite.js
@@ -1,13 +1,24 @@
-$('#zsite_link a').click(function(){
-    if(!$.cookie.get('S')){
-        login()
-        return false
-    }else if($("#i_verify").length){
-        $.fancybox({                                                                
-            content:'<div class="tc f16 pd16" style="width:250px"><p>想浏览此链接吗?<p><p><a href="/i/verify">先点此申请认证吧 , 亲</a></p></div>'
-        }) 
-        return false
-    }else{
-        this.target="_blank"
-    }
+$('#zsite_link a').click(function() {
+	if (!$.cookie.get('S')) {
+		login()
+		return false
+	} else if ($("#i_verify").length) {
+		$.fancybox({
+			content: '<div class="tc f16 pd16" style="width:250px"><p>想浏览此链接吗?<p><p><a href="/i/verify">先点此申请认证吧 , 亲</a></p></div>'
+		})
+		return false
+	} else {
+		this.target = "_blank"
+	}
+})
+$('#event_join_btn').submit(function() {
+	if (!$.cookie.get('S')) {
+		login()
+		return false
+	} else if ($("#i_verify").length) {
+		$.fancybox({
+			content: '<div class="tc f16 pd16" style="width:250px"><p>想报名吗?<p><p><a href="/i/verify">先点此申请认证吧 , 亲</a></p></div>'
+		})
+		return false
+	}
 })
