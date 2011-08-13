640271d912febd0ee4eee142185f877b0c983611
yupbank <yupbank@gmail.com>
2011-08-13 19:04:31 +0800

    ok

--- ctrl/meet/index.py
+++ ctrl/meet/index.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from _handler import Base, LoginBase, XsrfGetBase
 from ctrl._urlmap.meet import urlmap
-
+from model.namecard import namecard_get
+from zkit.earth import pid_city
 @urlmap('/')
 class Index(Base):
     def get(self):
         user_id = self.current_user_id
         if not user_id:
-            self.redirect('/guest')
+            return self.redirect('/guest')
+        namecard = namecard_get(user_id)
+        location = pid_city(namecard.pid_now)
+        if not location:
+            return self.redirect('/guest')
+
         return self.render()
