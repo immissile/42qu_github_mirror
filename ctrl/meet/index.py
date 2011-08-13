04930fe105216190f9104acd1202d8794fe053fc
yupbank <yupbank@gmail.com>
2011-08-13 18:52:31 +0800

    ok

--- ctrl/meet/index.py
+++ ctrl/meet/index.py
@@ -5,4 +5,7 @@ from ctrl._urlmap.meet import urlmap
 @urlmap('/')
 class Index(Base):
     def get(self):
+        user_id = self.current_user_id
+        if not user_id:
+            self.redirect('/guest')
         return self.render()
