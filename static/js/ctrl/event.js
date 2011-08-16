a03e906ec085dc52861c1bb80c8be34f1140d35f
zuroc <zsp007@gmail.com>
2011-08-16 22:24:28 +0800

    f

--- static/js/ctrl/event.js
+++ static/js/ctrl/event.js
@@ -1,7 +1,51 @@
-(function() {
+function fancybox_txt(tip, action, complete, post, submit){
+    var fancybox = $.fancybox;
+    fancybox({
+        'content': '<form method="POST" id="po_pop_form" class="po_pop_form"><div class="po_pop_tip">　</div><div id="po_pop_main"><textarea id="po_pop_txt" name="txt" class="po_pop_txt"></textarea></div><div class="btns"><span id="po_pop_error"></span><span class="btnw"><button type="submit">确认</button></span></div></form>',
+        "onComplete": function() {
+            $('.po_pop_tip').text(tip)
+            var form = $("#po_pop_form"),
+                pop_txt = $('#po_pop_txt').focus(),
+                error = $("#po_pop_error");
+            complete && complete()
+            form.submit(function() {
+                var txt = $.trim(pop_txt.val());
+                 
+                error.hide()
+                if (txt.length) {
+                    submit && submit()
+                    fancybox.showActivity()
+                    $.postJSON(action, {
+                        'txt': txt
+                    },
+                    function(data) {
+                        fancybox.close()
+                        post && post()
+                    })
+                } else {
+                    error.html('请输入文字').fadeIn()
+                    pop_txt.focus()
+                }
+
+                return false
+            })
+        },
+    })
+}
+
+
+function event_kill(){
+    fancybox_txt('解散原因如下 :', "/j/event/kill")
+}
+function event_notice(){
+    fancybox_txt('公告如下 :',"/j/event/notice")
+}
+
+
+$(function() {
 	var _txt = $('#_txt').val()
 
-	$('a.join_yes').live('click', function() {
+	$('.join_yes').click(function() {
 		var box = $(this.parentNode.parentNode)
 		if (!box.hasClass('join_y')) {
 			box.addClass('join_y')
@@ -10,41 +54,20 @@
 		}
 	})
 
-	$('a.join_no').live('click', function() {
+	$('.join_no').click(function() {
 		var id = this.rel
-		var fancybox = $.fancybox
-		fancybox({
-			'content': '<form method="POST" id="po_pop_form" class="po_pop_form"><div class="po_pop_tip">拒绝理由如下 :</div><div id="po_pop_main"><textarea id="po_pop_txt" name="txt" class="po_pop_txt"></textarea></div><div class="btns"><span id="po_pop_error"></span><span class="btnw"><button type="submit">提交</button></span></div></form>',
-			"onComplete": function() {
-				var form = $("#po_pop_form"),
-                    pop_txt = $('#po_pop_txt').focus().val(_txt),
-                    error = $("#po_pop_error");
-
-				form.submit(function() {
-					var txt = $.trim(pop_txt.val());
-                    
-                    error.hide()
-
-					if (txt.length) {
-						fancybox.showActivity()
-						_txt = txt
-						$.postJSON('/j/event/check/' + id + '/0', {
-							'txt': txt
-						},
-						function(data) {
-							fancybox.close()
-							$('#ndbk' + id).slideUp(500, function() {
-								$(this).remove()
-							})
-						})
-					} else {
-						error.html('请输入理由').fadeIn()
-                        pop_txt.focus()
-					}
-
-					return false
-				})
-			}
-		})
+        fancybox_txt('拒绝理由如下 :', '/j/event/check/' + id + '/0', 
+        function(){
+            $('#po_pop_txt').val(_txt)
+        },
+        function(){
+            $('#ndbk' + id).slideUp(500, function() {
+                $(this).remove()
+            })
+        }),
+        function(){
+            _txt = $('#po_pop_txt').val()
+        }
 	})
-})()
+})
+
