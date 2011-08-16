cbeddb6986b0f0e95e865623e02473e31d3d2006
zuroc <zsp007@gmail.com>
2011-08-16 19:46:23 +0800

    fx

--- static/js/ctrl/event.js
+++ static/js/ctrl/event.js
@@ -14,12 +14,18 @@
 		var id = this.rel
 		var fancybox = $.fancybox
 		fancybox({
-			'content': '<form method="POST" id="po_pop_form" class="po_pop_form"><div class="po_pop_tip">填写理由</div><div id="po_pop_main"><textarea id="po_pop_txt" name="txt" class="po_pop_txt">' + _txt + '</textarea></div><div class="btns"><span id="po_pop_error"></span><span class="btnw"><button type="submit">提交</button></span></div></form>',
+			'content': '<form method="POST" id="po_pop_form" class="po_pop_form"><div class="po_pop_tip">拒绝理由如下 :</div><div id="po_pop_main"><textarea id="po_pop_txt" name="txt" class="po_pop_txt"></textarea></div><div class="btns"><span id="po_pop_error"></span><span class="btnw"><button type="submit">提交</button></span></div></form>',
 			"onComplete": function() {
-				var form = $("#po_pop_form")
+				var form = $("#po_pop_form"),
+                    pop_txt = $('#po_pop_txt').focus().val(_txt),
+                    error = $("#po_pop_error");
+
 				form.submit(function() {
-					var txt = $.trim($('#po_pop_txt').val())
-					if (txt) {
+					var txt = $.trim(pop_txt.val());
+                    
+                    error.hide()
+
+					if (txt.length) {
 						fancybox.showActivity()
 						_txt = txt
 						$.postJSON('/j/event/check/' + id + '/0', {
@@ -32,8 +38,10 @@
 							})
 						})
 					} else {
-						$("#po_pop_error").html('请输入理由').fadeIn()
+						error.html('请输入理由').fadeIn()
+                        pop_txt.focus()
 					}
+
 					return false
 				})
 			}
