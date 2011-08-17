e02a35e6209c2112a9ace40ad7feb9669a654af2
zuroc <zsp007@gmail.com>
2011-08-17 16:44:16 +0800

    fx

--- ctrl/main/money.py
+++ ctrl/main/money.py
@@ -4,6 +4,7 @@ from _handler import Base
 from ctrl._urlmap.main import urlmap
 from config import SITE_URL
 from model.money_alipay import alipay_url_recall
+from model.money import pay_new, TRADE_STATE_NEW, TRADE_STATE_ONWAY, TRADE_STATE_FINISH, pay_account_get, bank, Trade, trade_log, pay_notice
 
 @urlmap('/money/alipay_sync')
 class AlipaySync(Base):
@@ -18,4 +19,15 @@ class AlipaySync(Base):
         return self.redirect(url)
 
 
+@urlmap('/pay/result/(\d+)')
+class Result(ZsiteBase):
+    def get(self, id):
+        t = Trade.get(id)
+        from_user = Zsite.mc_get(t.from_id)
+        to_user = Zsite.mc_get(t.to_id)
 
+        self.render(
+            from_user=from_user,
+            to_user=to_user,
+            trade=t,
+        )
