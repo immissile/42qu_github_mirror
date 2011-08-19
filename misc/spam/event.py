40727a1880abbb9b63a1a56e43f53076c43ea791
zuroc <zsp007@gmail.com>
2011-08-19 16:09:55 +0800

    fx

--- misc/spam/event.py
+++ misc/spam/event.py
@@ -1,12 +1,12 @@
+#coding:utf-8
 import _env
+from mail import rendermail
 
+mail = "zsp007@gmail.com"
+name = "张沈鹏"
 
-    rendermail(
-        '/mail/buzz/follow_new.htm', mail, name,
-        from_user=from_user,
-        format='html',
-        subject='%s ( %s ) 关注 你' % (
-            from_user.name,
-            ' , '.join(career),
-        )
-    )
+rendermail(
+    '/mail/spam/event_20110819.htm', mail, name,
+    format='html',
+    subject='道可道 . 线下活动: 42区 . 技术 . 创业 . 第一讲'
+)
