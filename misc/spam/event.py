905f4e971cf258109c00eb42c585bcf99c07c5f8
zuroc <zsp007@gmail.com>
2011-08-19 20:43:51 +0800

    fux

--- misc/spam/event.py
+++ misc/spam/event.py
@@ -3,14 +3,30 @@ import _env
 from model.mail import rendermail
 from model.mail_notice import mail_notice_iter, CID_MAIL_MONTH
 import time
+import traceback
+
 
 if __name__ == "__main__":
+    goon = False
     for mail, name in mail_notice_iter(CID_MAIL_MONTH):
-        #mail = "zsp007@gmail.com"
+        if mail == "yupbank@42qu.com":
+            goon = True
 
-        rendermail(
-            '/mail/spam/event_20110819.htm', mail, name,
-            format='html',
-            subject='42区 : 技术 . 创业 . 第一讲 - 线下活动 报名 ( 2011年08月21日 星期日 )'
-        )
+        if not goon:
+            continue
+       # print mail
+        
+
+        try:
+            rendermail(
+                '/mail/spam/event_20110819.htm', mail, name,
+                format='html',
+                subject='42区 : 技术 . 创业 . 第一讲 - 线下活动 报名 ( 2011年08月21日 星期日 )'
+            )
+        except:
+            traceback.print_exc()
+            continue
+        
+        print mail
+
         time.sleep(0.1)
