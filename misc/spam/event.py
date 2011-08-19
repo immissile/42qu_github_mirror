54b4f3126a272ca7d7f13c6bb1e6fe92565845fc
zuroc <zsp007@gmail.com>
2011-08-19 16:18:51 +0800

    fix

--- misc/spam/event.py
+++ misc/spam/event.py
@@ -1,12 +1,14 @@
 #coding:utf-8
 import _env
-from mail import rendermail
+from model.mail import rendermail
 
 mail = "zsp007@gmail.com"
 name = "张沈鹏"
+mail = "yupbank@gmail.com"
+name = "于鹏"
 
 rendermail(
     '/mail/spam/event_20110819.htm', mail, name,
     format='html',
-    subject='道可道 . 线下活动: 42区 . 技术 . 创业 . 第一讲'
+    subject='42区 线下活动 报名 : 技术 . 创业 . 第一讲'
 )
