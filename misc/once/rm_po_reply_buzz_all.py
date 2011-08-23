b1632e0d4f7e19e6c8592cb75287423fbc2cb63b
zuroc <zsp007@gmail.com>
2011-08-24 02:11:39 +0800

    fix

--- misc/once/rm_po_reply_buzz_all.py
+++ misc/once/rm_po_reply_buzz_all.py
@@ -3,3 +3,4 @@ from model.cid import CID_BUZZ_PO_REPLY
 from model.buzz import Buzz
 
 Buzz.raw_sql('delete from buzz where cid=%s', CID_BUZZ_PO_REPLY)
+Buzz.raw_sql('delete from buzz where cid=%s', 222)
