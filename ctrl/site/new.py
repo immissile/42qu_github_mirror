dad7944dc29cde3fc241e0b6d6da921f4b4eccbe
zuroc <zsp007@gmail.com>
2011-08-31 18:52:19 +0800

    fix

--- ctrl/site/new.py
+++ ctrl/site/new.py
@@ -5,7 +5,7 @@ from zkit.jsdict import JsDict
 from zkit.page import page_limit_offset
 from zkit.errtip import Errtip
 from model.zsite import zsite_new_site, ZSITE_STATE_VERIFY
-
+from zkit.txt import cnenlen
 
 @urlmap('/new')
 class SiteNew(LoginBase):
@@ -17,18 +17,21 @@ class SiteNew(LoginBase):
     def post(self):
         current_user_id = self.current_user_id
         current_user = self.current_user
-        if current_user.state < ZSITE_STATE_VERIFY:
-            return
 
         errtip = Errtip()
         name = self.get_argument('name', None)
         site = None
+        
         if not name:
-            errtip.name = '缺少名字'
-        elif len(name) > 9:
-            errtip.name = '最长9个字'
+            errtip.name = '请输入名称'
+        elif cnenlen(name) > 18:
+            errtip.name = '请不要超过九个汉字'
+        elif len(name.decode("utf-8","ignore")) > 15:
+            errtip.name = "请不要超过15个英文"
         else:
             site = zsite_new_site(name, current_user_id)
+            return self.redirect(site.link)
+
         self.render(
             errtip=errtip,
             site=site,
