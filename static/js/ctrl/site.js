1bd48275dcbc408631ef2605f420ab3ba88e331e
zuroc <zsp007@gmail.com>
2011-09-01 04:25:39 +0800

    fix

---
+++ static/js/ctrl/site.js
@@ -0,0 +1,26 @@
+$(function(){
+
+var po_word="po_word", tip='今天 , 你想说什么 ?', po_submit=$('<div class="po_word_submit"><a href="javascript:void(0)" class="po_word_close"></a><span class="po_word_tip"><span>32</span>字</span><a href="javascript:void(0)" class="po_word_submit_a">发布</a></div>');
+var word=$("#"+po_word).val(tip).blur().focus(function(){
+    if(!$.cookie.get('S')){
+        poplogin();
+        return
+    }
+    var j=$(this);
+    if(!j.hasClass(po_word)){
+        j.val('').addClass(po_word);
+        po_submit.show()
+    }
+}).blur(function(){
+    var j=$(this),v=j.val();
+    if(!v||v==''){
+        j.removeClass(po_word).val(tip)
+        po_submit.hide()
+    }
+}).after(po_submit)
+
+})
+/*
+<div style=" "><span style="color: rgb(153, 153, 153); padding: 0pt 0pt 0pt 14px; line-height: 38px;">32字</span>
+</div>
+*/
