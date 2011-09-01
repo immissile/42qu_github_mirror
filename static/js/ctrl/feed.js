eb7b99dd47ed9bbbbc641e52b5c8ae9bfde15ae6
zuroc <zsp007@gmail.com>
2011-09-01 17:33:45 +0800

    fix

--- static/js/ctrl/feed.js
+++ static/js/ctrl/feed.js
@@ -94,3 +94,41 @@
 		})
 	})
 })()
+
+function share(id){
+    var fancybox = $.fancybox;
+    fancybox({
+        content: '<form id="vote_reply" class="fancyreply"><h3>推荐语</h3><textarea name="txt"></textarea><div class="btns"><span class="btnw"><button class="btn" type="submit">分享</button></span></div></form>',
+        onComplete: function() {
+            var reply = $("#vote_reply"),
+            textarea = reply.find("textarea");
+
+            reply.submit(function() {
+                var txt = $.trim(textarea.val());
+                if (txt && txt.length) {
+                    fancybox.showActivity()
+                    $.postJSON(
+                        "/j/feed/up/" + id, 
+                        {
+                            'txt': txt
+                        },
+                        function(r){
+                            if (r.can_not_reply) {
+                                fancybox({
+                                    content: CANNOT_REPLY
+                                })
+                            } else {
+                                fancybox.close()
+                            }
+                        }
+                    )
+                } else {
+                    fancybox.close()
+                }
+                return false;
+            })
+            textarea.focus()
+        }
+    })
+
+} 
