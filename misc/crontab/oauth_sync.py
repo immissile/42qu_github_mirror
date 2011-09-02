d463888b448d3eaa5bf7f734808958d9a272f5d6
zuroc <zsp007@gmail.com>
2011-09-03 00:14:54 +0800

    fix

--- misc/crontab/oauth_sync.py
+++ misc/crontab/oauth_sync.py
@@ -5,14 +5,19 @@ KV_SYNC_JOIN_EVENT_BY_ZSITE_ID ,\
 KV_SYNC_RECOMMEND_BY_ZSITE_ID ,\
 KV_SYNC_FOLLOW_BY_SYNC_ID ,\
 kv_int_call
-
-#mq_sync_po_by_zsite_id = mq_client(sync_po_by_zsite_id)
-#mq_sync_join_event_by_zsite_id = mq_client(sync_join_event_by_zsite_id)
-#mq_sync_recommend_by_zsite_id = mq_client(sync_recommend_by_zsite_id)
-#mq_sync_follow_by_sync_id = mq_client(sync_follow_by_sync_id)
+from zweb.orm import ormiter
+from model.po import Po
+from model.state import STATE_ACTIVE
+from model.sync import sync_po, sync_join_event_by_zsite_id, \
+sync_recommend_by_zsite_id, sync_follow_by_sync_id
 
 def sync_po_by_zsite_id(begin_id):
-    pass
+    for po in ormiter(
+        Po, 'id>%s and state=%s' % (begin_id, STATE_ACTIVE)
+    ):
+        begin_id = po.id
+        #sync_po(po) 
+    return begin_id
 
 
 def sync_join_event_by_zsite_id(begin_id):
@@ -30,7 +35,10 @@ def sync_follow_by_sync_id(begin_id):
 
 @single_process
 def main():
-    kv_int_call(KV_SEO_PING, ping_po)
+    kv_int_call(KV_SYNC_PO_BY_ZSITE_ID, sync_po_by_zsite_id)
+    kv_int_call(KV_SYNC_JOIN_EVENT_BY_ZSITE_ID, sync_join_event_by_zsite_id)
+    kv_int_call(KV_SYNC_RECOMMEND_BY_ZSITE_ID, sync_recommend_by_zsite_id)
+    kv_int_call(KV_SYNC_FOLLOW_BY_SYNC_ID, sync_follow_by_sync_id)
 
 
 if __name__ == "__main__":
--- model/sync.py
+++ model/sync.py
@@ -12,17 +12,25 @@ from oauth_follow import oauth_follow_by_oauth_id
 mc_sync_state = McCache('SyncState:%s')
 mc_sync_state_all = McCacheA('SyncStateAll:%s')
 
+SYNC_CID_WORD = 1
+SYNC_CID_NOTE = 2
+SYNC_CID_EVENT = 3
+SYNC_CID_SHARE = 4
+
+
 SYNC_CID_CN = (
-    (1, '游吟碎语'),
-    (2, '撰写文章'),
-    (3, '线下活动'),
-    (4, '推荐分享'),
+    (SYNC_CID_WORD, '游吟碎语'),
+    (SYNC_CID_NOTE, '撰写文章'),
+    (SYNC_CID_EVENT, '线下活动'),
+    (SYNC_CID_SHARE, '推荐分享'),
 )
 
+
+
 SYNC_GET_CID = {
-    CID_WORD: 1,
-    CID_NOTE: 2,
-    CID_EVENT: 3,
+    CID_WORD: SYNC_CID_WORD,
+    CID_NOTE: SYNC_CID_NOTE,
+    CID_EVENT: SYNC_CID_EVENT,
 }
 
 SYNC_CID_TXT = (
@@ -63,27 +71,22 @@ def sync_state_set(user_id, cid, state, oauth_id):
         mc_sync_state_all.delete('%s_%s'%(user_id, oauth_id))
 
 
-def sync_po_by_zsite_id(id, po_id):
-    from po import Po
-    p = Po.mc_get(po_id)
+def state_oauth_id_by_zsite_id_cid(zsite_id, cid):
+    s = SyncTurn.where(
+        zsite_id=zsite_id, cid=cid
+    ).where('state>0').col_list('oauth_id')
+    return s
+
+def sync_po(po):
+    id = po.user_id
     cid = SYNC_GET_CID[p.cid]
-    s = SyncTurn.raw_sql('select state,oauth_id from sync_turn where zsite_id = %s and cid = %s', id, cid).fetchall()
-    if s:
-        for state, oauth_id in s:
-            if state:
-                sync_by_oauth_id(oauth_id, SYNC_CID_TXT[cid-1] + p.name_, 'http:%s'%p.link)
+    for oauth_id in state_oauth_id_by_zsite_id_cid(id, cid):
+        sync_by_oauth_id(oauth_id, SYNC_CID_TXT[cid-1] + p.name_, 'http:%s'%p.link)
 
-
-
-
-def sync_join_event_by_zsite_id(id, po_id, cid=3):
-    from po import Po
-    p = Po.mc_get(po_id)
-    s = SyncTurn.raw_sql('select state,oauth_id from sync_turn where zsite_id = %s and cid = %s', id, cid).fetchall()
-    if s:
-        for state, oauth_id in s:
-            if state:
-                sync_by_oauth_id(oauth_id, '报名活动 : '+ p.name_, 'http:%s'%p.link)
+def sync_join_event(id, po):
+    s = state_oauth_id_by_zsite_id_cid(id, SYNC_CID_EVENT)
+    for oauth_id in s:
+        sync_by_oauth_id(oauth_id, '报名活动 : '+ po.name_, 'http:%s'%po.link)
 
 def sync_recommend_by_zsite_id(id, po_id, cid=4):
     from po import Po
@@ -109,10 +112,10 @@ def sync_follow_by_sync_id(zsite_id, oauth_id):
         s.delete()
 
 
-mq_sync_po_by_zsite_id = mq_client(sync_po_by_zsite_id)
-mq_sync_join_event_by_zsite_id = mq_client(sync_join_event_by_zsite_id)
-mq_sync_recommend_by_zsite_id = mq_client(sync_recommend_by_zsite_id)
-mq_sync_follow_by_sync_id = mq_client(sync_follow_by_sync_id)
+#mq_sync_po_by_zsite_id = mq_client(sync_po_by_zsite_id)
+#mq_sync_join_event_by_zsite_id = mq_client(sync_join_event_by_zsite_id)
+#mq_sync_recommend_by_zsite_id = mq_client(sync_recommend_by_zsite_id)
+#mq_sync_follow_by_sync_id = mq_client(sync_follow_by_sync_id)
 
 
 if __name__ == '__main__':
