3091b4caa4c0b506c94261ba5658411a0c96aa22
zuroc <zsp007@gmail.com>
2011-09-09 15:39:04 +0800

    fix

--- misc/crontab/rss_po.py
+++ misc/crontab/rss_po.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 import _env
 from htm2po.htm2po import htm2po_by_po
-from model.rss import PrePo, RSS_PRE_PO,RSS_POED
+from model.rss import PrePo, RSS_PRE_PO, RSS_POED
 
 def rss_po():
-    for pre in PrePo.get(state = RSS_PRE_PO)
+    for pre in PrePo.where(state=RSS_PRE_PO):
         htm2po_by_po(pre)
         pre.state = RSS_POED
         pre.save()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     rss_po()
