473d08593632dd0b044d599c370aceea9808c16f
yupbank <yupbank@gmail.com>
2011-09-09 18:42:07 +0800

    maoxian

--- god/rss.py
+++ god/rss.py
@@ -13,7 +13,7 @@ class RssIndex(Base):
     def get(self,state=RSS_UNCHECK,n=1):
         total = PrePo.where(state=state).count()
         page, limit, offset = page_limit_offset(
-                 '/rss_index-%s',
+                 '/rss_index/%s-%%s'%state,
                  total,
                  n,
                  PAGE_LIMIT
@@ -23,14 +23,22 @@ class RssIndex(Base):
                 pre_po=pre_po,
                 page = page
                 )
-
+@urlmap('/rss')
 @urlmap('/rss/(\d+)/(\d+)')
 class RssCheck(Base):
     def get(self,state,id):
         po = PrePo.get(id=id)
         po.state = state
-        if state == RSS_PRE_PO:
-            txt = self.get_argument('%stxt'%id)
-            print txt
         po.save()
         self.redirect('/rss_index')
+
+    def post(self):
+        id = int(self.get_argument('id'))
+        txt = self.get_argument('%stxt'%id)
+        po = PrePo.get(id=id)
+        po.txt = txt
+        po.state = RSS_PRE_PO
+        po.save()
+        self.redirect('/rss_index')
+
+
