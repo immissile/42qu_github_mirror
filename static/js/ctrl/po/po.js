7a465381e892a6d268da57086db564a7db36d42d
realfex <i@realfex.com>
2011-09-14 13:22:39 +0800

    abcdef

--- static/js/ctrl/po/po.js
+++ static/js/ctrl/po/po.js
@@ -40,4 +40,32 @@ $(function(){
         
     }
 })
-
+INPUT_FOCUS = null
+$(document).keyup(function (e) {
+    if (!INPUT_FOCUS && !(e.ctrlKey || e.shiftKey || e.altKey) ) {
+        var id
+        switch (e.keyCode) {
+            case 37://←
+            id="sprev"
+            break
+            case 39://→
+            id="snext"
+            break
+        }
+        if (id) {
+            id = $('.'+id)
+            if (id) {
+                location.href = id.attr('href')
+            }
+        }
+    }
+})
+function input_ban () {
+    $('input, textarea').live('focus',function () {
+        INPUT_FOCUS = this
+    }).live('blur',function () {
+        INPUT_FOCUS = null
+    })
+}
+$(input_ban)
+$(document).ajaxComplete(input_ban)
