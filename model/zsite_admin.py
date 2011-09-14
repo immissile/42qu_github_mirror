b4cb2dcaf163c5e07d4fea839fb875de83e2173e
zuroc <zsp007@gmail.com>
2011-09-14 17:07:59 +0800

    fix

--- model/zsite_admin.py
+++ model/zsite_admin.py
@@ -7,7 +7,7 @@ from zsite import Zsite
 from model.cid import CID_SITE
 
 mc_zsite_id_list_by_admin_id = McCacheA('ZsiteIdListBYAdminId.%s')
-#mc_admin_id_list_by_zsite_id = McCacheA('AdminIdListByZsiteId.%s')
+mc_admin_id_list_by_zsite_id = McCacheA('AdminIdListByZsiteId.%s')
 
 ZSITE_ADMIN_STATE_DEL = 0
 ZSITE_ADMIN_STATE_OWNER = 100
@@ -24,14 +24,23 @@ def zsite_admin_new(zsite_id, admin_id, state=ZSITE_ADMIN_STATE_OWNER):
     zsite_admin.state = state
     zsite_admin.save()
 
-@mc_zsite_id_list_by_admin_id('{admin_id}')
-def zsite_id_list_by_admin_id(admin_id):
-    return ZsiteAdmin.where(
-        admin_id=admin_id
-    ).where('state>%s' % ZSITE_ADMIN_STATE_DEL).col_list(
-        col='zsite_id'
-    )
+    mc_flush(zsite_id, admin_id)
 
+def mc_flush(zsite_id, admin_id):
+    mc_admin_id_list_by_zsite_id.delete(zsite_id)
+    mc_zsite_id_list_by_admin_id.delete(admin_id)
+
+@mc_admin_id_list_by_zsite_id('{id}')
+def admin_id_list_by_zsite_id(id):
+    return ZsiteAdmin.where(
+        zsite_id=id
+    ).where('state>%s' % ZSITE_ADMIN_STATE_DEL).col_list(col='admin_id')
+
+@mc_zsite_id_list_by_admin_id('{id}')
+def zsite_id_list_by_admin_id(id):
+    return ZsiteAdmin.where(
+        admin_id=id
+    ).where('state>%s' % ZSITE_ADMIN_STATE_DEL).col_list(col='zsite_id')
 
 
 #def zsite_admin_new(zsite_id, admin_id, state=STATE_ACTIVE):
