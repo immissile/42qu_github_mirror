aa98bcc9f0d8b1b03cb0816bf30fa3fac52335c3
zuroc <zsp007@gmail.com>
2011-09-15 13:24:06 +0800

    fix

--- model/zsite_admin.py
+++ model/zsite_admin.py
@@ -43,42 +43,19 @@ def zsite_id_list_by_admin_id(id):
     ).where('state>%s' % ZSITE_ADMIN_STATE_DEL).col_list(col='zsite_id')
 
 
-#def zsite_admin_new(zsite_id, admin_id, state=STATE_ACTIVE):
-#    o = ZsiteAdmin.get_or_create(zsite_id=zsite_id, admin_id=admin_id)
-#    o.state = state
-#    o.save()
-#    mc_zsite_id_list_by_admin_id.delete(admin_id)
-#    mc_admin_id_list_by_zsite_id.delete(zsite_id)
-#    return o
-#
-#
-#def zsite_admin_rm(zsite_id, admin_id):
-#    o = ZsiteAdmin.get(zsite_id=zsite_id, admin_id=admin_id)
-#    if o.state >= STATE_ACTIVE:
-#        o.state = STATE_DEL
-#        o.save()
-#
-#
-#def zsite_rm_site_extra(zsite_id):
-#    for i in ZsiteAdmin.where(zsite_id=zsite_id).where('state>=%s', STATE_ACTIVE):
-#        i.state = STATE_DEL
-#        i.save()
-#        mc_zsite_id_list_by_admin_id.delete(i.admin_id)
-#    mc_admin_id_list_by_zsite_id.delete(zsite_id)
-#
-#
-#
-#
-#def zsite_list_by_admin_id(admin_id):
-#    id_list = zsite_id_list_by_admin_id(admin_id)
-#    return Zsite.mc_get_list(id_list)
-#
-#
-#@mc_admin_id_list_by_zsite_id('{zsite_id}')
-#def admin_id_list_by_zsite_id(zsite_id):
-#    return ZsiteAdmin.where(zsite_id=zsite_id).where('state>=%s' % STATE_ACTIVE).col_list(col='admin_id')
-#
-#
-#def admin_list_by_zsite_id(zsite_id):
-#    id_list = admin_id_list_by_zsite_id(zsite_id)
-#    return Zsite.mc_get_list(id_list)
+def zsite_admin_rm(zsite_id, admin_id):
+    o = ZsiteAdmin.get(zsite_id=zsite_id, admin_id=admin_id)
+    if o:
+        o.state = ZSITE_ADMIN_STATE_DEL
+        o.save()
+        mc_flush(zsite_id, admin_id)
+
+
+def zsite_admin_empty(zsite_id):
+    for i in ZsiteAdmin.where(zsite_id=zsite_id).where("state>%s"%STATE_DEL):
+        i.state = STATE_DEL
+        i.save()
+        mc_zsite_id_list_by_admin_id.delete(admin_id)
+    mc_admin_id_list_by_zsite_id.delete(zsite_id)
+
+
