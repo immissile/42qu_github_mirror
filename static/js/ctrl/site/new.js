9746ad0082dce9449c043620bb54abb768965a64
zuroc <zsp007@gmail.com>
2011-09-16 17:31:19 +0800

    fix

---
+++ static/js/ctrl/site/new.js
@@ -0,0 +1,39 @@
+
+$(function(){
+    function empty(self){
+        var val=self.val();
+        return !val||$.trim(val)==''
+    }
+    function verify(){
+        var self=$(this);
+        tip(self)
+    }
+    function tip(self){
+        var id=self[0].id;
+        if(empty(self)){
+            $("#errtip_"+id).remove()
+            var tip=$('<div class="errtip" id="errtip_'+id+'"/>');
+            self.after(tip);
+            tip.fadeOut(function(){tip.fadeIn()})
+            tip.html("这是必填项")
+        }
+    }
+    var elem=$('#name, #motto, #pic');
+    elem.blur(verify).focus(function(){
+        $("#errtip_"+this.id).remove()
+    })
+    site_new = function (){
+        var submit,i=0;
+        for(;i<elem.length;++i){
+            var self=$(elem[i]);
+            if(empty(self)){
+                self.focus();
+                $(window).scrollTop(Math.max(self.offset().top-50,0))
+                tip(self)
+                submit = false
+                break
+            }
+        }
+        return submit;
+    }
+})
