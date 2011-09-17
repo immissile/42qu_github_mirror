bd8e01131b2e1506314fffc14b0f3bae4900e2cb
zuroc <zsp007@gmail.com>
2011-09-17 17:34:09 +0800

    fix

--- god/user_list.py
+++ god/user_list.py
@@ -5,7 +5,7 @@ from _urlmap import urlmap
 from model.zsite import Zsite
 from model.cid import CID_USER
 from zkit.page import page_limit_offset
-from model.zsite_show import user_list_verify, zsite_show_count
+from model.zsite_show import user_show_id_list, zsite_show_count
 
 PAGE_LIMIT = 100
 
@@ -28,12 +28,12 @@ class Index(Base):
         )
 
 
-@urlmap('/user_list_verify')
-@urlmap('/user_list_verify-(\d+)')
+@urlmap('/user_show_id_list')
+@urlmap('/user_show_id_list-(\d+)')
 class IndexV(Base):
     def get(self, n=1):
         n = int(n)
         count = zsite_show_count(CID_USER)
-        page, limit, offset = page_limit_offset('/user_list_verify-%s', count, n, 64)
-        zsite_list = Zsite.mc_get_list(user_list_verify(limit, offset))
+        page, limit, offset = page_limit_offset('/user_show_id_list-%s', count, n, 64)
+        zsite_list = Zsite.mc_get_list(user_show_id_list(limit, offset))
         self.render(zsite_list=zsite_list, page=page)
