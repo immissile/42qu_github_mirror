ca389f43a75824bf96554be3322b577512eafe47
zuroc <zsp007@gmail.com>
2011-09-17 12:27:30 +0800

    fix

--- model/zsite_list_0.py
+++ model/zsite_list_0.py
@@ -5,11 +5,14 @@ from functools import partial
 from zsite_rank import zsite_rank_get
 from zweb.orm import ormiter
 from model.cid import CID_USER, CID_SITE
-
+from model.zsite import Zsite
 OWNER_ID = 0
 
-zsite_show = partial(zsite_id_list, 0, 0)
 user_list_verify = partial(zsite_id_list_order_id_desc, 0, 0)
+
+def zsite_show_list(cid=CID_USER, limit=None, offset=None):
+    id_list = zsite_id_list(0, cid, limit, offset)
+    return Zsite.mc_get_list(id_list)
 
 def zsite_show_update():
     for i in ormiter(ZsiteList, 'owner_id=0 and cid=0'):
