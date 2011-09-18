9b6ad9bf2906145efc3dc522d1998da57940dadd
zuroc <zsp007@gmail.com>
2011-09-18 23:24:09 +0800

    fix

--- ctrl/zsite/site.py
+++ ctrl/zsite/site.py
@@ -4,11 +4,19 @@
 from ctrl._urlmap.zsite import urlmap
 from _handler_site import SiteBase, LoginBase
 from model.zsite_admin import admin_id_list_by_zsite_id, zsite_user_state
+from zkit.jsdict import JsDict
+from model.zsite_link import link_list_cid_by_zsite_id,SITE_LINK_ZSITE_DICT
 
 @urlmap('/admin')
 class Admin(LoginBase):
     def get(self):
-        self.render()
+        zsite_id = self.zsite_id 
+        link_list , link_cid = link_list_cid_by_zsite_id(zsite_id, SITE_LINK_ZSITE_DICT)
+        self.render(
+            errtip=JsDict(),
+            link_cid=link_cid,
+            link_list=link_list
+        )
 
 
 @urlmap('/mark')
