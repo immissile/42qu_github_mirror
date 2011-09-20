add13cf37fdb75f3e1b989949fd6a9b92393c047
zuroc <zsp007@gmail.com>
2011-09-20 15:39:40 +0800

    fix

--- model/site_po.py
+++ model/site_po.py
@@ -1,13 +1,25 @@
 #coding:utf-8
 
 from _db import cursor_by_table, McModel, McLimitA, McCache, McNum, Model, McCacheM
+from po import Po
+from model.state import STATE_PO_ZSITE_REVIEW , STATE_PO_ZSITE_ACCPET 
+
 
 mc_pos_id_list_by_cid = McCacheM("PosIdListByCid:%s")
 
 
-def po_id_list_by_zsite_id(zsite_id, ):
-    pass
+def po_list_by_zsite_id(zsite_id, cid, limit, offset):
+    return Po.mc_get_list(
+        po_id_list_by_zsite_id(zsite_id, cid,  limit, offset)
+    )
 
+def po_id_list_by_zsite_id(zsite_id, cid,  limit, offset):
+
+    qs = Po.where(
+        zsite=zsite_id, cid=cid
+    ).where("state>=%s"%STATE_PO_ZSITE_ACCPET)
+ 
+    return qs.order_by('id desc').col_list(limit, offset)
 
 class ZsiteSiteMax(Model):
     pass
