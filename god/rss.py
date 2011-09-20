580b8f5230166b2e0efcd4358bb7738266cdb5bb
yupbank <yupbank@gmail.com>
2011-09-20 22:10:19 +0800

    ok

--- god/rss.py
+++ god/rss.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from _handler import Base
 from _urlmap import urlmap
-from model.rss import rss_po_list_by_state, RssPo, RSS_UNCHECK, RSS_PRE_PO, RSS_RM, rss_po_total
+from model.rss import rss_po_list_by_state, RssPo, RSS_UNCHECK, RSS_PRE_PO, RSS_RM, rss_po_total, get_rss_by_gid, rss_total_gid
 from zkit.page import page_limit_offset
 
 PAGE_LIMIT = 10
@@ -25,6 +25,34 @@ class RssIndex(Base):
                 rss_po_list=rss_po_list,
                 page=page
             )
+
+@urlmap('/rss_gid/(\d+)')
+@urlmap('/rss_gid/(\d+)-(\-?\d+)')
+class RssGid(Base):
+    def get(self, gid=0,n=1):
+        gid = int(gid)
+        total = rss_total_gid(gid)
+        page, limit, offset = page_limit_offset(
+                '/rss_gid/%s-%%s'%gid,
+                total,
+                n,
+                PAGE_LIMIT
+                )
+        rss_list = get_rss_by_gid(gid, limit, offset)
+        self.render(
+                rss = rss_list,
+                page = page
+                )
+
+@urlmap('/rss_gid/edit/(\d+)')
+class RssEdit(Base):
+    def post(self, id):
+        id=int(id)
+        rss = Rss.get(id=id)
+        rss.gid = rss.gid-1
+        rss.save()
+
+        self.redirect('/rss_gid/%s'%rss.gid)
 
 @urlmap('/rss/edit/(\d+)')
 class RssEdit(Base):
