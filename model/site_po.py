cbfa73705a894d7cf7630bd8f81eb3ec13d844f8
zuroc <zsp007@gmail.com>
2011-09-21 01:43:39 +0800

    fix

--- model/site_po.py
+++ model/site_po.py
@@ -1,35 +1,41 @@
 #coding:utf-8
 
-from _db import cursor_by_table, McModel, McLimitA, McCache, McNum, Model, McCacheM
-from po import Po
-from model.state import STATE_PO_ZSITE_REVIEW , STATE_PO_ZSITE_ACCPET 
-from model.po import PO_CID
+from _db import cursor_by_table, McModel, McLimitA, McCache, McNum, Model, McCacheM, McCacheA
+from po import Po, PO_CID
+from model.state import STATE_PO_ZSITE_REVIEW , STATE_PO_ZSITE_ACCPET
 
-mc_pos_id_list_by_cid = McCacheM("PosIdListByCid:%s")
-mc_po_count = McCacheM("PoCountByZsiteId")
+#mc_pos_id_list_by_cid = McCacheM("PosIdListByCid:%s")
+
 po_cid_count_by_zsite_id = McNum(
-    lambda zsite_id,cid:Po.where(
+    lambda zsite_id, cid:Po.where(
         zsite_id=zsite_id, cid=cid
-    ).where("state>=%s"%STATE_PO_ZSITE_ACCPET).count(),
-    "PoIdCount:%s"
+    ).where('state>=%s'%STATE_PO_ZSITE_ACCPET).count(),
+    'PoIdCount:%s'
 )
+po_count_by_zsite_id = McCacheA('PoCountByZsiteId:%s')
 
-def ():
-    return [
 
-    ] 
+@po_count_by_zsite_id("{zsite_id}") 
+def _po_count_by_zsite_id(zsite_id):
+    return tuple(
+        po_cid_count_by_zsite_id(zsite_id,i) for i in PO_CID
+    )
 
+def po_count_by_zsite_id(zsite_id):
+    return zip(
+        PO_CID, _po_count_by_zsite_id(zsite_id)
+    )
 
 def po_list_by_zsite_id(zsite_id, cid, limit, offset):
     return [
-        po_id_list_by_zsite_id(zsite_id, cid,  limit, offset)
+        po_id_list_by_zsite_id(zsite_id, cid, limit, offset)
     ]
 
-def po_id_list_by_zsite_id(zsite_id, cid,  limit, offset):
+def po_id_list_by_zsite_id(zsite_id, cid, limit, offset):
     qs = Po.where(
         zsite_id=zsite_id, cid=cid
-    ).where("state>=%s"%STATE_PO_ZSITE_ACCPET)
- 
+    ).where('state>=%s'%STATE_PO_ZSITE_ACCPET)
+
     return qs.order_by('id desc').col_list(limit, offset)
 
 class ZsiteSiteMax(Model):
@@ -55,6 +61,6 @@ def next_id_list(zsite_id, user_id):
     pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print pos_id_list_by_cid(1, 2)
 
