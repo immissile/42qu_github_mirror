1e7aef2bfde67112056f2fa29ae6bc2e565b0131
zuroc <zsp007@gmail.com>
2011-09-20 14:54:10 +0800

    fix

--- static/js/ctrl/zsite/site.js
+++ static/js/ctrl/zsite/site.js
@@ -1,4 +1,8 @@
 
+function init_say(){
+    document.write('<div class="say"><div class="say_txt">今天 ,  想说什么?</div></div> <div class="pop_say"> <textarea class="pop_txt"></textarea> <div class="pop_banner"><a class="pop_submit" href="javascript:post_say();void(0)">发表</a><div class="pop_tip"></div><a class="pop_close" href="javascript:close_pop();void(0)"></a></div></div>')
+}
+
 $(function(){
     var say = $('.say')
     var pop_say = $(".pop_say")
@@ -6,17 +10,19 @@ $(function(){
     var pop_txt = $('.pop_txt')
     var txt_tip = txt.text()
     var  pop_tip = $('.pop_tip') 
+    
     can_say = txt_maxlen(pop_txt,pop_tip,142)
 
     txt.click(function(){
-        if(pop_say.css("display")=='none'){
-            pop_say.css("display","block")
+        if(pop_say.is(":hidden")){
+            pop_say.show();
+            pop_txt.focus()
         }  
     })
     
     close_pop = function (){
         var cont = $('.pop_txt').val()
-        pop_say.css('display','none')
+        pop_say.hide()
         if($.trim(cont)=='') txt.text(txt_tip)
         else txt.text(cont)
     }
@@ -26,13 +32,14 @@ $(function(){
             pop_tip.fadeOut(function(){pop_tip.fadeIn()})
             return false
         }
-        pop_txt.replaceWith($('<pre class="pop_txt" id="pop_txt"/>').html(pop_txt.val()))
+        var val=pop_txt.val()
+        pop_txt.replaceWith($('<pre class="pop_txt" id="pop_txt"/>').html(val))
         pop_tip.replaceWith('<div class="say_loading pop_tip"></div>')
         $('.pop_close,.pop_submit').hide()
         $.postJSON(
             '/j/po/word',
             {
-                "txt":pop_txt.val()
+                "txt":val
             },
             function(){
                 $('.pop_tip').replaceWith('<div class="pop_ok pop_tip">发表成功</div>')
