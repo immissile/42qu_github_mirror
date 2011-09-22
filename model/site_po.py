29be8e74e30852faf01e1e5341fe6934f1e03b8a
zuroc <zsp007@gmail.com>
2011-09-22 18:44:47 +0800

    f

--- model/site_po.py
+++ model/site_po.py
@@ -47,29 +47,29 @@ def po_id_list_by_zsite_id(zsite_id, cid, limit, offset):
     return qs.order_by('id desc').col_list(limit, offset)
 
 
-class ZsiteSiteMax(Model):
-    pass
-
-
-class ZsiteSitePos(Model):
-    pass
-
-
-def pos_id_list_by_cid(zsite_id , user_id):
-    r = ZsiteSitePos.where(zsite_id=zsite_id, user_id=user_id).col_list(
-            col='cid,pos_id'
-        )
-    return dict(r)
-
-def pos_mark_all(zsite_id, user_id):
-    pass
-
-def pos_mark(zsite_id, user_id, cid):
-    pass
-
-def next_id_list(zsite_id, user_id):
-    pass
-
+#class ZsiteSiteMax(Model):
+#    pass
+#
+#
+#class ZsiteSitePos(Model):
+#    pass
+#
+#
+#def pos_id_list_by_cid(zsite_id , user_id):
+#    r = ZsiteSitePos.where(zsite_id=zsite_id, user_id=user_id).col_list(
+#            col='cid,pos_id'
+#        )
+#    return dict(r)
+#
+#def pos_mark_all(zsite_id, user_id):
+#    pass
+#
+#def pos_mark(zsite_id, user_id, cid):
+#    pass
+#
+#def next_id_list(zsite_id, user_id):
+#    pass
+#
 
 if __name__ == '__main__':
     print pos_id_list_by_cid(1, 2)
