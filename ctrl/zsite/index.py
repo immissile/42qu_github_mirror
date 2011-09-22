fab618ecda6977345270adb5dd30c20eefaf0de1
zuroc <zsp007@gmail.com>
2011-09-22 17:15:07 +0800

    fix

--- ctrl/zsite/index.py
+++ ctrl/zsite/index.py
@@ -8,35 +8,34 @@ from model.cid import CID_USER, CID_SITE
 from model.site_po import po_list_by_zsite_id, po_cid_count_by_zsite_id
 from zkit.page import page_limit_offset
 
+def render_zsite_site(self, n=1):
+    zsite_id = self.zsite_id
+    zsite = self.zsite
+    user_id = self.current_user_id
 
+    if zsite.cid == CID_SITE:
+        total = po_cid_count_by_zsite_id(zsite_id, 0)
+        page, limit, offset = page_limit_offset(
+            "/-%s",
+            total,
+            n,
+            20
+        )
+        li = po_list_by_zsite_id(user_id, zsite_id, 0, limit, offset),
+        page=page
+        return li, page
 
 @urlmap('/')
 @urlmap("/-\d+")
 class Index(ZsiteBase):
     def get(self, n=1):
         zsite_id = self.zsite_id
-        zsite = self.zsite
-        user_id = self.current_user_id
-
-        if zsite.cid == CID_SITE:
-            total = po_cid_count_by_zsite_id(zsite_id, 0)
-            page, limit, offset = page_limit_offset(
-                "/-%s",
-                total,
-                n,
-                20
-            )
-            self.render(
-                '/ctrl/zsite/po_view/site_po_page.htm',
-                li = po_list_by_zsite_id(user_id, zsite_id, 0, limit, offset),
-                page=page,
-                total=total
-            )
+        result = render_zsite_site(self, n)
+        if result:
+            li, page = result
+            self.render( '/ctrl/zsite/po_view/site_po_page.htm',li, page)
         else:
-
-            self.render(
-                motto=motto.get(zsite_id),
-            )
+            self.render( motto=motto.get(zsite_id) )
 
 
 @urlmap('/link/(\d+)')
