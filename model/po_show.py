54237f4031a4b8b187b3234815ae9858715d74ef
zuroc <zsp007@gmail.com>
2011-09-23 20:02:44 +0800

    fix

--- model/po_show.py
+++ model/po_show.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from _db import McCache
-from feed import feed_rt, feed_rt_rm
+from feed import feed_rt, feed_rt_rm, feed_rt_list, feed_rt_count
 from po import Po, po_new, po_word_new, po_note_new, po_rm, CID_QUESTION
-from rank import Rank, rank_po_id_list, rank_new, rank_rm, rank_po_id_count, rank_id_by_po_id_to_id, _rank_mv
 from state import STATE_DEL, STATE_SECRET, STATE_ACTIVE
 from zsite import Zsite
+#from rank import Rank, rank_po_id_list, rank_new, rank_rm, rank_po_id_count, rank_id_by_po_id_to_id, _rank_mv
 
-ORDER = ('id', 'hot', 'confidence')
 
-def po_show_new(po, cid=0):
-    r = rank_new(po, 0, cid)
+def po_show_new(po):
     feed_rt(0, po.id)
 
 def po_show_rm(po_id):
     feed_rt_rm(0, po_id)
-    rank_rm(po_id, 0)
 
-def po_show_list(cid, order, limit, offset):
-    ids = rank_po_id_list(0, cid, order, limit, offset)
+def po_show_list(limit, offset):
+    ids = feed_rt_list(0, limit, offset)
     li = Po.mc_get_list(ids)
     Zsite.mc_bind(li, 'user', 'user_id')
     return li
 
-def po_show_count(cid=0):
-    return rank_po_id_count(0, cid)
+def po_show_count():
+    return feed_rt_count(0)
 
 def po_is_show(po):
-    return bool(rank_id_by_po_id_to_id(po.id, 0))
+    return feed_rt_id(0, po.id)
 
-def po_show_set(po, cid):
+def po_show_set(po):
     po_id = po.id
     id = rank_id_by_po_id_to_id(po_id, 0)
     r = Rank.mc_get(id)
