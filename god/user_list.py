b0f7c1069d5e7b9b63958925047c3407195a95d9
zuroc <zsp007@gmail.com>
2011-09-23 13:53:50 +0800

    fix

--- god/user_list.py
+++ god/user_list.py
@@ -35,5 +35,5 @@ class IndexV(Base):
         n = int(n)
         count = zsite_user_verify_count()
         page, limit, offset = page_limit_offset('/user_show_id_list-%s', count, n, 64)
-        zsite_list = Zsite.where(cid=CID_USER).where("state>=%s"%ZSITE_STATE_VERIFY)[offset:offset+limit]
+        zsite_list = Zsite.where(cid=CID_USER).where("state>=%s"%ZSITE_STATE_VERIFY).order_by("id desc")[offset:offset+limit]
         self.render(zsite_list=zsite_list, page=page)
