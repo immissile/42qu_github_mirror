e3547631cb84edc2ee00de6bb56efea08ea7953b
zuroc <zsp007@gmail.com>
2011-09-23 13:46:27 +0800

    fix

--- god/user_list.py
+++ god/user_list.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from _handler import Base
 from _urlmap import urlmap
-from model.zsite import Zsite
+from model.zsite import Zsite,ZSITE_STATE_VERIFY 
 from model.cid import CID_USER
 from zkit.page import page_limit_offset
 from model.zsite_show import user_show_id_list, zsite_show_count
@@ -33,7 +33,13 @@ class Index(Base):
 class IndexV(Base):
     def get(self, n=1):
         n = int(n)
-        count = zsite_show_count(CID_USER)
+        count = Zsite.raw_sql(
+            "select max(id) from zsite where cid=%s and state=%s"%(
+                CID_USER,
+                ZSITE_STATE_VERIFY 
+            )
+
+        )
         page, limit, offset = page_limit_offset('/user_show_id_list-%s', count, n, 64)
-        zsite_list = Zsite.mc_get_list(user_show_id_list(limit, offset))
+        zsite_list = Zsite.where(cid=CID_USER).where("state>=%s"%ZSITE_STATE_VERIFY)[offset:offset+limit]
         self.render(zsite_list=zsite_list, page=page)
