ee1b6289700084c1a96a446f60265c93b6543651
zuroc <zsp007@gmail.com>
2011-09-24 17:34:54 +0800

    fix

--- static/js/ctrl/live.js
+++ static/js/ctrl/live.js
@@ -169,3 +169,19 @@
     )
     
 })()
+$(".reply_at").live("click", function(){
+    var txt=$("#reply_txt").focus(),
+        val=txt.val(),
+        name=$(this.parentNode).find(".replyer").text(),
+        add;
+    add =  "@"+name+'('+this.rel+') '
+    if(val.length){
+        if($.trim(val)==val){
+            val+=" "
+        }
+        val+=add;
+    }else val=add;
+
+    txt.val(val)
+
+})
