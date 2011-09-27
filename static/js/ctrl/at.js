453c46eff3f8de00d3900d54191c077f13836be7
realfex <i@realfex.com>
2011-09-27 23:01:07 +0800

    fix

--- static/js/ctrl/at.js
+++ static/js/ctrl/at.js
@@ -1,6 +1,6 @@
 (function ($) {  
 var pre,
-at_size,at_list,
+at_size,at_list
 
 methods={
     getCarePos: function (node, con) {
@@ -66,17 +66,17 @@ methods={
             range.select();
 
         } else {
-            //t.setSelectionRange(s,z);
+            t.setSelectionRange(s,z);
             t.focus();
         }
     },
     deleteRangeText: function (t, n) {
-        var p = this.getCursorPosition(t[0]),
+        var p = this.getCursorPosition(t),
             s = t.scrollTop,
-            val = t.val()
+            val = t.value
 
         // reset value  
-         t.val(val.slice(0,p-n))
+         t.value = val.slice(0,p-n)
         // reset cursor
         this.setCursorPosition(t, p - (n < 0 ? 0 : n));
         // for firefox
@@ -87,24 +87,35 @@ methods={
         }, 10);
     },
 
-    insertAfterCursor: function (t,str) {
-        var obj=t[0];
-        obj.focus();
+    insertAfterCursor: function (t, str) {
+        var val = t.value,
+            self = this; 
+                
+        // for IE
         if (document.selection) {
-            setCursorPosition(obj,pos);
-            var sel = document.selection.createRange();
-            sel.text = str;
-        } else if (typeof obj.selectionStart == 'number' && typeof obj.selectionEnd == 'number') {
-            var startPos = obj.selectionStart,
-            endPos = obj.selectionEnd,
-            cursorPos = startPos,
-            tmpStr = obj.value;
-            obj.value = tmpStr.substring(0, startPos) + str + tmpStr.substring(endPos, tmpStr.length);
-            cursorPos += str.length;
-            //obj.selectionStart = obj.selectionEnd = cursorPos;
+            t.focus();
+            document.selection.createRange().text = str + ' ';  
+
+           
         } else {
-            obj.value += str;
-        }
+            var obj=t;
+            obj.focus();
+            if (document.selection) {
+                setCursorPosition(obj,pos);
+                var sel = document.selection.createRange();
+                sel.text = str;
+            } else if (typeof obj.selectionStart == 'number' && typeof obj.selectionEnd == 'number') {
+                var startPos = obj.selectionStart,
+                endPos = obj.selectionEnd,
+                cursorPos = startPos,
+                tmpStr = obj.value;
+                obj.value = tmpStr.substring(0, startPos) + str + tmpStr.substring(endPos, tmpStr.length);
+                cursorPos += str.length;
+                //obj.selectionStart = obj.selectionEnd = cursorPos;
+            } else {
+                obj.value += str;
+            }    
+        };
     },
 
     moveSelectedItem: function (step) {
@@ -135,11 +146,11 @@ $.fn.pop_at = function(){
         methods.deleteRangeText(t, w.length);
         methods.insertAfterCursor(t,name+'('+id+') ');
         $('#at_list').remove()
-    },
+    }
     this.bind('keyup',function(e){
         at_list,at_size
         var self = $(this)
-        offset = methods.getCursorPosition(self)
+        offset = methods.getCursorPosition(self[0])
         var val = self.val()
         lastCharAt = val.substring(0, offset).lastIndexOf('@')
         hasSpace = val.substring(lastCharAt, offset).indexOf(' ')
@@ -151,10 +162,13 @@ $.fn.pop_at = function(){
                     "/j/at/",
                     function(data){
                         html = '<div class="at_list" id="at_list">'
+                        if(data.length<0){
+                            return;
+                        }else{
                         for(i=0;i<data.length;i++){
                             t=data[i]
                             html += '<div class="at_li"><img class="at_img L" src="'+t[3]+'"><span class="at_name" id="'+t[2]+'">'+t[0]+'</span><span class="at_title">'+t[1]+'</span></div>'
-                        }
+                        }}
                         at_list = html+'</div>'
                         at_size = data.length
                     }
@@ -164,7 +178,7 @@ $.fn.pop_at = function(){
                 $('body').append(at_list)
                 $('#at_list').css(pos)
                 $('.at_li').mouseover(function(){$('.at_li').removeClass('on');$(this).addClass('on')})
-                $('.at_li').click(function(){atComplete(self,wordsForSearch)})
+                $('.at_li').click(function(){atComplete(self[0],wordsForSearch)})
                 $("body:not(:.at_li)").click(function(){$('#at_list').remove()})
             }
         } else{$('#at_list').remove()}
@@ -192,7 +206,7 @@ $.fn.pop_at = function(){
 
             case 13:
             e.preventDefault();
-            atComplete($(this),wordsForSearch)
+            atComplete($(this)[0],wordsForSearch)
             break;
 
             default:
