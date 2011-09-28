a80edbc8ae9e50ffa62a98462e7614af04f2220c
zuroc <zsp007@gmail.com>
2011-09-28 14:17:33 +0800

    fix

--- misc/search/index.py
+++ misc/search/index.py
@@ -24,11 +24,12 @@ def flush_db():
 @single_process
 def index():
     from zsite_iter import zsite_keyword_iter
-    for id, rank, kw in zsite_keyword_iter():
+    for id, cid, rank, kw in zsite_keyword_iter():
 
         doc = xapian.Document()
         doc.add_value(0, id)
         doc.add_value(1, xapian.sortable_serialise(rank))
+        doc.add_value(2, cid)
 
         for word, value in kw:
             if word:
