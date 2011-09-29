e74c8a2ffee74e5d3784903e336d716cc597829c
realfex <i@realfex.com>
2011-09-29 22:52:16 +0800

    autoat_back

--- zkit/pinyin.py
+++ zkit/pinyin.py
@@ -297,6 +297,9 @@ def pinyin_by_str(text):
     return ''.join(pinyin_list)
 
 if __name__ == '__main__':
-    print startswith_pinyin_initial('z')('张沈鹏')
+    print dir(startswith_pinyin_initial('zh'))
+    print startswith_pinyin_initial('zh')('张沈鹏')
     print startswith_pinyin_initial('z')('沈鹏')
-    print startswith_pinyin_initial('s')('沈鹏')
+    print startswith_pinyin_initial('s')('沈鹏'),'!!!!'
+    print startswith_pinyin_initial('sh')('沈鹏')
+    print pinyin_by_str('张沈鹏')
