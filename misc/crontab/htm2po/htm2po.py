27e09df692f3fe9ff94c4861c733ea11b25f21e2
zuroc <zsp007@gmail.com>
2011-10-03 19:29:50 +0800

    fix

--- misc/crontab/htm2po/htm2po.py
+++ misc/crontab/htm2po/htm2po.py
@@ -11,20 +11,6 @@ from model.state import STATE_DEL, STATE_ACTIVE
 from model.rss import rss_po_id, RSS_RT_PO
 from model.po_show import po_show_new
 
-#def htm2po(user_id, title, htm):
-#    po = po_note_new(user_id, title, '', STATE_DEL)
-#    po_id = po.id
-#    try:
-#        txt, pic_list = htm2txt(htm)
-#    except:
-#        po.txt_set(htm)
-#    else:
-#        for seq, url in enumerate(pic_list, 1):
-#            img = fetch_pic(url)
-#            if img:
-#                po_pic_new(user_id, po_id, img, seq)
-#        po.txt_set(txt)
-#    return po
 
 def htm2po_by_po(pre):
     txt = pre.txt
