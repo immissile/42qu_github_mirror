392ea2c8752a5f3c570078b99f4d0f872b4e9faa
zuroc <zsp007@gmail.com>
2011-10-05 23:25:12 +0800

    fix

--- model/site_feed.py
+++ model/site_feed.py
@@ -4,6 +4,8 @@ from feed import PAGE_LIMIT
 from model.site_po import po_id_list_by_zsite_id
 from model.state import STATE_PO_ZSITE_SHOW_THEN_REVIEW
 from zkit.feed_merge import MAXINT, merge_iter as _merge_iter
+from model.zsite import Zsite
+from model.po import Po
 
 ID_SQL = 'select id from po where state>=%s and zsite_id=%%s and id<%%s order by id desc limit %s'%(
     STATE_PO_ZSITE_SHOW_THEN_REVIEW,
@@ -41,9 +43,30 @@ def merge_iter(
         feed_iter, id_list, limit, begin_id
     )    
 
+def site_po_iter(id_list,  limit=MAXINT, begin_id=MAXINT):
+    id_list = list(merge_iter(id_list, limit, begin_id))
+    po_list = Po.mc_get_list(id_list)
+
+    zsite_set = set() 
+
+    for i in po_list:
+        zsite_set.add(i.zsite_id)
+        zsite_set.add(i.user_id)
+    
+    zsite_dict = Zsite.mc_get_dict(zsite_set)
+
+    for i in po_list:
+        zsite_id = i.zsite_id
+        user_id = i.user_id
+
+        i.zsite = zsite_dict[zsite_id]
+        if zsite_id != user_id:
+            i.user = zsite_dict[user_id]
+
+    return po_list
 
 if __name__ == '__main__':
-    for i in merge_iter([10099440,10092249]):
+    for i in site_po_iter([10099440,10092249]):
         print i
 
 
