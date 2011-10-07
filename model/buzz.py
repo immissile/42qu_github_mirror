d8ab2d4acb9640235dfd65647ddb1a010e3f0d7f
zuroc <zsp007@gmail.com>
2011-10-07 16:06:05 +0800

    fix

--- model/buzz.py
+++ model/buzz.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from time import time
 from _db import Model, McModel, McCache, McLimitM, McNum
+from txt2htm import RE_AT
 from cid import CID_BUZZ_SYS, CID_BUZZ_SHOW, CID_BUZZ_FOLLOW, CID_BUZZ_WALL, CID_BUZZ_WALL_REPLY, CID_BUZZ_PO_REPLY, CID_BUZZ_ANSWER, CID_BUZZ_JOIN, CID_BUZZ_EVENT_JOIN_APPLY, CID_BUZZ_EVENT_FEEDBACK_JOINER, CID_BUZZ_EVENT_FEEDBACK_OWNER, CID_USER, CID_BUZZ_SITE_NEW , CID_BUZZ_SITE_FAV, CID_BUZZ_WORD
 
 from zsite import Zsite, ZSITE_STATE_ACTIVE
@@ -17,6 +18,7 @@ from reply import Reply
 from zweb.orm import ormiter
 from zkit.orderedset import OrderedSet
 from zkit.ordereddict import OrderedDict
+from zsite_url import id_by_url
 from collections import defaultdict
 
 class Buzz(Model):
@@ -113,8 +115,6 @@ mq_buzz_word_new = mq_client(buzz_word_new)
 
 def buzz_po_reply_new(from_id, reply_id, po_id, po_user_id):
     from txt import txt_get
-    from txt2htm import RE_AT
-    from zsite_url import id_by_url
     from po_pos import po_pos_state, STATE_MUTE
     txt = txt_get(reply_id)
     ated = set(filter(bool, [id_by_url(i[2]) for i in RE_AT.findall(txt)]))
