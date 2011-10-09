654ebecaca44a5707c777977659c24b83e590fe3
yupbank <yupbank@gmail.com>
2011-10-09 18:08:09 +0800

    add file

---
+++ god/mail_notify.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _handler import Base
+from _urlmap import urlmap
+
+
+@urlmap('/mail/notify')
+class Index(Base):
+    def get(self):
+        self.render()
