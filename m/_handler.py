e63cbee4e0492e4c0c46015a3326b35b8572aec9
yupbank <yupbank@gmail.com>
2011-10-11 18:31:12 +0800

    ok

--- m/_handler.py
+++ m/_handler.py
@@ -14,3 +14,28 @@ def post(self, *args, **kwds):
 class Base(_Base):
     def get(self, *args):
         self.redirect('/')
+
+def _login_redirect(self):
+    if self._finished:
+        return
+    if not self.current_user:
+        url = self.get_login_url()
+        if '?' not in url:
+            if urlparse.urlsplit(url).scheme:
+                # if login url is absolute, make next absolute too
+                next_url = self.request.full_url()
+            else:
+                next_url = self.request.uri
+            request = self.request
+            host = request.host
+            if host != SITE_DOMAIN and next_url.startswith('/') and not next_url.startswith('//'):
+                next_url = '//%s%s'%(host, next_url)
+            url += '?' + urllib.urlencode(dict(next=next_url))
+        self.redirect(url)
+        return True
+
+class LoginBase(_LoginBase):
+    def prepare(self):
+        super(LoginBase, self).prepare()
+        _login_redirect(self)
+
--- m/index.py
+++ m/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #coding:utf-8
-from _handler import Base
+from _handler import Base, LoginBase
 from _urlmap import urlmap
 
 
 @urlmap('/')
-class Index(Base):
+class Index(LoginBase):
     def get(self):
         self.finish('it works')
