1acec694e355c31ba193dbaff271069a36e9cdeb
yupbank <yupbank@gmail.com>
2011-10-19 21:57:42 +0800

    ok

--- misc/once/add_sites.py
+++ misc/once/add_sites.py
@@ -5,9 +5,10 @@ from model.zsite_site import site_new
 from model.oauth import linkify
 from model.motto import motto_set
 from model.ico import site_ico_new, site_ico_bind
-from model.zsite_link import link_list_save
+from model.zsite_link import ZsiteLink, mc_flush
 from zkit.pic import picopen
 import urllib
+import json
 
 def make_site(name,link,motto,img_src,current_user_id=10017321):
     f = urllib.urlopen(img_src).read()
@@ -15,16 +16,33 @@ def make_site(name,link,motto,img_src,current_user_id=10017321):
         pic = picopen(f)
         if pic:
             pic_id = site_ico_new(10017321,pic)
-    site = site_new(name,current_user_id,sitetype=40)
+    site = site_new(name,current_user_id,40)
     site_id = site.id
-    link_cid = (2,'豆瓣小站',linkify(link,2))
     site_ico_bind(current_user_id, pic_id, site_id)
-    link_list_save(site_id,link_cid,[])
+    zsite_link = ZsiteLink.get_or_create(zsite_id=site_id,cid=2)
+    zsite_link.link = link
+    zsite_link.name = '豆瓣小站'
+    zsite_link.save()
+    mc_flush(site_id)
     motto_set(site_id, motto)
     print site_id
 
-
-
+def get_in(id):
+    with open('intro') as i:
+        intro = json.loads(i.read())
+    with open('info') as i:
+        info = json.loads(i.read())
+    with open('meta') as i:
+        meta = json.loads(i.read())
+    idintro = intro.get(id)
+    if info.get(id):
+        like,link,img_src,name = info.get(id)
+    else:
+        print id,'数据未录入'
+    if meta.get(id):
+        motto = meta.get(id)[0]
+    else:
+        print id,'no motto data'
+    make_site(name,link,motto,img_src)
 if __name__ == "__main__":
-    make_site()
-
+    get_in('42qu')
