afd0558f806e47af8e00e2439bae73eb2082da3b
zuroc <zsp007@gmail.com>
2011-10-19 21:53:24 +0800

    fix

--- static/js/ctrl/site/rec.js
+++ static/js/ctrl/site/rec.js
@@ -3,22 +3,16 @@
     site_x = function(){
         $('.site_rec,.site_to,.site_x').remove()
     }
-    fav_site = function(){
+    site_fav = function(id){
         $('.site_unfav').slideUp()
         $('.site_fav').replaceWith('<span class="site_unfav" style="color:#000;">收藏成功</span>')
         $('.site_unfav').removeClass('site_fav')
         $('.site_more').slideDown()
-        $.postJSON(
-            'j',
-            {}
-        )
+        $.postJSON('/j/site/fav/'+id)
     }
-    unfav_site = function(){
-        $.postJSON(
-            'j',
-            {}
-        )
-        $('.site_rec,.site_to,.site_x').remove()
+    site_unfav = function(id){
+        $.postJSON( '/j/site/unfav/'+id)
+        $('.site_rec').remove()
     }
 });
 $("#site_rec").tmpl({}).prependTo(".H")
