9c66902bba970e181858bf2f4e1e7fae0f723aa1
zuroc <zsp007@gmail.com>
2011-10-19 23:35:21 +0800

    fix

--- misc/crontab/site_rec.py
+++ misc/crontab/site_rec.py
@@ -7,7 +7,8 @@ from zkit.single_process import single_process
 from model.site_po import po_cid_count_by_zsite_id
 from zweb.orm import ormiter
 from model.zsite_site import zsite_id_list_by_user_id
-
+from model.site_rec import SiteRecHistory, SiteRec
+from random import choice
 
 def can_rec_site_id_list():
     result = []
@@ -24,22 +25,34 @@ def user_id_site_can_rec():
 
     for i in ormiter(Zsite, 'cid=%s'%CID_USER):
         user_id = i.id
-        fav_site_id_set = set(zsite_id_list_by_user_id(user_id))
+        if SiteRec.get(user_id):
+            continue
+        fav_list = list(zsite_id_list_by_user_id(user_id))
+        fav_list.extend(
+            SiteRecHistory.where(user_id=user_id).col_list(col='zsite_id')
+        )
+
+        fav_site_id_set = set(fav_list)
         can_rec_id_list = id_list - fav_site_id_set
+
         if not can_rec_id_list:
             continue
-        yield user_id, can_rec_id_list        
+
+        yield user_id, can_rec_id_list
 
 
+def site_rec_by_user_id(user_id, can_rec_id_list):
+    site_id = choice(list(can_rec_id_list))
+    return site_id
 
-def site_rec():
+def run():
     for user_id, can_rec_id_list in user_id_site_can_rec():
-        print user_id, can_rec_id_list
-
+        site_id = site_rec_by_user_id(user_id, can_rec_id_list)
+        SiteRec.set(user_id, site_id)
 
 @single_process
 def main():
-    site_rec()
+    run()
 
 
 
--- model/site_rec.py
+++ model/site_rec.py
@@ -2,6 +2,26 @@
 
 from _db import cursor_by_table, McModel, McLimitA, McCache, McNum, Model, McCacheM, McCacheA
 from model.zsite import Zsite
+from kv import Kv
+
+SiteRec = Kv("site_rec",0)
+
+class SiteRecHistory(Model):
+    pass
 
 def site_rec(user_id):
-    return Zsite.mc_get(129) 
+    zsite_id = SiteRec.get(user_id) 
+    if zsite_id: 
+        return Zsite.mc_get(zsite_id)
+
+SITE_REC_STATE_REJECT = 0
+SITE_REC_STATE_PASS = 1
+SITE_REC_STATE_FAV = 2
+
+
+def site_rec_feeckback(user_id, zsite_id, state):
+    SiteRecHistory(user_id=user_id, zsite_id=zsite_id, state=state).save()
+    SiteRec.set(user_id,0)
+
+
+
