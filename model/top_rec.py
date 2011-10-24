e45ddc5a518d2f1b57750d59c37a7fcef2e357bc
zuroc <zsp007@gmail.com>
2011-10-24 17:02:45 +0800

    fix

---
+++ model/top_rec.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+
+from _db import cursor_by_table, McModel, McLimitA, McCache, McNum, Model, McCacheM, McCacheA
+from model.zsite import Zsite
+from kv import Kv
+from model.zsite import  Zsite
+from model.cid import CID_SITE
+
+TopRec = Kv('site_rec', 0)
+
+TOP_REC_CID   = 0b11111111111111111111111111111111 
+TOP_REC_CID_OAUTH_BIND = 0b00000000000000000000000000000001
+TOP_REC_CID_SITE_REC   = 0b00000000000000000000000000000010
+
+def top_rec(user_id):
+    return TopRec.get(user_id)
+
+def top_rec_mark(user_id, cid):
+    old =  top_rec(user_id)
+    old|=cid
+    TopRec.set(user_id, old)
+    return old
+
+def top_rec_unmark(user_id, cid):
+    old =  top_rec(user_id)
+    old&=(TOP_REC_CID^cid)
+    TopRec.set(user_id, old)
+    return old
+
+if __name__ == "__main__":
+    print top_rec_mark(1, TOP_REC_CID_SITE_REC)
+    print top_rec_unmark(1, TOP_REC_CID_SITE_REC)
+
