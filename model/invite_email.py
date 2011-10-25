e5e966715b1579bc73172de1b39058c1f066ebeb
zuroc <zsp007@gmail.com>
2011-10-25 23:01:06 +0800

    fix

--- model/invite_email.py
+++ model/invite_email.py
@@ -5,6 +5,7 @@ from _db import Model, McModel, McCache, McLimitM, McNum
 from zkit.import_msn.get_friend_list import get_friend_list as msn_friend_get
 #from zkit.google.ginvite import load_friend
 from user_mail import user_id_by_mail
+from json import dumps
 CID_MSN = 1
 CID_GOOGLE = 2
 CID_QQ = 3
@@ -26,8 +27,7 @@ class InviteMessage(Model):
     pass
 
 def new_invite_message(uid,email_list,txt):
-    for email in email_list:
-        InviteMessage.raw_sql('insert into invite_message (email,uid,txt) values(%s,%s,%s)',email,uid,txt)
+    InviteMessage.raw_sql('insert into invite_message (email,uid,txt) values (%s,%s,%s)',dumps(email_list),uid,txt)
 
 def new_invite_email(usr_id,cid_email,cid,res):
     InviteEmail.raw_sql('update invite_email set cid = %s where usr_id =%s and cid=%s ',-cid,usr_id,cid)
