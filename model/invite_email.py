a7dc86565ebc5edda1fd80384c67f7e6bb56601c
zuroc <zsp007@gmail.com>
2011-10-26 00:37:07 +0800

    fix

--- model/invite_email.py
+++ model/invite_email.py
@@ -15,7 +15,7 @@ INVITE_CID2CN = {
         CID_MSN:'MSN',
         }
 
-GET_CID ={
+GET_CID = {
         CID_MSN:msn_friend_get,
 #        CID_GOOGLE:google_friend_get,
         }
@@ -26,33 +26,41 @@ class InviteEmail(Model):
 class InviteMessage(Model):
     pass
 
-def new_invite_message(uid,email_list,txt):
-    InviteMessage.raw_sql('insert into invite_message (email,uid,txt) values (%s,%s,%s)',dumps(email_list),uid,txt)
+def new_invite_message(user_id, email_list, txt):
+    InviteMessage.raw_sql(
+        'insert into invite_message (email,user_id,txt) values (%s,%s,%s)', 
+        dumps(email_list), 
+        user_id, 
+        txt
+    )
 
-def new_invite_email(usr_id,cid_email,cid,res):
-    InviteEmail.raw_sql('update invite_email set cid = %s where usr_id =%s and cid=%s ',-cid,usr_id,cid)
-    for email,name in res.items():
-        InviteEmail.raw_sql('insert into invite_email (usr_id,cid,cid_email,email,name,email_uid) values(%s,%s,%s,%s,%s,%s)',usr_id,cid,cid_email,email,name,user_id_by_mail(email))
+def new_invite_email(user_id, cid, res):
+    InviteEmail.raw_sql('update invite_email set cid = %s where user_id =%s and cid=%s ', -cid, user_id, cid)
+    for email, name in res.items():
+        InviteEmail.raw_sql(
+            'insert into invite_email (user_id,cid, email,name,email_user_id) values (%s,%s,%s,%s,%s)', 
+            user_id, cid,  email, name, user_id_by_mail(email)
+        )
     return True
 
-def get_invite_uid_by_cid(usr_id,cid):
-    uid = InviteEmail.raw_sql('select email_uid from invite_email where usr_id = %s and cid=%s',usr_id,cid).fetchone()
+def get_invite_uid_by_cid(user_id, cid):
+    uid = InviteEmail.raw_sql('select email_user_id from invite_email where user_id = %s and cid=%s', user_id, cid).fetchone()
     if uid:
         return True
-def get_invite_email_list_by_cid(usr_id,cid):
-    emails = InviteEmail.raw_sql('select email,name from invite_email where usr_id = %s and cid=%s and email_uid = 0',usr_id,cid).fetchall()
+def get_invite_email_list_by_cid(user_id, cid):
+    emails = InviteEmail.raw_sql('select email,name from invite_email where user_id = %s and cid=%s and email_user_id = 0', user_id, cid).fetchall()
     return emails
 
-def get_invite_uid_list_by_cid(usr_id,cid):
-    uid_list = InviteEmail.raw_sql('select email_uid from invite_email where usr_id = %s and cid = %s',usr_id,cid).fetchall()
+def get_invite_uid_list_by_cid(user_id, cid):
+    uid_list = InviteEmail.raw_sql('select email_user_id from invite_email where user_id = %s and cid = %s', user_id, cid).fetchall()
     full_uid = []
     for uid in uid_list:
         if uid[0]:
             full_uid.append(uid[0])
     return full_uid
 
-def get_email_by_cid(usr_id,cid):
-    return [i.email for i in InviteEmail.where(usr_id=usr_id).where(cid=cid)]
+def get_email_by_cid(user_id, cid):
+    return [i.email for i in InviteEmail.where(user_id=user_id).where(cid=cid)]
 
-if __name__ == "__main__":
-    print msn_friend_get('wsyupeng@hotmail.com','yu6171446')
+if __name__ == '__main__':
+    print msn_friend_get('wsyupeng@hotmail.com', 'yu6171446')
