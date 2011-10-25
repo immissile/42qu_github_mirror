f8d23f317784985e6278e0c2e98d4247419ae8c1
zuroc <zsp007@gmail.com>
2011-10-26 02:55:44 +0800

    fix

--- model/invite_email.py
+++ model/invite_email.py
@@ -6,14 +6,17 @@ from zkit.import_msn.get_friend_list import get_friend_list as msn_friend_get
 #from zkit.google.ginvite import load_friend
 from user_mail import user_id_by_mail
 from json import dumps
+
+
 CID_MSN = 1
 CID_GOOGLE = 2
 CID_QQ = 3
+
 INVITE_CID2CN = {
-        CID_QQ:'QQ',
-        CID_GOOGLE:'Google',
-        CID_MSN:'MSN',
-        }
+    CID_QQ:'QQ',
+    CID_GOOGLE:'Google',
+    CID_MSN:'MSN',
+}
 
 GET_CID = {
         CID_MSN:msn_friend_get,
@@ -28,21 +31,21 @@ class InviteMessage(Model):
 
 def invite_message_new(user_id, email_list, txt):
     InviteMessage.raw_sql(
-        'insert into invite_message (email,user_id,txt) values (%s,%s,%s)', 
-        dumps(email_list), 
-        user_id, 
+        'insert into invite_message (email,user_id,txt) values (%s,%s,%s)',
+        dumps(email_list),
+        user_id,
         txt
     )
 
 def invite_email_new(user_id, cid, res):
     InviteEmail.raw_sql(
-        'update invite_email set cid = %s where user_id =%s and cid=%s ', 
+        'update invite_email set cid = %s where user_id =%s and cid=%s ',
         -cid, user_id, cid
     )
     for email, name in res.iteritems():
         InviteEmail.raw_sql(
-'insert into invite_email (user_id, cid, email, name, email_user_id) values (%s,%s,%s,%s,%s)', 
-user_id, cid,  email, name, user_id_by_mail(email)
+'insert into invite_email (user_id, cid, email, name, email_user_id) values (%s,%s,%s,%s,%s)',
+user_id, cid, email, name, user_id_by_mail(email)
         )
     return True
 
@@ -61,7 +64,7 @@ def get_invite_user_id_list_by_cid(user_id, cid):
     return InviteEmail.where('email_user_id>0').where(user_id=user_id, cid=cid).col_list(col='email_user_id')
 
 def get_email_by_cid(user_id, cid):
-    return InviteEmail.where(user_id=user_id,cid=cid).col_list(col="email")
+    return InviteEmail.where(user_id=user_id, cid=cid).col_list(col='email')
 
 if __name__ == '__main__':
     pass
