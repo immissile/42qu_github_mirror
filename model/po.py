c6a2aff7c7cf84c173be791662fe245e067c55d8
zuroc <zsp007@gmail.com>
2011-10-26 18:22:07 +0800

    fix

--- model/po.py
+++ model/po.py
@@ -431,6 +431,6 @@ def mc_flush_zsite_cid(zsite_id, cid):
 if __name__ == '__main__':
     from model.zsite_tag import tag_by_po_id, zsite_tag_new_by_tag_id
     from model.feed import feed_rm
-    for i in Po.where('user_id=%s'%10014854):
-        print feed_rm(i.id)
+    for i in Po.where('user_id=%s'%10124529):
+        po_rm(i.user_id, i.id)
 
