b65661ee3767cd071b9052b13d8778ab09a85fe3
zuroc <zsp007@gmail.com>
2011-10-27 05:11:51 +0800

    fix

--- ctrl/j/hero.py
+++ ctrl/j/hero.py
@@ -3,29 +3,28 @@ from zkit.bot_txt import txt_wrap_by
 from ctrl._urlmap.j import urlmap
 from _handler import JLoginBase
 from yajl import dumps
-from model.ico import ico_url_bind_with_default
-from model.career import career_bind
+from model.ico import ico_url_bind_with_default , ico_url_with_default
+from model.career import career_bind,career_current
 from model.zsite import Zsite
 from logging import info
 from model.zsite_url import id_by_url, url_or_id
 from model.follow import follow_count_by_to_id, follow_get
 from model.cid import CID_USER
 
-@urlmap('/j/hero')
+@urlmap('/j/hero/(\w+)')
 class HeroJson(JLoginBase):
-    def post(self):
-        url = self.get_argument('url', None)
-        id = id_by_url(txt_wrap_by('//','.',url))
-        id_list = []
-        id_list.append(id)
-        zsite_list = Zsite.mc_get_list(id_list)
-        ico_url_bind_with_default(zsite_list)
-        career_bind(zsite_list)
-        result = []
-        for i in zsite_list:
-            if follow_get(self.current_user_id, i.id):
-                word = "淡忘"
-            else:
-                word = "关注"
-            result = [i.name, ','.join(i.career), i.ico, i.link, i.id, word]
+    def get(self,id):
+        result = None
+        id = id_by_url(id)
+        zsite = Zsite.mc_get(id)
+        if zsite:
+            career = career_current(id) 
+            career = filter(bool, career)
+            current_user_id = self.current_user_id 
+            if current_user_id != id:
+                if follow_get(current_user_id, id):
+                    word = "淡忘"
+                else:
+                    word = "关注"
+                result = [zsite.name, ' , '.join(career), ico_url_with_default(id), zsite.link, zsite.id, word]
         return self.finish(dumps(result))
