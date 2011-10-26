6b961826270fb9f4907a8b9574b1db5abc3a971d
zuroc <zsp007@gmail.com>
2011-10-26 15:33:13 +0800

    fix

--- misc/crontab/invite_message.py
+++ misc/crontab/invite_message.py
@@ -7,27 +7,24 @@ from model.invite_email import InviteMessage, InviteEmail
 from model.mail import rendermail
 from model.zsite import Zsite
 from model.invite_email import INVITE_CID2CN
+
 @single_process
 def invite_message():
     pre_pos = 0 #kv_int.get(KV_INVITE_MESSAGE)
-    c = InviteMessage.raw_sql(
-            'select max(id) from invite_message'
-            )
+    c = InviteMessage.raw_sql( 'select max(id) from invite_message')
     pos = c.fetchone()[0]
 
     if pos > pre_pos:
-        for m in InviteMessage.where('id >%s and id<=%s',pre_pos,pos):
-            name,cids = InviteEmail.raw_sql('select name,cid from invite_email where email = %s',m.email).fetchone()
-            rendermail(
-                    '/mail/invite/invite_message.txt',
-                    m.email,name,
-                    invitor = Zsite.mc_get(m.uid),
-                    cid = INVITE_CID2CN[abs(cids)]
-                    )
+        for m in InviteMessage.where('id>%s and id<=%s', pre_pos, pos):
+            invitor = Zsite.mc_get(m.uid)
+            #name, cids = InviteEmail.raw_sql('select name,cid from invite_email where email = %s', m.email).fetchone()
+            #email = m.email
+            email = 'zsp007@gmail.com'
+            rendermail( '/mail/invite/invite_message.txt', email, name, invitor=invitor, cid=INVITE_CID2CN[abs(cids)])
 
 
         kv_int.set(KV_INVITE_MESSAGE, pos)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     invite_message()
