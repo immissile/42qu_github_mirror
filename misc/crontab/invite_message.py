fad7df8d9ebf96ba3d92ea2aba8d0ca9648950c2
zuroc <zsp007@gmail.com>
2011-10-26 15:41:20 +0800

    fix

--- misc/crontab/invite_message.py
+++ misc/crontab/invite_message.py
@@ -7,6 +7,7 @@ from model.invite_email import InviteMessage, InviteEmail
 from model.mail import rendermail
 from model.zsite import Zsite
 from model.invite_email import INVITE_CID2CN
+
 @single_process
 def invite_message():
     pre_pos = 0 #kv_int.get(KV_INVITE_MESSAGE)
@@ -16,14 +17,14 @@ def invite_message():
     pos = c.fetchone()[0]
 
     if pos > pre_pos:
-        for m in InviteMessage.where('id >%s and id<=%s',pre_pos,pos):
-            name,cids = InviteEmail.raw_sql('select name,cid from invite_email where email = %s',m.email).fetchone()
-            rendermail(
-                    '/mail/invite/invite_message.txt',
-                    m.email,name,
-                    invitor = Zsite.mc_get(m.uid),
-                    cid = INVITE_CID2CN[abs(cids)]
-                    )
+        for m in InviteMessage.where('id>%s and id<=%s',pre_pos,pos):
+            print m.email
+            #rendermail(
+            #        '/mail/invite/invite_message.txt',
+            #        m.email,name,
+            #        invitor = Zsite.mc_get(m.uid),
+            #        cid = INVITE_CID2CN[abs(cids)]
+            #        )
 
 
         kv_int.set(KV_INVITE_MESSAGE, pos)
