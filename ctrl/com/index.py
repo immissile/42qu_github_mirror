604a79c3b0929817376582345d77ba292e242df0
yupbank <yupbank@gmail.com>
2011-11-01 18:38:09 +0800

    ok

---
+++ ctrl/com/index.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from _handler import Base, LoginBase, XsrfGetBase
+from ctrl._urlmap.com import urlmap
+
+@urlmap('/')
+class Index(Base):
+    def get(self):
+        self.render()
+
+
+@urlmap('/new')
+class New(Base):
+    def get(self):
+        self.render()
