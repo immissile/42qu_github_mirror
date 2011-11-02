9b360acad138b9d7dcc16eb5475992271662b7b7
yupbank <yupbank@gmail.com>
2011-11-02 16:32:53 +0800

    ok

--- ctrl/com/index.py
+++ ctrl/com/index.py
@@ -4,6 +4,7 @@ from ctrl._urlmap.com import urlmap
 from ctrl._util.site import _SiteListBase as _ComListBase, FavBase, MyBase
 from model.cid import CID_COM
 from model.zsite_com import zsite_com_count, zsite_com_list
+from zkit.jsdict import JsDict
 
 class ComListBase(_ComListBase):
     template = '/ctrl/com/index/index.htm'
@@ -27,4 +28,22 @@ class Index(ComListBase, Base):
 @urlmap('/new')
 class New(Base):
     def get(self):
-        self.render()
+        self.render(errtip=JsDict())
+
+
+    def post(self):
+        files = self.request.files
+        pic_id = None
+
+        if 'pic' in files:
+            pic = files['pic'][0]['body']
+            pic = picopen(pic)
+            if pic:
+                pic_id = site_ico_new(current_user_id, pic)
+            else:
+                errtip.pic = '图片格式有误'
+        else:
+            pic_id = self.get_argument('pic_id', None)
+            if not pic_id:
+                errtip.pic = '请上传图片'
+
