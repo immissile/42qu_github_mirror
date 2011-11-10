cc71016152aedfa04f1f2e50584be7ee50516c69
zuroc <zsp007@gmail.com>
2011-11-10 13:07:05 +0800

    close

--- hg_close_branch.py
+++ hg_close_branch.py
@@ -4,10 +4,7 @@
 
 branches = """
 zuroc_20111108_com          7005:1fe04896e043
-zuroc_20111104_com          6987:cd71e64408f3
-realfex_20111101_job        6940:82cdb26bdf18 (inactive)
-yup_1031_site_note          6928:3816219f8044 (inactive)
-realfex_20111027_pop_hero   6872:5ad7a2956f08 (inactive)
+yup_20111101_com            7037:22dede514031 (inactive)
 """
 
 branches = branches.strip()
