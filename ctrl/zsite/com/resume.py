097296ef0284910aafe1f3b29e5ed934d6529ff6
realfex <i@realfex.com>
2011-11-15 22:40:40 +0800

    page

---
+++ ctrl/zsite/com/resume.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from ctrl.zsite._handler import ZsiteBase, LoginBase, XsrfGetBase
+from ctrl._urlmap.zsite import urlmap
+from _handler import AdminBase
+
+@urlmap('/resume/confirm')
+class ResumeConfirm(AdminBase):
+    def get(self):
+        return self.render()
+
+@urlmap('/resume/(\d+)')
+class Resume(AdminBase):
+    def get(self, id):
+        return self.render()
+
+
