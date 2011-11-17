21d9110f59405c621b550a3132389c58bcb6f8a1
zuroc <zsp007@gmail.com>
2011-11-17 18:09:49 +0800

    fix

--- model/zsite_member.py
+++ model/zsite_member.py
@@ -5,6 +5,7 @@ from model.zsite_list import zsite_list, zsite_list_new, STATE_DEL, STATE_ACTIVE
 from model.zsite import Zsite
 from model.cid import CID_ZSITE_LIST_MEMBER, CID_VERIFY_COM_MEMBER
 from model.verify import verify_new
+from user_mail import mail_by_user_id
 
 ZSITE_MEMBER_STATE_OWNER = STATE_OWNER    # 创始人 
 ZSITE_MEMBER_STATE_KERNEL = STATE_ADMIN   # 决策层
@@ -23,21 +24,6 @@ def zsite_member_new(zsite_id, member_id, cid=CID_ZSITE_LIST_MEMBER, state=ZSITE
         zsite_list_new(zsite_id, member_id, CID_ZSITE_LIST_MEMBER, state=state)
         return True
 
-def zsite_member_invite(zsite_id, member_id):
-    if zsite_member_new(zsite_id, member_id):
-
-        mail = "zsp007@gmail.com"
-
-        mq_rendermail(
-            '/mail/buzz/follow_new.htm', 
-            mail, name,
-            from_user=from_user,
-            format='html',
-            subject='%s ( %s ) 关注 你' % (
-                from_user.name,
-                ' , '.join(career),
-            )
-        )
 
 def zsite_member_rm(zsite_id, member_id):
     zsite_list_rm(zsite_id, member_id, cid=CID_ZSITE_LIST_MEMBER)
@@ -52,13 +38,43 @@ def zsite_member_can_admin(zsite_id, member_id):
     if id:
         return state >= ZSITE_MEMBER_STATE_ACTIVE
 
-def zsite_member_invite(zsite_id, member_id_list):
+def zsite_member_invite(
+    zsite, member_id_list, current_user
+):
     if type(member_id_list) in (str, int):
-        zsite_member_invite(zsite_id, member_id_list)
-    else:
-        for i in member_id_list:
-            zsite_member_invite(zsite_id, i)
+        member_id_list = [member_id_list,]
 
+    follower_list = [
+        i for i in
+        Zsite.mc_get_list(follow_id_list)
+        if i and i.cid == CID_USER
+    ]
+    member_id_list = map(int,member_id_list)
+
+    for i in member_id_list:
+        _zsite_member_invite(zsite, i, current_user)
+
+def _zsite_member_invite(zsite, member, current_user):
+    zsite_id = zsite.id
+    member_id =  member.id
+
+    if zsite_member_new(zsite_id, member_id):
+        mail = mail_by_user_id(member_id)
+        mail = "zsp007@gmail.com"
+
+        mq_rendermail(
+            '/mail/com/invite_member.htm', 
+            mail, 
+            member.name,
+            sender_name = current_user.name
+            format='html',
+            subject='%s 邀请您给 %s 未来的同事写几句话' % (
+                current_user.name, 
+                zsite.name
+            )
+            from_user=from_user,
+            com = zsite
+        )
 
 def zsite_member_invite_email_name_unit_title(zsite_id, name, unit, title):
     id, value = verify_new(member_id, CID_VERIFY_COM_MEMBER)
