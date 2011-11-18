87095e6ffc48ab11a48929333c91f573d692b34d
yupbank <yupbank@gmail.com>
2011-11-18 23:00:16 +0800

    ok

--- ctrl/zsite/com/bio.py
+++ ctrl/zsite/com/bio.py
@@ -3,8 +3,9 @@
 from ctrl.zsite._handler import ZsiteBase, LoginBase, XsrfGetBase
 from ctrl._urlmap.zsite import urlmap
 from _handler import AdminBase
-
-
+from model.com import com_pic_new, zsite_com_new
+from model.po_video import video_new
+from zkit.pic import picopen
 @urlmap('/bio/new')
 class BioNew(AdminBase):
     def get(self):
@@ -12,9 +13,33 @@ class BioNew(AdminBase):
         return self.render()
 
     def post(self):
-        print self.request.arguments
-        pics = self.request.files
-        print pics.keys()
-        self.get()
+        hope = self.get_argument('hope',None)
+        money = self.get_argument('money',None)
+        culture = self.get_argument('culture',None)
+        team = self.get_argument('team',None)
+        video = self.get_argument('video',None)
+        com_id = self.zsite.id
+        files = self.request.files
+        cover_id = None
+        
+        if files.get('cover'):
+            cover = files['cover'][0]['body']
+            if cover:
+                cover = picopen(cover)
+                print cover,'!!!'
+                if cover:
+                    cover_id = com_pic_new(com_id,cover)
+        
+        if files.get('pic'):
+            for pic in files['pic']:
+                if pic['body']:
+                    pic = picopen(pic['body'])
+                    if pic:
+                        com_pic_new(com_id,pic)
+        
+        if video:
+            video_new(com_id,video)
+        zsite_com_new(com_id,hope,money,culture,team,cover_id)
+        self.redirect('/')
 
 
--- model/com.py
+++ model/com.py
@@ -3,7 +3,10 @@
 from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM
 from job import JobType, JobPlace
 from zkit.attrcache import attrcache
-
+from cid import CID_COM_PIC
+from zkit.pic import pic_fit_width_cut_height_if_large
+from fs import fs_set_jpg, fs_url_jpg
+from pic import pic_new, pic_save
 
 JOB_ACTIVE = 5
 JOB_CLOSE = 4
@@ -15,6 +18,11 @@ JOBSALARY2CN = {
         2:'年薪'
         }
 
+
+
+class ZsiteCom(McModel):
+    pass
+
 class ComJob(McModel):
     @attrcache
     def job_department_list(self):
@@ -24,6 +32,23 @@ class ComJob(McModel):
     def com_job_needs(self):
         return ComJobNeeds.get(job_id=self.id)
     
+
+def com_pic_new(com_id,pic):
+    pic_id = pic_new(CID_COM_PIC,com_id)
+    pic_save(pic_id,pic)
+    p1 = pic_fit_width_cut_height_if_large(pic,357)
+    fs_set_jpg('357',pic_id,p1)
+    return pic_id
+
+def zsite_com_new(com_id,hope,money,culture,team,cover_id):
+    zsite_com = ZsiteCom.get_or_create(id=com_id)
+    zsite_com.hope=hope
+    zsite_com.money=money
+    zsite_com.culture = culture
+    zsite_com.cover_id = cover_id
+    zsite_com.team = team
+    zsite_com.save()
+
 def com_job_by_com_id(com_id):
     return ComJob.where(com_id=com_id)
 
