9110e24971b3aaec05868e3be622d82aca8468c3
zuroc <zsp007@gmail.com>
2011-11-18 22:20:17 +0800

    fix

--- ctrl/zsite/com/review.py
+++ ctrl/zsite/com/review.py
@@ -25,24 +25,3 @@ class Review(LoginBase):
         self.render()
 
 
-    def post(self):
-        zsite = self.zsite
-        current_user = self.current_user
-        zsite_id = self.zsite_id
-        current_user_id = self.current_user_id
-        txt = self.get_argument('txt', None)
-
-        if txt:
-            wall = wall_by_from_id_to_id(current_user_id, zsite_id)
-            if wall: 
-                reply_last =  wall.reply_last()
-            else:
-                reply_last = None
-
-            if reply_last:
-                reply_last.txt_set(txt)
-            else:
-                zsite = self.zsite
-                from model.reply import STATE_ACTIVE
-                zsite.reply_new(current_user,txt,STATE_ACTIVE)
-        self.get()
