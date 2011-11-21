2b6ba29844c095ce7e9d042c2ca93fd257322646
zuroc <zsp007@gmail.com>
2011-11-21 21:29:29 +0800

    fix

--- ctrl/zsite/com/review.py
+++ ctrl/zsite/com/review.py
@@ -3,6 +3,7 @@
 from ctrl.zsite._handler import ZsiteBase, LoginBase, XsrfGetBase
 from ctrl._urlmap.zsite import urlmap
 from _handler import AdminBase
+from model.zsite_member import zsite_member_can_admin
 
 @urlmap('/review/admin')
 class ReviewAdmin(AdminBase):
@@ -19,5 +20,14 @@ class Review(LoginBase):
     def get(self):
         zsite_id = self.zsite_id
         current_user_id = self.current_user_id
-        self.render()
+        can_admin = zsite_member_can_admin(zsite_id, current_user_id)
+        current_user_id = self.current_user_id
+        self.render(can_admin=can_admin)
 
+
+@urlmap('/review-(\d+)')
+class ReviewPage(ZsiteBase):
+    def get(self, n):
+        can_admin = zsite_member_can_admin(zsite_id, current_user_id)
+        self.render(can_admin=can_admin)
+        
