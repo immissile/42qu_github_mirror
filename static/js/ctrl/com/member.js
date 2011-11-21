d16064d8d3fd3e6f4a2dab3310f565123a5d7edb
realfex <i@realfex.com>
2011-11-21 17:15:43 +0800

    fix

--- static/js/ctrl/com/member.js
+++ static/js/ctrl/com/member.js
@@ -58,8 +58,8 @@ $(function(){
 
     $('.member_rm_a').click(function(){
         var self = $(this)
-        var url = $(this).attr('id').split('-')[0]=='in'?'/member/invite':'/member/rm'
-        var id = $(this).attr('id').split('-')[1]
+        var url = $(this).attr('id').split('_')[0]=='in'?'/member/invite':'/member/rm'
+        var id = $(this).attr('id').split('_')[1]
         self.parent().remove()
         $.postJSON(
             url,
