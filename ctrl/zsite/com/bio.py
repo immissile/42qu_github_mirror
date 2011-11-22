0b2cbe97fda9b8f29ac43a8047f1ff683825e228
yupbank <yupbank@gmail.com>
2011-11-22 16:58:31 +0800

    ok

--- ctrl/zsite/com/bio.py
+++ ctrl/zsite/com/bio.py
@@ -3,9 +3,14 @@
 from ctrl.zsite._handler import ZsiteBase, LoginBase, XsrfGetBase
 from ctrl._urlmap.zsite import urlmap
 from _handler import AdminBase
-from model.zsite_com import com_pic_new, zsite_com_new
+from model.zsite_com import com_pic_new, zsite_com_new, pid_by_com_id, ZsiteCom, zsite_com_place_new
 from model.po_video import video_new, video_filter
 from zkit.pic import picopen
+from zkit.errtip import Errtip
+from model.ico import ico96
+from model.motto import motto as _motto, motto_set
+from model.ico import site_ico_new, site_ico_bind
+
 @urlmap('/bio/new')
 class BioNew(AdminBase):
     def get(self):
@@ -46,3 +51,91 @@ class BioNew(AdminBase):
 class Guide(AdminBase):
     def get(self):
         self.render()
+
+
+@urlmap('/bio/admin')
+class BioAdmin(AdminBase):
+    def get(self):
+        errtip = Errtip()
+        name = self.zsite.name
+        com_id = self.zsite_id
+        pic_id = ico96.get(com_id)
+        motto = _motto.get(com_id)
+        pid_add = pid_by_com_id(com_id)
+        pid_add = [[i.pid,i.address] for i in pid_add]
+        zc = ZsiteCom.mc_get(com_id)
+        phone = None
+        if zc:
+            phone = zc.phone
+        
+        
+        self.render(
+                    '/ctrl/com/index/com_new.htm',
+                    errtip=errtip,
+                    motto = motto,
+                    pic_id = pic_id,
+                    name = name,
+                    phone = phone,
+                    pid_add = pid_add,
+                    edit=True)
+    
+        
+    def post(self):
+        errtip = Errtip()
+        current_user = self.current_user
+        current_user_id = current_user.id
+        name = self.get_argument('name', None)
+        motto = self.get_argument('motto', None)
+        pid = self.get_arguments('pid', None)
+        address = self.get_arguments('address',None)
+        phone = self.get_argument('phone',None)        
+        pid_add = zip(pid,address)
+        
+        if not name:
+            errtip.name = '请输入名称'
+
+        if not motto:
+            errtip.motto = '请编写签名'
+        
+
+
+        files = self.request.files
+        pic_id = None
+
+        if 'pic' in files:
+            pic = files['pic'][0]['body']
+            pic = picopen(pic)
+            if pic:
+                pic_id = site_ico_new(current_user_id, pic)
+            else:
+                errtip.pic = '图片格式有误'
+        else:
+            pic_id = self.get_argument('pic_id', None)
+            if not pic_id:
+                errtip.pic = '请上传图片'
+        
+
+        if not errtip:
+            current_user_id = self.current_user_id
+            com_id = self.zsite_id
+            zsite_com_new(com_id,phone=phone)
+            site_ico_bind(current_user_id, pic_id, com_id)
+            motto_set(com_id, motto)
+            if pid_add:
+                for pa in pid_add:
+                    zsite_com_place_new(com_id,int(pa[0]),pa[1])
+            else:
+                pid_add = self.get_argument('pid_add',None)
+            return self.redirect('/')
+        
+        
+        return self.render(
+            errtip=errtip,
+            name=name,
+            motto=motto,
+            #txt=txt,
+            phone = phone,
+            pic_id=pic_id,
+            pid_add = pid_add
+        )
+
