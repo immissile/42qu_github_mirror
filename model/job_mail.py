019d1db87eea6f85bbfe3ad2bc5e964838869cf6
zuroc <zsp007@gmail.com>
2011-11-24 14:26:14 +0800

    f

--- model/job_mail.py
+++ model/job_mail.py
@@ -17,6 +17,8 @@ mc_job_mail_if_exist = McCache("JobMailIfExist:%s")
 def job_mail_new(zsite_id, mail, department_id=0, state=JOB_MAIL_STATE_VERIFY):
     mail = mail.strip().lower()
     if mail:
+        if mail == job_mail_by_com_id(id, department_id):
+            return
         jm = JobMail.get_or_create(zsite_id=zsite_id,department_id=department_id)
         jm.mail = mail
         jm.department_id = department_id
@@ -27,29 +29,30 @@ def job_mail_new(zsite_id, mail, department_id=0, state=JOB_MAIL_STATE_VERIFY):
 
     return jm
 
-def mc_flush(zsite_id):
-    mc_job_mail_by_com_id.delete(zsite_id)
-    mc_job_mail_if_exist.delete(zsite_id)
+def mc_flush(zsite_id, department_id):
+    key = "%s_%s"%(zsite_id,department_id)
+    mc_job_mail_by_com_id.delete(key)
+    mc_job_mail_if_exist.delete(department_id)
 
-@mc_job_mail_by_com_id("{id}")
-def job_mail_by_com_id(id):
-    jm = JobMail.get(zsite_id=id,department_id=0)
+@mc_job_mail_by_com_id("{id}_{department_id}")
+def job_mail_by_com_id(id, department_id):
+    jm = JobMail.get(zsite_id=id,department_id=department_id)
     if jm and jm.state >= JOB_MAIL_STATE_VERIFIED:
         return jm.mail
     return ''
 
 
-@mc_job_mail_if_exist("{id}")
-def job_mail_if_exist(id):
-    jm = JobMail.get(zsite_id=id,department_id=0)
+@mc_job_mail_if_exist("{id}_{department_id}")
+def job_mail_if_exist(id,department_id=0):
+    jm = JobMail.get(zsite_id=id,department_id=department_id)
     if jm:
         return jm.mail
     return ''
 
-def job_mail_verifyed(id):
-    jm = JobMail.get(zsite_id=id,department_id=0)
+def job_mail_verifyed(id,department_id=0):
+    jm = JobMail.get(zsite_id=id,department_id)
     if jm:
         jm.state = JOB_MAIL_STATE_VERIFIED
         jm.save()
-        mc_flush(id)
+        mc_flush(id,department_id)
 
