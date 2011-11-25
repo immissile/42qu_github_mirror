32084ebb2cfae08de747ab145f82ca503fadeaca
zuroc <zsp007@gmail.com>
2011-11-25 15:54:23 +0800

    close

--- hg_close_branch.py
+++ hg_close_branch.py
@@ -3,13 +3,19 @@
 
 
 branches = """
+
+zuroc_201111118_com         7525:824ba9ed30ce (inactive)
+zuroc_201111123_com2        7510:87552bf8298e (inactive)
+zuroc_201111123_com         7498:030a2d387eaa (inactive)
+zuroc_201111110_com         7251:623e72ee290b (inactive)
+
 """
 
 branches = branches.strip()
 
-for i in branches.split("\n"):
-    j = i.split(" ",1)[0]
-    print "hg update %s ; hg commit --close-branch -m close;"%j
+for i in branches.split('\n'):
+    j = i.split(' ', 1)[0]
+    print 'hg update %s ; hg commit --close-branch -m close;'%j
 
-print "hg update default"
+print 'hg update default'
 
