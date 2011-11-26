c9aa4c34e91a85cc53763e028395aed5a4b17840
zuroc <zsp007@gmail.com>
2011-11-26 16:08:40 +0800

    fix

--- static/js/ctrl/com/review.js
+++ static/js/ctrl/com/review.js
@@ -7,11 +7,10 @@ $(function(){
         $.postJSON('/j/review/show/'+(this.checked?"new":"rm")+"/"+id)
     })
 
-auto_add('member_mail','<div class="line"><input name="mail" class="member_mail" placeholder="邮箱"><input name="name" class="member_name" placeholder="姓名"></div>','mail_block','','blur')
     $('.invite_link').mouseover(function(){$(this).select()})
 
-auto_add('admin_member_mail','<div class="line"><input class="admin_member_mail" name="mail" placeholder="邮箱"><input  class="admin_member_name" name="name" placeholder="姓名"></div>','mate_mail','','blur')
+auto_add('admin_member_mail','<div class="line"><input class="admin_member_mail" name="mail" placeholder="邮箱"><input  class="admin_member_name" name="name" placeholder="姓名"></div>','mate_mail','','focus')
 
-auto_add('admin_friend_mail','<div class="line"><input class="admin_friend_mail" name="mail" placeholder="邮箱"><input  class="admin_friend_name" name="name" placeholder="姓名"></div>','friend_mail','','blur')
+auto_add('admin_friend_mail','<div class="line"><input class="admin_friend_mail" name="mail" placeholder="邮箱"><input  class="admin_friend_name" name="name" placeholder="姓名"></div>','friend_mail','','focus')
 
 })
