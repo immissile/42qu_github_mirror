287d48e893212a0268ec75219a29c51b4f207389
realfex <i@realfex.com>
2011-11-30 15:04:16 +0800

    new_reg

--- ctrl/me/_url.py
+++ ctrl/me/_url.py
@@ -2,3 +2,4 @@ import i
 import money
 import notice
 import newbie
+import guide
---
+++ ctrl/me/guide.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from ctrl._urlmap.me import urlmap
+from ctrl.me.i import UserInfoEdit, CareerEdit , PicEdit, LinkEdit
+from _handler import LoginBase , XsrfGetBase
+from model.zsite import Zsite
+from zkit.errtip import Errtip
+from model.zsite_show import SHOW_LIST
+from model.user_auth import user_password_verify, UserPassword, user_password_new
+from model.user_info import user_info_new
+
+
+@urlmap('/me/guide')
+class Guide(LoginBase):
+    def get(self, mail=''):
+        id_list = SHOW_LIST
+        zsite_list = filter(bool, Zsite.mc_get_list(id_list))
+        self.render(
+            mail=mail,
+            sex=0,
+            password='',
+            errtip=Errtip(),
+            zsite_list=zsite_list,
+        )
+
