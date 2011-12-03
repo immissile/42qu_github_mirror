c892d596e529c43455a487846c8094029ff62563
zuroc <zsp007@gmail.com>
2011-12-03 17:17:33 +0800

    fix

---
+++ misc/crontab/ppt_upload.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import _env
+
+from model.ppt import Ppt, ppt_file
+from zkit.single_process import single_process
+
+
+@single_process
+def main():
+    for p in Ppt.where(state=0):
+        p.upload()
+
+    for p in Ppt.where(state=1):
+        p.publish()
+
+#        zc = ZsiteCom.mc_get(p.com_id)
+#        video,video_cid = video_filter(url)
+#        video_id = video_new(p.com_id,video)
+#        zc.video_cid = video_cid 
+#        zc.video_id = video_id
+#        zc.save()
+#        p.delete()
+
+if __name__ == "__main__":
+    main()
+
+
--- model/ppt.py
+++ model/ppt.py
@@ -2,21 +2,57 @@
 # -*- coding: utf-8 -*-
 from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM
 from model.fs import fs_set, fs_path, fs_url, fs_file
+from zkit.sildeshare import slideshare_upload, slideshare_url
+from config import SLIDESHARE_KEY, SLIDESHARE_SECRET , SLIDESHARE_USERNAME , SLIDESHARE_PASSWORD
+from model.com import com
+from model.zsite_com import ZsiteCom
+from model.po_video import VIDEO_CID_SLIDESHARE
 
 class Ppt(Model):
-    pass
+    def upload(self):
+        id = self.id
+        sid = slideshare_upload(
+            SLIDESHARE_KEY,
+            SLIDESHARE_SECRET,
+            SLIDESHARE_USERNAME,
+            SLIDESHARE_PASSWORD,
+            ppt_file(id)
+        )
+        self.state = 1
+        self.slideshare_id = sid
+        self.save()
+
+    def publish(self):
+        state , swf = slideshare_url(
+            SLIDESHARE_KEY, 
+            SLIDESHARE_SECRET,
+            p.slideshare_id
+        )
+        if state >= 2:
+            self.state = state
+            self.save()
+            if state == 2:
+                com_id = self.com_id
+                video_id = video_new(com_id,swf)
+                zc = ZsiteCom.mc_get(com_id)
+                zc.video_cid = VIDEO_CID_SLIDESHARE
+                zc.video_id = video_id
+                zc.save()
+ 
+        return sid 
 
 def ppt_new(com_id, ppt):
     p = Ppt(com_id=com_id)
     p.save()
-    fs_set("ppt",p.id,"ppt",ppt)
+    fs_set('ppt', p.id, 'ppt', ppt)
     return p.id
 
 def ppt_file(id):
-    return fs_file("ppt", id, "ppt")
+    return fs_file('ppt', id, 'ppt')
 
-if __name__ ==  "__main__":
-    id = ppt_new(1,"12345")
+
+if __name__ == '__main__':
+    id = ppt_new(1, '12345')
     print ppt_file(id)
 
 
