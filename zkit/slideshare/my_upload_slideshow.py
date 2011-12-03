40494c5b558c04e55e3b0a43fcb718c9c33cf6c1
zuroc <zsp007@gmail.com>
2011-12-03 16:11:45 +0800

    fix

--- zkit/slideshare/my_upload_slideshow.py
+++ zkit/slideshare/my_upload_slideshow.py
@@ -1,6 +1,8 @@
 from pyslideshare import pyslideshare
 from os.path import basename
 
+# Slideshow status : 0 if queued for conversion, 1 if converting , 2 if converted , 3 if conversion failed
+
 def slideshare_upload(api_key, secret_key, username, password, filename, title=None):
     if title is None:
         title = basename(filename).rsplit('.', 1)[0]
@@ -14,7 +16,7 @@ def slideshare_upload(api_key, secret_key, username, password, filename, title=N
     )
     json = obj.upload_slideshow(username=username, password=password, slideshow_title=title, slideshow_srcfile=filename)
     slideshow_id = json.SlideShowUploaded.SlideShowID
-    print json
+
     return slideshow_id
 
 def slideshare_url(api_key, secret_key, id):
@@ -26,14 +28,14 @@ def slideshare_url(api_key, secret_key, id):
         verbose=False
     )
     json = obj.get_slideshow(slideshow_id=id)
-    return json
+    return state, json
 
 
 if __name__ == '__main__':
-    api_key = 'WcxW55e6'
-    secret_key = 'L7bFrKKX'
-    username = 'zuroc'
-    password = '198662'
-    showId = slideshare_upload(api_key, secret_key, username, password, 'test.ppt')
-    print showId
+    api_key     = 'WcxW55e6'
+    secret_key  = 'L7bFrKKX'
+    username    = 'zuroc'
+    password    = '198662'
+    id = slideshare_upload(api_key, secret_key, username, password, 'test.ppt')
+    print slideshare_url(api_key, secret_key, id)
     print slideshare_url(api_key, secret_key, 10442155)
