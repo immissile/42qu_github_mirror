52343bb8edfbd9f28da8516cfdf4178d1769d1cc
zuroc <zsp007@gmail.com>
2011-12-05 14:37:09 +0800

    fix

--- misc/crontab/oauth_site_sync.py
+++ misc/crontab/oauth_site_sync.py
@@ -8,13 +8,13 @@ from zkit.single_process import single_process
 from model.zsite import Zsite
 from model.kv_misc import KV_SYNC_SITE_PO_BY_ZSITE_ID, kv_int_call
 from zweb.orm import ormiter
-from config import SYNC_ZSITE
+from config import ZSITE_BIND_FOR_SYNC
 
 def _sync_site_po(begin_id):
     ss = SiteSync.where('id>%s', begin_id).order_by('id')[0]
     if ss:
         begin_id = ss.id
-        zsite = Zsite.mc_get(SYNC_ZSITE)
+        zsite = Zsite.mc_get(ZSITE_BIND_FOR_SYNC)
         if zsite:
             po = Po.mc_get(ss.po_id)
             sync_site_po(po, zsite)
