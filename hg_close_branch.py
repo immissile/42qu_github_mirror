3668b8dab4096f1ced4b37da8e1d8100ad3848a1
zuroc <zsp007@gmail.com>
2011-12-09 12:25:08 +0800

    close

--- hg_close_branch.py
+++ hg_close_branch.py
@@ -3,6 +3,11 @@
 
 
 branches = """
+
+yup_20111202_addppt         7873:335d081d14ff (inactive)
+zuroc_20111201_login2       7866:faf955228806 (inactive)
+yup_20111130_change_mail    7799:c153a1b7e52e (inactive)
+yup_20111125_product_show   7706:3bd253e2ca0b (inactive)
 """
 
 branches = branches.strip()
