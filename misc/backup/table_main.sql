1738400758a81ae1fbe7716c13ca55a9d90db2ea
zuroc <zsp007@gmail.com>
2011-12-10 11:50:47 +0800

    fix

--- misc/backup/table_main.sql
+++ misc/backup/table_main.sql
@@ -827,6 +827,15 @@ CREATE TABLE `rate` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=binary;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `rec_rep`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rec_rep` (
+  `id` int(11) NOT NULL,
+  `reply_id` int(11) NOT NULL,
+  KEY `new_index` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=binary;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `reply`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
