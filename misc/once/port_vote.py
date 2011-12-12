570be66d8c9d9048fd96cd84a2400a30f8e2cc64
wooparadog <guohaochuan@gmail.com>
2011-12-12 14:22:56 +0800

    f

--- misc/once/port_vote.py
+++ misc/once/port_vote.py
@@ -8,36 +8,24 @@ from zweb.orm import ormiter
 from model.po_recommend import mc_po_recommend_id_by_rid_user_id,RecRep
 from model.feed import Feed
 
-def po_recommend_new(rid, user_id, name, vote_id): 
-    
-    recommend = po_new(
-        CID_REC,
-        user_id,
-        name,
-        state=STATE_ACTIVE,
-        rid=rid,
-        id=vote_id
-    )
 
-    mc_po_recommend_id_by_rid_user_id.set(
-        '%s_%s'%(rid, user_id),
-        recommend.id
-    )
-
-    if reply_id:
-        rr = RecRep(
-            id=recommend.id,
-            reply_id=reply_id
-        )
-        rr.save()
-
-    
-
-    return recommend
 
 def main():
     for vote in ormiter(Feed,'rid !=0'):
-        po_recommend_new(vote.rid,vote.zsite_id,'',vote.id)
+        recommend = po_new(
+            CID_REC,
+            vote.zsite_id,
+            '',
+            state=STATE_ACTIVE,
+            rid=vote.rid,
+            id=vote.id
+        )
+        mc_po_recommend_id_by_rid_user_id.set(
+            '%s_%s'%(vote.rid, vote.zsite_id),
+            recommend.id
+        )
+        vote.cid  =CID_REC
+        vote.save()
 
 if __name__ == '__main__':
     main()
