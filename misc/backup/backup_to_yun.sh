8e0090f32cdc431fb344e61f968abd789a038ea5
zuroc <zsp007@gmail.com>
2011-12-14 01:24:50 +0800

    fix

--- misc/backup/backup_to_yun.sh
+++ misc/backup/backup_to_yun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUP_PATH=work@vm-192-168-16-114.shengyun.grandcloud.cn:/mnt/42qu/backup/
+BACKUP_PATH=work@vps.42qu.me:/mnt/42qu/backup/
 
 rsync -avl  -e ssh /mnt/zpage_db/mysql $BACKUP_PATH 
 rsync -avl  -e ssh /mnt/zpage $BACKUP_PATH 
