32bf264c373e03655a4d9df513a46e753924d5a9
zuroc <zsp007@gmail.com>
2011-12-14 21:33:02 +0800

    fix:w

--- model/user_school.py
+++ model/user_school.py
@@ -4,17 +4,17 @@ from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM, Mc
 from zkit.job import JOBKIND2CN
 from zsite_com import pid_by_com_id
 from zkit.attrcache import attrcache
-
+from zkit.school_university import SCHOOL_UNIVERSITY
 
 class UserSchool(McModel):
     pass
 
 def user_school_new(user_id, school_id, school_year, school_degree, school_department):
     school_id = int(school_id)
-    school_year = int(school_year)
-    school_degree = int(school_degree)
-    school_department = int(school_department)
-    if school_id:
+    if school_id and school_id in SCHOOL_UNIVERSITY and user_id:
+        school_year = int(school_year)
+        school_degree = int(school_degree)
+        school_department = int(school_department)
         u = UserSchool(
             user_id=user_id,
             school_id=school_id,
