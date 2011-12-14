e9a12e0ab384419ab05ebbf522e1ca35e03596f9
zuroc <zsp007@gmail.com>
2011-12-15 00:49:48 +0800

    fix:w

--- model/user_school.py
+++ model/user_school.py
@@ -4,11 +4,16 @@ from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM, Mc
 from zkit.job import JOBKIND2CN
 from zsite_com import pid_by_com_id
 from zkit.attrcache import attrcache
-from zkit.school_university import SCHOOL_UNIVERSITY, SCHOOL_UNIVERSITY_DEPARTMENT_ID2NAME, SCHOOL_UNIVERSITY_DEPARTMENT_ID
+from zkit.school_university import SCHOOL_UNIVERSITY, SCHOOL_UNIVERSITY_DEPARTMENT_ID2NAME, SCHOOL_UNIVERSITY_DEPARTMENT_ID, SCHOOL_DEGREE
+
+mc_user_school_id_list = McCacheA("UserSchoolIdList:%s")
 
 class UserSchool(McModel):
     pass
 
+@mc_user_school_id_list("{user_id}")
+def user_school_id_list(user_id):
+    return UserSchool.where(user_id=user_id).order_by("id desc").col_list()
 
 def user_school_new(user_id, school_id, school_year, school_degree, school_department):
     school_id = int(school_id or 0)
@@ -16,6 +21,14 @@ def user_school_new(user_id, school_id, school_year, school_degree, school_depar
         school_year = int(school_year or 0)
         school_degree = int(school_degree or 0)
         school_department = int(school_department or 0)
+
+        if school_year and school_year < 1000:
+            return
+        if school_degree and school_degree not in SCHOOL_DEGREE:
+            return
+        if school_department and school_department not in SCHOOL_UNIVERSITY_DEPARTMENT_ID2NAME:
+            return
+    
         u = UserSchool(
             user_id=user_id,
             school_id=school_id,
@@ -24,8 +37,9 @@ def user_school_new(user_id, school_id, school_year, school_degree, school_depar
             school_department=school_department
         )
         u.save()
-
+        mc_user_school_id_list.delete(user_id)
         return u
 
 if __name__ == '__main__':
     pass
+    print user_school_id_list(10000000)
