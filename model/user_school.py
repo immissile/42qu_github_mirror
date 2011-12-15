ee6e42fdf1aa50cc2071e207ca92fcea87106a83
zuroc <zsp007@gmail.com>
2011-12-15 22:35:13 +0800

    fix

--- model/user_school.py
+++ model/user_school.py
@@ -30,6 +30,7 @@ def user_school_json(user_id):
     ])
     
 
+
 def user_school_new(user_id, school_id, school_year, school_degree, school_department,txt=''):
     school_id = int(school_id or 0)
     if school_id and school_id in SCHOOL_UNIVERSITY and user_id:
