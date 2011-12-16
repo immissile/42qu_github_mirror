8bc72fcf24f30af8d3c252eada6feebc15d9fccd
zuroc <zsp007@gmail.com>
2011-12-16 13:09:52 +0800

    fix

--- model/user_school.py
+++ model/user_school.py
@@ -7,31 +7,40 @@ from zkit.attrcache import attrcache
 from zkit.school_university import SCHOOL_UNIVERSITY, SCHOOL_UNIVERSITY_DEPARTMENT_ID2NAME, SCHOOL_UNIVERSITY_DEPARTMENT_ID, SCHOOL_DEGREE
 from json import dumps
 
-mc_user_school_id_list = McCacheA("UserSchoolIdList:%s")
+mc_user_school_id_list = McCacheA('UserSchoolIdList:%s')
+mc_user_school_tuple = McCache('UserSchoolTuple:%s')
 
 class UserSchool(McModel):
     pass
 
-@mc_user_school_id_list("{user_id}")
+def mc_flush(user_id):
+    mc_user_school_id_list.delete(user_id)
+    mc_user_school_tuple.delete(user_id)
+
+@mc_user_school_id_list('{user_id}')
 def user_school_id_list(user_id):
-    return UserSchool.where(user_id=user_id).order_by("id desc").col_list()
+    return UserSchool.where(user_id=user_id).order_by('id desc').col_list()
 
 def user_school_list(user_id):
     return UserSchool.mc_get_list(user_school_id_list(user_id))
 
 def user_school_rm(id, user_id):
     UserSchool.where(id=id, user_id=user_id).delete()
-    mc_user_school_id_list.delete(user_id) 
+    mc_flush(user_id)
 
-def user_school_json(user_id):
-    return dumps([
+@mc_user_school_tuple('{user_id}')
+def user_school_tuple(user_id):
+    return (
         (i.id, i.school_id, i.school_year, i.school_degree, i.school_department, i.txt)
         for i in user_school_list(user_id)
-    ])
-    
+    )
 
 
-def user_school_new(user_id, school_id, school_year, school_degree, school_department,txt='', id=None):
+def user_school_json(user_id):
+    return dumps(user_school_tuple(user_id))
+
+
+def user_school_new(user_id, school_id, school_year, school_degree, school_department, txt='', id=None):
     school_id = int(school_id or 0)
     if school_id and school_id in SCHOOL_UNIVERSITY and user_id:
         school_year = int(school_year or 0)
@@ -50,15 +59,14 @@ def user_school_new(user_id, school_id, school_year, school_degree, school_depar
         else:
             u = UserSchool()
 
-        u.user_id=user_id
-        u.school_id=school_id
-        u.school_year=school_year
-        u.school_degree=school_degree
-        u.school_department=school_department
-        u.txt=txt
+        u.user_id = user_id
+        u.school_id = school_id
+        u.school_year = school_year
+        u.school_degree = school_degree
+        u.school_department = school_department
+        u.txt = txt
         u.save()
-        print id
-        mc_user_school_id_list.delete(user_id)
+        mc_flush(user_id)
         return u
 
 if __name__ == '__main__':
