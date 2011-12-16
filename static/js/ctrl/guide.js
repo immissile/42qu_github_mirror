a4c115c8a958245fc71f4d7639738a4567c875ac
zuroc <zsp007@gmail.com>
2011-12-16 14:41:17 +0800

    fix

--- static/js/ctrl/guide.js
+++ static/js/ctrl/guide.js
@@ -47,21 +47,34 @@ $(function(){
     function job_now_label(elem){
         var id = uuid();
         elem.find('.job_now').attr('id',id).change(function(){
-            var last = elem.find('.job_end'),
-                job_now = elem.find('input[name=job_now]')
+            var last = elem.find('.job_end');
             if(this.checked){
-                job_now.val(1)
                 last.hide()
+                last.find("input[name=job_end]").val(0)
             }else{
                 last.show()
-                job_now.val(0)
             }
         })
         elem.find('.label_now').attr('for',id)
         return elem
     }
 
+    $("#career_job").delegate('.unit:last,.title:last', "change", function(){
+        var val = $.trim(this.value);
+        if(val&&val.length&&val!=this.placeholder){
+            career();
+        }
+    })
+
     var job_block = job_now_label($('.job_block'));
+
+    function select_workday(name){
+        var date=new Date(), year=date.getFullYear();
+        select_span($(".job_"+name), "job_"+name, 0, year, year-99)
+    }
+    select_workday("begin")
+    select_workday("end")
+
     var job_html = job_block.html()
     job_block.find('.job_now').attr('checked',true)
     job_block.find('.job_end').hide()
