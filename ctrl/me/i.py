662e7814188a8ae1ab9ada2398d0ff98deb86656
zuroc <zsp007@gmail.com>
2011-12-16 15:59:23 +0800

    fix

--- ctrl/me/i.py
+++ ctrl/me/i.py
@@ -15,7 +15,7 @@ from model.zsite import zsite_name_edit, user_can_reply, ZSITE_STATE_VERIFY, ZSI
 from model.user_auth import user_password_new, user_password_verify
 from cgi import escape
 from urlparse import parse_qs, urlparse
-from model.zsite_link import OAUTH2NAME_DICT, link_list_save, link_id_name_by_zsite_id, link_id_cid, link_by_id, OAUTH_LINK_DEFAULT, link_list_cid_by_zsite_id
+from model.zsite_link import OAUTH2NAME_DICT, link_list_save, link_id_name_by_zsite_id, link_id_cid, link_by_id, OAUTH_LINK_DEFAULT
 from model.oauth2 import oauth_access_token_by_user_id, oauth_token_rm_if_can, OauthClient
 from config import SITE_URL, SITE_DOMAIN
 from model.oauth import OAUTH_DOUBAN, OAUTH_SINA, OAUTH_TWITTER, OAUTH_QQ, oauth_by_zsite_id, oauth_rm_by_oauth_id, OAUTH_SYNC_TXT, linkify
@@ -49,11 +49,7 @@ class LinkEdit(LoginBase):
 
     def get(self):
         zsite_id = self.zsite_id
-        link_list, link_cid = link_list_cid_by_zsite_id(zsite_id)
-        return self.render(
-            link_list=link_list,
-            link_cid=link_cid
-        )
+        return self.render()
 
     def save(self):
         zsite_id = self.zsite_id
