e3fb8e2d6457cd2221834cecdf11b03094d862eb
zuroc <zsp007@gmail.com>
2011-12-21 13:30:04 +0800

    fix

--- ctrl/hero/invite.py
+++ ctrl/hero/invite.py
@@ -3,13 +3,13 @@ from _handler import Base, LoginBase, XsrfGetBase
 
 from model.invite_email import invite_user_id_list_by_cid, CID_MSN, CID_QQ, invite_invite_email_list_by_cid, invite_message_new
 
-@urlmap('/i/invite')
+@urlmap('/invite')
 class Invite(LoginBase):
     def get(self):
         self.render()
 
 
-@urlmap('/i/invite/login/(\d+)')
+@urlmap('/invite/login/(\d+)')
 class InviteLogin(LoginBase):
     def get(self, cid=CID_MSN):
         if cid != CID_QQ:
@@ -17,8 +17,8 @@ class InviteLogin(LoginBase):
         else:
             self.render('/ctrl/me/i/import_qq.htm')
 
-@urlmap('/i/invite/show')
-@urlmap('/i/invite/show/(\d+)')
+@urlmap('/invite/show')
+@urlmap('/invite/show/(\d+)')
 class InviteShow(LoginBase):
     def get(self, cid=CID_MSN):
         uid = self.current_user_id
@@ -26,7 +26,7 @@ class InviteShow(LoginBase):
         follow_id_list = follow_id_list_by_from_id(uid)
         user_id_list = set(user_id_list) - set(follow_id_list)
         if not user_id_list:
-            return self.redirect('/i/invite/email/%s'%cid)
+            return self.redirect('/invite/email/%s'%cid)
         self.render(cid=cid, user_id_list=user_id_list)
 
     def post(self, cid=CID_MSN):
@@ -38,10 +38,10 @@ class InviteShow(LoginBase):
                 if i.id != user_id:
                     follow_new(user_id, i.id)
 
-        self.redirect('/i/invite/email/%s'%cid)
+        self.redirect('/invite/email/%s'%cid)
 
-@urlmap('/i/invite/email')
-@urlmap('/i/invite/email/(\d+)')
+@urlmap('/invite/email')
+@urlmap('/invite/email/(\d+)')
 class InviteEmail(LoginBase):
     def get(self, cid=CID_MSN):
         emails = invite_invite_email_list_by_cid(self.current_user_id, cid)
