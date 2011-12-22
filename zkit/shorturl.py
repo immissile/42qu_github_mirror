4b97ca3a87e9def718f71852fc0c2120f4620e75
zuroc <zsp007@gmail.com>
2011-12-22 22:25:36 +0800

    fix

--- zkit/shorturl.py
+++ zkit/shorturl.py
@@ -9,35 +9,11 @@ import traceback
 import time
 import sys
 from urllib import  urlencode
-
+from urlfetch import urlfetch
 
 SINA_API_KEY = '3152496704'
 
 SINA_SHORT_URL = 'http://api.t.sina.com.cn/short_url/shorten.json?source=%s&url_long=%%s'%SINA_API_KEY
-
-def retry(func):
-    def _(*args, **kwargs):
-        tries = 2
-        while tries:
-            try:
-                return func(*args, **kwargs)
-            except urllib2.HTTPError, e:
-                if e.getcode() == 404:
-                    return
-            except :
-                time.sleep(0.1)
-                tries -= 1
-                sys.stdout.flush()
-                traceback.print_exc()
-        return func(*args, **kwargs)
-    return _
-
-
-@retry
-def urlfetch(url, data=None):
-    r = urlopen(url, data, timeout=30)
-    c = r.read()
-    return c
 
 def t_cn(url):
     url = urllib.quote(url)
