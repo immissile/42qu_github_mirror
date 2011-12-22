e50abad51ca235646eb90c113c5cddd2caac5863
wooparadog <guohaochuan@gmail.com>
2011-12-22 17:22:17 +0800

    f

---
+++ misc/crontab/mail_mq_fail.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import _env
+import datetime
+from model.kv_misc import kv_int_call, KV_EVENT_STATE
+from model._db import cursor_by_table
+from model.mail import sendmail
+import time
+def event_begin_end(begin):
+    pass
+    
+def main():
+    cursor = cursor_by_table('failed_mq')
+    now  = datetime.datetime.now().strftime('%Y%m%d')
+    cursor.execute('select id,body,exc,func,time from failed_mq where time>%s'%str(int(now)-1))
+
+    out = ''
+    for id,body,exc,func,ctime in cursor.fetchall():
+        out+='---------Traceback----------\n'
+        out+=exc
+        out+='\n--------function----------\n'
+        out+=func
+        out+='\n-----------time-----------\n'
+        out+=ctime
+        out+='\n******************************\n'
+    
+    sendmail(now,out,'guohaochuan@gmail.com',name='42qu')
+
+if __name__ == '__main__':
+    main()
