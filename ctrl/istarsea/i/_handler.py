6aa1b6abd87016b197270b43551de816b0c35bb6
zuroc <zsp007@gmail.com>
2011-12-24 04:34:59 +0800

    fix

---
+++ ctrl/istarsea/i/_handler.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from config import render
+import model._db
+from ctrl.zsite._handler import LoginBase as _LoginBase, XsrfGetBase
+
+class LoginBase(_LoginBase):
+    def prepare(self):
+        super(LoginBase, self).prepare()
+        if not self._finished and self.zsite_id != self.current_user_id:
+            current_user_link = self.current_user.link
+            path = self.request.path
+            link = '%s%s'%(current_user_link, path)
+            return self.redirect(link)
---
+++ ctrl/istarsea/i/guide.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from ctrl._urlmap.me import urlmap
+from config import SITE_URL
+from _handler import LoginBase , XsrfGetBase
+from model.zsite import Zsite
+from zkit.errtip import Errtip
+from model.zsite_show import SHOW_LIST
+from model.user_auth import user_password_verify, UserPassword, user_password_new
+from model.user_info import user_info_new
+from ctrl.me.i import PicEdit
+
+@urlmap('/i/guide')
+class Guide(LoginBase):
+    def get(self, mail=''):
+        id_list = SHOW_LIST
+        zsite_list = filter(bool, Zsite.mc_get_list(id_list))
+        self.render(
+            mail=mail,
+            sex=0,
+            password='',
+            errtip=Errtip(),
+            zsite_list=zsite_list,
+        )
+
+
+@urlmap('/i/guide/pic')
+class Pic(PicEdit):
+    def post(self):
+        error_pic = self.save()
+        if error_pic is None:
+            if self.get_argument('pos', ''):
+                self.redirect(SITE_URL)
+        self.render(error_pic=error_pic, pos='')
+
+
+
