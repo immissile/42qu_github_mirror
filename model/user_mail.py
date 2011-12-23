d9b391974c3493a7b981a6492ac0f2378e246aa0
zuroc <zsp007@gmail.com>
2011-12-24 03:42:57 +0800

    fix

--- model/user_mail.py
+++ model/user_mail.py
@@ -90,7 +90,6 @@ if __name__ == '__main__':
     #from zsite import Zsite, CID_USER, STATE_ACTIVE
     #for i in Zsite.where(cid=CID_USER).where("state>=%s"%STATE_ACTIVE):
     #    print i.id
-    print mail_by_user_id(10017321)
-    print mail_by_user_id_if_login(10017321)
+    print mail_by_user_id(1)
 
 
