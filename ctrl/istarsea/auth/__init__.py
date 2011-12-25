0717904e327643b597773f233ba2a620e7389ddf
zuroc <zsp007@gmail.com>
2011-12-25 23:50:59 +0800

    fx

--- ctrl/istarsea/auth/__init__.py
+++ ctrl/istarsea/auth/__init__.py
@@ -78,6 +78,40 @@ class Reg(NoLoginBase):
         )
 
 
+    def post(self):
+        name = self.get_argument('name', '')
+        mail = self.get_argument('mail', '')
+        errtip = Errtip()
+        if mail:
+            mail = mail.lower()
+        if not mail:
+            errtip.mail = '请输入邮箱'
+        elif not EMAIL_VALID.match(mail):
+            errtip.mail = '邮箱格式有误'
+
+        if not name:
+            errtip.name = '请输入姓名'
+
+
+        if not errtip:
+            user_id = user_id_by_mail(mail)
+            if not user_id:
+                user_id = user_new(mail, name=name)
+                #session = user_session(user_id)
+                #self.set_cookie('S', session)
+                self.set_cookie('E', mail)
+
+                phone = self.get_argument('phone', '')
+                namecard_new(user_id,phone=phone)
+
+            return self.redirect('/auth/reged/%s'%user_id)
+
+        self.render(
+            mail=mail,
+            name=name,
+            errtip=errtip
+        )
+
 @urlmap('/auth/reg2/?(.*)')
 class Reg2(NoLoginBase):
     def get(self, mail=''):
