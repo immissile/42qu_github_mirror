6a36952ab24c7a2cb5dd4eca158099a4b5da8f0b
zuroc <zsp007@gmail.com>
2011-12-28 05:34:57 +0800

    fix

--- model/buzz_at.py
+++ model/buzz_at.py
@@ -6,19 +6,11 @@ from mq import mq_client
 # id
 # from_id
 # to_id
-# cid
-# rid
+# po_id
+# reply_id
 
-BUZZ_AT_CID_PO = 1
-BUZZ_AT_CID_REPLY = 2
 
-def buzz_at_po_new(user_id, po_id, txt):
-    return buzz_at_new(BUZZ_AT_CID_PO, user_id, po_id, txt)
-
-def buzz_at_reply_new(user_id, reply_id, txt):
-    return buzz_at_new(BUZZ_AT_CID_REPLY, user_id, po_id, txt)
-
-def buzz_at_new(cid, user_id, rid, txt):
+def buzz_at_new(from_id, po_id, reply_id, txt):
     at_id_set = set(filter(bool, [id_by_url(i[2]) for i in RE_AT.findall(txt)]))
 
     for to_id in at_id_set:
@@ -29,6 +21,7 @@ def buzz_at_new(cid, user_id, rid, txt):
     return at_id_set
 
 
-mq_buzz_at_po_new = mq_client(buzz_at_po_new)
+
+mq_buzz_at_new = mq_client(buzz_at_new)
 
 
