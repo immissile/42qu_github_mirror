77817e57a94e22fae80dd91c1434cbe79069145a
zuroc <zsp007@gmail.com>
2011-12-28 06:19:51 +0800

    fix

--- model/buzz_at.py
+++ model/buzz_at.py
@@ -38,6 +38,8 @@ def buzz_at_new(from_id, txt, po_id, reply_id=0):
     for to_id in at_id_set:
         buzz_at = BuzzAt(from_id=from_id, to_id=to_id, reply_id=reply_id, po_id=po_id,state=BUZZ_AT_SHOW)
         buzz_at.save()
+        mc_flush(to_id)
+        mc_buzz_at_by_user_id_for_show.delete(user_id)
 
     return at_id_set
 
@@ -52,19 +54,32 @@ def buzz_at_reply_rm(reply_id):
     at_id_set = at_id_set_by_txt(txt)
     for to_id in at_id_set:
         BuzzAt.where(to_id=to_id, reply_id=reply_id).update(state=BUZZ_AT_RMED)
+        mc_flush(to_id)
 
+BUZZ_AT_COL ="id,from_id,po_id,reply_id"
+
+mc_buzz_at_by_user_id_for_show = McCache("BuzzAtByUserIdForShow:%s")
 
 def buzz_at_by_user_id_for_show(user_id):
+    if mc_buzz_at_by_user_id_for_show.get(user_id) == 0:
+        return 0
     begin_id = buzz_at_pos.get(user_id)
-    buzz_at_list = reversed(BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).where("id>%s",begin_id).order_by("id")[:5])
-    result = []
-    for i in buzz_at_list:
-        result.append( (i.from_id, i.po_id, i.reply_id) )
-    return result
+    result = reversed(BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).where("id>%s",begin_id).order_by("id").col_list(5,0, BUZZ_AT_COL))
+    if result:
+        buzz_at_pos.set(user_id, result[0][0])
+        return result
+    else:
+        mc_buzz_at_by_user_id_for_show.set(user_id, 0)
+        return 0
+
+buzz_at_count = McNum(lambda user_id: BuzzAt.where(user_id=user_id,state=BUZZ_AT_SHOW).count() , 'BuzzAtCount:%s')
+
+def mc_flush(user_id):
+    buzz_at_count.delete(user_id)
+
+def _buzz_at_list(user_id, limit, offset):
+    return BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).order_by("id desc").col_list(limit, offset, BUZZ_AT_COL)
 
 
 
-def buzz_at_list(user_id, limit, offset):
-    BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).order_by("id desc")[offset:offset+limit]
-    pass
 
