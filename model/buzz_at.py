e6005e956969df4ae30a290b2d168a77f4ed216b
zuroc <zsp007@gmail.com>
2011-12-28 06:03:09 +0800

    fix

--- model/buzz_at.py
+++ model/buzz_at.py
@@ -5,7 +5,9 @@ from txt2htm import RE_AT
 from txt import txt_bind, txt_get, txt_new
 from mq import mq_client
 from buzz_at import buzz_at_new
+from kv import Kv
 
+# buzz_at
 # id
 # from_id
 # to_id
@@ -13,9 +15,15 @@ from buzz_at import buzz_at_new
 # reply_id
 # state
 
+# buzz_at_pos
+# id
+# value
+
 BUZZ_AT_SHOW    = 30
 BUZZ_AT_HIDE    = 20
 BUZZ_AT_RMED    =  0
+
+buzz_at_pos = Kv("buzz_at_pos", int)
 
 class BuzzAt(Model):
     pass
@@ -46,6 +54,17 @@ def buzz_at_reply_rm(reply_id):
         BuzzAt.where(to_id=to_id, reply_id=reply_id).update(state=BUZZ_AT_RMED)
 
 
+def buzz_at_by_user_id_for_show(user_id):
+    begin_id = buzz_at_pos.get(user_id)
+    buzz_at_list = reversed(BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).where("id>%s",begin_id).order_by("id")[:5])
+    result = []
+    for i in buzz_at_list:
+        result.append( (i.from_id, i.po_id, i.reply_id) )
+    return result
 
 
+
+def buzz_at_list(user_id, limit, offset):
+    BuzzAt.where(to_id=user_id, state=BUZZ_AT_SHOW).order_by("id desc")[offset:offset+limit]
+    pass
 
