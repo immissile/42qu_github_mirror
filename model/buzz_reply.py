a1371f951fb018e232d1b2c19c04ab022ad7fa51
zuroc <zsp007@gmail.com>
2011-12-27 13:52:40 +0800

    fix

--- model/buzz_reply.py
+++ model/buzz_reply.py
@@ -22,8 +22,8 @@ def mq_client(f):
 
 BUZZ_REPLY_STATE_SHOW = 30
 BUZZ_REPLY_STATE_HIDE = 20
-BUZZ_REPLY_STATE_RM   = 10
-BUZZ_REPLY_STATE_PO_RMED   = 0
+BUZZ_REPLY_STATE_RM = 10
+BUZZ_REPLY_STATE_PO_RMED = 0
 
 class BuzzReply(McModel):
     pass
@@ -32,9 +32,9 @@ class BuzzReply(McModel):
 
 def buzz_po_reply_new(from_id, reply_id, po_id, po_user_id):
     from po_pos import user_id_list_by_po_pos_buzz
-    buzz_to = user_id_list_by_po_pos_buzz(po_id) 
+    buzz_to = user_id_list_by_po_pos_buzz(po_id)
     txt = txt_get(reply_id)
-    excepted = buzz_at_new(from_id, po_id,txt)
+    excepted = buzz_at_new(from_id, po_id, txt)
 
     for user_id in excepted:
         buzz_reply_hide(user_id, po_id)
@@ -63,7 +63,7 @@ def buzz_po_reply_rm(po_id, reply_id):
     from model.po import Po
     po = Po.mc_get(po_id)
     if po and reply_id >= po.reply_id_last:
-        for user_id in PoPos.where(po_id=po_id, state=STATE_BUZZ).where("po_pos>%s",reply_id).col_list(col="user_id"):
+        for user_id in PoPos.where(po_id=po_id, state=STATE_BUZZ).where('po_pos>%s', reply_id).col_list(col='user_id'):
             BuzzReply.where(po_id=po_id, user_id=user_id, state=BUZZ_REPLY_STATE_SHOW).update(state=BUZZ_REPLY_STATE_HIDE)
             mc_flush(user_id)
 
@@ -71,7 +71,7 @@ mq_buzz_po_reply_rm = mq_client(buzz_po_reply_rm)
 
 
 def buzz_po_rm(po_id):
-    for user_id in BuzzReply.where(po_id=po_id, state=BUZZ_REPLY_STATE_SHOW).col_list(col="user_id"):
+    for user_id in BuzzReply.where(po_id=po_id, state=BUZZ_REPLY_STATE_SHOW).col_list(col='user_id'):
         mc_flush(user_id)
 
     BuzzReply.where(po_id=po_id).update(state=BUZZ_REPLY_STATE_PO_RMED )
@@ -79,11 +79,11 @@ def buzz_po_rm(po_id):
 
 mq_buzz_po_rm = mq_client(buzz_po_rm)
 
-mc_po_id_list_by_buzz_reply_user_id = McCacheA("PoIdListByBuzzReplyUserId:%s")
+mc_po_id_list_by_buzz_reply_user_id = McCacheA('PoIdListByBuzzReplyUserId:%s')
 
-@mc_po_id_list_by_buzz_reply_user_id("{user_id}")
+@mc_po_id_list_by_buzz_reply_user_id('{user_id}')
 def po_id_list_by_buzz_reply_user_id(user_id):
-    BuzzReply.where(user_id=user_id, state=BUZZ_REPLY_STATE_SHOW).order_by("update_time desc").col_list(0, 7, "po_id")
+    return BuzzReply.where(user_id=user_id, state=BUZZ_REPLY_STATE_SHOW).order_by('update_time desc').col_list(0, 7, 'po_id')
 
 
 def buzz_reply_hide_or_rm(id):
@@ -92,20 +92,20 @@ def buzz_reply_hide_or_rm(id):
         state = buzz.state
         if state == BUZZ_REPLY_STATE_SHOW :
             buzz.state = BUZZ_REPLY_STATE_RM
-            buzz.save() 
+            buzz.save()
             from model.po_pos import po_pos_state_mute
             po_pos_state_mute(buzz.user_id, buzz.po_id)
-               
+
 def buzz_reply_hide(user_id, po_id):
     buzz_reply = BuzzReply.get(
         user_id=user_id, po_id=po_id, state=BUZZ_REPLY_STATE_SHOW
     )
     if buzz_reply:
         buzz_reply.state = BUZZ_REPLY_STATE_HIDE
-        buzz_reply.save() 
+        buzz_reply.save()
         if buzz_reply.id in po_id_list_by_buzz_reply_user_id(user_id):
             mc_flush(user_id)
- 
+
 def po_id_list_by_buzz_reply_user_id_rm(user_id):
     for i in po_id_list_by_buzz_reply_user_id(user_id):
         buzz_reply_hide_or_rm(i)
@@ -114,5 +114,9 @@ def po_id_list_by_buzz_reply_user_id_rm(user_id):
 def mc_flush(user_id):
     mc_po_id_list_by_buzz_reply_user_id.delete(user_id)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pass
+    user_id = 10016605
+    print po_id_list_by_buzz_reply_user_id(user_id)
+
+
