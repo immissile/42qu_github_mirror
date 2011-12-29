77d257ba975c6fc4503e8e5d074fd0783f49888b
zuroc <zsp007@gmail.com>
2011-12-29 19:17:45 +0800

    fix

--- misc/once/newindex.py
+++ misc/once/newindex.py
@@ -2,23 +2,29 @@
 # -*- coding: utf-8 -*-
 import _env
 from model.zsite_list import ZsiteList
+from zweb.orm import ormiter
 from model.zsite import Zsite
 from model.buzz import Buzz
 from model.kv_misc import kv_int
+from model.cid import CID_BUZZ_FOLLOW , CID_BUZZ_EVENT_JOIN , CID_BUZZ_SITE_NEW , CID_BUZZ_SITE_FAV , CID_BUZZ_PO_REPLY , CID_BUZZ_SYS , CID_BUZZ_EVENT_FEEDBACK_JOINER , CID_BUZZ_EVENT_FEEDBACK_OWNER  
 
+cid_set = (CID_BUZZ_FOLLOW , 
+CID_BUZZ_EVENT_JOIN , 
+CID_BUZZ_SITE_NEW , 
+CID_BUZZ_SITE_FAV , 
 
-CID_BUZZ_SHOW = 212
-CID_BUZZ_EVENT_JOIN_APPLY = 219
+CID_BUZZ_PO_REPLY , 
 
-for i in Buzz.where(cid=CID_BUZZ_SHOW):
-    i.delete()
-    print "delete buzz show", i.id
+CID_BUZZ_SYS , 
+CID_BUZZ_EVENT_FEEDBACK_JOINER , 
+CID_BUZZ_EVENT_FEEDBACK_OWNER  , 
+)
+ 
 
-for i in Buzz.where(cid=CID_BUZZ_EVENT_JOIN_APPLY):
-    i.delete()
-    print "delete buzz event join apply", i.id
-
-
+for i in ormiter(Buzz):
+    if i.cid not in cid_set:
+        i.delete()
+        print i.id
 KV_SHOW_BUZZ_POS = 1 # 加入Show的非重要通知
 kv_int.delete(KV_SHOW_BUZZ_POS)
 
