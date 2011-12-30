a053ee92ca14791a1d20cda5f97e649bc9a8a6c4
wooparadog <guohaochuan@gmail.com>
2011-12-30 12:15:31 +0800

    f

--- static/js/ctrl/at.js
+++ static/js/ctrl/at.js
@@ -5,8 +5,7 @@ function htmlescape(escaped){
 }
 
 (function ($) {  
-var pre, pre0,at_size, at_list , wordsForSearch, req=0, space_at =/\s+@/, ieselect;
-
+var pre, pre0,at_size, at_list , wordsForSearch, req=0, space_at =/\s+@/, ieselect; 
 
 methods={
 
@@ -150,6 +149,38 @@ methods={
     }
 };
  
+
+reply = function(id,at_txt){
+	if (!$.cookie.get('S')) {
+		return login();
+	}
+	var fancybox = $.fancybox;
+	fancybox({
+        content: '<form id="vote_reply" class="fancyreply"><h3>回复</h3><textarea tabindex="1" style="height:140px" name="txt"></textarea><div class="btns"><span class="rec_tip share_sync"></span><span class="btnw"><button class="btn" tabindex="3" type="submit">回复</button></span><span class="syncp"></div></form>',
+		onComplete: function() {
+			var reply = $("#vote_reply"),
+			textarea = reply.find("textarea")
+            textarea.focus().val(at_txt)
+			reply.submit(function() {
+				var txt = $.trim(textarea.val());
+				fancybox.showActivity()
+				$.postJSON("/j/po/reply/"+id, {
+					'txt': txt
+				},
+				function(r) {
+					if (r.can_not_reply) {
+						fancybox({
+							content: CANNOT_REPLY
+						})
+					} else {
+						fancybox.close()
+					}
+				})
+                })
+			textarea.focus()
+		}
+	})
+}
 
 $.fn.pop_at = function(url, line_height){
     if(!islogin())return this;
