3de52f1ce73926963682e42fc29659b438f29d77
zuroc <zsp007@gmail.com>
2011-12-30 13:46:58 +0800

    fix

--- static/js/ctrl/notice.js
+++ static/js/ctrl/notice.js
@@ -1,7 +1,5 @@
 function reply(id,at_txt){
-	if (!$.cookie.get('S')) {
-		return login();
-	}
+    if(!islogin())return;
 	var fancybox = $.fancybox;
 	fancybox({
         content: '<form id="vote_reply" class="fancyreply"><h3>回复</h3><textarea tabindex="1" style="height:140px" name="txt"></textarea><div class="btns"><span class="rec_tip share_sync"></span><span class="btnw"><button class="btn" tabindex="3" type="submit">确定</button></span><span class="syncp"></div></form>',
