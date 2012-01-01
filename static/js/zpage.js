459c595edd1882c690ba08c99ffcaf5c026b7a21
zuroc <zsp007@gmail.com>
2012-01-01 23:15:57 +0800

    fix

--- static/js/zpage.js
+++ static/js/zpage.js
@@ -408,7 +408,7 @@ function init_say(){
             function(result){
                 if(!result)return;
                 if(!$('.pop_hero')[0]){
-                        var pop = $('<div class="pop_hero"><div class="pop_hero_to"></div><div class="pop_hero_banner"><a href="'+result[3]+'" target="_blank"><img class="pop_hero_avatar" src="'+result[2]+'"></a><a href="javascript:follow_a('+result[4]+');void(0)" id="follow_a'+result[4]+'" class="xa pop_hero_follow">'+result[5]+'</a></div><a href="'+result[3]+'" target="_blank" class="pop_hero_name"></a><div class="pop_hero_bio"></div><div class="pop_hero_motto"></div></div>')
+                        var pop = $('<div class="pop_hero"><div class="pop_hero_to"></div><div class="pop_hero_banner"><a href="'+result[3]+'" target="_blank"><img class="pop_hero_avatar" src="'+result[2]+'"></a><a href="javascript:follow_a('+result[4]+');void(0)" id="follow_a'+result[4]+'" class="xa pop_hero_follow">'+result[5]+'</a></div><div class="L"><a href="'+result[3]+'" target="_blank" class="pop_hero_name"></a><div class="pop_hero_bio"></div><div class="pop_hero_motto"></div></div>')
 
                         pop.find('.pop_hero_bio').text(result[1])
                         pop.find('.pop_hero_motto').text(result[6])
