9004b2a8a729330a39f099f7eac94338105a5ed1
zuroc <zsp007@gmail.com>
2012-01-03 20:49:29 +0800

    fix

--- model/zsite_verify.py
+++ model/zsite_verify.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-
+from _db import Model
 from zsite import Zsite, ZSITE_STATE_ACTIVE, ZSITE_STATE_VERIFY, \
 CID_USER,  ZSITE_STATE_FAILED_VERIFY
 from model.ico import ico
@@ -10,14 +10,37 @@ ZSITE_VERIFY_NEED_NAME     = 0b1
 ZSITE_VERIFY_NEED_ICO      = 0b10
 ZSITE_VERIFY_NEED_CAREER   = 0b100
 
+#CREATE TABLE `zpage`.`zsite_user_verifyed` (
+#  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+#  `user_id` INTEGER UNSIGNED NOT NULL,
+#  `state` INTEGER UNSIGNED NOT NULL,
+#  PRIMARY KEY (`id`),
+#  INDEX `Index_2`(`user_id`)
+#)
+#ENGINE = MyISAM;
+ZSITE_USER_VERIFYED_UNCHECK = 1
+ZSITE_USER_VERIFYED_CHECKED = 2
+
+class ZsiteUserVerifyed(Model):
+    pass
+
 def zsite_verify_rm(zsite):
     zsite.state = ZSITE_STATE_FAILED_VERIFY
     zsite.save()
     zsite_show_rm(zsite)
 
 def zsite_verify_new(zsite):
+    if zsite.state >= ZSITE_STATE_VERIFY:
+        return
     zsite.state = ZSITE_STATE_VERIFY
     zsite.save()
+
+    if ZsiteUserVerifyed.get(user_id=user_id, state=ZSITE_USER_VERIFYED_UNCHECK):
+        return
+
+    v = ZsiteUserVerifyed(user_id=user_id, state=ZSITE_USER_VERIFYED_UNCHECK)
+    v.save()
+
 
 def zsite_verify_ajust(zsite):
     if zsite.cid != CID_USER:
