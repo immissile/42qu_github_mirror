8146a65c0279b6c07a08d0695ba34939c04289e8
zuroc <zsp007@gmail.com>
2012-01-03 19:44:12 +0800

    fix

--- ctrl/me/i.py
+++ ctrl/me/i.py
@@ -11,7 +11,7 @@ from model.zsite_url import url_by_id, url_new, url_valid, RE_URL
 from model.user_mail import mail_by_user_id, user_mail_new
 from model.txt import txt_get, txt_new
 from model.mail_notice import CID_MAIL_NOTICE_ALL, mail_notice_all, mail_notice_set
-from model.zsite import zsite_name_edit, user_can_reply, ZSITE_STATE_VERIFY, ZSITE_STATE_ACTIVE, ZSITE_STATE_WAIT_VERIFY, ZSITE_STATE_APPLY
+from model.zsite import zsite_name_edit, user_can_reply, ZSITE_STATE_VERIFY, ZSITE_STATE_ACTIVE,  ZSITE_STATE_APPLY
 from model.user_auth import user_password_new, user_password_verify
 from cgi import escape
 from urlparse import parse_qs, urlparse
@@ -226,20 +226,20 @@ class Verify(LoginBase):
             current_user = self.current_user
             current_user_id = self.current_user_id
             state = current_user.state
+
+            from model.zsite_verify import zsite_verify_ajust
+            zsite_verify_ajust(current_user)
+
             if state >= ZSITE_STATE_VERIFY:
                 return self.redirect('/')
             elif state <= ZSITE_STATE_APPLY:
                 return self.redirect('/auth/verify/sended/%s' % current_user_id)
 
-    def post(self):
-        current_user = self.current_user
-        current_user.state = ZSITE_STATE_WAIT_VERIFY
-        current_user.save()
-        return self.get()
 
     def get(self):
         self.render()
-
+    
+    post = get
 
 
 @urlmap('/i/link')
