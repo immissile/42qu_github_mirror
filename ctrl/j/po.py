f4446ee0ae84513dd28032aa84649da3919914ca
zuroc <zsp007@gmail.com>
2012-01-04 17:02:26 +0800

    fix

--- ctrl/j/po.py
+++ ctrl/j/po.py
@@ -19,6 +19,7 @@ from model.feed_render import feed_tuple_by_db
 from model.career import career_current, career_bind, career_dict
 from model.txt2htm import txt_withlink
 from model.buzz_reply import buzz_reply_hide
+from model.reply import Reply
 
 def post_reply(self, id):
     user = self.current_user
@@ -26,7 +27,7 @@ def post_reply(self, id):
     if not user_can_reply(user):
         self.finish('{"can_not_reply":1}')
     else:
-        result = {}
+        result = []
         txt = self.get_argument('txt', None)
 
         reply_id = None
@@ -35,45 +36,52 @@ def post_reply(self, id):
             po = Po.mc_get(id)
             if po.can_view(user_id):
                 reply_id = po.reply_new(user, txt, po.state)
-
-        self.finish(result)
+                if reply_id:
+                    reply = Reply.mc_get(reply_id)
+                    reply.user = user
+                    result = _reply_list_dump([reply])
+        self.finish(dumps(result))
         return reply_id
 
+def _reply_list_dump(reply_list):
+    result = []
+    career_bind(reply_list, "user_id")
+    ico_url_bind_with_default(tuple(i.user for i in reply_list))
+
+    pre_user_id = None
+
+    for reply in reply_list:
+        user = reply.user
+        career = reply.career
+        career = " , ".join(filter(bool,career))
+        if not career:
+            career = 0
+
+        user_id = user.id
+
+        if user_id == pre_user_id:
+            result[-1][-1].append(reply.htm)
+        else:
+            result.append(
+                (url_or_id(user_id), user.name , career, user.ico, [reply.htm])
+            )
+
+        pre_user_id = user_id
+    
+    return result
 
 @urlmap('/j/po/reply/json/(\d+)')
 class PoReplyJson(JLoginBase):
     def get(self, id):
         po = Po.mc_get(id)
         user_id = self.current_user_id
-        result = []
         buzz_reply_hide(user_id, id)
 
 
         if po and po.can_view(user_id):
-            reply_list = po.reply_list()
-            career_bind(reply_list, "user_id")
-            ico_url_bind_with_default(tuple(i.user for i in reply_list))
-
-            pre_user_id = None
-
-            for reply in reply_list:
-                user = reply.user
-                career = reply.career
-                career = " , ".join(filter(bool,career))
-                if not career:
-                    career = 0
-
-                user_id = user.id
-
-                if user_id == pre_user_id:
-                    result[-1][-1].append(reply.htm)
-                else:
-                    result.append(
-                        (url_or_id(user_id), user.name , career, user.ico, [reply.htm])
-                    )
-
-                pre_user_id = user_id
-
+            result = _reply_list_dump( po.reply_list() )
+        else:
+            result = ()
         return self.finish(dumps(result))
 
 
@@ -138,7 +146,7 @@ class Word(JLoginBase):
         self.finish(dumps(result))
 
 @urlmap('/j/po/reply/(\d+)')
-class Reply(JLoginBase):
+class JPoReply(JLoginBase):
     post = get = post_reply
 
 @urlmap('/j/po/tag/edit')
