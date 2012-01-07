244b67266e780dd7578b1f0aa10a137ae64fe893
zuroc <zsp007@gmail.com>
2012-01-08 01:38:37 +0800

    fix

--- misc/log/log.py
+++ misc/log/log.py
@@ -2,35 +2,39 @@ pre_last_time = 0
 pre_line = 0
 from collections import defaultdict
 import datetime
+import pylzma
 
 min_count = defaultdict(int)
 begin_time = 41000
 
 
+logfile = """
+/var/log/nginx_backup/42qu_com_zsite.access_log-20111231.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120101.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120102.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120103.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120104.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120105.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120106.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120107.lzma
+/var/log/nginx_backup/42qu_com_zsite.access_log-20120108.lzma
+""".strip().split()
+import lzma
 
-with open('42qu_com_zsite.access_log') as log42qu:
-    for pos , line in enumerate(log42qu):
-        if pos < begin_time or pos > begin_time+700:
+
+
+def error499_count(data):
+    count = 0
+    for line in data.split("\n"):
+        line = line.split()
+        if len(line)<3:
             continue
+        code = int(line[2])
+        if code == 499:
+            count += 1
+    return count
 
-        line = line.strip()
-        log_line = line.split(' ', 8)[:7]
-        last_time = float(log_line[-1])
-        diff = last_time - pre_last_time
-        if pre_line:
-            dt = str(datetime.datetime.fromtimestamp(last_time))
-            pre_dt = str(datetime.datetime.fromtimestamp(pre_last_time))
-            if dt[11:17] != pre_dt[11:17]:
-                print ''
-            url = pre_line.split('"')[-2].split()
-            code = int(pre_line.split()[2])
-            if code not in (200,302,404,304,301):
-                print dt[11:19], code, 'http://%s%s'%(pre_line.split()[1], url[1]), url[0]
-
-        pre_last_time = last_time
-        pre_line = line
-        min_count[int(last_time/60)] += 1
-
-
-#for k, v in sorted(min_count.iteritems(), key=lambda x:x[0]):
-#    print datetime.datetime.fromtimestamp(k*60), v
+for line in logfile:
+    with open(line) as nginxlog:
+        data = lzma.decompress(nginxlog.read())
+    print line,error499_count(data)
