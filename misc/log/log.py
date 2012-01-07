c6c8374aec4040e4b3b6ca36d50bcf2bf03dcd24
zuroc <zsp007@gmail.com>
2012-01-08 01:44:28 +0800

    fix

--- misc/log/log.py
+++ misc/log/log.py
@@ -2,39 +2,28 @@ pre_last_time = 0
 pre_line = 0
 from collections import defaultdict
 import datetime
-import pylzma
+import lzma
 
 min_count = defaultdict(int)
 begin_time = 41000
+import glob
 
-
-logfile = """
-/var/log/nginx_backup/42qu_com_zsite.access_log-20111231.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120101.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120102.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120103.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120104.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120105.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120106.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120107.lzma
-/var/log/nginx_backup/42qu_com_zsite.access_log-20120108.lzma
-""".strip().split()
-import lzma
+logfile = """/var/log/nginx_backup/42qu_com_zsite.access_log-*.lzma"""
 
 
 
 def error499_count(data):
     count = 0
-    for pline in data.split("\n"):
+    for pline in data.split('\n'):
         line = pline.split()
-        if len(line)<3:
+        if len(line) < 3:
             continue
         code = int(line[2])
         if code == 499:
             count += 1
     return count
 
-for line in logfile:
+for line in sorted(glob.glob(logfile))[-30:]:
     with open(line) as nginxlog:
         data = lzma.decompress(nginxlog.read())
-    print line,error499_count(data)
+    print line, error499_count(data)
