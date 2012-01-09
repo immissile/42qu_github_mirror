e53528762892134132102ff4bc4fe0b77bd15745
zuroc <zsp007@gmail.com>
2012-01-09 22:19:01 +0800

    fix

--- static/js/zpage.js
+++ static/js/zpage.js
@@ -231,14 +231,17 @@ function doc_height(){
 var TMPL_REPLY;
 
 function render_reply(data, i){
-    TMPL_REPLY = TMPL_REPLY||$('<script type="text/x-jquery-tmpl"><div class="fcmi">{{if $data[3]}}<a target="_blank" href="//${$data[0]}'+HOST_SUFFIX+'" ><img class="fcico" src="${$data[3]}"></a>{{else}}<div class="fcico"></div>{{/if}}<div class="fcrb">{{each $data[4]}}<pre class="fcpre fcpre${$index}">{{html $value[0]}}{{if $value[2]}}<a href="javascript:void(0)" rel="${$value[1]}" class="rm"></a>{{/if}}</pre>{{/each}}<div class="fcname"><a href="//${$data[0]}'+HOST_SUFFIX+'" class="fcmname c9 TPH" target="_blank"><span>${$data[1]}</span>{{if $data[2]}} ( ${$data[2]} ){{/if}}</a><a class="zsite_reply reply_at" rel="${$data[0]}" href="javascript:void(0)"></a></div></div></div></script>');
-    var result = TMPL_REPLY.tmpl(data),
-        rm_list = result.find('.rm'),
-        t;
-    for(i=i||0;i<rm_list.length;++i){
-        t=rm_list[i]
-        t.rel='reply/'+t.rel
+    TMPL_REPLY = TMPL_REPLY||$('<script type="text/x-jquery-tmpl"><div class="fcmi">{{if $data[3]}}<a target="_blank" href="//${$data[0]}'+HOST_SUFFIX+'" ><img class="fcico" src="${$data[3]}"></a>{{else}}<div class="fcico"></div>{{/if}}<div class="fcrb">{{each $data[4]}}<pre class="fcpre fcpre${$index}">{{html $value[0]}}{{if $value[2]}}<a href="javascript:void(0)" rel="${$value[3]}" class="rm"></a>{{/if}}</pre>{{/each}}<div class="fcname"><a href="//${$data[0]}'+HOST_SUFFIX+'" class="fcmname c9 TPH" target="_blank"><span>${$data[1]}</span>{{if $data[2]}} ( ${$data[2]} ){{/if}}</a><a class="zsite_reply reply_at" rel="${$data[0]}" href="javascript:void(0)"></a></div></div></div></script>');
+    var d4, j, k, result;
+    for(i=i||0;i<data.length;++i){
+        d4=data[i][4]
+        for(j=0;j<d4.length;++j){
+            k = d4[j]
+            k.push("reply/"+k[1])
+        }
     }
+    var result = TMPL_REPLY.tmpl(data);
+
     result.find(".rm").click(function(){
         if(confirm("删除 , 确定 ?")){
             var fcmi = $(this).parents('.fcmi'),fcpre=$(this).parents('.fcpre');
