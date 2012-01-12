56cf4bcbaa007fcb0fec44725650e0be21a30963
zuroc <zsp007@gmail.com>
2012-01-12 19:45:05 +0800

    fix

--- ctrl/tag/index.py
+++ ctrl/tag/index.py
@@ -2,8 +2,15 @@
 
 from _handler import Base, LoginBase
 from ctrl._urlmap.tag import urlmap
+from zkit.page import page_limit_offset
+
 
 @urlmap('/')
 class Index(Base):
     def get(self):
+        page, limit, offset = page_limit_offset(
+
+        )
         self.render()
+
+
