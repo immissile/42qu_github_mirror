1dab2b1d2e553289e0aa0504ee4f7643b271c018
zuroc <zsp007@gmail.com>
2012-01-14 20:18:13 +0800

    fix

--- static/js/ctrl/tag.js
+++ static/js/ctrl/tag.js
@@ -61,6 +61,7 @@ b1024()
         winj.scrollTop(scrollTop)
         $.get("/j/fdtxt/3875",function(txt){
             feed_loading.replaceWith(render_txt.tmpl({txt:txt}))
+            winj.scrollTop(scrollTop)
         })
         return false; 
     })
@@ -71,7 +72,7 @@ b1024()
         if(main_nav_txt[0]){
             var top = main_nav_txt.offset().top, win=$(window).scroll(function() {
                 if(win.scrollTop() >= scrollTop+14){
-                    main_nav_txt.css({'position':'fixed',"marginTop":"-152px"})
+                    main_nav_txt.css({'position':'fixed',"marginTop":-scrollTop-14})
                 }else{
                     main_nav_txt.css({'position':'absolute',"marginTop":"0"})
                 }
