64c5ca0ecfa82db53e96fa26ed461d1b93538f12
zuroc <zsp007@gmail.com>
2012-01-16 12:12:26 +0800

    fix

--- model/po_by_tag.py
+++ model/po_by_tag.py
@@ -3,20 +3,36 @@ import _db
 from model.po import Po
 from model.cid import CID_NOTE
 from model.zsite import Zsite
+from model.txt import txt_bind
+from zkit.txt import cnenlen , cnenoverflow  
 
 # id , name, txt,  author_id , author_name , is_star 
 def po_by_tag(tag_id):
     po_list = Po.where(cid=CID_NOTE)[:25]
+    txt_bind(po_list)
+
     Zsite.mc_bind(po_list,"user", "user_id")
     result = []
+
+
     for po in po_list:
+
+        name = po.name
         user = po.user
+
+
+        name_len = cnenlen(name)
+        txt = po.txt
+        tip = cnenoverflow(txt,38-name_len)[0]
+    
         result.append((
             po.id, 
-            po.name, 
-            po.txt,
-            po.user_id
+            name, 
+            tip,
+            user.id,
+            user.name
         ))
+
     return result
 
 if __name__ == "__main__":
