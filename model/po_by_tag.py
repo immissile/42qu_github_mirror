478f74b5c523e33909e3f5c18a3e76690409a648
zuroc <zsp007@gmail.com>
2012-01-16 12:38:11 +0800

    fix

--- model/po_by_tag.py
+++ model/po_by_tag.py
@@ -5,15 +5,19 @@ from model.cid import CID_NOTE
 from model.zsite import Zsite
 from model.txt import txt_bind
 from zkit.txt import cnenlen , cnenoverflow
+from model.fav import fav_cid_dict
 
 # id , name, txt,  author_id , author_name , is_star 
-def po_by_tag(tag_id):
+def po_by_tag(tag_id, user_id=0):
     po_list = Po.where(cid=CID_NOTE)[:25]
     txt_bind(po_list)
 
     Zsite.mc_bind(po_list, 'user', 'user_id')
     result = []
 
+    po_id_list = [i.id for i in po_list]
+
+    fav_dict = fav_cid_dict(user_id, po_id_list)
 
     for po in po_list:
 
@@ -29,12 +33,15 @@ def po_by_tag(tag_id):
         else:
             tip = ''
 
+        id = po.id
+
         result.append((
-            po.id,
+            id,
             name,
             tip,
             user.id,
-            user.name
+            user.name,
+            fav_dict[id]
         ))
 
     return result
