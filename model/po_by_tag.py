7c36c6bf017b5d1432e3478bdf71da6b26e50467
zuroc <zsp007@gmail.com>
2012-01-16 11:55:01 +0800

    fix

--- model/po_by_tag.py
+++ model/po_by_tag.py
@@ -1,12 +1,16 @@
 #coding:utf-8
+import _db
 from model.po import Po
 from model.cid import CID_NOTE
+from model.zsite import Zsite
 
 # id , name, txt,  author_id , author_name , is_star 
 def po_by_tag(tag_id):
     po_list = Po.where(cid=CID_NOTE)[:25]
+    Zsite.mc_bind(po_list,"user", "user_id")
     result = []
     for po in po_list:
+        user = po.user
         result.append((
             po.id, 
             po.name, 
@@ -16,7 +20,7 @@ def po_by_tag(tag_id):
     return result
 
 if __name__ == "__main__":
-    pass
+    print po_by_tag(1)
 
 
 
