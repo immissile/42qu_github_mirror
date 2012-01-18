7dee462391bcaf20a0172e409879347f40186c58
wooparadog <guohaochuan@gmail.com>
2012-01-18 19:40:37 +0800

    f

--- model/short_url.py
+++ model/short_url.py
@@ -5,16 +5,17 @@ import _env
 from _db import  McModel
 from config import SHORT_DOMAIN
 from zkit.base58 import b58decode, b58encode
+from zkit.img_filter import img_filter
 from txt2htm import RE_LINK_TARGET
 
 class UrlShort(McModel):
     pass
 
 def replace_link(match):
+    from po_video import  video_filter
     gs = match.groups()
     b, g , e = gs
-    if not (g.startswith('http://v.youku.com/v_show/id_') or g.startswith('http://player.youku.com/player.php/sid/')
-            or g.endswith('.swf') or g.endswith('.jpg') or g.endswith('.png') or g.endswith('.gif') or g.startswith('http://%s'%SHORT_DOMAIN)):
+    if not ( video_filter(g)[0] or img_filter(g) or g.startswith('http://%s'%SHORT_DOMAIN)):
         g = url_short(g)
     return g
 
@@ -49,5 +50,5 @@ if __name__ == '__main__':
 #    print url_short("http://google"+str(i))
 
 #print url_short_by_id('3T')
-    print url_short_txt('sfsdfsdf http://google.com https://mail.google.com/mail/u/0/#inbox/134ec4da6de5b5a7 https://mail.google.com/mail/u/0/#inbox')
-    print url_short_by_id('4x')
+    print url_short_txt('sfsdfsdf http://g.cn/df.png http://google.com https://mail.google.com/mail/u/0/#inbox/134ec4da6de5b5a7 https://mail.google.com/mail/u/0/#inbox')
+    #print url_short_by_id('4x')
