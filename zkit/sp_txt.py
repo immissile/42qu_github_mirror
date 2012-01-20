806e2971f895e009e5bc2bf5eb0f85d28cda82ca
wooparadog <guohaochuan@gmail.com>
2012-01-20 16:17:50 +0800

    f

--- zkit/sp_txt.py
+++ zkit/sp_txt.py
@@ -5,10 +5,9 @@ def sp_txt(txt):
     if str(txt).replace(" ",'').isalnum():
         yield txt 
     else:
-        txt = txt.decode('utf-8')
+        txt = txt.decode('utf-8','ignore')
         for i in range(len(txt)-1):
             yield txt[i:i+2]
-
 
 if __name__ == '__main__':
     for i in sp_txt('这是什么东西'):
