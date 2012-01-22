771cceb9d1420097d925dde7a5374ddcfe1c96c8
你的用户名 <>
2012-01-22 21:49:57 +0800

    fix

--- misc/rec/test.py
+++ misc/rec/test.py
@@ -5,8 +5,8 @@ from redis import Redis
 redis_client = Redis(
     host='localhost', port=6379, db=0, password=None, socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None
 )
-print redis_client.get('key1')
-redis_client.set('key1','value1')
-print redis_client.get('key2')
-
 help(redis_client)
+print redis_client.get(1)
+redis_client.set(1, 2)
+print redis_client.get(1)
+
