08360dff3fc57ec41eefb1a93f2afa15e9667fc6
zuroc <zsp007@gmail.com>
2012-01-23 12:10:15 +0800

    fix

--- model/rec_read.py
+++ model/rec_read.py
@@ -5,7 +5,10 @@ from zkit.zitertools import lineiter
 REDIS_REC_READ = 'RecRead:%s'
 
 def rec_read(user_id, limit=7):
-    result = redis.zrevrange(REDIS_REC_READ%user_id, 0, limit, False)
+    limit = limit-1
+    key =  REDIS_REC_READ%user_id
+    result = redis.zrevrange(key , 0, limit, False)
+    redis.zremrangebyrank(key, -limit-1 , -1)
     return result
 
 def rec_read_extend(user_id , id_score_list):
@@ -16,4 +19,8 @@ def rec_read_extend(user_id , id_score_list):
 if __name__ == '__main__':
     pass
     rec_read_extend(1, ((7, 9), (5, 1) , (1, 2), (2, 3)))
+    print rec_read(1, 1)
+    print rec_read(1, 1)
+    print rec_read(1, 1)
+    print rec_read(1, 1)
     print rec_read(1, 7)
