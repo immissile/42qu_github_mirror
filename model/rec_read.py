dda2a4ca62045e8ccb5a37d947209c296c9d3d77
zuroc <zsp007@gmail.com>
2012-01-23 11:41:45 +0800

    fix

--- model/rec_read.py
+++ model/rec_read.py
@@ -1,10 +1,18 @@
 #coding:utf-8
 from _db import redis
 
-def rec_read(user_id):
+
+REDIS_REC_READ = "RecRead:%s"
+
+def rec_read(user_id, limit):
+    result = redis.zrange(REDIS_REC_READ%user_id, 0, 7, False) 
+    return result 
+
+def rec_read_extend(user_id , id_score_list):
+    return redis.zadd(REDIS_REC_READ%user_id, *id_score_list)
+
+
+
+if __name__ == "__main__":
     pass
-
-
-
-
-
+    rec_read_extend(1, (1,2, 2, 3))
