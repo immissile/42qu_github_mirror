b35d2cdc1ad6cd06448ccb9037aad925617ca54b
zuroc <zsp007@gmail.com>
2012-01-23 12:12:06 +0800

    fix

--- model/rec_read.py
+++ model/rec_read.py
@@ -6,7 +6,7 @@ REDIS_REC_READ = 'RecRead:%s'
 
 def rec_read(user_id, limit=7):
     limit = limit-1
-    key =  REDIS_REC_READ%user_id
+    key = REDIS_REC_READ%user_id
     result = redis.zrevrange(key , 0, limit, False)
     redis.zremrangebyrank(key, -limit-1 , -1)
     return result
@@ -15,12 +15,12 @@ def rec_read_extend(user_id , id_score_list):
     return redis.zadd(REDIS_REC_READ%user_id, *lineiter(id_score_list))
 
 
+from model.po import Po
+def po_by_rec_read(user_id, limit=7):
+    return Po.mc_get_list(rec_read(user_id, limit))
 
 if __name__ == '__main__':
     pass
-    rec_read_extend(1, ((7, 9), (5, 1) , (1, 2), (2, 3)))
-    print rec_read(1, 1)
-    print rec_read(1, 1)
-    print rec_read(1, 1)
-    print rec_read(1, 1)
-    print rec_read(1, 7)
+    print po_by_rec_read(1, 3)
+
+
