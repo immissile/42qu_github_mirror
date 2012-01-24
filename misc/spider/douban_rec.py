9217ad203405c61d9e8e6a60f66b1b3c16bc8f8f
你的用户名 <>
2012-01-25 00:50:07 +0800

    fix

--- misc/spider/douban_rec.py
+++ misc/spider/douban_rec.py
@@ -6,7 +6,12 @@ from json import loads
 from kyotocabinet import DB
 import sys
 from os.path import join, abspath, dirname
-
+from time import time
+fetched = DB()
+fetched.open(
+    join(dirname(abspath(__file__)), "douban_rec_fetched.kch"),
+    DB.OWRITER | DB.OCREATE
+)
 
 db = DB()
 db.open(
@@ -15,21 +20,47 @@ db.open(
 )
 API_KEY = "00d9bb33af90bf5c028d319b0eb23e14"
 
-REC_URL="http://api.douban.com/people/%%s/recommendations?alt=json&apikey=%s"%API_KEY
+REC_URL = "http://api.douban.com/people/%%s/recommendations?alt=json&apikey=%s"%API_KEY
 
 LIKE_URL = "http://www.douban.com/j/like?tkind=%s&tid=%s"
+
+NOW = int(time()/60)
 
 def user_id_list_by_like(data, url):
     for i in loads(data):
         id = int(i['id'])
         uid = i['uid']
+
         if not uid.isdigit():
             db[uid] = id
-        yield user_id_list_by_rec, REC_URL%id , id
 
+        url = REC_URL%id 
 
-def user_id_list_by_rec(data, url, id):
-    print loads(data)
+        if id not in fetched:
+            fetched[id] = NOW
+            yield user_id_list_by_rec, url , id, 0 
+        else:
+            yield user_id_list_by_rec, url , id
+
+def url_id_list_by_rec_all(id):
+    start_index = 0
+    start_index += 10
+
+def user_id_list_by_rec(data, url, id, start_index=None):
+    data = loads(data)
+    entry_list = data['entry']
+    #print len(entry_list)
+    if entry_list: 
+        for i in entry_list:
+            title = i[u'title'][u'$t']
+            if title.startswith("推荐"):
+                title = title[2:]
+            print title
+        if start_index is not None:
+            start = start_index+10
+            url = "%s&max-result=10&start-index=%s"%(REC_URL%i, start)
+            yield user_id_list_by_rec, url, id, start
+
 
 def main():
     url_list = [
@@ -37,7 +68,6 @@ def main():
     ]
 
     headers = {
-
         'Cookie': 'bid="i9gsK/lU40A"; ll="108288"; __gads=ID=94ec68b017d7ed73:T=1324993947:S=ALNI_MaYLBDGa57C4diSiOVJspHn0IAVQw; __utma=164037162.1587489682.1327387877.1327387877.1327387877.1; __utmz=164037162.1327387877.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); viewed="7153184"; __utmb=164037162.2.8.1327387877; __utmc=164037162; RT=s=1327387895075&r=http%3A%2F%2Fwww.douban.com%2Fnote%2F196951541%2F; dbcl2="13593891:UKKGQFylj18"; ck="lLgF"'
 
     }
@@ -52,3 +82,5 @@ if __name__ == "__main__":
 
     main()
     db.close()
+    fetched.close()
+
