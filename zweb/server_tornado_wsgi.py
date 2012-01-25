820e77277c6e735add3ef5879c09249212e4933a
你的用户名 <>
2012-01-25 23:53:56 +0800

    fix

--- zweb/server_tornado_wsgi.py
+++ zweb/server_tornado_wsgi.py
@@ -14,3 +14,25 @@ def WSGIServer(port, application):
     http_server = tornado.httpserver.HTTPServer(container)
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+
+class Run(object):
+    def __init__(self, port, application):
+        self.port = port
+        self.application = application
+
+    def __call__(self):
+        import sys
+        if len(sys.argv) > 1 and sys.argv[1].isdigit():
+            port = int(sys.argv[1])
+        else:
+            port = self.port
+            if type(port) in (list, tuple):
+                port = port[0]
+
+        print 'server on port %s'%port
+
+        WSGIServer(port, self.application)
+
+
