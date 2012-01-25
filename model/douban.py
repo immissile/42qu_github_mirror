31ddd222583c6a6abd9b95a1028e8769a8deecbd
你的用户名 <>
2012-01-25 19:44:31 +0800

    fix

---
+++ model/douban.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM
+
+class DoubanUrl(Model):
+    pass
+
+
+
+if __name__ == '__main__':
+    pass
