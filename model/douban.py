8ae5cc61377fb7447958dba48f3d0289fc7cfec4
你的用户名 <>
2012-01-25 19:48:14 +0800

    fix

--- model/douban.py
+++ model/douban.py
@@ -5,7 +5,11 @@ from _db import Model, McModel, McCache, McLimitM, McNum, McCacheA, McCacheM
 class DoubanUrl(Model):
     pass
 
+class DoubanFeed(Model):
+    pass
 
+class DoubanUserFeed(Model):
+    pass
 
 if __name__ == '__main__':
     pass
