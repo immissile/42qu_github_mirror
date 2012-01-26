464a0b83a8b912ae97aa0dbd12dac8338cadba99
你的用户名 <>
2012-01-27 02:39:36 +0800

    fix

--- misc/backup/table_google.sql
+++ misc/backup/table_google.sql
@@ -4,19 +4,74 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `douban_feed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `douban_feed` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `cid` int(10) unsigned NOT NULL,
+  `rid` int(10) unsigned NOT NULL,
+  `rec` int(10) unsigned zerofill NOT NULL,
+  `like` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `topic_id` int(10) unsigned NOT NULL,
+  `title` varchar(255) collate utf8_bin NOT NULL,
+  `html` mediumtext collate utf8_bin NOT NULL,
+  `state` tinyint(3) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `Index_2` (`cid`,`rid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `douban_rec`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `douban_rec` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `cid` tinyint(3) unsigned NOT NULL,
+  `htm` text NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `douban_url`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `douban_url` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `cid` int(10) unsigned NOT NULL,
+  `rid` int(10) unsigned NOT NULL,
+  `url` varchar(64) collate utf8_bin NOT NULL,
+  `name` varchar(128) collate utf8_bin NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `Index_2` (`cid`,`url`),
+  KEY `Index_3` (`cid`,`rid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `douban_user_feed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `douban_user_feed` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `rid` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `cid` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `Index_2` USING BTREE (`rid`,`user_id`,`cid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `google_rank`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `google_rank` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL auto_increment,
   `uid` char(32) NOT NULL,
   `update_time` int(10) unsigned NOT NULL,
-  `follower` int(10) unsigned NOT NULL DEFAULT '0',
+  `follower` int(10) unsigned NOT NULL default '0',
   `ico` varchar(1024) NOT NULL,
   `name` varchar(256) NOT NULL,
   `txt` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uid` (`uid`) USING BTREE,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uid` (`uid`),
   KEY `Index_3` (`follower`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -24,7 +79,7 @@ DROP TABLE IF EXISTS `rss_po`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rss_po` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL auto_increment,
   `user_id` int(10) unsigned NOT NULL,
   `rss_id` int(10) unsigned NOT NULL,
   `rss_uid` varbinary(128) NOT NULL,
@@ -33,9 +88,9 @@ CREATE TABLE `rss_po` (
   `state` int(10) unsigned NOT NULL,
   `link` varbinary(1000) NOT NULL,
   `pic_list` blob NOT NULL,
-  `site_id` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Index_3` (`rss_uid`) USING BTREE,
+  `site_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `Index_3` (`rss_uid`),
   KEY `Index_2` (`rss_id`),
   KEY `link` (`link`)
 ) ENGINE=MyISAM DEFAULT CHARSET=binary;
