b193191fee7f32b24d9683021d711b762d132b1a
zuroc <zsp007@gmail.com>
2012-01-29 09:10:14 +0800

    fix

--- misc/spider/douban_parse.py
+++ misc/spider/douban_parse.py
@@ -19,27 +19,27 @@ def parse_like(data, url, cid, rid):
         douban_user_feed_new(DOUBAN_USER_FEED_VOTE_LIKE, cid, rid, id)
 
 def url_last(url):
-    return url.rstrip("/").rsplit("/", 1)[1]
+    return url.rstrip('/').rsplit('/', 1)[1]
 
 class ParseRec(object):
     cid = None
-    
+
     def func_url(self, title):
-        return (None, "") 
+        return (None, '')
 
     def __call__(self, title, user_id):
         cid = self.cid
-        
+
         func , url = self.func_url(title)
         rid = url_last(url)
         id = id_by_douban_feed(cid, rid)
 
-        if not id and func: 
+        if not id and func:
             yield func , url
             yield parse_like , URL_LIKE%(cid, rid), cid, rid
         else:
             douban_user_feed_new(DOUBAN_USER_FEED_VOTE_REC, cid, rid, user_id)
-       
+
 class ParseRecTopic(ParseRec):
     cid = CID_DOUBAN_FEED_TOPIC
 
@@ -58,9 +58,9 @@ class ParseRecNote(ParseRec):
         t = [i.split('">', 1) for i in txt_wrap_by_all('<a href="', '</a>', title)]
         url , note_title = t[1]
 
-        if url.startswith("http://www.douban.com/note/"):
+        if url.startswith('http://www.douban.com/note/'):
             func = parse_note_people_htm
-        elif url.startswith("http://site.douban.com/widget/notes/"):
+        elif url.startswith('http://site.douban.com/widget/notes/'):
             func = parse_note_site_htm
         else:
             func = 0
@@ -73,7 +73,7 @@ class ParseHtm(object):
     cid = None
 
     def htm(self, data):
-        return ""
+        return ''
 
     def user_id(self, data):
         return 0
@@ -82,7 +82,7 @@ class ParseHtm(object):
         return 0
 
     def title(self, data):
-        title = txt_wrap_by("<title>", "</title>", data)
+        title = txt_wrap_by('<title>', '</title>', data)
         return title
 
     def __call__(self, data, url):
@@ -90,13 +90,13 @@ class ParseHtm(object):
 
         title = self.title(data)
 
-        rec_num = txt_wrap_by('<span class="rec-num">', "人</span>", data) or 0
+        rec_num = txt_wrap_by('<span class="rec-num">', '人</span>', data) or 0
         like_num = txt_wrap_by('<span class="fav-num" data-tid="', '</a>喜欢</span>', data) or 0
         if like_num:
             like_num = txt_wrap_by('<a href="#">', '人', like_num)
 
         _topic = _owner = 0
-        
+
         owner_id = self.user_id(data)
         try:
             owner_id = int(owner_id)
@@ -116,16 +116,16 @@ class ParseHtm(object):
             topic_id = 0
 
         feed_id = douban_feed_new(
-            self.cid, 
-            rid, 
-            rec_num, 
-            like_num, 
-            title, 
+            self.cid,
+            rid,
+            rec_num,
+            like_num,
+            title,
             self.htm(data),
             owner_id  ,
             topic_id
-        )     
- 
+        )
+
         if _owner or _topic:
             DoubanFeedOwner(id=feed_id, topic=_topic, owner=_owner).save()
 
@@ -140,32 +140,32 @@ class ParseTopicHtm(ParseHtm):
         if html:
             result.append(html)
         user_id = self.user_id(data)
-        topic_reply =  txt_wrap_by('<ul class="topic-reply">','</ul>',data)
-        topic_reply =  txt_wrap_by_all(' <div class="reply-doc">',' class="lnk-reply">回应</a>',topic_reply)
-        
+        topic_reply = txt_wrap_by('<ul class="topic-reply">', '</ul>', data)
+        topic_reply = txt_wrap_by_all(' <div class="reply-doc">', ' class="lnk-reply">回应</a>', topic_reply)
+
         for i in topic_reply:
-            owner_id = txt_wrap_by('<div class="bg-img-green">','</h4>',i)
-            owner_id = txt_wrap_by('<a href="http://www.douban.com/people/','/">',owner_id)
-            if owner_id!=user_id:
+            owner_id = txt_wrap_by('<div class="bg-img-green">', '</h4>', i)
+            owner_id = txt_wrap_by('<a href="http://www.douban.com/people/', '/">', owner_id)
+            if owner_id != user_id:
                 break
-            result.append(txt_wrap_by('</div>','<div class="operation_div"',i)) 
-                    
+            result.append(txt_wrap_by('</div>', '<div class="operation_div"', i))
+
         return '\n'.join(result)
 
     def user_id(self, data):
-        line = txt_wrap_by('<div class="user-face">','">',data)
-        line = txt_wrap_by('"http://www.douban.com/people/','/',line)
+        line = txt_wrap_by('<div class="user-face">', '">', data)
+        line = txt_wrap_by('"http://www.douban.com/people/', '/', line)
         return line
 
     def topic_id(self, data):
-        line = txt_wrap_by('<div class="aside">','">回',data)
-        line = txt_wrap_by('"http://www.douban.com/group/','/',line)
+        line = txt_wrap_by('<div class="aside">', '">回', data)
+        line = txt_wrap_by('"http://www.douban.com/group/', '/', line)
         return line
-    
+
     def title(self, data):
-        title = txt_wrap_by('<tr><td class="tablelc"></td><td class="tablecc"><strong>标题：</strong>','</td>', data)
+        title = txt_wrap_by('<tr><td class="tablelc"></td><td class="tablecc"><strong>标题：</strong>', '</td>', data)
         if not title:
-            title = txt_wrap_by("<title>", "</title>", data)
+            title = txt_wrap_by('<title>', '</title>', data)
         return title
 
 parse_topic_htm = ParseTopicHtm()
@@ -173,10 +173,10 @@ parse_topic_htm = ParseTopicHtm()
 class ParseNoteSiteHtm(ParseHtm):
     cid = CID_DOUBAN_FEED_NOTE
     def htm(self, data):
-        return txt_wrap_by(' class="note-content"><pre>', "</pre>", data)
+        return txt_wrap_by(' class="note-content"><pre>', '</pre>', data)
 
     def topic_id(self, data):
-        line = txt_wrap_by('http://site.douban.com/widget/notes/','/',data) 
+        line = txt_wrap_by('http://site.douban.com/widget/notes/', '/', data)
         return line
 
 parse_note_site_htm = ParseNoteSiteHtm()
@@ -185,11 +185,11 @@ class ParseNotePeopleHtm(ParseHtm):
     cid = CID_DOUBAN_FEED_NOTE
 
     def htm(self, data):
-        return txt_wrap_by('<pre class="note">', "</pre>", data)
+        return txt_wrap_by('<pre class="note">', '</pre>', data)
 
     def user_id(self, data):
-        line = txt_wrap_by('<div class="pic">','">',data)
-        line = txt_wrap_by('"http://www.douban.com/people/','/',line)
+        line = txt_wrap_by('<div class="pic">', '">', data)
+        line = txt_wrap_by('"http://www.douban.com/people/', '/', line)
         return line
 
 
@@ -200,8 +200,8 @@ DOUBAN_REC_PARSE = {
     DOUBAN_REC_CID['topic']:parse_topic,
 }
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     html = """ """
 
-    
+
     print parse_topic_htm.htm(html)
