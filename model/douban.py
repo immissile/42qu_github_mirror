ba9fbfffb19e2f22594eada4c28f2e4918b094d8
zuroc <zsp007@gmail.com>
2012-01-29 21:31:57 +0800

    ifx

--- model/douban.py
+++ model/douban.py
@@ -44,7 +44,7 @@ DOUBAN_REC_CID = {
 
 mc_id_by_douban_url = McCache('IdByDoubanUrl%s')
 mc_id_by_douban_feed = McCache('IdByDoubanFeed%s')
-mc_model_url = "%s.%s"
+mc_model_url = '%s.%s'
 
 class ModelUrl(object):
     @classmethod
@@ -201,50 +201,53 @@ def title_normal(title):
 
 if __name__ == '__main__':
     pass
-#    print DoubanUser.by_url('zuroc')
-#   from zweb.orm import ormiter
-#   for i in ormiter(DoubanUser):
-#       print i.id, i.name, i.url
-#    print dir(DoubanUser.table)
-#    print user_id_by_douban_url("catcabinet")
-#    print len("在非相对论系统中，粒子运动速度远小于光速，它们间的相互作用仍很频繁，参与相互作用的粒子数目较多")
-#    raise
-#    pass
-    is_douban_count = 0
-    not_douban_count = 0
-
-    for i in sorted(DoubanFeed.where(state=DOUBAN_FEED_STATE_TO_REIVEW), key=lambda x:-x.rec-x.like):
-        txt = '\n'.join([i.title, i.htm])
-        is_douban = False
-
-        for word in ('豆瓣', '豆邮', '豆友', '?start=', '>http://www.douban.'):
-            if word in txt:
-                is_douban = True
-                break
-
-        if is_douban:
-            is_douban_count += 1
-        else:
-            not_douban_count += 1
-
-        if not is_douban:
-
-            if i.cid == CID_DOUBAN_FEED_TOPIC:
-                link = 'http://www.douban.com/group/topic/%s'%i.rid
-            elif i.cid == CID_DOUBAN_FEED_NOTE:
-                link = 'http://www.douban.com/note/%s'%i.rid
-
-            print '%60s %5s %5s %s %s'%( link, i.rec, i.like, title_normal(i.title), len(i.htm))
-
-    print is_douban_count, not_douban_count
-
-
-#    for i in """
-#TRUNCATE TABLE douban_feed
-#TRUNCATE TABLE douban_feed_owner
-#TRUNCATE TABLE douban_rec
-#TRUNCATE TABLE douban_user
-#TRUNCATE TABLE douban_user_feed 
-#    """.strip().split('\n'):
-#        if i.strip():
-#            DoubanFeed.raw_sql(i.strip()+';')
+    print "DoubanUser.count()", DoubanUser.count()
+    print "DoubanFeed.count()", DoubanFeed.count()
+#
+##    print DoubanUser.by_url('zuroc')
+##   from zweb.orm import ormiter
+##   for i in ormiter(DoubanUser):
+##       print i.id, i.name, i.url
+##    print dir(DoubanUser.table)
+##    print user_id_by_douban_url("catcabinet")
+##    print len("在非相对论系统中，粒子运动速度远小于光速，它们间的相互作用仍很频繁，参与相互作用的粒子数目较多")
+##    raise
+##    pass
+#    is_douban_count = 0
+#    not_douban_count = 0
+#
+#    for i in sorted(DoubanFeed.where(state=DOUBAN_FEED_STATE_TO_REIVEW), key=lambda x:-x.rec-x.like):
+#        txt = '\n'.join([i.title, i.htm])
+#        is_douban = False
+#
+#        for word in ('豆瓣', '豆邮', '豆友', '?start=', '>http://www.douban.'):
+#            if word in txt:
+#                is_douban = True
+#                break
+#
+#        if is_douban:
+#            is_douban_count += 1
+#        else:
+#            not_douban_count += 1
+#
+#        if not is_douban:
+#
+#            if i.cid == CID_DOUBAN_FEED_TOPIC:
+#                link = 'http://www.douban.com/group/topic/%s'%i.rid
+#            elif i.cid == CID_DOUBAN_FEED_NOTE:
+#                link = 'http://www.douban.com/note/%s'%i.rid
+#
+#            print '%60s %5s %5s %s %s'%( link, i.rec, i.like, title_normal(i.title), len(i.htm))
+#
+#    print is_douban_count, not_douban_count
+#
+#
+##    for i in """
+##TRUNCATE TABLE douban_feed
+##TRUNCATE TABLE douban_feed_owner
+##TRUNCATE TABLE douban_rec
+##TRUNCATE TABLE douban_user
+##TRUNCATE TABLE douban_user_feed 
+##    """.strip().split('\n'):
+##        if i.strip():
+##            DoubanFeed.raw_sql(i.strip()+';')
