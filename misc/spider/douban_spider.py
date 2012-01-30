f76aea625a88fe4345559a9bd3aa3bd4ce0a8b88
zuroc <zsp007@gmail.com>
2012-01-30 15:37:32 +0800

    fix

--- misc/spider/douban_spider.py
+++ misc/spider/douban_spider.py
@@ -9,7 +9,7 @@ def spider(url_list):
         'Cookie': 'bid="i9gsK/lU40A"; ll="108288"; __gads=ID=94ec68b017d7ed73:T=1324993947:S=ALNI_MaYLBDGa57C4diSiOVJspHn0IAVQw; __utma=164037162.1587489682.1327387877.1327387877.1327387877.1; __utmz=164037162.1327387877.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); viewed="7153184"; __utmb=164037162.2.8.1327387877; __utmc=164037162; RT=s=1327387895075&r=http%3A%2F%2Fwww.douban.com%2Fnote%2F196951541%2F; dbcl2="13593891:UKKGQFylj18"; ck="lLgF"'
 
     }
-
+    #headers = {}
     fetcher = NoCacheFetch( headers=headers)
     spider = Rolling( fetcher, url_list )
 
