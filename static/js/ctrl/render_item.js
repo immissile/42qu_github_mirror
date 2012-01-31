992dc973f5ff290f7a34be12768b92a64dd02f90
zuroc <zsp007@gmail.com>
2012-01-31 23:07:55 +0800

    fix

--- static/js/ctrl/render_item.js
+++ static/js/ctrl/render_item.js
@@ -1,8 +1,17 @@
 $.template(
     'note',
     '<div class="readl c">'+
-        '<a rel="${$data[0]}" href="javascript:void(0)" id="fav${$data[0]}" class="fav{{if $data[5]}}ed{{/if}}"></a>' +
-        '<div id="reado${$data[0]}" class="reado"><span class="reada"><span class="title">${$data[1]}</span><span class="rtip">${$data[2]}</span></span></div><div class="zname"><a href="#" rel="${$data[3]}" class="TPH" target="_blank">${$data[4]}</a></div></div>'
+        '<a rel="${$data[0]}" href="javascript:void(0)" id="fav${$data[0]}" class="fav{{if $data[5]}}ed{{/if}}"></a>'+
+        '<div id="reado${$data[0]}" class="reado">'+
+            '<span class="reada">'+
+                '<span class="title">${$data[1]}</span>'+
+                '<span class="rtip">${$data[2]}</span>'+
+            '</span>'+
+        '</div>'+
+        '<div class="zname">'+
+            '<a href="#" rel="${$data[3]}" class="TPH" target="_blank">${$data[4]}</a>'+
+        '</div>'+
+    '</div>'
 )
 
 
