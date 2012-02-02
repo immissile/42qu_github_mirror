7f3588184390c0e5ee91f07f1d969e7b1879986b
wooparadog <guohaochuan@gmail.com>
2012-02-02 17:23:53 +0800

    f

--- model/rec_read.py
+++ model/rec_read.py
@@ -294,7 +294,7 @@ def rec_read_cid(user_id, limit):
     shuffle( result )
     return result
 
-def rec_id_by_cid(cid, offset=0, limit = -1):
+def rec_id_by_cid(cid, limit = -1,offset=0):
     return redis.zrevrange(REDIS_REC_CID%cid, offset, limit)
 
 def rec_change(po_id, old_cid, new_cid):
