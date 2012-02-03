79594c56c04be2dc79fdadae974d3e66a6118784
wooparadog <guohaochuan@gmail.com>
2012-02-03 19:04:14 +0800

    eliminate other head

--- god/_handler.py
+++ god/_handler.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from config import SITE_DOMAIN, SITE_URL
+from config import SITE_DOMAIN, SITE_URL, ADMINISTRATORS
 from zweb._handler.main import LoginBase
 
 class Base(LoginBase):
     def prepare(self):
+        current_user_id = self.current_user_id
+        if current_user_id in ADMINISTRATORS:
+            print "!!!!!"
+        else:
+            print current_user_id, self.request.path
+
         super(Base, self).prepare()
 
     def post(self, *arg, **kwds):
