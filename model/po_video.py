1dc4c5eae67612000ac0336d7bf421f83de00033
zuroc <zsp007@gmail.com>
2012-02-05 14:16:04 +0800

    fix

--- model/po_video.py
+++ model/po_video.py
@@ -5,58 +5,12 @@ from _db import Model, McModel, McCache, McCacheA, McNum, McCacheM
 from cid import CID_VIDEO
 from model.po import po_new , txt_new , is_same_post , STATE_SECRET, STATE_ACTIVE, time_title
 from config import FS_URL
-
-VIDEO_CID_YOUKU = 1
-VIDEO_CID_TUDOU = 2
-VIDEO_CID_SINA = 3
-VIDEO_CID_SLIDESHARE = 4
-
-HTM_YOUKU = '''<embed src="http://static.youku.com/v/swf/qplayer.swf?VideoIDS=%s=&isShowRelatedVideo=false&showAd=0&winType=interior" quality="high" class="video" allowfullscreen="true" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" wmode= "Opaque"></embed>'''
-
-HTM_AUTOPLAY_YOUKU = '''<embed src="http://static.youku.com/v/swf/qplayer.swf?VideoIDS=%s=&isShowRelatedVideo=false&showAd=0&winType=interior&isAutoPlay=true" quality="high" class="video" allowfullscreen="true" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" wmode= "Opaque"></embed>'''
-
-HTM_TUDOU = """<embed src="http://www.tudou.com/v/%s/v.swf" class="video" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="opaque"></embed>"""
-
-HTM_AUTOPLAY_TUDOU = """<embed src="http://www.tudou.com/v/%s&autoPlay=true/v.swf" class="video" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="opaque"></embed>"""
-
-HTM_AUTOPLAY_SINA = """<embed wmode="opaque" src="http://p.you.video.sina.com.cn/swf/quotePlayer20110627_V4_4_41_20.swf?autoplay=1&vid=%s&uid=%s" class="video" allowFullScreen="true" "></embed>"""
-
-HTM_SINA = """<embed wmode="opaque" src="http://p.you.video.sina.com.cn/swf/quotePlayer20110627_V4_4_41_20.swf?vid=%s&uid=%s&autoPlay=0" class="video" allowFullScreen="true" "></embed>"""
-
-HTM_SLIDESHARE = """<embed wmode="opaque" src="%s/swf/ssplayer2.swf?doc=%%s&rel=0" class="video" allowFullScreen="true" "></embed>"""%FS_URL
-
-
-
-VIDEO_CID2HTM = {
-    VIDEO_CID_YOUKU:HTM_YOUKU,
-    VIDEO_CID_TUDOU:HTM_TUDOU,
-    VIDEO_CID_SINA:HTM_SINA,
-    VIDEO_CID_SLIDESHARE:HTM_SLIDESHARE,
-}
-
-VIDEO_CID2HTM_AUTOPLAY = {
-    VIDEO_CID_YOUKU:HTM_AUTOPLAY_YOUKU,
-    VIDEO_CID_TUDOU:HTM_AUTOPLAY_TUDOU,
-    VIDEO_CID_SINA:HTM_AUTOPLAY_SINA,
-    VIDEO_CID_SLIDESHARE:HTM_SLIDESHARE,
-}
-
+from video_swf import video_filter, video_htm, video_htm_autoplay
 
 mc_video_uri = McCache('VideoUri:%s')
 
 class Video(Model):
     pass
-
-
-def video_htm_autoplay(cid, id):
-    if cid == VIDEO_CID_SINA:
-        return VIDEO_CID2HTM_AUTOPLAY[cid] % tuple(video_uri(id).split('-'))
-    return VIDEO_CID2HTM_AUTOPLAY[cid] % video_uri(id)
-
-def video_htm(cid, id):
-    if cid == VIDEO_CID_SINA:
-        return VIDEO_CID2HTM[cid] % tuple(video_uri(id).split('-'))
-    return VIDEO_CID2HTM[cid] % video_uri(id)
 
 
 @mc_video_uri('{id}')
@@ -73,27 +27,6 @@ def video_new(id, uri):
     v.save()
     mc_video_uri.set(id, uri)
 
-def video_filter(url):
-    if url.startswith('http://v.youku.com/v_show/id_'):
-        video = url[29:url.rfind('.')]
-        video_site = VIDEO_CID_YOUKU
-    elif url.startswith('http://player.youku.com/player.php/sid/'):
-        video = url[39:url.find('/', 39)]
-        video_site = VIDEO_CID_YOUKU
-    elif url.startswith('http://www.tudou.com/programs/view/'):
-        video = url[35:].rstrip('/')
-        video_site = VIDEO_CID_TUDOU
-    elif url.startswith('http://video.sina.com.cn/v/b/'):
-        video = url[29:url.rfind('.')]
-        video_site = VIDEO_CID_SINA
-    elif url.startswith('http://static.slidesharecdn.com/swf/ssplayer2.swf?'):
-        begin = url.find('doc=')+4
-        video = url[begin:url.find('&', begin)]
-        video_site = VIDEO_CID_SLIDESHARE
-    else:
-        video = None
-        video_site = None
-    return video, video_site
 
 def po_video_new(user_id, name, txt, uri, video_site, state, zsite_id):
 
