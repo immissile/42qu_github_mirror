b373a319d72885c60c69bd37448918c702470cc5
wooparadog <guohaochuan@gmail.com>
2012-02-07 20:15:29 +0800

    f

--- misc/backup/table_google.sql
+++ misc/backup/table_google.sql
@@ -124,41 +124,6 @@ CREATE TABLE `import_feed` (
   KEY `Index_2` (`state`)
 ) ENGINE=MyISAM DEFAULT CHARSET=binary;
 /*!40101 SET character_set_client = @saved_cs_client */;
-DROP TABLE IF EXISTS `netease_album`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `netease_album` (
-  `id` int(11) unsigned NOT NULL,
-  `title` varchar(256) DEFAULT NULL,
-  `user_id` int(11) unsigned DEFAULT NULL,
-  `place` varchar(20) DEFAULT NULL,
-  `published` varchar(20) DEFAULT NULL,
-  `url` varchar(1024) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-DROP TABLE IF EXISTS `netease_photo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `netease_photo` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(1024) NOT NULL,
-  `album_id` int(11) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `url_idx` (`url`(333)) USING HASH
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-DROP TABLE IF EXISTS `netease_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `netease_user` (
-  `id` int(11) unsigned NOT NULL,
-  `url` varchar(1024) DEFAULT NULL,
-  `nickname` varchar(256) DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `rss_po`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
