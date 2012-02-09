cc2a112caa3d479d8bd4a88d64de5718e8b5987b
zuroc <zsp007@gmail.com>
2012-02-09 19:14:45 +0800

    fix

--- misc/once/import_tags_redis.py
+++ misc/once/import_tags_redis.py
@@ -8,23 +8,23 @@ from zweb.orm import ormiter
 from model.cid import CID_TAG
 from model._db import redis
 from import_tags import TMP_REDIS
-from model.zsite_fav import zsite_fav_count_by_zsite  
+from model.zsite_fav import zsite_fav_count_by_zsite
 
 if __name__ == '__main__':
     for i in ormiter(Zsite, 'cid=%s'%CID_TAG):
         print i.id, i.name
         #tag_tag.set(i.name,i.id)
-        rank = redis.hget(TMP_REDIS,i.name)
-        auto_complete_tag.append(i.name, i.id,rank)
-    
+        rank = redis.hget(TMP_REDIS, i.name)
+        auto_complete_tag.append(i.name, i.id, rank)
+
     redis.delete(TMP_REDIS)
 
-    for id,name_rank in redis.hgetall(auto_complete_tag.ID2NAME).iteritems():
-        zsite = Zsite.mc_get(id) 
+    for id, name_rank in redis.hgetall(auto_complete_tag.ID2NAME).iteritems():
+        zsite = Zsite.mc_get(id)
         if zsite:
-            score =  zsite_fav_count_by_zsite(zsite)
-            name = name_rank.rsplit('`',1)[0]
-            redis.set(auto_complete_tag.ID2NAME,id,'name`%s'%score)
+            score = zsite_fav_count_by_zsite(zsite)
+            name = name_rank.rsplit('`', 1)[0]
+            redis.hset(auto_complete_tag.ID2NAME, id, '%s`%s'%(name, score))
 
 
 
