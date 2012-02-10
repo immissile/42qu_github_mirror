b199eb16f62bca868746059b3ce08e124f6c6fea
zuroc <zsp007@gmail.com>
2012-02-10 12:06:37 +0800

    fix

--- misc/once/import_default_rec_rank.py
+++ misc/once/import_default_rec_rank.py
@@ -12,7 +12,8 @@ from os import path
 from yajl import loads
 from model.po_by_tag import  tag_by_name
 from zkit.bot_txt import txt_map
-from model.zsite import zsite_new
+from model.zsite import zsite_new, ZSITE_STATE_ACTIVE
+
 
 CURRNET_PATH = path.dirname(path.abspath(__file__))
 TMP_REDIS = 'tmp_import_redis'
@@ -21,21 +22,22 @@ import re
 RE_X = re.compile('#.+?#')
 
 if __name__ == '__main__':
-    kv = redis.hgetall(auto_complete_tag.ID2NAME)
-    for id, name in kv.iteritems():
-        name = name.split('`')[0]
-        zsite_new(name, CID_TAG, id=id)
-        print id, name
- 
-#    with open(path.join(CURRNET_PATH, 'tag_score.js')) as d:
-#        score_dict = loads(d.read())
-#
-#        nr_dict = dict(
-#            (i.name, i.id) for i in
-#            Zsite.where('cid=%s', CID_TAG)
-#        )
-#        print nr_dict
-#        for pos, (name , rank) in enumerate(sorted(score_dict.iteritems(), key=lambda x:-x[1])[:512]):
-#            name = RE_X.sub('', name)
-#            print pos, name, rank, nr_dict[str(name)]
+    #Zsite.where('cid=%s', CID_TAG).delete()
+    #kv = redis.hgetall(auto_complete_tag.ID2NAME)
+    #for id, name in kv.iteritems():
+    #    name = name.split('`')[0]
+    #    zsite_new(name, CID_TAG, ZSITE_STATE_ACTIVE, id)
+    #    print id, name
+
+     
+    with open(path.join(CURRNET_PATH, 'tag_score.js')) as d:
+        score_dict = loads(d.read())
+
+        nr_dict = dict(
+            (i.name, i.id) for i in
+            Zsite.where('cid=%s', CID_TAG)
+        )
+        for pos, (name , rank) in enumerate(sorted(score_dict.iteritems(), key=lambda x:-x[1])[:1024]):
+            name = RE_X.sub('', name)
+            print pos, name, rank, nr_dict[str(name)]
 
