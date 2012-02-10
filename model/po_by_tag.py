e1c12de4712129834cebbc7b553115e536bf8d12
zuroc <zsp007@gmail.com>
2012-02-10 19:52:01 +0800

    fix

--- model/po_by_tag.py
+++ model/po_by_tag.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-from _db import  McModel, McLimitA, McNum
+from _db import  McModel, McLimitA, McNum, McCacheA
 from model.po_json import po_json, Po
 from po import Po
 from cid import CID_NOTE, CID_TAG, CID_USER
@@ -16,12 +16,15 @@ from zsite_json import zsite_json
 
 
 mc_po_id_list_by_tag_id = McLimitA('PoIdListByTagId.%s', 512)
+mc_tag_id_list_by_po_id = McCacheA("TagIdListByPoId.%s")
 
 class PoZsiteTag(McModel):
     pass
 
-def zsite_tag_po_new(zsite_id, po, rank):
-    tag_po = PoZsiteTag.get_or_create(po_id=po.id, cid=po.cid, zsite_id=zsite_id)
+def zsite_tag_po_new(zsite_id, po, rank=1):
+    po_id = po.id
+
+    tag_po = PoZsiteTag.get_or_create(po_id=po_id, cid=po.cid, zsite_id=zsite_id)
     tag_po.rank = rank
     tag_po.save()
 
@@ -32,7 +35,7 @@ def zsite_tag_po_new(zsite_id, po, rank):
         user_rank.rank += 1
         user_rank.save()
 
-    mc_flush(zsite_id)
+    mc_flush(zsite_id, po_id)
 
     return tag_po
 
@@ -40,7 +43,12 @@ zsite_tag_po_count = McNum(
     lambda tag_id: PoZsiteTag.where(zsite_id=tag_id).count(),
     'ZsiteTagPoCount:%s'
 )
-def mc_flush(zsite_id):
+
+def mc_flush(zsite_id, po_id):
+    mc_flush_by_zsite_id(zsite_id)
+    mc_flush_by_po_id(po_id)
+
+def mc_flush_by_zsite_id(zsite_id):
     zsite_tag_po_count.delete(zsite_id)
     mc_po_id_list_by_tag_id.delete(zsite_id)
 
@@ -72,18 +80,42 @@ def zsite_tag_po_new_by_name(tag_name, po, rank):
     tag = tag_by_name(tag_name)
     return zsite_tag_po_new(tag.id, po, rank)
 
-def tag_po_rm_by_po_id(po_id):
-    for tag in PoZsiteTag.where(po_id=po_id):
-        tag.delete()
-        mc_flush(tag.zsite_id)
+def tag_rm_by_po_id(po_id):
+    for tag_id in tag_id_list_by_po_id(po_id):
+        PoZsiteTag.where(zsite_id=tag_id).delete()
+        mc_flush_by_zsite_id(tag_id)
+    mc_flush_by_po_id(po_id)
+
+@mc_tag_id_list_by_po_id("{po_id}")
+def tag_id_list_by_po_id(po_id):
+    zsite_id_list = PoZsiteTag.where(po_id=po_id).col_list(col='zsite_id')
+    return zsite_id_list(po_id) 
+
+def mc_flush_by_po_id(po_id):
+    mc_tag_id_list_by_po_id.delete(po_id)
 
 def tag_list_by_po_id(po_id):
-    zsite_id_list = PoZsiteTag.where(po_id=po_id).col_list(col='zsite_id')
+    zsite_id_list = tag_id_list_by_po_id(po_id)
     return Zsite.mc_get_list(zsite_id_list)
 
-def po_id_tag_id_list_new(po, tag_id_list):
-    pass
-    #tag_po_rm_by_po_id(po.id)
+def po_tag_id_list_new(po_id, tag_id_list):
+    new_tag_id_list = set(map(int, tag_id_list))
+    old_tag_id_list = set(tag_id_list_by_po_id)
+    
+    to_add = new_tag_id_list - old_tag_id_list
+    to_rm = old_tag_id_list - new_tag_id_list
+
+
+    for tag_id in to_rm:
+        PoZsiteTag.where(zsite_id=tag_id).delete()
+        mc_flush_by_zsite_id(tag_id)
+    
+    for tag_id in to_add:
+        zsite_tag_po_new(zsite_id, po)
+
+
+
+    #tag_rm_by_po_id(po.id)
 
     #tag_id_list = tag_id_list.split(',')
     #for tag in tag_id_list:
