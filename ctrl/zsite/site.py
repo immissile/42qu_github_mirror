c0ca33694c4f5562bdd4660e6a2e6e8b2cf51ae7
zuroc <zsp007@gmail.com>
2012-02-10 15:20:45 +0800

    fix

--- ctrl/zsite/site.py
+++ ctrl/zsite/site.py
@@ -178,7 +178,7 @@ class MySite( SiteListBase, MyBase, ZsiteBase):
 class Read(LoginBase):
     def get(self, n=1):
         zsite = self.zsite
-        if self.cid == CID_SITE:
+        if zsite.cid == CID_SITE:
             current_user = self.current_user
             current_user_id = self.current_user_id
             zsite_fav_get_and_touch(zsite, current_user_id)
