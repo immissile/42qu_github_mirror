bac391a3f8c69bd96c54cc68015a76395dee03de
zuroc <zsp007@gmail.com>
2012-02-11 23:57:42 +0800

    fix

--- misc/spider/zhihu_topic_page.py
+++ misc/spider/zhihu_topic_page.py
@@ -8,6 +8,7 @@ from zkit.pprint import pformat
 from zhihu_topic_data import ZHIHU_TOPIC
 from operator import itemgetter
 from zkit.spider import Rolling, Fetch, MultiHeadersFetch, GSpider, NoCacheFetch
+from zkit.bot_txt import txt_wrap_by, txt_wrap_by_all
 
 ZHIHU_TOPIC.sort(key=lambda x:-x[4])
 
@@ -19,7 +20,8 @@ def zhihu_topic_url():
 
 
 def zhihu_topic_parser(html, url):
-    print html
+    print txt_wrap_by('<title>'," - 知乎</title>",html)
+
 
 COOKIE = (
     '__utmz=155987696.1328970860.126.10.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/register; __utma=155987696.922373387.1325132903.1328965603.1328970860.126; __utmv=155987696.Logged%20In; q_c0=MjE4NjYyfFV1YjRvdGczalJFOWlCd0g=|1328973675|4be5e7eae08c14109a129099780c733abe350bba; __utmb=155987696.90.9.1328973170544; __utmc=155987696',
@@ -34,7 +36,7 @@ def spider(url_list):
     debug = False
     debug = True
 
-    spider_runner = GSpider(spider, workers_count=30, debug=debug)
+    spider_runner = GSpider(spider, workers_count=500, debug=debug)
     spider_runner.start()
 
 spider(zhihu_topic_url())
