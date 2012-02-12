84c6eb1dfca31bc0a793125c26cb85939545cc53
zuroc <zsp007@gmail.com>
2012-02-12 20:58:18 +0800

    fix

--- misc/once/import_tags_redis.py
+++ misc/once/import_tags_redis.py
@@ -12,29 +12,24 @@ from os import path
 from yajl import loads
 from model.po_by_tag import  tag_by_str
 from zkit.bot_txt import txt_map
-from misc.spider.zhihu_topic_id_rank import ID2RANK 
+from misc.spider.zhihu_topic_id_rank import ID2RANK
 
 CURRNET_PATH = path.dirname(path.abspath(__file__))
 TMP_REDIS = 'tmp_import_redis'
 import re
 
-RE_X = re.compile('#.+?#')
 
 if __name__ == '__main__':
+    name_id = {}
     for i in Zsite.where(cid=CID_TAG):
-        rank = ID2RANK[i.id]
-        print rank
-#
-#        for name , rank in score_dict.iteritems():
-#            name = RE_X.sub('', name)
-#            print name    
-#            id =  tag_by_str(name).id
-#            autocomplete_tag.append(name, id, rank)
-#            name_id[name] = id
-#            print name, rank, "rank"
-#
-#        for name , id in name_id.iteritems():
-#            redis.hset(autocomplete_tag.ID2NAME, id, '%s`%s'%(name, 0))
-#            print name, rank, "id"
+        id = i.id
+        rank = ID2RANK.get(id, 0)
+        name = i.name
+        autocomplete_tag.append(name, id, rank)
+        name_id[name] = id
+
+    for name , id in name_id.iteritems():
+        redis.hset(autocomplete_tag.ID2NAME, id, '%s`%s'%(name, 0))
+        print name, rank, 'id'
 
 
