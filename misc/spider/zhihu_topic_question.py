c766fc6e4ca22b1d0c1f468f5de40b37844361ec
zuroc <zsp007@gmail.com>
2012-02-14 01:47:59 +0800

    fix

--- misc/spider/zhihu_topic_question.py
+++ misc/spider/zhihu_topic_question.py
@@ -34,6 +34,7 @@ how_long = HowLong(len(ID2RANK))
 def zhihu_topic_title(url , html):
     if '请输入图中的数字：' in html:
         print '请输入图中的数字：'
+        return
 
     if 'offset=' in url and '<!doctype'  in html:
         return False
@@ -58,6 +59,8 @@ def zhihu_topic_parser(html, url):
     #print loads(txt)[:2][0][0]
     question_id_list = filter(str.isdigit, txt_wrap_by_all('href="/question/', '">', html))
     feed_id_list = txt_wrap_by_all('id="feed-', '">', html)
+#    for i in feed_id_list:
+#        yield zhihu_question_parser, "http://www.zhihu.com/question/%s"%i
     if len(feed_id_list) >= 20:
         last_one = feed_id_list[-1]
         yield zhihu_topic_feed, {'url':url, 'data':urlencode(dict(start=last_one, offset=20))}, 20
@@ -73,27 +76,33 @@ def zhihu_topic_feed(html, url, offset):
     #o = loads(html)
     #pprint(o)
     id_list = txt_wrap_by_all('href=\\"/question/', '\\">', html)
+#    for i in id_list:
+#        yield zhihu_question_parser, "http://www.zhihu.com/question/%s"%i
     if len(id_list) == 20:
         offset += 20
         yield zhihu_topic_feed, {'url':url['url'], 'data':urlencode(dict(start=id_list[-1], offset=offset))}, offset
 
 
 
-#def zhihu_question_parser(html, url):
+def zhihu_question_parser(html, url):
 #    print url
+    pass
 
 COOKIE = (
+    """__utma=155987696.1921085572.1329154757.1329154757.1329154757.1; __utmb=155987696.14.9.1329154905385; __utmc=155987696; __utmz=155987696.1329154757.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjIwMDA0fGVHSXZJTFl1ZGxRVHBMSGQ=|1329154797|58efd2296add506f3d88428926aaeae92587a465""",
     '__utmz=155987696.1328970860.126.10.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/register; __utma=155987696.922373387.1325132903.1328965603.1328970860.126; __utmv=155987696.Logged%20In; q_c0=MjE4NjYyfFV1YjRvdGczalJFOWlCd0g=|1328973675|4be5e7eae08c14109a129099780c733abe350bba; __utmb=155987696.90.9.1328973170544; __utmc=155987696',
     '_xsrf=921952144fdd481582474494f379a7bd; __utma=155987696.322433586.1328975813.1328975813.1328975813.1; __utmb=155987696.64.9.1328977105353; __utmc=155987696; __utmz=155987696.1328975813.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjE4NzM2fExGUUlaOEtTNnp3V2dXWEw=|1328976244|d2ffe0742678bf469312a7fa3638ccbbe70046df',
     '__utma=155987696.426164303.1328980648.1329003296.1329007976.3; __utmz=155987696.1329007976.3.2.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=155987696.Logged%20In; _xsrf=c0ea4fa60d62410983766dda27adfd24; __utmc=155987696; q_c0=MjE4Nzg4fEx4QTlvUlFsdVhGZmJId1g=|1329008428|12e51876ed895fe84c38d7430fd02728cf256a3d',
+    """__utma=155987696.426164303.1328980648.1329130544.1329152677.14; __utmz=155987696.1329049453.9.5.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/topic/%E8%AE%A1%E7%AE%97%E7%94%9F%E7%89%A9%E5%AD%A6; __utmv=155987696.Logged%20In; _xsrf=a7839ee1b6124c78b347f54d781c1ace; __utmb=155987696.27.9.1329154417037; __utmc=155987696; q_c0=MjIwMDAzfGFwbm9GbjMxNG5qZkVmSFA=|1329154405|e0e8bef5982dc80930ed1e64ee941b8e186d18fa""",
 )
+
 
 def spider(url_list):
 #    fetcher = MultiHeadersFetch(  headers=tuple( { 'Cookie': i } for i in COOKIE))
     fetcher = Fetch(
         '/tmp',
         tuple( { 'Cookie': i } for i in COOKIE),
-        3.33,
+        2.6,
         zhihu_topic_title
     )
     spider = Rolling( fetcher, url_list )
