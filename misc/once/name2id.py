b4802832c12c62c9ee55bb67771f9ec4624b40df
zuroc <zsp007@gmail.com>
2012-02-13 15:26:34 +0800

    fix

--- misc/once/name2id.py
+++ misc/once/name2id.py
@@ -23,53 +23,56 @@ from zkit.pprint import pprint
 #    print i
 #    i.delete()
 
-ID2MY = dict((k,v) for k,v in ID2MY.iteritems() if Zsite.mc_get(v))
-print """
-import _env
-ID2MY = 
-""",
-pprint(ID2MY)
-print """
-
-if __name__ == "__main__":
-    
-    url2id = {}
-    for i in ZHIHU_TOPIC:
-        url = i[2] or i[1]
-        id = i[0]
-        rank = i[-1]
-
-        if id in ID2MY:
-            url2id[ID2MY[id] ] = rank 
-
-
-    from zkit.pprint import pprint
-
-    pprint(url2id)
-#"""
-#RESULT = {}
-#for tag in Zsite.where(cid=CID_TAG):
-#    name_list = map(str.strip, tag.name.split('/'))
-#    for name in name_list:
-#        RESULT[name.replace('·', '.').lower()] = tag.id
+#ID2MY = dict((k,v) for k,v in ID2MY.iteritems() if Zsite.mc_get(v))
+#print """
+#import _env
+#ID2MY = 
+#""",
+#pprint(ID2MY)
+#print """
 #
-#for i in ZHIHU_TOPIC:
-#    id = i[0]
-#    if id not in ID2MY:
-#        continue
-#    rename = i[-1]
-#    for name in rename:
-#        name = name.lower()
-#        if name in RESULT and RESULT[name] != ID2MY[id]:
-#            zsite = Zsite.mc_get(ID2MY[id])
-#            print zsite.id, zsite.name
-#            zsite = Zsite.mc_get(RESULT[name])
-#            print zsite.id, zsite.name
-#        else:
-#            continue
+#if __name__ == "__main__":
+#    
+#    url2id = {}
+#    for i in ZHIHU_TOPIC:
+#        url = i[2] or i[1]
+#        id = i[0]
+#        rank = i[-1]
+#
+#        if id in ID2MY:
+#            url2id[ID2MY[id] ] = rank 
+#
+#
+#    from zkit.pprint import pprint
+#
+#    pprint(url2id)
+#"""
+RESULT = {}
+for tag in Zsite.where(cid=CID_TAG):
+    name_list = map(str.strip, tag.name.split('/'))
+    for name in name_list:
+        RESULT[name.replace('·', '.').lower()] = tag.id
+
+for i in ZHIHU_TOPIC:
+    id = i[0]
+    if id not in ID2MY:
+        continue
+    rename = i[-1]
+    for name in rename:
+        name = name.lower()
+        if name in RESULT and RESULT[name] != ID2MY[id]:
+            zsite = Zsite.mc_get(ID2MY[id])
+            print zsite.id, zsite.name
+            zsite = Zsite.mc_get(RESULT[name])
+            print zsite.id, zsite.name
+        else:
+            continue
 
 
-
-#pprint(RESULT)
+print """
+#coding:utf-8
+TAG2ID = 
+""",
+pprint(RESULT)
 
 
