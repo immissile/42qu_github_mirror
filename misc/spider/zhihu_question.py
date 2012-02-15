b88c2f593454e75fd8c05eaece09a7e971345ec6
zuroc <zsp007@gmail.com>
2012-02-16 00:23:57 +0800

    fix

--- misc/spider/zhihu_question.py
+++ misc/spider/zhihu_question.py
@@ -40,19 +40,25 @@ def zhihu_topic_title(url , html):
     return r
 
 COOKIE = (
-"""__utma=155987696.1921085572.1329154757.1329154757.1329154757.1; __utmb=155987696.14.9.1329154905385; __utmc=155987696; __utmz=155987696.1329154757.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjIwMDA0fGVHSXZJTFl1ZGxRVHBMSGQ=|1329154797|58efd2296add506f3d88428926aaeae92587a465""",
-'__utmz=155987696.1328970860.126.10.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/register; __utma=155987696.922373387.1325132903.1328965603.1328970860.126; __utmv=155987696.Logged%20In; q_c0=MjE4NjYyfFV1YjRvdGczalJFOWlCd0g=|1328973675|4be5e7eae08c14109a129099780c733abe350bba; __utmb=155987696.90.9.1328973170544; __utmc=155987696',
-'_xsrf=921952144fdd481582474494f379a7bd; __utma=155987696.322433586.1328975813.1328975813.1328975813.1; __utmb=155987696.64.9.1328977105353; __utmc=155987696; __utmz=155987696.1328975813.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjE4NzM2fExGUUlaOEtTNnp3V2dXWEw=|1328976244|d2ffe0742678bf469312a7fa3638ccbbe70046df',
-'__utma=155987696.426164303.1328980648.1329003296.1329007976.3; __utmz=155987696.1329007976.3.2.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=155987696.Logged%20In; _xsrf=c0ea4fa60d62410983766dda27adfd24; __utmc=155987696; q_c0=MjE4Nzg4fEx4QTlvUlFsdVhGZmJId1g=|1329008428|12e51876ed895fe84c38d7430fd02728cf256a3d',
-"""__utma=155987696.426164303.1328980648.1329130544.1329152677.14; __utmz=155987696.1329049453.9.5.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/topic/%E8%AE%A1%E7%AE%97%E7%94%9F%E7%89%A9%E5%AD%A6; __utmv=155987696.Logged%20In; _xsrf=a7839ee1b6124c78b347f54d781c1ace; __utmb=155987696.27.9.1329154417037; __utmc=155987696; q_c0=MjIwMDAzfGFwbm9GbjMxNG5qZkVmSFA=|1329154405|e0e8bef5982dc80930ed1e64ee941b8e186d18fa""",
+"""_xsrf=da3db09b91514bba9938504036341fe1; __utma=155987696.321760361.1329322590.1329322590.1329322590.1; __utmb=155987696.11.9.1329322707028; __utmc=155987696; __utmz=155987696.1329322590.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjE4NjYyfFV1YjRvdGczalJFOWlCd0g=|1329322681|199897e47544f20d0e26af99f06bb2ef3169ae01""",
+
+"""Cookie: __utma=155987696.225301714.1329322755.1329322755.1329322755.1; __utmb=155987696.4.10.1329322755; __utmc=155987696; __utmz=155987696.1329322755.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjE4NzM2fExGUUlaOEtTNnp3V2dXWEw=|1329322766|6dbbb4c6adbb8c4faa60c5fc84940cc0d9635a8d""",
+
+"""Cookie: _xsrf=3b6911a3e7a14d33b9853c463e6b8967; __utma=155987696.1049756401.1329322821.1329322821.1329322821.1; __utmb=155987696.7.9.1329322838469; __utmc=155987696; __utmz=155987696.1329322821.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjE4Nzg4fEx4QTlvUlFsdVhGZmJId1g=|1329322826|bd290c68d942dd308a43792f7c56a12236c46fc8""",
+
+"""Cookie: __utma=155987696.1049756401.1329322821.1329322821.1329322821.1; __utmb=155987696.13.9.1329322888421; __utmz=155987696.1329322821.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; __utmc=155987696; q_c0=MjIwMDAzfGFwbm9GbjMxNG5qZkVmSFA=|1329322886|d8b27e8388545d7b845faa7cdc10adac05420f6d""",
+
+"""Cookie: _xsrf=af5b78fb77e842258c87716af77115c7; __utma=155987696.1623286680.1329322924.1329322924.1329322924.1; __utmb=155987696.4.10.1329322924; __utmc=155987696; __utmz=155987696.1329322924.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=155987696.Logged%20In; q_c0=MjIwMDA0fGVHSXZJTFl1ZGxRVHBMSGQ=|1329322930|a98a0b215fb8dd6f698193149447c98e352ef13c""",
+
+
 )
 
 
 def spider(url_list):
     fetcher = Fetch(
         '/tmp',
-        tuple( { 'Cookie': i } for i in COOKIE),
-        5,
+        tuple( { 'Cookie': i.replace('Cookie:','').strip() } for i in COOKIE),
+        30,
         zhihu_topic_title
     )
     spider = Rolling( fetcher, url_list )
