6a621922ca0502fb48442fb5c47e48d85aa544a8
zuroc <zsp007@gmail.com>
2012-02-15 21:42:56 +0800

    fix

--- misc/spider/zhihu_question_print.py
+++ misc/spider/zhihu_question_print.py
@@ -5,8 +5,12 @@ from zhihu_topic_data_with_follow import ZHIHU_TOPIC
 from zkit.pprint import pprint
 from name2id import NAME2ID
 from zdata.tag.name_tidy import name_tidy
+from zhihu_topic_url2id import ID2MY
 
 id2topic = dict([(i[1], i[0]) for i in ZHIHU_TOPIC])
+
+myidset = set(NAME2ID.itervalues())
+
 
 with open('zhihu_question_dumped.json') as zhihu_question_dump:
     for line in zhihu_question_dump:
@@ -19,8 +23,16 @@ with open('zhihu_question_dumped.json') as zhihu_question_dump:
                 tag = name_tidy(tag)
                 id = NAME2ID.get(tag, 0)
             else:
-                pass
+                if id in ID2MY:
+                    id = ID2MY[id]
+                    print id
+                    if id not in myidset:
+                        id = 0
+                else:
+                    id = 0 
             if not id:
+                print tag
+                raw_input()
                 continue
 
 if __name__ == '__main__':
