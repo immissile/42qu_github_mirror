d1f002eb5087eeb015658d391f93cb294a82f52f
zuroc <zsp007@gmail.com>
2012-02-17 15:02:38 +0800

    fix

--- misc/spider/zhihu_question_load.py
+++ misc/spider/zhihu_question_load.py
@@ -1,35 +1,46 @@
 #coding:utf-8
 
 import _env
+from hashlib import md5
+from yajl import loads
 from glob import glob
 from zkit.bot_txt import txt_wrap_by, txt_wrap_by_all
 from zkit.htm2txt import unescape, htm2txt
+from os.path import exists
 
 def parse_content(txt):
-    id = txt_wrap_by('<a href="/question/', '/log" class="xrv">', txt)
-    t = unescape(txt_wrap_by('<title>', ' - 知乎</title>', txt))
+    #id = txt_wrap_by('<a href="/question/', '/log" class="xrv">', txt)
+    #t = unescape(txt_wrap_by('<title>', ' - 知乎</title>', txt))
     tlist = txt_wrap_by_all('<div class="xmrw">', '</div>', txt)
 
-    print id
-    for i in tlist:
-        print htm2txt(i)
+    return [htm2txt(i) for i in tlist]
 
-    #raw_input() 
+def zhihu_to_dump():
+    with open('zhihu_question_to_dump.json') as zhihu_question_dump:
+        for line in zhihu_question_dump:
+            line = loads(line)
+            key = line[1]
+            filename = md5(key).hexdigest()
+            path = '/tmp/www.zhihu.com/%s'%filename
+            if exists(path):
+                yield line_parser(path, line)
+            else:
+                pass
 
+def line_parser(path, line):
+    if exists(path):
+        with open(path) as f:
+            t = f.read()
+            if '<h3>邀请别人回答问题</h3>' in t:
+                s = parse_content(t)
+                return line[-2], line[2], s
 
+    return line[-2], line[2], line[-1]
 
-
-
-filelist = glob('/tmp/www.zhihu.com/*')
-
-for i in filelist:
-    with open(i) as f:
-        t = f.read()
-        if '<h3>邀请别人回答问题</h3>' in t:
-            parse_content(t)
 
 if __name__ == '__main__':
     pass
 
 
-
+    for i in zhihu_to_dump():
+        print i[0], i[1]
--- misc/spider/zhihu_question_tag.py
+++ misc/spider/zhihu_question_tag.py
@@ -8,6 +8,7 @@ from name2id import NAME2ID
 from zdata.tag.name_tidy import name_tidy
 from zhihu_topic_url2id import ID2MY
 from itertools import chain
+from zhihu_question_load import zhihu_to_dump
 
 id2topic = dict([(i[1], i[0]) for i in ZHIHU_TOPIC])
 
@@ -45,23 +46,45 @@ def txt_tag(filename):
     with open(filename) as zhihu_question_dump:
         for line in zhihu_question_dump:
             line = loads(line)
-            tag_list = tag_id_list_by_str_list(line[-2])
-            yield line[2], tag_list
-            for t in line[-1]:
-                yield t, tag_list
+            yield line[-2], line[2], line[-1]
 
+        #    tag_list = tag_id_list_by_str_list(line[-2])
+        #    yield line[2], tag_list
+        #    for t in line[-1]:
+        #        yield t, tag_list
+
+
+def txt_tag_id_list():
+    for tag_string , title, txt_list in chain(
+        zhihu_to_dump(),
+        txt_tag('zhihu_question_to_dump.json'),
+    ):
+        tag_list = tag_id_list_by_str_list(tag_string)
+
+        yield title, tag_list
+        for i in txt_list:
+            yield i, tag_list
 
 if __name__ == '__main__':
     pass
 
-    for txt, tag_list in chain(
-        txt_tag('zhihu_question_dumped.json'),
-        txt_tag('zhihu_question_to_dump.json')
-    ):
-        print txt
-        for i in tag_list:
-            print myiddict[i],
-        print '\n'
+    for txt , tag_list in txt_tag_id_list():
+        pass
+
+#    for i in :
+#        yield i[0], i[]
+
+        #yield line[2], tag_list
+        #for t in line[-1]:
+        #    yield t, tag_list
+        #for tag in tag_string:
+        #    print tag
+
+
+#        print txt
+#        for i in tag_list:
+#            print myiddict[i],
+#        print '\n'
 
 
 
