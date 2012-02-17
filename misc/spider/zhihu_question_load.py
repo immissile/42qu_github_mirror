b95d5942481f3c5258bc25669b84e062ca7977f4
zuroc <zsp007@gmail.com>
2012-02-17 12:04:13 +0800

    fix

--- misc/spider/zhihu_question_load.py
+++ misc/spider/zhihu_question_load.py
@@ -5,11 +5,19 @@ from glob import glob
 from zkit.bot_txt import txt_wrap_by, txt_wrap_by_all
 
 def parse_content(txt):
-    t = txt_wrap_by("<title>"," - 知乎</title>")
-    print t
+    id = txt_wrap_by('<a href="/question/', '/log" class="xrv">', txt)
+    t = txt_wrap_by('<title>', ' - 知乎</title>', txt)
+    tlist = txt_wrap_by_all('<div class="xmrw">', '</div>', txt)
+    print id
+    for i in tlist:
+        print i
+    raw_input() 
 
 
-filelist = glob("/tmp/www.zhihu.com/*")
+
+
+
+filelist = glob('/tmp/www.zhihu.com/*')
 
 for i in filelist:
     with open(i) as f:
@@ -17,7 +25,7 @@ for i in filelist:
         if '<h3>邀请别人回答问题</h3>' in t:
             parse_content(t)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pass
 
 
