8740b73b772f044f733254895adec9ee2212d613
zuroc <zsp007@gmail.com>
2012-02-18 11:54:22 +0800

    fix

--- zdata/tag/word2freq.py
+++ zdata/tag/word2freq.py
@@ -19,7 +19,7 @@ redis = redis.Redis(**REDIS_CONFIG)
 def merge():
     topic_count = defaultdict(int)
 
-    f = "word2freq.txt"
+    f = "word2count.txt"
 
     with open(f,"w") as word2freq:
         for key in redis.keys("*"):
@@ -29,17 +29,18 @@ def merge():
                 word2freq.write("%s %s %s\n"%(k,v,key))
 
     with open(f) as freq:
-        for line in freq:
-            line = line.strip()
-            topic , freq , word = line.split(" ",2)
-            topic = int(topic)
-            count = topic_freq_count[topic]
-            if count < 10000:
-                continue
-            freq = int(freq)*500000/count
-            if freq > 0:
-                print topic, freq, word
-
+        with open("topic_freq_word.txt", "w") as topic_freq_word:
+            for line in freq:
+                line = line.strip()
+                topic , freq , word = line.split(" ",2)
+                topic = int(topic)
+                count = topic_freq_count[topic]
+                if count < 10000:
+                    continue
+                freq = int(freq)*500000/count
+                if freq > 0:
+#                    print topic, freq, word
+                    topic_freq_word.write("%s %s %s\n"%(topic, freq, word))
 
 
 
