7dd5d21e2f652b3ca93a1250331207e5f1b48fda
zuroc <zsp007@gmail.com>
2012-02-18 10:52:46 +0800

    fix

--- zdata/tag/topic_word_count_by_freq.py
+++ zdata/tag/topic_word_count_by_freq.py
@@ -4,7 +4,9 @@
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+from collections import defaultdict
 
+topic_freq_count = defaultdict(int)
 
 with open("freq/w2.txt") as freq:
     for line in freq:
@@ -12,7 +14,22 @@ with open("freq/w2.txt") as freq:
         topic , freq , word = line.split(" ",2)
         topic = int(topic)
         freq = int(freq)
-        print topic, freq, word
+        #print topic, freq, word
+        topic_freq_count[topic] += freq
+
+
+
+with open("freq/w2.txt") as freq:
+    for line in freq:
+        line = line.strip()
+        topic , freq , word = line.split(" ",2)
+        topic = int(topic)
+        count = topic_freq_count[topic]
+        if count < 10000:
+            continue
+        freq = freq*500000/count
+        if freq > 0:
+            print topic, freq, word
 
 def main():
     pass
