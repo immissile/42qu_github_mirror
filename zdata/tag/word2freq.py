7d7ee7d738aa5afcb68a229a94eacfa714460c01
zuroc <zsp007@gmail.com>
2012-02-18 14:54:03 +0800

    fix

--- zdata/tag/word2freq.py
+++ zdata/tag/word2freq.py
@@ -28,30 +28,34 @@ def merge():
         for k,v in l.iteritems():
             topic_count[int(k)]+=int(v)
 
-    word_topic_freq = defaultdict(list)
+    #word_topic_freq = defaultdict(list)
 
-    for pos, word in enumerate(keys):
-        l = redis.hgetall(word)
-        for topic, freq in l.iteritems():
-            topic = int(topic)
-            count = topic_count[topic]
-            if count < 10000:
-                continue
-            freq = int(freq)*500000/count
-            if freq > 0:
-                word_topic_freq[word].append((topic, freq))
-        print "2",pos, word
+    with open("word_freq.txt", "w") as word_freq:
+        for pos, word in enumerate(keys):
+            l = redis.hgetall(word)
+            for topic, freq in l.iteritems():
+                topic = int(topic)
+                count = topic_count[topic]
+                if count < 10000:
+                    continue
+                freq = int(freq)*500000/count
+                if freq > 0:
+                    word_freq.write("%s %s %s\n"%(topic, freq, word))
+                #word_topic_freq[word].append((topic, freq))
+        #print "2",pos, word
 
-    with open("word_tf.txt", "w") as word_tf:
-        for pos,(word, tf) in enumerate(word_topic_freq.iteritems()):
-            print "3",pos, word
-            fcount = sum(i[1] for i in tf)
-            t = []
-            s = [word, t]
-            for topic, f in tf:
-                t.append((topic, int(f*10000/fcount)))
+    #with open("word_tf.txt", "w") as word_tf:
+    #    for pos,(word, tf) in enumerate(word_topic_freq.iteritems()):
+    #        print "3",pos, word
+    #        fcount = sum(i[1] for i in tf)
+    #        t = []
+    #        s = [word, t]
+    #        for topic, f in tf:
+    #            rank = int(f*10000/fcount)
+    #            if rank:
+    #                t.append((topic, rank))
 
-            word_tf.write(dumps(s)+"\n")
+    #        word_tf.write(dumps(s)+"\n")
 
 
 
