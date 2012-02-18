83c21ada373450af006d812e1cc40fcee8e2529f
zuroc <zsp007@gmail.com>
2012-02-18 23:39:01 +0800

    fix

--- misc/crontab/feed_import.py
+++ misc/crontab/feed_import.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import _env
-from model.feed_import import feed2po_new, zsite_id_by_douban_user_id, FeedImport, FEED_IMPORT_STATE_INIT, FEED_IMPORT_CID_DICT
+from model.feed_import import feed2po_new, zsite_id_by_douban_user_id, FeedImport, FEED_IMPORT_STATE_WITHOUT_TAG, FEED_IMPORT_STATE_INIT, FEED_IMPORT_STATE_POED, FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR_SYNC, FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR, FEED_IMPORT_STATE_REVIEWED_SYNC
 from config import ZSITE_DOUBAN_ID
 from model.duplicate import Duplicator
 from model.zsite import Zsite
@@ -10,52 +10,42 @@ from zkit.txt import format_txt
 from config import DUMPLICATE_DB_PREFIX
 from zkit.htm2txt import htm2txt
 from zkit.single_process import single_process
+from model.txt_img_fetch import txt_img_fetch
+from url_short import url_short_id
+from douban import DoubanUser, douban_feed_to_review_iter, douban_user_by_feed_id , title_normal
+from site_sync import site_sync_new
+from po_by_tag import zsite_tag_po_new_by_name, po_tag_id_list_new
 
-tag_getter = GetTag()
 
 import_feed_duplicator = Duplicator(DUMPLICATE_DB_PREFIX%'import_feed')
 
 def feed_import_by_douban_feed():
     from model.douban import douban_feed_to_review_iter, DoubanUser
     for i in douban_feed_to_review_iter():
+
+        txt = i.htm.replace(
+            '豆友', '网友'
+        ).replace('豆油', '私信').replace('豆邮', '私信')
+
         feed_import_new(
-            i.title, i.htm, i.link, i.id, ZSITE_DOUBAN_ID
+            i.title, txt, i.link, i.id, ZSITE_DOUBAN_ID
         )
 
-def feed_import_new(title, txt, url, src_id, zsite_id, tag_id_list='', state=FEED_IMPORT_STATE_INIT):
+def feed_import_new(title, txt, url, rid, zsite_id):
     txt = format_txt(htm2txt(txt))
 
     if import_feed_duplicator.txt_is_duplicate(txt):
-        return 
-    
-    if zsite_id == ZSITE_DOUBAN_ID:
-        txt = txt.replace('豆友', '网友').replace('豆油', '私信').replace('豆邮', '私信')
+        return
+
 
     new_feed = FeedImport(
         title=title,
         txt=txt,
         zsite_id=zsite_id,
-        state=state,
-        rid=src_id,
+        rid=rid,
         url=url,
         tag_id_list='',
-        
-    )
-
-    #TODO
-    if not tag_id_list:
-        name_list = tag_getter.get_tag(txt)
-        for tag_name in  name_list:
-            zsite_list.append(Zsite.get(name=tag_name))
-
-    new_feed = FeedImport(
-        title=title,
-        txt=txt,
-        zsite_id=zsite_id,
-        state=state,
-        rid=src_id,
-        url=url,
-        tag_id_list=tag_id_list,
+        state=FEED_IMPORT_STATE_WITHOUT_TAG
     )
 
     new_feed.save()
@@ -63,6 +53,71 @@ def feed_import_new(title, txt, url, src_id, zsite_id, tag_id_list='', state=FEE
 
     return new_feed
 
+
+def feed2po_new():
+    from zweb.orm import ormiter
+    for feed in ormiter(
+            FeedImport,
+            'state>%s and state<%s'%(
+                FEED_IMPORT_STATE_INIT,
+                FEED_IMPORT_STATE_POED
+            )
+        ):
+        feed_new(feed)
+
+def user_by_feed_id_zsite_id(feed_id, zsite_id):
+    if zsite_id == ZSITE_DOUBAN_ID:
+        return douban_user_by_feed_id(feed_id)
+
+def feed_new(feed):
+    txt = txt_img_fetch(feed.txt)
+    feed_user = user_by_feed_id_zsite_id(feed.rid, feed.zsite_id)
+    user_id = zsite_id_by_douban_user_id(feed_user)
+
+    zsite_id = feed.zsite_id
+
+    is_without_author = (
+        (feed.state == FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR_SYNC)
+        or
+        (feed.state == FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR)
+    )
+
+    if is_without_author:
+        user_id = 0
+        zsite_id = 0
+
+    title = title_normal(feed.title)
+    po = po_note_new(user_id, title, txt, zsite_id=zsite_id)
+
+    if po:
+        if not feed_user:
+            feed_user_id = 0
+        else:
+            user = PoMetaUser.get_or_create(name=feed_user.name, cid=zsite_id)
+            user.url = feed_user.id
+            user.save()
+
+            feed_user_id = user.id
+
+        record = PoMeta.get_or_create(id=po.id)
+        record.user_id = feed_user_id
+        record.url_id = url_short_id(feed.url)
+
+        record.save()
+
+        if not is_without_author:
+            po.rid = record.id
+            po.save()
+
+        if feed.state >= FEED_IMPORT_STATE_REVIEWED_SYNC:
+            site_sync_new(po.id)
+
+        feed.state = FEED_IMPORT_STATE_POED
+        feed.save()
+
+        cid = feed.cid
+
+        po_tag_id_list_new(po, feed.tag_id_list.split(' '), cid)
 
 @single_process
 def main():
--- model/feed_import.py
+++ model/feed_import.py
@@ -4,11 +4,7 @@
 import _env
 from _db import Model, McModel, redis
 from po import po_note_new
-from douban import DoubanUser, douban_feed_to_review_iter, douban_user_by_feed_id , title_normal
-from model.txt_img_fetch import txt_img_fetch
 from kv import Kv
-from url_short import url_short_id
-from site_sync import site_sync_new
 from rec_read import  rec_read_new, REDIS_REC_CID_DICT
 from po_by_tag import zsite_tag_po_new_by_name, po_tag_id_list_new
 from part_time_job import part_time_job_new
@@ -17,14 +13,13 @@ from zrank.sorts import hot
 
 
 FEED_IMPORT_STATE_RM = 0
-FEED_IMPORT_STATE_INIT = 10
-
-FEED_IMPORT_STATE_REVIEWED = 20
-FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR = 30
-FEED_IMPORT_STATE_REVIEWED_SYNC = 40
-FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR_SYNC = 50
-
-FEED_IMPORT_STATE_POED = 60 
+FEED_IMPORT_STATE_WITHOUT_TAG = 10
+FEED_IMPORT_STATE_INIT = 20
+FEED_IMPORT_STATE_REVIEWED = 30
+FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR = 40
+FEED_IMPORT_STATE_REVIEWED_SYNC = 50
+FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR_SYNC = 60
+FEED_IMPORT_STATE_POED = 70 
 
 
 
@@ -47,9 +42,6 @@ def user_url_by_po_meta_user_id(id):
         if user.cid == ZSITE_DOUBAN_ID:
             return 'http://www.douban.com/people/%s'%user_id.url
 
-def user_by_feed_id_zsite_id(feed_id, zsite_id):
-    if zsite_id == ZSITE_DOUBAN_ID:
-        return douban_user_by_feed_id(feed_id)
 
 def feed_next():
     fdlist = FeedImport.where(state=FEED_IMPORT_STATE_INIT)[1:2]
@@ -76,64 +68,8 @@ def zsite_id_by_douban_user_id(douban_user):
         zsite_id = 10001518
     return zsite_id
 
-def feed_new(feed):
-    txt = txt_img_fetch(feed.txt)
-    feed_user = user_by_feed_id_zsite_id(feed.rid, feed.zsite_id)
-    user_id = zsite_id_by_douban_user_id(feed_user)
-
-    zsite_id = feed.zsite_id
-
-    is_without_author = ((feed.state == FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR_SYNC) or (feed.state == FEED_IMPORT_STATE_REVIEWED_WITHOUT_AUTHOR))
-
-    if is_without_author:
-        user_id = 0
-        zsite_id = 0
-
-    title = title_normal(feed.title)
-    po = po_note_new(user_id, title, txt, zsite_id=zsite_id)
-
-    if po:
-        if not feed_user:
-            feed_user_id = 0
-        else:
-            user = PoMetaUser.get_or_create(name = feed_user.name, cid = zsite_id)
-            user.url = feed_user.id
-
-            user.save()
-
-            feed_user_id = user.id
-
-        record = PoMeta.get_or_create(id = po.id)
-        record.user_id = feed_user_id
-        record.url_id = url_short_id(feed.url)
-
-        record.save()
-
-        if not is_without_author:
-            po.rid = record.id
-            po.save()
-
-        if feed.state >= FEED_IMPORT_STATE_REVIEWED_SYNC:
-            site_sync_new(po.id)
-
-        feed.state = FEED_IMPORT_STATE_POED
-        feed.save()
-
-        cid  = feed.cid
-
-        po_tag_id_list_new(po, feed.tag_id_list.split(','),cid)
 
 
-def feed2po_new():
-    from zweb.orm import ormiter
-    for feed in ormiter(
-            FeedImport,
-            'state>%s and state<%s'%(
-                FEED_IMPORT_STATE_INIT,
-                FEED_IMPORT_STATE_POED
-            )
-        ):
-        feed_new(feed)
 
 def feed_review(id,  cid, title, txt, tag_id_list, current_user_id, author_rm=False, sync=False):
     feed = FeedImport.get(id)
@@ -163,7 +99,7 @@ if __name__ == '__main__':
     pass
     #feed_import_by_douban_feed()
     #print FeedImport.where(state = FEED_IMPORT_STATE_INIT)
-    feed2po_new()
+    #feed2po_new()
     #from zweb.orm import ormiter
     #for i in ormiter(FeedImport):
     #    i.txt = i.txt.replace("豆友","网友").replace("豆油","私信").replace("豆邮","私信")
