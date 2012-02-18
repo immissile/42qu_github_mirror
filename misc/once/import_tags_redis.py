e1e8f642399bd37f4b9d88758dd8a3db18921550
zuroc <zsp007@gmail.com>
2012-02-18 18:17:46 +0800

    fix

--- misc/once/import_tags_redis.py
+++ misc/once/import_tags_redis.py
@@ -13,6 +13,7 @@ from yajl import loads
 from model.po_by_tag import  tag_by_str
 from zkit.bot_txt import txt_map
 from misc.spider.zhihu_topic_id_rank import ID2RANK
+from zdata.tag.name2id import NAME2ID
 
 CURRNET_PATH = path.dirname(path.abspath(__file__))
 TMP_REDIS = 'tmp_import_redis'
@@ -22,12 +23,12 @@ import re
 if __name__ == '__main__':
     name_id = {}
     for i in Zsite.where(cid=CID_TAG):
-        id = i.id
-        rank = ID2RANK.get(id, 0)
-        name = i.name
-        autocomplete_tag.append(name, id, rank)
-        name_id[name] = id
-
-    for name , id in name_id.iteritems():
-        redis.hset(autocomplete_tag.ID2NAME, id, '%s`%s'%(name, 0))
-        print name, rank, 'id'
+#        id = i.id
+#        rank = ID2RANK.get(id, 0)
+#        name = i.name
+#        autocomplete_tag.append(name, id, rank)
+#        name_id[name] = id
+#
+#    for name , id in name_id.iteritems():
+#        redis.hset(autocomplete_tag.ID2NAME, id, '%s`%s'%(name, 0))
+#        print name, rank, 'id'
