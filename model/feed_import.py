a7e22a9059fec01f240886ec42feaea93f555539
zuroc <zsp007@gmail.com>
2012-02-21 12:41:38 +0800

    fix

--- model/feed_import.py
+++ model/feed_import.py
@@ -96,6 +96,8 @@ def feed_review(id,  cid, title, txt, tag_id_list, current_user_id, author_rm=Fa
 
 if __name__ == '__main__':
     pass
+    from zweb.orm import ormiter
+
     #feed_import_by_douban_feed()
     #print FeedImport.where(state = FEED_IMPORT_STATE_INIT)
     #feed2po_new()
