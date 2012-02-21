24ede4974a14c22a37c5245643b1d237e3459193
zuroc <zsp007@gmail.com>
2012-02-21 13:18:35 +0800

    fix

--- model/tag_admin.py
+++ model/tag_admin.py
@@ -1,5 +1,7 @@
 #coding:utf-8
 from _db import redis
+from model.zsite import Zsite
+from operator import itemgetter
 
 REDIS_TAG_ADMIN = 'TagAdmin'
 REDIS_TAG_ADMIN_TAG_ID = 'TagAdmin:%s'
@@ -26,15 +28,22 @@ def tag_admin_rm(id, tag_id_list):
             p.zincrby(REDIS_TAG_ADMIN, tag_id, -1)
             p.execute()
 
-def tag_list_count_by_tag_admin(limit, offset):
+def tag_id_name_count_list_by_tag_admin(limit, offset):
     id_count = redis.zrevrange(REDIS_TAG_ADMIN, offset, offset+limit-1, True, int)
-    return id_count
+    zsite_list = Zsite.mc_get_list(map(itemgetter(0), id_count))
+    r = []
+    for i,count in zip(zsite_list, map(itemgetter(1),id_count)):
+        r.append((i.id, i.name, count)) 
+
+    return r, redis.zcard(REDIS_TAG_ADMIN)
+
 
 #def po_list_by_tag_admin(tag_id, limit, offset):
 #    pass
 
 if __name__ == '__main__':
     pass
-
-    print tag_list_count_by_tag_admin(10, 0)
-
+    
+    limit = 50
+    offset = 0
+    print tag_id_name_count_list_by_tag_admin(limit, offset)
