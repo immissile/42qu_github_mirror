79e429306237e5c03c094058a5d71707f9f0df88
zuroc <zsp007@gmail.com>
2012-02-23 20:27:29 +0800

    fix

--- misc/vps/turn.py
+++ misc/vps/turn.py
@@ -15,20 +15,25 @@ STATE_VPS_TO_CLOSE = 30 #等待关闭
 STATE_VPS_CLOSED = 40   #已经关闭
 
 
-def vps
+def vps_open(group, id_in_group):
+    ssh_port = id_in_group + PORT_SSH_OFFSET
+    pass
+
+def vps_close(group, id_in_group):
+    pass
+
+
 
 def main():
-    cursor.execute("select id, id_in_group, `group` from vps where state=%s", STATE_VPS_TO_OPEN)
-    for id, state, ssh_port, group in cursor.fetchall():
-        #TODO
-        cursor.execute("update state=%s where id=%s", (STATE_VPS_OPENED, id))
+    def _(from_state, to_state, recall):
+        cursor.execute("select id, id_in_group, `group` from vps where state=%s", from_state)
+        for id,  id_in_group, group in cursor.fetchall():
+            recall(group, id_in_group)
+            cursor.execute("update state=%s where id=%s", (to_state, id))
 
+    _(STATE_VPS_TO_OPEN, STATE_VPS_OPENED, vps_open)
+    _(STATE_VPS_TO_CLOSE, STATE_VPS_CLOSED, vps_close)
 
-    cursor.execute("select id, id_in_group, `group` from vps where state=%s", STATE_VPS_TO_CLOSE)
-    for id, state, ssh_port, group in cursor.fetchall():
-        ssh_port = id_in_group+ PORT_SSH_OFFSET
-        #TODO
-        cursor.execute("update state=%s where id=%s", (STATE_VPS_CLOSED, id))
 
 
 if "__main__" == __name__:
