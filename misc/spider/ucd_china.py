332348e44abe6f2f759ae56f073c6c11529b0084
zuroc <zsp007@gmail.com>
2012-02-24 11:01:30 +0800

    fix

--- misc/spider/ucd_china.py
+++ misc/spider/ucd_china.py
@@ -18,7 +18,7 @@ def main():
     with open("ucd_china.js","w") as ucd_china:
         for id in (1, 2, 3, 6, 8):
             feed = 'feed/%s'%quote(TEMPLATE_UCD_CHINA_RSS%id)
-            #print feed
+            print feed
             for i in greader.feed(feed):
                 ucd_china.write(dumps(i)+"\n")
                 count += 1 
