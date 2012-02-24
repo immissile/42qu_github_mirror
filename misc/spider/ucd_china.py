928b2ee5b479228ba4d15a997d51deb6d1e0bcdf
zuroc <zsp007@gmail.com>
2012-02-24 10:23:05 +0800

    fix

--- misc/spider/ucd_china.py
+++ misc/spider/ucd_china.py
@@ -3,19 +3,22 @@
 
 import _env
 from config import GREADER_USERNAME, GREADER_PASSWORD
+from zkit.google.greader import Reader
+from urllib import quote
+from zkit.pprint import pprint
 
-TEMPLATE_UCD_CHINA_RSS = "http://ucdchina.com/rss/category?id=%s"
+TEMPLATE_UCD_CHINA_RSS = 'http://ucdchina.com/rss/category?id=%s'
 
 
 
 def main():
     greader = Reader(GREADER_USERNAME, GREADER_PASSWORD)
-    for id in (1,2,3,6,8):
-        feed = "feed/%s"%(TEMPLATE_UCD_CHINA_RSS%id)
-
+    for id in (1, 2, 3, 6, 8):
+        feed = 'feed/%s'%quote(TEMPLATE_UCD_CHINA_RSS%id)
+        print feed
         for i in greader.feed(feed):
-            print i
+            pprint(i)
 
-if "__main__" == __name__:
+if '__main__' == __name__:
     main()
 
