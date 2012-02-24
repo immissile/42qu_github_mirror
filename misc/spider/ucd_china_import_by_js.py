9f7a92ad8b2611c4b1102d8eb43168944a9092e7
zuroc <zsp007@gmail.com>
2012-02-24 20:30:21 +0800

    fix

--- misc/spider/ucd_china_import_by_js.py
+++ misc/spider/ucd_china_import_by_js.py
@@ -3,14 +3,20 @@ import _env
 from json import loads
 from zkit.htm2txt import htm2txt
 from config import ZSITE_UCD_CHINA_ID
-from misc.crontab.feed_import import feed_import_new, PoMetaUser
+from misc.crontab.feed_import import feed_import_new, PoMetaUser, FeedImport
 from urllib import quote
 #{u'origin': {u'streamId': u'feed/http://ucdchina.com/rss/category?id=1', u'htmlUrl': u'http://ucdchina.com/rss/category?id=1', u'title': u'\u4ea7\u54c1\u5e02\u573a - UCD\u5927\u793e\u533a'}, u'updated': 1322773790, u'isReadStateLocked': True, u'author': u'\u80d6\u80e1\u6590', u'title': u'\u80d6\u80e1\u6590\u8bf4\u6dd8\u5b9d\u4fc3\u9500\u4e4b\u4e00\uff1a\u4fc3\u9500\u4e4b\u201c\u5546\u201d', u'alternate': [{u'href': u'http://ucdchina.com/snap/11171', u'type': u'text/html'}], u'timestampUsec': u'1322773790821277', u'comments': [], u'summary': {u'content': 
 #
-for i in PoMetaUser.where( cid=ZSITE_UCD_CHINA_ID):
-    print i.id
-    i.rid = i.id
-    i.save()
+
+for i in FeedImport.where(zsite_id=ZSITE_UCD_CHINA_ID):
+    txt = i.txt
+    txt = txt.split("\n")
+    r = []
+    for line in txt:
+        if 'www1.feedsky.com' not in line:
+            r.append(line)
+    txt = "\n".join(r)
+    print txt
 
 raise
 with open('/mnt/zdata/ucd_china.js') as ucd_china:
