cd42435288c45f58099973e8c746cc03e3697d50
zuroc <zsp007@gmail.com>
2012-02-25 19:41:17 +0800

    fix

--- misc/vps/turn.py
+++ misc/vps/turn.py
@@ -3,6 +3,8 @@
 
 import _env
 from model._db import cursor_by_table
+import sys
+import os
 
 cursor = cursor_by_table('vps')
 
@@ -15,9 +17,8 @@ STATE_VPS_TO_CLOSE = 30 #等待关闭
 STATE_VPS_CLOSED = 40   #已经关闭
 
 
-def vps_open(group, id_in_group):
+def vps_open(group, id_in_group, vm_id, passwd):
     ssh_port = id_in_group + PORT_SSH_OFFSET
-    pass
 
 def vps_close(group, id_in_group):
     pass
@@ -26,13 +27,20 @@ def vps_close(group, id_in_group):
 
 def main():
     def _(from_state, to_state, recall):
-        cursor.execute("select id, id_in_group, `group` from vps where state=%s", from_state)
-        for id,  id_in_group, group in cursor.fetchall():
+        cursor.execute("select id, id_in_group, `passwd`, `group` from vps where state=%s", from_state)
+        for id,  id_in_group, passwd, group in cursor.fetchall():
             recall(group, id_in_group)
             cursor.execute("update state=%s where id=%s", (to_state, id))
 
-    _(STATE_VPS_TO_OPEN, STATE_VPS_OPENED, vps_open)
-    _(STATE_VPS_TO_CLOSE, STATE_VPS_CLOSED, vps_close)
+    if len (sys.argv) <= 1:
+        print "need param"
+        os._exit (1)
+    action = sys.argv[1]
+    if action == 'open':
+        _(STATE_VPS_TO_OPEN, STATE_VPS_OPENED, vps_open)
+    else:
+        raise Exception ("param error")
+#    _(STATE_VPS_TO_CLOSE, STATE_VPS_CLOSED, vps_close)
 
 
 
