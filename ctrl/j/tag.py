fe0aa569915376bcb2298383124b1ff45bde1741
zuroc <zsp007@gmail.com>
2012-02-27 19:25:59 +0800

    fix

--- ctrl/j/tag.py
+++ ctrl/j/tag.py
@@ -4,7 +4,7 @@
 from ctrl._urlmap.j import urlmap
 from _handler import JLoginZsiteBase
 from model.cid import CID_TAG
-from zkit.page import limit_offset
+from zkit.page import page_limit_offset, Page
 from json import dumps
 from model.po_tag import po_tag_by_cid, tag_cid_count 
 
@@ -13,15 +13,21 @@ PAGE_LIMIT = 12
 @urlmap('/j/tag/(\d+)-(\-?\d+)')
 class TagMore(JLoginZsiteBase):
     def get(self, cid, n):
-
-        n, limit, offset = limit_offset(n, PAGE_LIMIT)
         zsite_id = self.zsite_id        
         current_user_id = self.current_user_id
+
+        page, limit, offset = page_limit_offset(
+            'javascript:tag_cid_page(%s,%%s)'%cid,
+            int(tag_cid_count(zsite_id, cid) or 0),
+            n,
+            PAGE_LIMIT
+        )
+        page = str(page) or 0
  
         self.finish({
-            'n':n,
-            'li':po_tag_by_cid(cid, zsite_id, current_user_id, limit, offset),
-            'count':tag_cid_count(zsite_id, cid)
+'li':po_tag_by_cid(cid, zsite_id, current_user_id, limit, offset),
+'page':page
+
         })
 
 
