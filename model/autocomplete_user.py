87794011a89978fcd70f859694a90d97c41e984c
zuroc <zsp007@gmail.com>
2012-02-27 21:13:20 +0800

    fix

---
+++ model/autocomplete_user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -`- coding: utf-8 -`-
+
+from _db import redis
+from model.zsite import Zsite , CID_USER
+
+def name_to_user(user):
+    r = []
+    user = user.
+
+if __name__ == '__main__':
+    pass
+    for i in Zsite.where(cid=CID_USER):
+        print i.name
+
