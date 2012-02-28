310c683ceaacca59cc21f090f46d2f2feda9621e
zuroc <zsp007@gmail.com>
2012-02-28 23:50:46 +0800

    fix

--- model/feed_import_user.py
+++ model/feed_import_user.py
@@ -8,14 +8,17 @@ REDIS_FEED_IMPORT_USER_ID_LIST = 'FeedImportUser:%s'
 
 def feed_import_user_new(user_id, feed_import_id):
     redis.zincrby(REDIS_FEED_IMPORT_USER, user_id, 1)
-    redis.lpush(REDIS_FEED_IMPORT_USER_ID_LIST%user_id, feed_import_id)
+    redis.sadd(REDIS_FEED_IMPORT_USER_ID_LIST%user_id, feed_import_id)
 
 
 def feed_import_user_rm(user_id, feed_import_id):
     key = REDIS_FEED_IMPORT_USER_ID_LIST%user_id
-    redis.lrem(key, feed_import_id)
-    length = redis.llen(key)
-    redis.zadd(REDIS_FEED_IMPORT_USER, user_id, length)
+    redis.srem(key, feed_import_id)
+    length = redis.scard(key)
+    if length:
+        redis.zadd(REDIS_FEED_IMPORT_USER, user_id, length)
+    else:
+        redis.zrem(REDIS_FEED_IMPORT_USER, user_id)
 
 def feed_import_user_count():
     return redis.lzcat(REDIS_FEED_IMPORT_USER)
@@ -23,8 +26,8 @@ def feed_import_user_count():
 def id_count_by_feed_import_user(limit, offset):
     return redis.zrevrange(REDIS_FEED_IMPORT_USER, offset, limit+offset-1, True, int)
 
-def feed_import_id_list_by_user_id(user_id):
-    return redis.lrange(REDIS_FEED_IMPORT_USER_ID_LIST%user_id, 0, -1)
+def feed_import_id_by_user_id(user_id):
+    return redis.srandmember(REDIS_FEED_IMPORT_USER_ID_LIST%user_id) 
 
 
 if __name__ == '__main__':
@@ -35,8 +38,8 @@ if __name__ == '__main__':
 
     feed_import_user_new(user_id, feed_import_id)
     print id_count_by_feed_import_user(11, 0)
-    print feed_import_id_list_by_user_id(user_id)
+    print feed_import_id_by_user_id(user_id)
     feed_import_user_rm(user_id, feed_import_id)
-    print feed_import_id_list_by_user_id(user_id)
+    print feed_import_id_by_user_id(user_id)
     print id_count_by_feed_import_user(11, 0)
 
