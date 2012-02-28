6f6d079ebc729a4c06e61ac885588b0c86465129
zuroc <zsp007@gmail.com>
2012-02-29 00:01:11 +0800

    fix

--- model/feed_import_user.py
+++ model/feed_import_user.py
@@ -22,23 +22,22 @@ def feed_import_user_rm(user_id, feed_import_id):
         redis.zrem(REDIS_FEED_IMPORT_USER, user_id)
 
 
-def id_count_by_feed_import_user(limit, offset):
-    return redis.zrevrange(REDIS_FEED_IMPORT_USER, offset, limit+offset-1, True, int)
 
 def feed_import_id_by_user_id(user_id):
     return redis.srandmember(REDIS_FEED_IMPORT_USER_ID_LIST%user_id) 
 
-def user_list_count_by_feed_import_user(limit, offset):
-    count = redis.zcard(REDIS_FEED_IMPORT_USER) 
-    id_list = id_count_by_feed_import_user(limit, offset) 
-    zsite_list = Zsite.mc_get_list(map(itemgetter(0), id_list))
-    for i, count in zip(zsite_list, map(itemgetter(1), id_list)):
+def user_list_count_by_feed_import_user():
+    id_count_list = redis.zrevrange(REDIS_FEED_IMPORT_USER, 0, -1, True, int)
+    zsite_list = Zsite.mc_get_list(map(itemgetter(0), id_count_list))
+    for i, count in zip(zsite_list, map(itemgetter(1), id_count_list)):
         i.feed_import_count = count 
-    return count,  zsite_list
+    return zsite_list
+    #count = redis.zcard(REDIS_FEED_IMPORT_USER) 
+    #return count,  zsite_list
 
 if __name__ == '__main__':
     pass
-    print user_list_count_by_feed_import_user(100, 0)
+    print user_list_count_by_feed_import_user()
 
 #    user_id = 10000000
 #    feed_import_id = 1
