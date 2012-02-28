6ae35e8660b012e6446cf8cee828fb9331ea2c1c
zuroc <zsp007@gmail.com>
2012-02-28 23:44:20 +0800

    fix

--- misc/once/ucd.py
+++ misc/once/ucd.py
@@ -4,6 +4,7 @@ from model.feed_import import PoMetaUser, FeedImport
 from model.zsite import zsite_by_query , Zsite
 from model.user_mail import user_by_mail
 from model.ico import pic_url
+from model.feed_import_user import feed_import_user_new
 
 count = 0
 with open('ucd.csv') as ucd:
@@ -29,9 +30,12 @@ with open('ucd.csv') as ucd:
 
 
             if zsite:
-                if pic_url(zsite.id):
+                user_id = zsite.id
+                if pic_url(user_id):
                     for i in FeedImport.where(po_meta_user_id=u.id):
-                        print i.title
+                        feed_import_user_new(user_id, i.id)
+
+                    #    print i.title
                     #print zsite.name, "http:"+zsite.link
                     #print u.name , u.link
                     #count += 1
--- model/feed_import_user.py
+++ model/feed_import_user.py
@@ -7,13 +7,15 @@ REDIS_FEED_IMPORT_USER_ID_LIST = 'FeedImportUser:%s'
 
 
 def feed_import_user_new(user_id, feed_import_id):
-    redis.zadd(REDIS_FEED_IMPORT_USER, user_id, 1)
+    redis.zincrby(REDIS_FEED_IMPORT_USER, user_id, 1)
     redis.lpush(REDIS_FEED_IMPORT_USER_ID_LIST%user_id, feed_import_id)
 
 
 def feed_import_user_rm(user_id, feed_import_id):
-    redis.zadd(REDIS_FEED_IMPORT_USER, user_id, -1)
-    redis.lrem(REDIS_FEED_IMPORT_USER_ID_LIST%user_id, feed_import_id)
+    key = REDIS_FEED_IMPORT_USER_ID_LIST%user_id
+    redis.lrem(key, feed_import_id)
+    length = redis.llen(key)
+    redis.zadd(REDIS_FEED_IMPORT_USER, user_id, length)
 
 def feed_import_user_count():
     return redis.lzcat(REDIS_FEED_IMPORT_USER)
@@ -36,4 +38,5 @@ if __name__ == '__main__':
     print feed_import_id_list_by_user_id(user_id)
     feed_import_user_rm(user_id, feed_import_id)
     print feed_import_id_list_by_user_id(user_id)
+    print id_count_by_feed_import_user(11, 0)
 
