ff1c98b0306942d53d33f82c0c5bebf14676e8a2
zuroc <zsp007@gmail.com>
2012-03-01 22:39:43 +0800

    fix

--- model/po_tag_user.py
+++ model/po_tag_user.py
@@ -4,6 +4,17 @@ from _db import  McModel, Model, McLimitA, McNum, McCacheA, redis
 REDIS_PO_TAG_USER = "PoTagUser:%s"
 REDIS_UESR_PO_TAG = "UserPoTag:%s" 
 
+
+
+#返回 count
+#返回 id_list
+
+class PoUserTag(object):
+    def __init__(self, key):
+        pass
+
+ 
+
 #def po_tag_user_new(user_id, po_tag_id):
 #    p = redis.pipeline()
 #    key = REDIS_PO_TAG_USER%po_tag_id
