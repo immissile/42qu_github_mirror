fc731b4324a4f430c2c80332f5e996f5e74053e6
zuroc <zsp007@gmail.com>
2012-03-01 22:18:38 +0800

    fix

--- model/po_tag_user.py
+++ model/po_tag_user.py
@@ -1,6 +1,15 @@
 #coding:utf-8
 from _db import  McModel, Model, McLimitA, McNum, McCacheA, redis
 
+REDIS_PO_TAG_USER = "PoTagUser:%s"
+REDIS_UESR_PO_TAG = "UserPoTag:%s" 
+
+#def po_tag_user_new(user_id, po_tag_id):
+#    p = redis.pipeline()
+#    key = REDIS_PO_TAG_USER%po_tag_id
+#    redis.hset(key, p)
+#
+#    p.execute() 
 
 
 if __name__ == '__main__':
