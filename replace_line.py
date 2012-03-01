7000fd82aee27eb38161ca485b84abb26eeb31e3
zuroc <zsp007@gmail.com>
2012-03-01 13:42:03 +0800

    fix

--- replace_line.py
+++ replace_line.py
@@ -4,11 +4,11 @@ from os.path import abspath, dirname, basename, join
 from os import walk
 
 FROM_STRING = """
-from config import PART_TIME_JOBS_RULE
+rss_index
 """
 
 TO_STRING = """
-from model.privilege import PART_TIME_JOBS_RULE
+rss/index
 """
 
 def run():
