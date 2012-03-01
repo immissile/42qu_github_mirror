92146ff5724ce614359cede95f27ae10e19988b2
zuroc <zsp007@gmail.com>
2012-03-01 19:11:07 +0800

    fix

--- model/po_tag_admin.py
+++ model/po_tag_admin.py
@@ -1,5 +1,10 @@
 #coding:utf-8
 from _db import Model, McModel
+from time import time
+
+TAG_EXP_ADMIN_STATE_REJECT = 5
+TAG_EXP_ADMIN_STATE_INIT = 10
+TAG_EXP_ADMIN_STATE_ACCEPT = 15
 
 class TagExp(Model):
     pass
@@ -7,70 +12,49 @@ class TagExp(Model):
 class TagExpAdmin(Model):
     pass
 
-#Base=
-#
-#class Tag_exp(Base):
-#    __tablename__ = "tag_exp"
-#    user_id = 
-#    tag_id = 
-#    txt =
-#    create_time = 
-#
-#    def __init__(self,user_id,tag_id,txt,create_time):
-#        self.user_id = user_id
-#        self.tag_id = tag_id
-#        self.txt = txt
-#        self.create_time = create_time
-#
-#    def edit_tag_exp_txt(cls,newtxt):
-#        cls.txt = newtxt
-#
-#    def check_exist(cls,user_id,tag_id):
-#        if cls.user_id == user_id and cls.tag_id == tag_id:
-#            return True
-#        else:
-#            return False
-#
-#
-#
-#class Tag_exp_for_admin(Base):
-#    __tablename__ = "tag_exp_for_admin"
-#    tag_id =
-#    tag_exp_id =
-#    state = 
-#    admin_id = 
-#    create_time =
-#
-#    def __init__(self,tag_id,tag_exp_id,state = 10,admin_id = None,create_time):
-#        self.tag_id = tag_id
-#        self.tag_exp_id = tag_exp_id
-#        self.create_time = create_time
-#    
-#    def _get_state(self):
-#        return self.state
-#
-#    def edit_state(cls,admin_id,newstate):
-#        cls.admin_id = int(admin_id)
-#        cls.state = int(newstate)
-#        
-#    def check_exist(cls,tag_id,tag_exp_id):
-#        if cls.tag_id == tag_id and cls.tag_exp_id == tag_exp_id:
-#            return True
-#        else:
-#            return False
-#
-#
-#
-#
-#if not (check_exist() and check_exist):
-#    Tag_exp()
-#    Tag_exp_for_admin()
-#elif a._get_state == 10 and 表单内容不为空:
-#    Tag_exp.edit_tag_exp_txt()
-#elif a._get_state == 20 and 表单内容不为空:
-#    已经被拒绝
-#    Tag_exp()
-#    Tag_exp_for_admin()
-#else a._get_state == 30:
-#    show somgthing
-#        
+def tag_exp_admin_new(user_id, tag_id, tag_exp_id):
+    #TODO 先 select id 检查是否有未审核的 , 如果有update它的state , 如果没有 就insert新的
+    c = TagExpAdmin.raw_sql("..... limit 1", )
+    id = c.fetchone()
+    if id:
+        id = id[0]
+        c = TagExpAdmin.raw_sql()
+    else:
+        c = TagExpAdmin.raw_sql()
+
+    return c.lastrowid
+
+def tag_exp_admin_accept(tag_exp_admin_id):
+    _tag_exp_admin_state_set(tag_exp_admin_id, TAG_EXP_ADMIN_STATE_ACCEPT)
+
+def tag_exp_admin_reject(tag_exp_admin_id):
+    _tag_exp_admin_state_set(tag_exp_admin_id, TAG_EXP_ADMIN_STATE_REJECT)
+
+def _tag_exp_admin_state_set(tag_exp_admin_id, state):
+    #TODO
+    pass
+    
+def tag_exp_txt_by_user_id_tag_id(user_id, tag_id):
+    #TODO
+    return ""
+
+def tag_exp_new_apply_for_admin(user_id, tag_id, txt):
+    tag_exp_id = tag_exp_new(user_id, tag_id, txt)
+    tag_exp_admin_new(user_id, tag_id, tag_exp_id)
+
+def tag_exp_new(user_id, tag_id, txt):
+    now = time()
+    c = TagExp.raw_sql(
+        "insert into tag_exp (user_id, tag_id, txt, time) values (%s,%s,%s,%s) on duplicate key update txt=%s, time=%s",
+        user_id, tag_id, txt, now, txt, now
+    )
+    id = c.lastrowid
+    return id
+
+if __name__ == "__main__":
+    #c = TagExp.raw_sql("select count(1) from tag_exp")
+    #c.fetchone()
+    #user_id = 1
+    #tag_id = 2
+    #print tag_exp_new(user_id, tag_id, "zsp331245t6yuzzzzzzzzzzzzzzzzzzzz")
+
