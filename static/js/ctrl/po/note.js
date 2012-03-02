978246812bea8ee242858368ab4b1685781d15be
realfex <i@realfex.com>
2012-03-02 12:05:09 +0800

    dasd

--- static/js/ctrl/po/note.js
+++ static/js/ctrl/po/note.js
@@ -44,5 +44,32 @@ $(function(){
         txt.height(txt_height)
     })
 */
+    function show_placeholder(){
+        if(!$('#token-input-search').val().length>0 && !$('.token-input-token').length>0){
+            $('.token-input-list').hide()
+            $('#search').val("").show()
+            $('#token-input-search').unbind('blur')
+        }
+        if(document.activeElement.id!='token-input-search'){
+            $('.token-input-dropdown').hide()
+        }
+    }
+    function show_token_input(){
+        $('#token-input-search').focus().blur(show_placeholder).focus(function(){
+            if($('#token-input-search').val().length>0)
+            $('.token-input-dropdown').show()}
+        )
+    }
+    show_placeholder()
+    $('#search').bind('click',function(){
+        $(".token-input-list").show()
+        $(this).hide()
+        if(navigator.userAgent.indexOf("MSIE")>0) { 
+            setTimeout(show_token_input,10)
+        }else{
+            show_token_input()
+        }
+    })
+
 })
 
