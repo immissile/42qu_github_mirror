314c3996ba46631709c731e47dfd90837a407b6d
zuroc <zsp007@gmail.com>
2012-03-03 17:56:02 +0800

    fix

--- model/po_tag_user.py
+++ model/po_tag_user.py
@@ -2,7 +2,7 @@
 from _db import  McModel, Model, McLimitA, McNum, McCacheA, redis
 from model.tag_id_list import Tag2IdList
 from model.zsite import Zsite
-
+from operator import itemgetter
 
 tag2idlist_po_user = Tag2IdList('PoUser')
 tag2idlist_po = Tag2IdList('Po')
@@ -13,11 +13,27 @@ def tag_list_by_user_id(user_id):
     return tag_list
 
 
+def tag_list_with_user_count():
+    id_count_list = tag2idlist_po_user.tag_id_count_list()
+    tag_list = Zsite.mc_get_list(map(itemgetter(0), id_count_list))
+    for tag, count in zip(tag_list, map(itemgetter(1), id_count_list)):
+        tag.user_count = count 
+    return tag_list
+
+def user_list_by_tag_id(tag_id):
+    user_id_list = tag2idlist_po_user.id_list_by_tag_id(tag_id)
+    user_list = Zsite.mc_get_list(user_id_list)
+    return user_list
+
 
 if __name__ == '__main__':
     pass
 
-    for i in tag_list_by_user_id(10000000):
-        print i
-
-
+#    for i in tag_list_by_user_id(10000000):
+#        print i
+#    print tag2idlist_po_user.tag_id_count_list()
+    for i in tag_list_with_user_count():
+        print i.name
+         
+        for j in user_list_by_tag_id(i.id):
+            print "\t",j.name
