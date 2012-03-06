c53dd00016b25cba97f89f262f5d82a69c4492cf
zuroc <zsp007@gmail.com>
2012-03-06 12:29:41 +0800

    fix

--- ctrl/zsite/read.py
+++ ctrl/zsite/read.py
@@ -5,8 +5,9 @@ from ctrl._urlmap.zsite import urlmap
 from model.cid import CID_SITE
 from model.zsite_fav import zsite_fav_get_and_touch
 from ctrl.zsite.index import render_zsite_site
-from model.rec_read import rec_read_log_by_user_id_auto_more
+from model.rec_read import rec_read_log_by_user_id_auto_more, rec_read_log_count_by_user_id
 from tornado.escape import json_encode 
+from model.po_json import po_json
 #from model.po_tag import po_tag
 
 PAGE_LIMIT = 50
@@ -28,19 +29,26 @@ class Index(LoginBase):
 #            zsite_id = 137110
 #            item_list = po_tag(zsite_id, current_user_id, 15, 0 )
             item_list = []
+            po_id_list = rec_read_log_by_user_id_auto_more(
+                current_user_id, PAGE_LIMIT/2, 0
+            )
             t = [
-                0,
-                "推荐",
-                rec_read_log_count_by_user_id(current_user_id),
-                rec_read_log_by_user_id_auto_more(
-                    current_user_id, PAGE_LIMIT/2, 0
-                ),
-                1
+0,
+"推荐",
+rec_read_log_count_by_user_id(current_user_id),
+po_json(current_user_id, po_id_list, 47) ,
+1
             ]
             item_list.append(t)
+
+
             self.render(
                 item_list = json_encode(item_list)
             )
+
+
+
+
 
 
 
