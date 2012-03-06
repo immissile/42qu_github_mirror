a8f1ed2c57518058c857ccc43e2a1512eb821a0a
realfex <i@realfex.com>
2012-03-06 15:23:56 +0800

    index_read_fix

--- static/js/zpage.js
+++ static/js/zpage.js
@@ -538,11 +538,14 @@ RegExp.escape = function(text) {
 
 function scroll_to_fixed(id, size, style1, style2){
     if(!IE6){
-        var elem = $(id);
+        var elem = $(id) 
         if(elem[0]){
             elem.css('position','absolute')
-            var top = elem.offset().top, win=$(window).scroll(function() {
-                if(win.scrollTop() >= top+size){
+            var wrap = elem.parent(),wraptop = wrap.offset().top,
+            top = elem.offset().top, win=$(window).scroll(function() {
+                var scroll = win.scrollTop(),
+                limit =  wraptop + wrap.height()
+                if((scroll >= top+size) && (scroll<limit-80)){
                     elem.css(style1)
                 }else{
                     elem.css(style2)
