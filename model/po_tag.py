7bb12c6b1c3e285f6c73a7cf08e8ad9980864a19
zuroc <zsp007@gmail.com>
2012-03-07 19:59:18 +0800

    fix

--- model/po_tag.py
+++ model/po_tag.py
@@ -282,8 +282,8 @@ def _tag_rm_by_user_id_list(po, user_id, id_list):
         user_rank = zsite_list_get(user_id, tag_id, CID_TAG)
         if not user_rank and user_rank.rank:
             user_rank.rank -= 1
-            if user_rank.rank < 1:
-                user_rank.rank = 1
+            if user_rank.rank < 0:
+                user_rank.rank = 0
             user_rank.save()
 
     cid = tag_cid_by_po_id(po_id)
