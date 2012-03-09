aa075eeb10243d5082c4b8ac58e131e934f02a49
zuroc <zsp007@gmail.com>
2012-03-09 21:23:50 +0800

    fix

--- ctrl/star/index.py
+++ ctrl/star/index.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from _handler import Base, LoginBase, XsrfGetBase
 from ctrl._urlmap.star import urlmap
+from zkit.errtip import Errtip
+from zkit.pic import picopen
 
 @urlmap('/')
 class Index(Base):
@@ -10,5 +12,32 @@ class Index(Base):
 @urlmap('/new')
 class New(Base):
     def get(self):
-        self.render()
+        self.render(errtip=Errtip())
+
+    def post(self):
+        errtip=Errtip()
+
+        name = self.get_argument('name', None)
+        donate_min = self.get_argument('donate_min', None)
+        end_time = self.get_argument('end_time', None)
+        txt = self.get_argument('txt', '')
+        files = self.request.files
+
+        if 'pic' in files:
+            pic = files['pic'][0]['body']
+            if pic:
+                pic = picopen(pic)
+            else:
+                errtip.pic = "图片格式有误"
+        else:
+            errtip.pic = "请上传图片"
+
+        if errtip:
+            self.render(
+                errtip = errtip,
+                name = name,
+                donate_min = donate_min,
+                txt = txt,
+                end_time = end_time
+            )
 
