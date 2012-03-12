b94df85a7b7cfd7499eea347c4ec1fcc80a1df3e
zuroc <zsp007@gmail.com>
2012-03-12 21:18:20 +0000

    fix

---
+++ config/host/e1.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+import getpass
+
+def prepare(o):
+    o.SITE_DOMAIN = '%squ.tk'%getpass.getuser()
+
+    o.FILE_DOMAIN = 'p.%s'%o.SITE_DOMAIN
+    o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
+    o.MYSQL_PASSWD = '42qudev'
+
+    try:
+        import _private
+    except ImportError:
+        pass
+    else:
+        _private.prepare(o)
+
