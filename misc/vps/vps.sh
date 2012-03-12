c4d7f38ae179a494660a4fd6d73298fb5091eb55
zuroc <zsp007@gmail.com>
2012-03-12 18:47:58 +0000

    fix

--- misc/vps/vps.sh
+++ misc/vps/vps.sh
@@ -6,8 +6,12 @@ ssh-keygen -t rsa -f ~/.ssh/id_rsa -P "" -q;
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys;
 fi
 
-virtualenv .env
+PREFIX=$(cd "$(dirname "$0")"; pwd)
+cp -r PREFIX/config/. ~/ 
+
 source ~/.bash_profile
+
+virtualenv .env
 pip install setuptools --upgrad
 
 
