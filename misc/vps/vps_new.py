280c35f625f6ab41328473b578d10f74de7fe83a
zuroc <zsp007@gmail.com>
2012-03-12 17:01:11 +0000

    fix

--- misc/vps/vps_new.py
+++ misc/vps/vps_new.py
@@ -11,6 +11,10 @@ from model.user_mail import mail_by_user_id
 from model.mail import sendmail
 from mako.template import Template
 
+import socket
+host = socket.gethostname()
+print host
+
 class Vps(Model):
     pass
 
@@ -49,121 +53,121 @@ def vps_id_by_user_id(user_id, group=1):
 
 
 
-def main():
-
-    count = 0
-    user_id_list = event_joiner_user_id_list(10236239)
-    print len(user_id_list)
-    for i in user_id_list:
-
-        count += 1
-
-
-        ip_offset = 19
-        ssh_port_offset = 53000
-        id_in_group = vps.id_in_group
-
-        ip = '10.10.1.%s'%(ip_offset + id_in_group)
-        ssh_port = ssh_port_offset + id_in_group
-
-
-#假定虚机名为vmtest, ip 是10.10.1.150 , work的密码是hahaha，要映射ssh到 20352端口的话
+#def main():
 #
-
-        vps_new(vps.id, i, ip, ssh_port, vps.passwd)
-
-
-def vps_new(_id, user_id, ip, ssh_port, passwd):
-    username = 'v%s'%_id
-    mail = mail_by_user_id(user_id)
-
-#    return
-    if _id != 149:
-        return
-    print 'echo %s:%s| chpasswd'%(username, passwd)
-    print mail
-    print ""
-#    subject = "[42qu.培训班] 下节课的预习材料"
+#    count = 0
+#    user_id_list = event_joiner_user_id_list(10236239)
+#    print len(user_id_list)
+#    for i in user_id_list:
+#
+#        count += 1
+#
+#
+#        ip_offset = 19
+#        ssh_port_offset = 53000
+#        id_in_group = vps.id_in_group
+#
+#        ip = '10.10.1.%s'%(ip_offset + id_in_group)
+#        ssh_port = ssh_port_offset + id_in_group
+#
+#
+##假定虚机名为vmtest, ip 是10.10.1.150 , work的密码是hahaha，要映射ssh到 20352端口的话
+##
+#
+#        vps_new(vps.id, i, ip, ssh_port, vps.passwd)
+#
+#
+#def vps_new(_id, user_id, ip, ssh_port, passwd):
+#    username = 'v%s'%_id
+#    mail = mail_by_user_id(user_id)
+#
+##    return
+#    if _id != 149:
+#        return
+#    print 'echo %s:%s| chpasswd'%(username, passwd)
+#    print mail
+#    print ""
+##    subject = "[42qu.培训班] 下节课的预习材料"
+##    text = """
+##
+##上节课的讲义
+##http://book.42qu.com/linux/vim.html
+##
+##下节课的预习
+##
+##版本控制
+##http://book.42qu.com/tool/hg.html
+##http://book.42qu.com/tool/git.html
+##
+##数据库
+##http://book.42qu.com/database/index.html
+##
+##PS: 没有开通主机的同学 请邮件到我邮箱 zsp007@gmail.com 标题为 "[主机.42培训班] + 你的42qu注册帐号"
+##
+##另外请加入
+##Google groups https://groups.google.com/group/42qu-school
+##QQ群 : 211707205
+##"""
+#
+#
+#    subject = '[42qu.培训班] 学习用的主机帐号'
 #    text = """
+#主机 : 0002.42qu.us
+#用户名 : %s
+#密码 : %s
 #
-#上节课的讲义
-#http://book.42qu.com/linux/vim.html
+#如何登录主机
+#http://book.42qu.com/linux/introduction.html
 #
-#下节课的预习
+#预习材料:
+#http://book.42qu.com/python/before_started.html
 #
-#版本控制
-#http://book.42qu.com/tool/hg.html
-#http://book.42qu.com/tool/git.html
-#
-#数据库
-#http://book.42qu.com/database/index.html
-#
-#PS: 没有开通主机的同学 请邮件到我邮箱 zsp007@gmail.com 标题为 "[主机.42培训班] + 你的42qu注册帐号"
-#
-#另外请加入
 #Google groups https://groups.google.com/group/42qu-school
 #QQ群 : 211707205
-#"""
-
-
-    subject = '[42qu.培训班] 学习用的主机帐号'
-    text = """
-主机 : 0002.42qu.us
-用户名 : %s
-密码 : %s
-
-如何登录主机
-http://book.42qu.com/linux/introduction.html
-
-预习材料:
-http://book.42qu.com/python/before_started.html
-
-Google groups https://groups.google.com/group/42qu-school
-QQ群 : 211707205
-    """%(username, passwd)
-
-    mail = "epal@qq.com"
-    #mail = 'zsp007@gmail.com'
-    print subject
-    print text
-    sendmail(
-        subject,
-        text, mail
-    )
-
-#    result = []
-#    cmd = """python create_vm.py --baseimg /mnt/nova/xen/template/ext4_root.img --name vps%s --ip %s/24 --gateway 10.10.1.1 --user work:%s"""%(
-#        _id, ip, passwd
+#    """%(username, passwd)
+#
+#    mail = "epal@qq.com"
+#    #mail = 'zsp007@gmail.com'
+#    print subject
+#    print text
+#    sendmail(
+#        subject,
+#        text, mail
 #    )
-#    result.append(cmd)
 #
-#    cmd = """python iptables.py portmap --outip 119.254.32.167 --outport %s --inip %s --inport 22"""%(ssh_port, ip)
-#
-#    result.append(cmd)
-#    cmd = """xm create /mnt/nova/xen/vm/vps%s/config """ % (_id)
-#    result.append(cmd)
-#
-#    print "\n".join(result)
-#    print ""
-#    def _(from_state, to_state, recall):
-#        cursor.execute("select id, id_in_group, `passwd`, `group` from vps where state=%s", from_state)
-#        for id,  id_in_group, passwd, group in cursor.fetchall():
-#            recall(group, id_in_group)
-#            cursor.execute("update state=%s where id=%s", (to_state, id))
-#
-#    if len (sys.argv) <= 1:
-#        print "need param"
-#        os._exit (1)
-#    action = sys.argv[1]
-#    if action == 'open':
-#        _(STATE_VPS_TO_OPEN, STATE_VPS_OPENED, vps_open)
-#    else:
-#        raise Exception ("param error")
-##    _(STATE_VPS_TO_CLOSE, STATE_VPS_CLOSED, vps_close)
-#
-#
-#
-#
-if '__main__' == __name__:
-    main()
-#
+##    result = []
+##    cmd = """python create_vm.py --baseimg /mnt/nova/xen/template/ext4_root.img --name vps%s --ip %s/24 --gateway 10.10.1.1 --user work:%s"""%(
+##        _id, ip, passwd
+##    )
+##    result.append(cmd)
+##
+##    cmd = """python iptables.py portmap --outip 119.254.32.167 --outport %s --inip %s --inport 22"""%(ssh_port, ip)
+##
+##    result.append(cmd)
+##    cmd = """xm create /mnt/nova/xen/vm/vps%s/config """ % (_id)
+##    result.append(cmd)
+##
+##    print "\n".join(result)
+##    print ""
+##    def _(from_state, to_state, recall):
+##        cursor.execute("select id, id_in_group, `passwd`, `group` from vps where state=%s", from_state)
+##        for id,  id_in_group, passwd, group in cursor.fetchall():
+##            recall(group, id_in_group)
+##            cursor.execute("update state=%s where id=%s", (to_state, id))
+##
+##    if len (sys.argv) <= 1:
+##        print "need param"
+##        os._exit (1)
+##    action = sys.argv[1]
+##    if action == 'open':
+##        _(STATE_VPS_TO_OPEN, STATE_VPS_OPENED, vps_open)
+##    else:
+##        raise Exception ("param error")
+###    _(STATE_VPS_TO_CLOSE, STATE_VPS_CLOSED, vps_close)
+##
+##
+##
+##
+#if '__main__' == __name__:
+#    main()
+##
