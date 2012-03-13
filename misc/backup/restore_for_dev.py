fa3e840e3b270f83603e750bdb5d03052fd344ed
zuroc <zsp007@gmail.com>
2012-03-13 15:24:48 +0000

    fix

--- misc/backup/restore_for_dev.py
+++ misc/backup/restore_for_dev.py
@@ -1,11 +1,14 @@
+#coding:utf-8
 import socket
 hostname = socket.gethostname()
 if hostname != "e1":
     raise "hostname is not e1"
 
 
-#import _env
-#from model.vps import Vps
-#from model.mail import Ma
+import _env
+from model.txt import Txt
+from zweb.orm import ormiter
 
-
+Txt.where().update(txt="测试数据")
+for i in ormiter(Txt):
+    print i.txt
