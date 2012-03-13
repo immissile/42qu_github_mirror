09253e99112eb9f842a979102c984152beba380a
zuroc <zsp007@gmail.com>
2012-03-13 02:06:29 +0000

    fix

--- misc/backup/restore_for_dev.sh
+++ misc/backup/restore_for_dev.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 DB=2012_03_12_03_20_01
+DBPATH=~/data
 
-
-mkdir -p ~/data
+mkdir -p $DBPATH
 #scp work@42qu.com:/mnt/zpage_db/mysql/$DB.7z ~/data
 
 
-mysql -uzpage -p42qudev zpage < table_main.sql
+#mysql -uzpage -p42qudev zpage < table_main.sql
 #mysql -uzpage -p42qudev zpage_google < table_google.sql
  
 
-cd ~/data
+cd $DBPATH
 #7za x $DB.7z 
-mysql -uzpage -p42qudev zpage < $DB
+#mysql -uzpage -p42qudev zpage < $DB
 #rm $DB
- 
+
+sudo /etc/init.d/redis stop
+scp work@42qu.com:/var/lib/redis/dump.rdb  $DBPATH/dump.rdb
+sudo mv  $DBPATH/dump.rdb /var/lib/redis/dump.rdb
+sudo chown redis:redis /var/lib/redis/dump.rdb
+sudo /etc/init.d/redis start
