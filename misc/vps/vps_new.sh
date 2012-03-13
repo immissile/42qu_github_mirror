4a2d13918d29f234a73608f245b57b0da317c4ae
zuroc <zsp007@gmail.com>
2012-03-13 14:46:37 +0000

    vps new

--- misc/vps/vps_new.sh
+++ misc/vps/vps_new.sh
@@ -1,3 +1,7 @@
+hg commit -m"vps new"
+hg fetch
+
+python vps_new.py
 
 cd ~/zpage/config/
 python make_dev_config.py
@@ -6,7 +10,6 @@ python nginx.py
 cd ~/zpage/config/user
 hg add .
 hg commit -m"add user config"
-hg fetch
 hg push
 
 sudo /etc/init.d/nginx reload
