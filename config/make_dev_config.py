6fca1c4a87c037a8c963bca9965b7fb1aff5d26e
zuroc <zsp007@gmail.com>
2012-03-13 12:16:21 +0000

    fix

---
+++ config/make_dev_config.py
@@ -0,0 +1,26 @@
+from glob import glob
+from os.path import exists
+from mako.template import Template
+from socket import gethostname
+
+USER = glob('/home/z*')
+USER = [
+    i[6:] for i in USER if i[7:].isdigit()
+]
+
+template = Template("""
+#coding:utf-8
+
+def prepare(o):
+    o.SITE_DOMAIN = '${user}${hostname}.tk' 
+    o.FILE_DOMAIN = 'p.%s'%o.SITE_DOMAIN
+    o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
+""")
+
+for i in USER:
+    if not exists(i):
+        with open('user/z%s.py'%i, 'w') as user_z:
+            user_z.write(
+                template.render(hostname = gethostname(), user=i)
+            )
+
