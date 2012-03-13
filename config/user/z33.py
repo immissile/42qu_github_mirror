5350e825483c0acc74bb0b08f708b505b31b1345
realfex <i@realfex.com>
2012-03-13 19:27:06 +0000

    fix

--- config/user/z33.py
+++ config/user/z33.py
@@ -7,8 +7,7 @@ def prepare(o):
     o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
     o.PORT = 31650
    
-    #参阅 http://book.42qu.com/mail/smtp.html , 申请一个免费的发邮件的SMTP 
-    o.SMTP = ''
-    o.SMTP_USERNAME = ''
-    o.SMTP_PASSWORD = ''
+    o.SMTP = 'smtp.mailgun.org'
+    o.SMTP_USERNAME = 'postmaster@z33.mailgun.org'
+    o.SMTP_PASSWORD = '6a433suwib56'
     o.SENDER_MAIL = o.SMTP_USERNAME
