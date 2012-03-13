c4e85dafa4dd199b12e409aaea4f27d93e870df1
zuroc <zsp007@gmail.com>
2012-03-13 02:00:45 +0000

    fix

--- server_ctrl_dev.py
+++ server_ctrl_dev.py
@@ -9,6 +9,9 @@ from zweb.server_cherry import WSGIServer
 run.wsgi_server = WSGIServer
 
 if __name__ == '__main__':
+    print "\n%s\n"%config.SITE_HTTP
 
     from zkit.reloader.reload_server import auto_reload
     auto_reload(run)
+    
+
