af8eb2517b992b9326cabbdaf9f054a1fb813a04
zuroc <zsp007@gmail.com>
2012-03-13 21:36:58 +0000

    add user config

---
+++ config/user/z31.py
@@ -0,0 +1,15 @@
+
+#coding:utf-8
+
+def prepare(o):
+    o.SITE_DOMAIN = 'z31e1.tk' 
+    o.FILE_DOMAIN = 'p.%s'%o.SITE_DOMAIN
+    o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
+    o.PORT = 31550
+   
+    o.SMTP = 'smtp.mailgun.org'
+    o.SMTP_USERNAME = ''
+    o.SMTP_PASSWORD = ''
+    o.SENDER_MAIL = o.SMTP_USERNAME
+
+    o.ZDATA_PATH = "/home/z31/file/"
