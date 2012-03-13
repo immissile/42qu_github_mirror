9303e1f61176922a0e4662859f8ffe5dc9f42fc1
zuroc <zsp007@gmail.com>
2012-03-13 01:43:24 +0000

    fix

---
+++ misc/backup/restore_for_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DB=2012_03_12_03_20_01
+
+
+mkdir -p ~/data
+#scp work@42qu.com:/mnt/zpage_db/mysql/$DB.7z ~/data
+
+
+#mysql -uzpage -p42qudev zpage < table_main.sql
+#mysql -uzpage -p42qudev zpage_google < table_google.sql
+ 
+
+cd ~/data
+#7za x $DB.7z 
+mysql -uzpage -p42qudev zpage < $DB
+ 
