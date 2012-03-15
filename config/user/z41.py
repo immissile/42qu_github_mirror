8be60ddf4d368d34a58688fac0ef6750c7b239a3
zuroc <zsp007@gmail.com>
2012-03-15 16:48:55 +0000

    add user config

---
+++ config/user/z41.py
@@ -0,0 +1,15 @@
+
+#coding:utf-8
+
+def prepare(o):
+    o.SITE_DOMAIN = 'z41e1.tk' 
+    o.FILE_DOMAIN = 'p.%s'%o.SITE_DOMAIN
+    o.FS_DOMAIN = 's.%s'%o.SITE_DOMAIN
+    o.PORT = 32050
+   
+    o.SMTP = 'smtp.mailgun.org'
+    o.SMTP_USERNAME = ''
+    o.SMTP_PASSWORD = ''
+    o.SENDER_MAIL = o.SMTP_USERNAME
+
+    o.ZDATA_PATH = "/home/z41/file/"
