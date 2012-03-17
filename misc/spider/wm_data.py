90cb67356b3f836461410bbc1d8a7d0e51731276
zuroc <zsp007@gmail.com>
2012-03-17 10:23:42 +0800

    fix

---
+++ misc/spider/wm_data.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+from json import loads
+from datetime import datetime
+import _env
+from model.days import time_by_string
+
+now = datetime.today()
+
+with open('wm_rec.txt') as wm_rec:
+    for line in wm_rec:
+        id, like, title, source, link, time, txt = loads(line)
+        if "前" in time:
+            time = now
+        else:
+            time = time_by_string(time)
+        like = int(like or 0)
+        print id, title, like, time
+
+if __name__ == '__main__':
+    pass
+
+
+
