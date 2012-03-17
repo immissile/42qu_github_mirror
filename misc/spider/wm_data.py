f080334442c7b14413779bc522dcc66913e4a997
zuroc <zsp007@gmail.com>
2012-03-17 13:13:11 +0800

    fix

--- misc/spider/wm_data.py
+++ misc/spider/wm_data.py
@@ -2,9 +2,9 @@
 from json import loads
 from datetime import datetime
 import _env
-from model.days import time_by_string
+from model.days import time_by_string, int_by_string
 from model._db import Model
-
+from time import time
 
 class SpiderWm(Model):
     pass
@@ -26,30 +26,28 @@ def wm_fav(user_id, wm_id):
     smu = SpiderWmFav.get_or_create(user_id=user_id, wm_id=wm_id)
     smu.save()
 
-def wm_save(id, like, name, author, link, time, txt):
+def wm_save(id, like, name, author, link, create_time, txt):
     wm = SpiderWm.get(wmid=id)
     if wm:
         return wm 
 
-    now = datetime.today()
+    now = time() 
 
-    id, like, name, author, link, time, txt = loads(line)
-    if '前' in time:
-        time = now
+    if '前' in create_time:
+        create_time = now
     else:
-        time = time_by_string(time)
+        create_time = int_by_string(create_time)
 
     like = int(like or 0)
 
     wm = SpiderWm(
         wmid=id,
-        like=like, name=name, author=author, link=link, time=time, txt=txt
+        like=like, name=name, author=author, link=link, time=create_time, txt=txt
     )
     wm.save()
     return wm
     
 if __name__ == '__main__':
     pass
-
 
 
