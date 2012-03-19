c2bbd8f135baaebfd127699c6c232d46a46fad1b
zuroc <zsp007@gmail.com>
2012-03-19 17:30:37 +0800

    fix

--- model/po_tag.py
+++ model/po_tag.py
@@ -166,7 +166,8 @@ def tag_new(name):
     if '/' in name:
         for i in map(utf8_ftoj, map(str.strip, name.split('/'))):
             _tag_alias_new(id, i)
-
+    else:
+            _tag_alias_new(id, name)
     return id
 
 def _tag_alias_new(id, name):
